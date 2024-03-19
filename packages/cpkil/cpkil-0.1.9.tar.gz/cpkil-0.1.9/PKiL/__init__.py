0689472e15acd4edf47db09aeffed24a8e4b153b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:03:47 +0000

    Add package cpkil/0.1.9/cpkil-0.1.9.tar.gz

---
+++ packages/cpkil/cpkil-0.1.9.tar.gz/cpkil-0.1.9/PKiL/__init__.py
@@ -0,0 +1,1 @@
+from PKiL.process import ProKnow
---
+++ packages/cpkil/cpkil-0.1.9.tar.gz/cpkil-0.1.9/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+from pathlib import Path
+this_directory = Path(__file__).parent
+# long_description = (this_directory / "readme.md").read_text()
+
+
+VERSION = '0.1.9'
+DESCRIPTION = 'CPR Python Package'
+LONG_DESCRIPTION = (this_directory / "README.md").read_text()
+
+# Setting up
+setup(
+    name="cpkil",
+    version=VERSION,
+    author="Jinendra Malekar",
+    author_email="<jmalekar@email.sc.edu>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['certifi', 'charset-normalizer', 'click', 'filelock', 'Flask', 'Flask-Cors', 'gensim', 'huggingface-hub', 'idna', 'itsdangerous', 'Jinja2', 'joblib', 'MarkupSafe', 'nltk', 'numpy', 'packaging', 'Pillow', 'pyparsing', 'PyYAML', 'regex', 'requests', 'scikit-learn', 'scipy', 'sentence-transformers', 'sentencepiece', 'six', 'smart-open', 'threadpoolctl', 'tokenizers', 'torch', 'torchvision', 'tqdm', 'transformers', 'typing_extensions', 'urllib3', 'Werkzeug'],
+    keywords=[],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
