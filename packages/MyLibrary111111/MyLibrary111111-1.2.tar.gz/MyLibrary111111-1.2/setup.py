60c80ba539b7f1e49cedbc23d219bcb8574d80cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:03:08 +0000

    Add package MyLibrary111111/1.2/MyLibrary111111-1.2.tar.gz

---
+++ packages/MyLibrary111111/MyLibrary111111-1.2.tar.gz/MyLibrary111111-1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MyLibrary111111',
+    version='1.2',
+    packages=find_packages(),
+    install_requires=[],  
+    url='https://github.com/OFFdevil/PythonCourse2024/blob/HW1/hw_1/task1/nl.py',
+    license='Plotnikov Licence',
+    author='ONdevil',
+    author_email='dplotnikovon@gmail.com',
+    description='Some words',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
