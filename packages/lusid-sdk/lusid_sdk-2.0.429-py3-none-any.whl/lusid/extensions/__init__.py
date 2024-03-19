d24f0ad2df5956bb3c9555c136e7353cc4138dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 03:03:25 +0000

    Add package lusid-sdk/2.0.429/lusid_sdk-2.0.429-py3-none-any.whl

---
+++ packages/lusid-sdk/lusid_sdk-2.0.429-py3-none-any.whl/lusid/extensions/__init__.py
@@ -0,0 +1,7 @@
+from lusid.extensions.api_client_factory import SyncApiClientFactory, ApiClientFactory
+from lusid.extensions.configuration_loaders import (
+    ConfigurationLoader,
+    SecretsFileConfigurationLoader,
+    EnvironmentVariablesConfigurationLoader,
+    ArgsConfigurationLoader,
+)
