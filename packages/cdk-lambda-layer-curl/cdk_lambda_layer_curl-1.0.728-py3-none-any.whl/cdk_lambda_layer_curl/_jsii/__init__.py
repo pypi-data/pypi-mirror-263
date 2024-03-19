a27948069c8494b99b181bd25b8a40ba37377718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 14:03:17 +0000

    Add package cdk-lambda-layer-curl/1.0.728/cdk_lambda_layer_curl-1.0.728-py3-none-any.whl

---
+++ packages/cdk-lambda-layer-curl/cdk_lambda_layer_curl-1.0.728-py3-none-any.whl/cdk_lambda_layer_curl/_jsii/__init__.py
@@ -0,0 +1,28 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-lambda-layer-curl",
+    "1.0.728",
+    __name__[0:-6],
+    "cdk-lambda-layer-curl@1.0.728.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
