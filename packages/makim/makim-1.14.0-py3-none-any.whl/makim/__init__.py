c0ec850cf0b4822ef610e2d36e48962fd7dd3a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 14:02:57 +0000

    Add package makim/1.14.0/makim-1.14.0-py3-none-any.whl

---
+++ packages/makim/makim-1.14.0-py3-none-any.whl/makim/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for makim."""
+
+
+__author__ = 'Ivan Ogasawara'
+__email__ = 'ivan.ogasawara@gmail.com'
+__version__ = '1.14.0'  # semantic-release
+
+
+from makim.core import Makim  # noqa: F401
