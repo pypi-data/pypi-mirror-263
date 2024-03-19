23f5e66db0c0ef4ce409cc396b417bc8166c1385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 03:03:11 +0000

    Add package gsf/2.0.1.dev34/gsf-2.0.1.dev34-py3-none-any.whl

---
+++ packages/gsf/gsf-2.0.1.dev34-py3-none-any.whl/gsf/__init__.py
@@ -0,0 +1,11 @@
+'''
+Created on Feb 1, 2016
+
+@author: elefebvre
+'''
+
+# Define the gsf implementation you want to use here.
+from .sdk.server import Server
+from .sdk.service import Service
+from .sdk.task import Task
+from .sdk.job import Job
