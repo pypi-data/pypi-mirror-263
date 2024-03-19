c41e50df96adf05ba58c5e653f92f97a515ff0f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 03:03:08 +0000

    Add package nothion/0.3.0/nothion-0.3.0-py3-none-any.whl

---
+++ packages/nothion/nothion-0.3.0-py3-none-any.whl/nothion/_config.py
@@ -0,0 +1,7 @@
+import os
+
+NT_AUTH = os.getenv("NT_AUTH", default="")
+NT_TASKS_DB_ID = os.getenv("NT_TASKS_DB_ID", default="")
+NT_NOTES_DB_ID = os.getenv("NT_NOTES_DB_ID", default="")
+NT_STATS_DB_ID = os.getenv("NT_STATS_DB_ID", default="")
+NT_EXPENSES_DB_ID = os.getenv("NT_EXPENSES_DB_ID", default="")
