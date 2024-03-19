160ad62cd35f45630782d59986260c3ff596f0ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 15:02:55 +0000

    Add package sequifier/1.1.4/sequifier-1.1.4.tar.gz

---
+++ packages/sequifier/sequifier-1.1.4.tar.gz/sequifier-1.1.4/src/sequifier/helpers.py
@@ -0,0 +1,51 @@
+import subprocess
+
+import torch
+from torch import tensor
+
+PANDAS_TO_TORCH_TYPES = {"int64": torch.int64, "float64": torch.float32}
+
+
+def numpy_to_pytorch(data, column_types, target_column, seq_length, device):
+
+    sequence = {
+        col: (
+            tensor(
+                data.query(f"inputCol=='{col}'")[
+                    [str(c) for c in range(seq_length, 0, -1)]
+                ].values
+            )
+            .to(column_types[col])
+            .to(device)
+        )
+        for col in column_types.keys()
+    }
+
+    if "target" in data:
+        target = (
+            tensor(data.query(f"inputCol=='{target_column}'")["target"].values)
+            .to(column_types[target_column])
+            .to(device)
+        )
+    else:
+        target = None
+
+    return (sequence, target)
+
+
+class LogFile(object):
+    def __init__(self, path, open_mode):
+        self._file = open(path, open_mode)
+        self._path = path
+
+    def write(self, string):
+        self._file.write(f"{string}\n")
+        self._file.flush()
+        n_lines = string.count("\n")
+        output = subprocess.check_output(
+            f"tail -{n_lines} {self._path}", shell=True, text=True
+        )
+        print(output)
+
+    def close(self):
+        self._file.close()
