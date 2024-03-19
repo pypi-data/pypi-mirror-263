e8274069799816adce118c2d5c9e5bdebdcd3acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 03:02:55 +0000

    Add package forcealign/1.0.0/forcealign-1.0.0.tar.gz

---
+++ packages/forcealign/forcealign-1.0.0.tar.gz/forcealign-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='forcealign',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=['torch==2.2.1', 'torchaudio==2.2.1', 'pydub==0.25.1', 'g2p-en==2.1.0'],
+    author='Luke Kerbs',
+    description='A Python library for forced alignment of English text to English audio.',
+    url='https://github.com/lukerbs/forcealign',
+    keywords=['force align', 'forced alignment', 'audio segmentation', 'audio forced alignment', 'python forced alignment', 'phoneme', 'generate subtitles'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
