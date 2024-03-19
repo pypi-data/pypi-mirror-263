8ec1be3aa274de0d0066a582b83d5a406817b3e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 16:03:15 +0000

    Add package infrahub-sdk/0.9.1/infrahub_sdk-0.9.1.tar.gz

---
+++ packages/infrahub-sdk/infrahub_sdk-0.9.1.tar.gz/infrahub_sdk-0.9.1/infrahub_sdk/__init__.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+
+import importlib.metadata
+
+from infrahub_sdk.analyzer import GraphQLOperation, GraphQLQueryAnalyzer, GraphQLQueryVariable
+from infrahub_sdk.batch import InfrahubBatch
+from infrahub_sdk.branch import InfrahubBranchManager, InfrahubBranchManagerSync
+from infrahub_sdk.client import InfrahubClient, InfrahubClientSync
+from infrahub_sdk.config import Config
+from infrahub_sdk.exceptions import (
+    AuthenticationError,
+    Error,
+    FilterNotFoundError,
+    GraphQLError,
+    NodeNotFoundError,
+    ServerNotReachableError,
+    ServerNotResponsiveError,
+    ValidationError,
+)
+from infrahub_sdk.graphql import Mutation, Query
+from infrahub_sdk.node import InfrahubNode, InfrahubNodeSync
+from infrahub_sdk.schema import (
+    AttributeSchema,
+    InfrahubRepositoryConfig,
+    InfrahubSchema,
+    NodeSchema,
+    RelationshipCardinality,
+    RelationshipKind,
+    RelationshipSchema,
+    SchemaRoot,
+)
+from infrahub_sdk.store import NodeStore, NodeStoreSync
+from infrahub_sdk.timestamp import Timestamp
+from infrahub_sdk.uuidt import UUIDT, generate_uuid
+
+__all__ = [
+    "AttributeSchema",
+    "AuthenticationError",
+    "Config",
+    "Error",
+    "InfrahubBatch",
+    "InfrahubBranchManager",
+    "InfrahubBranchManagerSync",
+    "InfrahubClient",
+    "InfrahubClientSync",
+    "InfrahubNode",
+    "InfrahubNodeSync",
+    "InfrahubRepositoryConfig",
+    "InfrahubSchema",
+    "FilterNotFoundError",
+    "generate_uuid",
+    "GraphQLQueryAnalyzer",
+    "GraphQLQueryVariable",
+    "GraphQLError",
+    "GraphQLOperation",
+    "NodeNotFoundError",
+    "NodeSchema",
+    "Mutation",
+    "NodeStore",
+    "NodeStoreSync",
+    "Query",
+    "RelationshipSchema",
+    "RelationshipCardinality",
+    "RelationshipKind",
+    "SchemaRoot",
+    "ServerNotReachableError",
+    "ServerNotResponsiveError",
+    "Timestamp",
+    "UUIDT",
+    "ValidationError",
+]
+
+__version__ = importlib.metadata.version("infrahub-sdk")
---
+++ packages/infrahub-sdk/infrahub_sdk-0.9.1.tar.gz/infrahub_sdk-0.9.1/infrahub_sdk/ctl/client.py
@@ -0,0 +1,55 @@
+from typing import Any, Optional
+
+from infrahub_sdk import InfrahubClient, InfrahubClientSync
+from infrahub_sdk.config import Config
+from infrahub_sdk.ctl import config
+
+
+async def initialize_client(
+    branch: Optional[str] = None, identifier: Optional[str] = None, timeout: Optional[int] = None, **kwargs: Any
+) -> InfrahubClient:
+    client_config = {}
+
+    if config.SETTINGS.api_token:
+        client_config["api_token"] = config.SETTINGS.api_token
+
+    if timeout:
+        client_config["timeout"] = timeout
+
+    if branch:
+        client_config["default_branch"] = branch
+
+    client = await InfrahubClient.init(
+        address=config.SETTINGS.server_address,
+        config=Config(**client_config),
+        insert_tracker=True,
+        identifier=identifier,
+        **kwargs,
+    )
+
+    return client
+
+
+def initialize_client_sync(
+    branch: Optional[str] = None, identifier: Optional[str] = None, timeout: Optional[int] = None, **kwargs: Any
+) -> InfrahubClientSync:
+    client_config = {}
+
+    if config.SETTINGS.api_token:
+        client_config["api_token"] = config.SETTINGS.api_token
+
+    if timeout:
+        client_config["timeout"] = timeout
+
+    if branch:
+        client_config["default_branch"] = branch
+
+    client = InfrahubClientSync.init(
+        address=config.SETTINGS.server_address,
+        config=Config(**client_config),
+        insert_tracker=True,
+        identifier=identifier,
+        **kwargs,
+    )
+
+    return client
