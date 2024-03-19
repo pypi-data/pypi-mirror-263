0271361ae388386b8ff3dc2776ec26a9a35f442d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 15:03:40 +0000

    Add package lyytaode/1.1/lyytaode-1.1.tar.gz

---
+++ packages/lyytaode/lyytaode-1.1.tar.gz/lyytaode-1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='lyytaode',
+    version='1.1',
+    author='td',
+    author_email='',
+    description='lyytaode for lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+    
+    ],
+)
