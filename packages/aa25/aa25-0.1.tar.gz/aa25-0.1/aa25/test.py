d616128bc0f12d3be7ce1cbd6845ef1de145ac0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 16:04:59 +0000

    Add package aa25/0.1/aa25-0.1.tar.gz

---
+++ packages/aa25/aa25-0.1.tar.gz/aa25-0.1/aa25/test.py
@@ -0,0 +1,5 @@
+def add_one(number):
+    return number + 1
+
+def aaa():
+	    print("傻憨憨")
---
+++ packages/aa25/aa25-0.1.tar.gz/aa25-0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aa25",
+    version="0.1",
+    author="gao",
+    author_email="",
+    description="add douyin effect to image",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pythonml/douyin_image",
+    packages=setuptools.find_packages(),
+)
