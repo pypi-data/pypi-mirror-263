0821d42807337a07195c9c5652e4560080b55ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:03:44 +0000

    Add package dillagent/1.0.0/dillagent-1.0.0.tar.gz

---
+++ packages/dillagent/dillagent-1.0.0.tar.gz/dillagent-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required_packages = f.read().splitlines()
+
+setup(
+    name='dillagent',
+    version='1.0.0',
+    author='buzzoo123',
+    author_email='buzzoo123@gmail.com',
+    description='Agentic LLM library',
+    long_description='DillAgent is an AI agent library based on the principles of ReAct: Synergizing Reasoning and Acting in Language Models. While there are existing libraries such as Langchain, Langroid, and Llamaindex, DillAgent takes a different approach by prioritizing modularity and abstraction to provide developers with more control over their LLM-powered applications.',
+    url='https://github.com/buzzoo123/dillagent',
+    packages=find_packages(),
+    install_requires=required_packages,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
