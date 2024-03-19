7011306cf97d352816c78453c3a3393fd6806357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:03:06 +0000

    Add package locust/2.24.1.dev21/locust-2.24.1.dev21.tar.gz

---
+++ packages/locust/locust-2.24.1.dev21.tar.gz/locust-2.24.1.dev21/locust/webui/src/components/ExceptionsTable/ExceptionsTable.tsx
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+
+import Table from 'components/Table/Table';
+import { IRootState } from 'redux/store';
+import { ISwarmException } from 'types/ui.types';
+
+const tableStructure = [
+  { key: 'count', title: '# occurrences' },
+  { key: 'msg', title: 'Message' },
+  { key: 'traceback', title: 'Traceback' },
+];
+
+export function ExceptionsTable({ exceptions }: { exceptions: ISwarmException[] }) {
+  return <Table<ISwarmException> rows={exceptions} structure={tableStructure} />;
+}
+
+const storeConnector = ({ ui: { exceptions } }: IRootState) => ({ exceptions });
+
+export default connect(storeConnector)(ExceptionsTable);
