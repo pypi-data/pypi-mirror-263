aa6c59d47b4d84c98470cf6c275b62d4b6bcde63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:03:09 +0000

    Add package latex-gen-3000/0.0.4/latex_gen_3000-0.0.4.tar.gz

---
+++ packages/latex-gen-3000/latex_gen_3000-0.0.4.tar.gz/latex_gen_3000-0.0.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="latex_gen_3000",
+    version="0.0.4",
+    author="AlexandrBon",
+    author_email="bon527870@mail.ru",
+    description="A small latex generator package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AlexandrBon/latex_gen",
+    packages=setuptools.find_packages(),
+    py_modules=["latex_gen"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+# if __name__ == "__main__":
+#     print(setuptools.find_packages())
