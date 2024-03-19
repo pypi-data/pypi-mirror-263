75829442b1b8884ad44b7001b99afcf82428bf06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 14:02:56 +0000

    Add package kraken-build/0.35.7/kraken_build-0.35.7-py3-none-any.whl

---
+++ packages/kraken-build/kraken_build-0.35.7-py3-none-any.whl/kraken/std/mitm/__init__.py
@@ -0,0 +1,85 @@
+"""
+Manages a `mitmweb` instance to run in the background for injecting basic-auth into requests to hosts for which
+credentials are passed down. The `mitmweb` web interface can be reached on `localhost:8900` while it is running.
+The proxy stays alive as a daemon process until its configuration changes.
+"""
+
+from __future__ import annotations
+
+import json
+import logging
+import time
+from collections.abc import Mapping, Sequence
+from pathlib import Path
+
+from kraken.std.util.daemon_controller import DaemonController
+
+logger = logging.getLogger(__name__)
+
+daemon_state_file = Path("~/.config/krakenw/.mitmweb-daemon-state.json").expanduser()
+daemon_log_file = daemon_state_file.with_suffix(".log")
+mitmweb_port = 8899
+mitmweb_ui_port = 8900
+mitmproxy_ca_cert_file = Path("~/.mitmproxy/mitmproxy-ca-cert.pem").expanduser()
+inject_auth_addon_file = Path(__file__).parent / "mitm_addon.py"
+
+
+def start_mitmweb_proxy(
+    auth: Mapping[str, tuple[str, str]],
+    startup_wait_time: float = 3.0,
+    mitmweb_bin: str = "mitmweb",
+    additional_args: Sequence[str] = (),
+) -> tuple[str, Path]:
+    """
+    Ensure that a `mitmweb` process with the given *auth* configuration and *additional_args* is running. If a
+    process is already running that doens't match the spec, it will be stopped and a new one will be started.
+
+    Note:
+        This process is managed globally and the state is stored under `~/.mitmproxy`. Switching between projects
+        that require a different configuration will stop and start the proxy constantly.
+    """
+
+    controller = DaemonController("kraken.mitmweb", daemon_state_file)
+    started = controller.run(
+        command=[
+            mitmweb_bin,
+            "--no-web-open-browser",
+            "--web-port",
+            str(mitmweb_ui_port),
+            "--listen-port",
+            str(mitmweb_port),
+            "-s",
+            str(inject_auth_addon_file),
+            "--set",
+            "auth=" + json.dumps(auth),
+            # `mitmproxy` buffers the entire response before forwarding it to
+            # the client. This is problematic when e.g. cloning large git repos
+            # via http. As we're not using any filtering mechanism, we can just
+            # stream the bodies through without `mitmproxy` storing them.
+            #
+            # See https://github.com/mitmproxy/mitmproxy/issues/6237 for
+            # context.
+            "--set",
+            "stream_large_bodies=3m",
+            *additional_args,
+        ],
+        cwd=Path("~").expanduser(),
+        stdout=daemon_log_file,
+        stderr="stdout",
+    )
+    if started:
+        # Give the proxy some time to fully start up.
+        time.sleep(startup_wait_time)
+        if not controller.is_alive():
+            raise RuntimeError("The mitmweb proxy failed to start. Check its logs at %s" % daemon_log_file)
+        print("mitmweb was started successfully")
+    else:
+        print("mitmweb already running")
+    print(f"proxy available at http://localhost:{mitmweb_port}")
+    print(f"web ui available at http://localhost:{mitmweb_ui_port}")
+    return f"localhost:{mitmweb_port}", mitmproxy_ca_cert_file
+
+
+def stop_mitmweb_proxy() -> None:
+    controller = DaemonController("kraken.mitmweb", daemon_state_file)
+    controller.stop()
