e94039e1acf2f481946f6d37d28e900f7d6cf41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 03:03:09 +0000

    Add package catnekaise.actions-constructs/0.2.18/catnekaise.actions-constructs-0.2.18.tar.gz

---
+++ packages/catnekaise.actions-constructs/catnekaise.actions-constructs-0.2.18.tar.gz/catnekaise.actions-constructs-0.2.18/src/catnekaise_actions_constructs/_jsii/__init__.py
@@ -0,0 +1,28 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import catnekaise_cdk_iam_utilities._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@catnekaise/actions-constructs",
+    "0.2.18",
+    __name__[0:-6],
+    "actions-constructs@0.2.18.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
