2cb1695accf273c3203746cbe45f109917012e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 14:03:17 +0000

    Add package aiosalesforce/0.2.0/aiosalesforce-0.2.0.tar.gz

---
+++ packages/aiosalesforce/aiosalesforce-0.2.0.tar.gz/aiosalesforce-0.2.0/src/aiosalesforce/auth/client_credentials_flow.py
@@ -0,0 +1,68 @@
+from httpx import AsyncClient
+
+from aiosalesforce.auth.base import Auth
+from aiosalesforce.events import EventBus, RestApiCallConsumptionEvent
+from aiosalesforce.exceptions import AuthenticationError
+from aiosalesforce.utils import json_loads
+
+
+class ClientCredentialsFlow(Auth):
+    """
+    Authenticate using the OAuth 2.0 Client Credentials Flow.
+
+    https://help.salesforce.com/s/articleView?id=sf.remoteaccess_oauth_client_credentials_flow.htm&type=5
+
+    Parameters
+    ----------
+    client_id : str
+        Client ID.
+    client_secret : str
+        Client secret.
+
+    """
+
+    def __init__(self, client_id: str, client_secret: str) -> None:
+        super().__init__()
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    async def _acquire_new_access_token(
+        self,
+        client: AsyncClient,
+        base_url: str,
+        version: str,
+        event_bus: EventBus,
+    ) -> str:
+        del version  # not used in this flow (this line is for linter)
+        response = await client.post(
+            f"{base_url}/services/oauth2/token",
+            headers={
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            data={
+                "grant_type": "client_credentials",
+                "client_id": self.client_id,
+                "client_secret": self.client_secret,
+            },
+        )
+        await event_bus.publish_event(
+            RestApiCallConsumptionEvent(
+                type="rest_api_call_consumption",
+                response=response,
+            )
+        )
+        if not response.is_success:
+            try:
+                response_json = json_loads(response.content)
+                error_code = response_json["error"]
+                error_message = response_json["error_description"]
+            except Exception:  # pragma: no cover
+                error_code = None
+                error_message = response.text
+            raise AuthenticationError(
+                f"[{error_code}] {error_message}" if error_code else error_message,
+                response=response,
+                error_code=error_code,
+                error_message=error_message,
+            )
+        return json_loads(response.content)["access_token"]
---
+++ packages/aiosalesforce/aiosalesforce-0.2.0.tar.gz/aiosalesforce-0.2.0/src/aiosalesforce/auth/soap.py
@@ -0,0 +1,135 @@
+import re
+import textwrap
+import time
+
+from httpx import AsyncClient
+
+from aiosalesforce.auth.base import Auth
+from aiosalesforce.events import EventBus, RestApiCallConsumptionEvent
+from aiosalesforce.exceptions import AuthenticationError
+
+
+class SoapLogin(Auth):
+    """
+    Authenticate using the SOAP login method.
+
+    https://developer.salesforce.com/docs/atlas.en-us.api.meta/api/sforce_api_calls_login.htm
+
+    Parameters
+    ----------
+    username : str
+        Username.
+    password : str
+        Password.
+    security_token : str
+        Security token.
+
+    """
+
+    def __init__(
+        self,
+        username: str,
+        password: str,
+        security_token: str,
+    ):
+        super().__init__()
+        self.username = username
+        self.password = password
+        self.security_token = security_token
+
+        self._expiration_time: float | None = None
+
+    async def _acquire_new_access_token(
+        self,
+        client: AsyncClient,
+        base_url: str,
+        version: str,
+        event_bus: EventBus,
+    ) -> str:
+        soap_xml_payload = f"""
+        <?xml version="1.0" encoding="utf-8" ?>
+        <env:Envelope
+            xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xmlns:env="http://schemas.xmlsoap.org/soap/envelope/">
+            <env:Body>
+                <n1:login xmlns:n1="urn:partner.soap.sforce.com">
+                    <n1:username>{self.username}</n1:username>
+                    <n1:password>{self.password}{self.security_token}</n1:password>
+                </n1:login>
+            </env:Body>
+        </env:Envelope>
+        """
+        response = await client.post(
+            f"{base_url}/services/Soap/u/{version}",
+            content=textwrap.dedent(soap_xml_payload).strip(),
+            headers={
+                "Content-Type": "text/xml; charset=UTF-8",
+                "SOAPAction": "login",
+                "Accept": "text/xml",
+            },
+        )
+        await event_bus.publish_event(
+            RestApiCallConsumptionEvent(
+                type="rest_api_call_consumption",
+                response=response,
+            )
+        )
+        response_text = response.text
+        if not response.is_success:
+            try:
+                exception_code = str(
+                    re.search(
+                        r"<sf:exceptionCode>(.+)<\/sf:exceptionCode>",
+                        response_text,
+                    ).groups()[0]  # type: ignore
+                )
+            except AttributeError:  # pragma: no cover
+                exception_code = None
+            try:
+                exception_message = str(
+                    re.search(
+                        r"<sf:exceptionMessage>(.+)<\/sf:exceptionMessage>",
+                        response_text,
+                    ).groups()[0]  # type: ignore
+                )
+            except AttributeError:  # pragma: no cover
+                exception_message = response_text
+            raise AuthenticationError(
+                message=(
+                    f"[{exception_code}] {exception_message}"
+                    if exception_code
+                    else exception_message
+                ),
+                response=response,
+                error_code=exception_code,
+                error_message=exception_message,
+            )
+        match_ = re.search(r"<sessionId>(.+)<\/sessionId>", response_text)
+        if match_ is None:  # pragma: no cover
+            raise AuthenticationError(
+                f"Failed to parse sessionId from the SOAP response: {response_text}",
+                response,
+            )
+        session_id = match_.groups()[0]
+
+        # Parse expiration time
+        match_ = re.search(
+            r"<sessionSecondsValid>(.+)<\/sessionSecondsValid>",
+            response_text,
+        )
+        self._expiration_time = None
+        if match_ is not None:
+            try:
+                self._expiration_time = time.time() + int(match_.groups()[0])
+            except ValueError:  # pragma: no cover
+                pass
+
+        return session_id
+
+    @property
+    def expired(self) -> bool:
+        super().expired
+        if self._expiration_time is None:  # pragma: no cover
+            return False
+        return self._expiration_time <= time.time()
