8de7a156cbb986e6d9202363c79aad9e4c596b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 14:03:17 +0000

    Add package aiosalesforce/0.2.0/aiosalesforce-0.2.0-py3-none-any.whl

---
+++ packages/aiosalesforce/aiosalesforce-0.2.0-py3-none-any.whl/aiosalesforce/__init__.py
@@ -0,0 +1,29 @@
+__version__ = "0.2.0"
+
+__all__ = [
+    "Salesforce",
+    "BulkApiBatchConsumptionEvent",
+    "RequestEvent",
+    "ResponseEvent",
+    "RestApiCallConsumptionEvent",
+    "RetryEvent",
+    "ExceptionRule",
+    "ResponseRule",
+    "RetryPolicy",
+    "format_soql",
+]
+
+from .client import Salesforce
+from .events import (
+    BulkApiBatchConsumptionEvent,
+    RequestEvent,
+    ResponseEvent,
+    RestApiCallConsumptionEvent,
+    RetryEvent,
+)
+from .retries import (
+    ExceptionRule,
+    ResponseRule,
+    RetryPolicy,
+)
+from .utils import format_soql
