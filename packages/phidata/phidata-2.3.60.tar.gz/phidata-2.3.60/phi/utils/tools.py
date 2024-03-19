409771270ab461c3ba93f29640a176a9d57eab81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 03:03:36 +0000

    Add package phidata/2.3.60/phidata-2.3.60.tar.gz

---
+++ packages/phidata/phidata-2.3.60.tar.gz/phidata-2.3.60/phi/utils/tools.py
@@ -0,0 +1,40 @@
+from typing import Dict, Any, Optional
+
+from phi.tools.function import Function, FunctionCall
+from phi.utils.functions import get_function_call
+
+
+def get_function_call_for_tool_call(
+    tool_call: Dict[str, Any], functions: Optional[Dict[str, Function]] = None
+) -> Optional[FunctionCall]:
+    if tool_call.get("type") == "function":
+        _tool_call_id = tool_call.get("id")
+        _tool_call_function = tool_call.get("function")
+        if _tool_call_function is not None:
+            _tool_call_function_name = _tool_call_function.get("name")
+            _tool_call_function_arguments_str = _tool_call_function.get("arguments")
+            if _tool_call_function_name is not None:
+                return get_function_call(
+                    name=_tool_call_function_name,
+                    arguments=_tool_call_function_arguments_str,
+                    call_id=_tool_call_id,
+                    functions=functions,
+                )
+    return None
+
+
+def extract_tool_call_from_string(text: str, start_tag: str = "<tool_call>", end_tag: str = "</tool_call>"):
+    start_index = text.find(start_tag) + len(start_tag)
+    end_index = text.find(end_tag)
+
+    # Extracting the content between the tags
+    return text[start_index:end_index].strip().replace("'", '"')
+
+
+def remove_tool_calls_from_string(text: str, start_tag: str = "<tool_call>", end_tag: str = "</tool_call>"):
+    """Remove multiple tool calls from a string."""
+    while start_tag in text and end_tag in text:
+        start_index = text.find(start_tag)
+        end_index = text.find(end_tag) + len(end_tag)
+        text = text[:start_index] + text[end_index:]
+    return text
