311b974bc8ea58390dbe48154cb14345238a631d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 14:03:09 +0000

    Add package cdk-wordpress/0.0.858/cdk_wordpress-0.0.858-py3-none-any.whl

---
+++ packages/cdk-wordpress/cdk_wordpress-0.0.858-py3-none-any.whl/cdk_wordpress/_jsii/__init__.py
@@ -0,0 +1,29 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_ecs._jsii
+import aws_cdk.aws_ecs_patterns._jsii
+import aws_cdk.aws_efs._jsii
+import aws_cdk.aws_rds._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-wordpress", "0.0.858", __name__[0:-6], "cdk-wordpress@0.0.858.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
