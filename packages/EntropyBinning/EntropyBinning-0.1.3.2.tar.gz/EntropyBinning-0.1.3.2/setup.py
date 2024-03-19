3e81cabb0877db2c561353551efa8907d5e8ab31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:03:11 +0000

    Add package EntropyBinning/0.1.3.2/EntropyBinning-0.1.3.2.tar.gz

---
+++ packages/EntropyBinning/EntropyBinning-0.1.3.2.tar.gz/EntropyBinning-0.1.3.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.1.3.2'
+DESCRIPTION = 'Entropy base binning for data analysis'
+
+# Setting up
+setup(
+    name="EntropyBinning",
+    version=VERSION,
+    author="chingDev.Official (Prince Carl Ajoc)",
+    author_email="chingace471@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["numpy", "pandas"],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
