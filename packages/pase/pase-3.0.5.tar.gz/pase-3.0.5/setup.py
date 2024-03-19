2b40641c8fcb5eb5a4c042623a82cbb29e199172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 16:05:41 +0000

    Add package pase/3.0.5/pase-3.0.5.tar.gz

---
+++ packages/pase/pase-3.0.5.tar.gz/pase-3.0.5/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "readme.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="pase",
+    version="3.0.5",
+    description="Python Audio Spectrogram Explorer: a GUI to visualize audio files as spectrograms, log annotations and extract time-frequency shapes",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sebastianmenze/Python-Audio-Spectrogram-Explorer",
+    author="Sebastian Menze",
+    author_email="sebastian.menze@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["pase"],
+    include_package_data=True,
+    install_requires=["soundfile","simpleaudio","scikit-image","moviepy"])
+    # entry_points={
+    #     "console_scripts": [
+    #         "pase=pase.__main__:main",
+        # ]
+    # },
+# )
