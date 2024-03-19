3c9d0ffdab03b39283d0781462cede1eb203d56d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:03:02 +0000

    Add package morgana-engine/0.3.1/morgana_engine-0.3.1-py3-none-any.whl

---
+++ packages/morgana-engine/morgana_engine-0.3.1-py3-none-any.whl/morgana_engine/__init__.py
@@ -0,0 +1,11 @@
+"""
+morgana_engine
+=====================
+
+morgana_engine is a Python module for handling partitioned data
+using SQL queries.
+"""
+
+__version__ = "0.3.1"
+
+from .engine import select_lambda_endpoint  # noqa
