4307e6453df1cadebb18f9ed9f93028dbcf7598a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 15:03:33 +0000

    Add package xl2roefact/0.4.1.dev1/xl2roefact-0.4.1.dev1-py3-none-any.whl

---
+++ packages/xl2roefact/xl2roefact-0.4.1.dev1-py3-none-any.whl/xl2roefact/__version__.py
@@ -0,0 +1,49 @@
+#!../.venv/bin/python3
+"""xl2roefact version info.
+```
+ ######## ####              ########  ######
+ #  ##  # #  #              #      # ##    ##
+ ##    ## #  #              ###  ### #  ##  #
+  ##  ##  #  #                #  #   #  ##  #
+ ##    ## #  #                #  #   #  ##  #
+ #  ##  # #  #####            #  #   #  ##  #
+ #  ##  # #      #            #  #   ##    ##
+ ######## ########            ####    ######
+
+                            ######## ########  ######   ######  ########
+ #######   ######           #      # #     #  ##    ## ##    ## #      #
+ #     ## ##    ## #######  #  ##### #  ####  #  ##  # #  ##  # ###  ###
+ #  ##  # #  ##  # #     #  #    #   #    #   #      # #  #####   #  #
+ #  ##### #  ##  # #######  #  ###   #  ###   #  ##  # #  #####   #  #
+ #  # #   #  ##  #          #  ##### #  #     #  ##  # #  ##  #   #  #
+ #  #  #  ##    ##          #      # #  #     #  ##  # ##    ##   #  #
+ ####   #  ######           ######## ####     ########  ######    ####
+```
+"""
+
+import packaging.utils
+
+__version__ = "0.4.1.dev1"
+
+
+
+
+
+def normalized_version(
+    raw_version: str = __version__
+) -> str:
+    """transform version string in canonical form.
+
+    Usage:
+    - `import xl2roefact`
+    - `xl2roefact.__version__.normalized_version()`
+
+    Args:
+        `raw_version (str)`: a raw version string. Defaults to package current version string.
+
+    Returns:
+        `str:` canonical version string
+    """
+    return packaging.utils.canonicalize_version(raw_version)
+
+
