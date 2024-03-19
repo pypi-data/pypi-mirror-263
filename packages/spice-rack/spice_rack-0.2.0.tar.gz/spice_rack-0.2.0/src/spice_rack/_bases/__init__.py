e79c479f8af1f2d9a0e591bd64433e5048d570dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:03:32 +0000

    Add package spice-rack/0.2.0/spice_rack-0.2.0.tar.gz

---
+++ packages/spice-rack/spice_rack-0.2.0.tar.gz/spice_rack-0.2.0/src/spice_rack/_bases/__init__.py
@@ -0,0 +1,9 @@
+from spice_rack._bases import (
+    _base_base as base_base,
+    _mixins as mixins,
+    _value_model as value_model,
+    _dispatchable as dispatchable,
+    _special_str as special_str,
+    _settings as settings,
+    _exception as exceptions
+)
