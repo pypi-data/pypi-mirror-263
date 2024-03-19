be6aaa477aaef1ce793871a981ac77a98f7941b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:03:16 +0000

    Add package DCM2/0.1/DCM2-0.1.tar.gz

---
+++ packages/DCM2/DCM2-0.1.tar.gz/DCM2-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DCM2',
+    version='0.1',
+    packages=find_packages(),
+    license='MIT',
+    author='Axel Osses, Emir Chacra',
+    install_requires=[
+        'numpy',
+        'graphviz',
+        'matplotlib'
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
