58937674c416e713f7172c5f4f2cf82e0f032b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:03:02 +0000

    Add package dateroll/0.0.41/dateroll-0.0.41.tar.gz

---
+++ packages/dateroll/dateroll-0.0.41.tar.gz/dateroll-0.0.41/dateroll/date/date.py
@@ -0,0 +1,252 @@
+import calendar
+import datetime
+from datetime import timezone
+import code
+
+import dateutil.relativedelta
+from dateutil.parser import parse
+from dateroll.settings import settings
+from dateroll.calendars.calendarmath import calmath
+from dateroll.duration.duration import Duration
+from dateroll import utils
+from dateroll import pretty
+
+DateLike = (datetime.datetime, datetime.date)
+
+
+class Date(datetime.date):
+    '''
+    A Date, inherits from datetime.date, represents a specific day (no sub-units of day)
+    '''
+
+    @staticmethod
+    def from_string(o, **dateparser_kwargs):
+        """
+        if string provided use dateutil's parser
+        note: dateutil's parser implements fall backs
+        future: create custom string parser that explicitly checks mask matches string
+        """
+        if isinstance(o,Date):
+            return o
+        elif isinstance(o,str):
+            me = parse(o, **dateparser_kwargs)
+            return Date.from_datetime(me)
+        
+        else:
+            raise TypeError(f'from_string requires string, cannot use {type(o).__name__}')
+
+    @staticmethod
+    def from_datetime(o):
+        """
+        Create a Date instance from a datetime.datetime (drops time information), or datetime.date
+        """
+        if isinstance(o,Date):
+            return o
+        elif isinstance(o, DateLike):
+            y, m, d = o.year, o.month, o.day
+            return Date(y, m, d)
+        else:
+            raise TypeError(f'from_datetime requires string, cannot use {type(o).__name__}')
+
+    def to_string(self):
+        fmt = utils.convention_map[settings.convention]
+        date_string = self.strftime(fmt)
+        return date_string
+
+    def __str__(self):
+        return self.to_string()
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}("{self.to_string()}")'
+
+    @staticmethod
+    def today():
+        return Date.from_datetime(datetime.date.today())
+
+    @property
+    def datetime(self):
+        return datetime.datetime(self.year, self.month, self.day)
+
+    @property
+    def date(self):
+        return datetime.date(self.year, self.month, self.day)
+
+    def is_bd(self, cals=None):
+        """
+        am i a business day?
+        """
+        return calmath.is_bd(self.date, cals=cals)
+
+    @property
+    def dotw(self):
+        """
+        which day of the week am i
+        """
+        dt_mapping = {
+            0: "Mon",
+            1: "Tue",
+            2: "Wed",
+            3: "Thu",
+            4: "Fri",
+            5: "Sat",
+            6: "Sun",
+        }
+        day_of_week = dt_mapping[self.weekday()]
+        return day_of_week
+
+    @property
+    def woty(self):
+        """
+        week of the year, iso 8601
+        """
+        return self.isocalendar()[1]
+
+    @property
+    def xls(self):
+        offset = 693594
+        n = self.toordinal()
+        rs = n - offset
+        return rs
+
+    @property
+    def unix(self):
+        return self.datetime.timestamp()
+
+    @property
+    def iso(self):
+        return self.isoformat().split("T")[0]
+
+    def __add__(self, o):
+        """
+        add, do str conversion first, then handle suborinate cases
+        """
+        # convert string if first
+        if isinstance(o, str):
+            from dateroll.ddh.ddh import ddh
+            o = ddh(o)
+
+        # apply rules by type
+        if isinstance(o, DateLike):
+            # date + date
+            raise TypeError("unsupported operand type(s) for +: 'Date' and 'Date'")
+        
+        elif isinstance(o, Duration):
+            # date + duration
+            # goes to __radd__ of Duration
+            return Date.from_datetime(o.__radd__(self))
+        elif isinstance(o, int):
+            # date + int, int -> duration, -> date + duration -> __radd__ duration
+            # goes to __radd__ of Duration
+            return Duration(days=o).__radd__(self)
+        elif isinstance(o, datetime.timedelta):
+            return Date.from_datetime(self.date + o)
+        elif isinstance(o, dateutil.relativedelta.relativedelta):
+            return Date.from_datetime(self.date + o)
+        else:
+            raise TypeError(
+                f"unsupported operand type(s) for +: 'Date' and {type(o).__name__}"
+            )
+
+    def __pos__(self):
+        return self
+
+    def __sub__(self, o):
+        
+        """
+        sub
+        """
+        # convert string if first
+        if isinstance(o, str):
+            from dateroll.ddh.ddh import ddh
+
+            o = ddh(o)
+
+        # apply rules by type
+        
+        if isinstance(o, DateLike):
+            # date - date
+            # convert all datetimes to datetime.date for sub to be timedelta, then create Duration from timedelta
+            if isinstance(o,Date):
+                dt = o.date
+            elif isinstance(o,datetime.datetime):
+                # truncates time!
+                dt = datetime.date(o.year,o.month,o.day) 
+            else:
+                if isinstance(o, datetime.date):
+                    dt = o
+
+            relative_delta = dateutil.relativedelta.relativedelta(self.date,dt)
+            return Duration.from_relativedelta(relative_delta,_anchor_start=dt,_anchor_end=self.date)
+        
+        elif isinstance(o, Duration) or 'Duration' in o.__class__.__name__:
+            # sometimes Duration does not go here instead go as a dateutil, to make sure come here we use .__name__
+            # date + duration
+            # goes to __radd__ of Duration
+            x = o.__rsub__(self)
+            return x
+        elif isinstance(o, int):
+            # date + int, int -> duration, -> date + duration -> __radd__ duration
+            # goes to __rsub__ of Duration
+            return Duration(days=o).__rsub__(self)
+        elif isinstance(o, datetime.timedelta):
+            return Date.from_datetime(self.date - o)
+        elif isinstance(o, dateutil.relativedelta.relativedelta):
+            return Date.from_datetime(self.date - o)
+        else:
+            raise TypeError(
+                f"unsupported operand type(s) for : 'Date' and {type(o).__name__}"
+            )
+        
+    def __rsub__(self,o):
+        if isinstance(o,(datetime.date,datetime.datetime)):
+            return Date.from_datetime(o) - self
+
+        raise TypeError(f'Cannot subtract Date from {type(o).__name__}, what are you trying to do?')
+
+    def __iadd__(self, o):
+        if isinstance(o, str):
+            from dateroll.ddh.ddh import ddh
+
+            o = ddh(o)
+        self = self + o
+        return self
+
+    def __isub__(self, o):
+        if isinstance(o, str):
+            from dateroll.ddh.ddh import ddh
+
+            o = ddh(o)
+        if isinstance(o, DateLike):
+            self = self - (self - o)
+        else:
+            self = self - o
+        return self
+    
+    @property
+    def cal(self):
+        if hasattr(self,'origin_dur_date'):
+            pretty_calendars = pretty.pretty_between_two_dates(self.date,self.origin_dur_date,self.origin_dur_cals,calmath)
+            print(pretty_calendars)
+        else:
+            pretty_calendars = pretty.pretty_between_two_dates(self.date,self.date,None,calmath)
+            print(pretty_calendars)
+
+    def __repr__(self):
+        return f'Date(year={self.year},month={self.month},day={self.day})'
+    
+    def to_string(self):
+        '''
+        should print as string according to convention
+        '''
+        if settings.convention=='MDY':
+            return self.strftime('%m-%d-%Y')
+        elif settings.convention=='DMY':
+            return self.strftime('%d-%m-%Y')
+        else:
+            return self.strftime('%Y-%m-%d')
+
+
+DateLike = DateLike + (Date,)
+
+if __name__ == "__main__": # pragma: no cover
+    ...
---
+++ packages/dateroll/dateroll-0.0.41.tar.gz/dateroll-0.0.41/dateroll/settings.py
@@ -0,0 +1,124 @@
+import warnings
+import pathlib
+import importlib.util
+import code
+
+path = pathlib.Path("~/.dateroll/settings.py").expanduser()
+
+default_settings = {
+    'debug':True,
+    'convention':'MDY',
+    'ie':'(]'
+}
+
+default_settings_validation ={
+    'debug':(
+        lambda x: isinstance(x,bool),
+        TypeError('debug must be bool')
+    ),
+    'convention': (
+        lambda x: isinstance(x,str) and x in ['YMD','MDY','DMY'],
+        ValueError('must be one of "MDY", "DMY" or "YMD".')
+    ),
+    'ie': (
+        lambda x: isinstance(x,str) and x in ['()','(]','[)','[]'],
+        ValueError('must be one of (), (], [), or [] such that is the interval: (a,b])')
+    )
+
+}
+
+class Settings:
+    '''
+    singleton whose attributes are stored in a plain .py file as name=value
+    '''
+    def __init__(self):
+        '''
+        if settings load and validate, if not load defaults and save
+        '''
+        self.load_default()
+
+        if path.exists():
+            self.load()
+            self.validate()
+        else:
+            self.save()
+
+    def load_default(self):
+        '''
+        update singleton with settings
+        '''
+        self.__dict__.update(default_settings)
+    
+    def load(self):
+        '''
+        try to load settings file, if corrupted replace with defaults
+        '''
+        try:
+            spec = importlib.util.spec_from_file_location("module.name", path)
+            mod = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(mod)
+            settings = mod.__dict__
+            self.__dict__.update(settings)
+        except Exception as e:
+            msg = f'Settings corrupted in {path}, restoring defaults.'
+            warnings.warn(msg)
+            self.load_default()
+            self.save()
+
+    def save(self):
+        '''
+        save settings file
+        '''
+        path.parent.mkdir(parents=True, exist_ok=True)
+        with path.open('w') as f:
+            txt = f'# Dateroll settings file\n# {path}\n\n'
+            for k,v in self.__dict__.items():
+                if not k.startswith('__'):
+                    txt += f'{k}={repr(v)}\n'
+            f.write(txt)
+
+    def validate(self):
+        '''
+        3 validate checks per setting / key is valid, type of value is valid, and value of value is valid
+        '''
+        # key check
+        reset = False
+        for k,v in self.__dict__.items():
+            if k.startswith('__'):
+                continue
+            key_is_valid = k in default_settings
+            type_is_valid = isinstance(v,type(default_settings.get(k,None)))
+            missing_validation = (lambda x:False,ValueError(f'Setting {k} not found'))
+            func_value_is_valid, exc = default_settings_validation.get(k,missing_validation)
+            if  not (key_is_valid and type_is_valid and func_value_is_valid(v)):
+                msg = f'Settings corrupted in {path}, restoring defaults.'
+                warnings.warn(msg)
+                self.load_default()
+                break
+
+    def __setattr__(self,k,v):
+        '''
+        add a new setting
+        '''
+        # check, set, save
+        func_value_is_valid,exc = default_settings_validation[k]
+        
+        if not func_value_is_valid(v):
+            raise exc
+        
+        super().__setattr__(k,v)
+        self.save()
+
+    def __repr__(self):
+        ''' 
+        show settings
+        '''
+        kwargs = ','.join([f'{k}={repr(v)}' for k,v in self.__dict__.items()])
+        string = f'{self.__class__.__name__}({kwargs})'
+        return string
+
+                
+settings = Settings()
+
+if __name__=='__main__':  # pragma: no cover
+    settings = Settings()
