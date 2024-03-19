9d791af029b1899cd95f8403f56e1ead82d9f3d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 14:03:11 +0000

    Add package cdk-events-notify/1.0.761/cdk_events_notify-1.0.761-py3-none-any.whl

---
+++ packages/cdk-events-notify/cdk_events_notify-1.0.761-py3-none-any.whl/cdk_events_notify/_jsii/__init__.py
@@ -0,0 +1,32 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk.assertions._jsii
+import aws_cdk.aws_events._jsii
+import aws_cdk.aws_events_targets._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_logs._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-events-notify",
+    "1.0.761",
+    __name__[0:-6],
+    "cdk-events-notify@1.0.761.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
