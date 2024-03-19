23920730a5270da43d9886be1c493f6d1b2f0039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 15:03:28 +0000

    Add package package-dev-tools/0.5.16/package_dev_tools-0.5.16-py3-none-any.whl

---
+++ packages/package-dev-tools/package_dev_tools-0.5.16-py3-none-any.whl/package_dev_tools/models/path.py
@@ -0,0 +1,25 @@
+from typing import TypeVar
+
+import superpathlib
+from simple_classproperty import classproperty
+
+T = TypeVar("T", bound="Path")
+
+
+class Path(superpathlib.Path):
+    @classproperty
+    def readme(cls: type[T]) -> T:  # type: ignore
+        return cls("README.md")
+
+    @classproperty
+    def workflows(cls: type[T]) -> T:  # type: ignore
+        return cls(".github") / "workflows"
+
+    @property
+    def has_text_content(self) -> bool:
+        try:
+            self.text
+            has_text = True
+        except UnicodeDecodeError:
+            has_text = False
+        return has_text
