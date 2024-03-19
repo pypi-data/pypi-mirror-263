6047226dd3768418fde096d839fd6969fb781c03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 14:03:03 +0000

    Add package spelunker/1.1.5/spelunker-1.1.5.tar.gz

---
+++ packages/spelunker/spelunker-1.1.5.tar.gz/spelunker-1.1.5/setup.py
@@ -0,0 +1,27 @@
+import re
+import numpy
+from setuptools import setup, Extension, find_packages
+
+VERSIONFILE='src/_version.py'
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
+setup(name='spelunker',
+      version=verstr,
+      description='spelunker: a library to extract guidestar data and observe technical and stellar events',
+      url='https://github.com/GalagaBits/JWST-FGS-Spelunker',
+      author='Derod Deal',
+      author_email='dealderod@ufl.edu',
+      license='MIT',
+      packages=['spelunker'],
+      package_dir={'spelunker': 'src'},
+      requires=['numpy','scipy', 'pandas', 'jwst', 'astroquery', 'astropy','astroplan','matplotlib', 'ray',],
+      install_requires=['numpy','scipy', 'pandas', 'jwst', 'astroquery', 'astropy','astroplan','matplotlib', 'ray', 'wheel'],
+      keywords='guidestars',
+      python_requires='>=3.10',
+      zip_safe=False)
