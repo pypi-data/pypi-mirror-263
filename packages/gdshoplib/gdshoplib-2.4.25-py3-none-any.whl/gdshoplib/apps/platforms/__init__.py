28d72268cde61cc863f39fc807c02e18345db66c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:03:12 +0000

    Add package gdshoplib/2.4.25/gdshoplib-2.4.25-py3-none-any.whl

---
+++ packages/gdshoplib/gdshoplib-2.4.25-py3-none-any.whl/gdshoplib/apps/platforms/__init__.py
@@ -0,0 +1,30 @@
+from .avito import AvitoManager
+from .base import Platform
+from .instagram import InstagramManager
+from .ok import OkManager
+from .sm import SberMarketManager
+from .tg import TgManager
+from .ula import UlaManager
+from .vk import VKManager
+from .yam import YandexMarketManager
+from .yamb import YandexBussinesManager
+
+
+def search_platform(key):
+    for platform_class in Platform.__subclasses__():
+        if platform_class.KEY.lower() == key.lower():
+            return platform_class
+
+
+__all__ = (
+    "AvitoManager",
+    "InstagramManager",
+    "OkManager",
+    "TgManager",
+    "UlaManager",
+    "VKManager",
+    "YandexMarketManager",
+    "YandexBussinesManager" "SberMarketManager",
+    "Platform",
+    "search_platform",
+)
