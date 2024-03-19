6048d578587aa890981960a0a6664c9e99276b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 03:02:41 +0000

    Add package simplesingletable/3.3.0/simplesingletable-3.3.0-py3-none-any.whl

---
+++ packages/simplesingletable/simplesingletable-3.3.0-py3-none-any.whl/simplesingletable/__init__.py
@@ -0,0 +1,8 @@
+from .dynamodb_memory import DynamoDbMemory, DynamoDbResource, DynamoDbVersionedResource, PaginatedList
+
+package_version = "3.3.0"
+
+_ = DynamoDbMemory
+_ = DynamoDbResource
+_ = DynamoDbVersionedResource
+_ = PaginatedList
