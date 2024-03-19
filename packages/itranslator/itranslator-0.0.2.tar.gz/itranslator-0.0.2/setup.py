225e9d6d4fbe737377a59db2bb41f1a1fc284244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 03:02:48 +0000

    Add package itranslator/0.0.2/itranslator-0.0.2.tar.gz

---
+++ packages/itranslator/itranslator-0.0.2.tar.gz/itranslator-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(
+    name='itranslator',
+    packages=find_packages(),
+    version='0.0.2',
+    license='MIT',
+    description='A Python package to use in translating texts based on api',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_context_type='text/markdown',
+    author='amirali irvany',
+    author_email='dev.amirali.irvany@gmail.com',
+    url='https://github.com/metect/itranslator',
+    keywords=['translate', 'translator', 'google', 'google-translate', 'free-translator'],
+    install_requires=['requests', 'user_agent'],
+)
