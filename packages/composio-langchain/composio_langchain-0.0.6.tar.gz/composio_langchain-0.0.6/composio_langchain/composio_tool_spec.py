3c6bbd92e1983f49066e112cdf0c94db677b8b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 16:06:07 +0000

    Add package composio-langchain/0.0.6/composio_langchain-0.0.6.tar.gz

---
+++ packages/composio-langchain/composio_langchain-0.0.6.tar.gz/composio_langchain-0.0.6/composio_langchain/composio_tool_spec.py
@@ -0,0 +1,24 @@
+from .pydantic_utils import json_schema_to_model
+from langchain_core.tools import StructuredTool 
+from composio.sdk import ComposioCore, App, Action
+from typing import List
+    
+def ComposioTool(client : ComposioCore, action_schema: dict[str, any]) ->  StructuredTool:
+    name = action_schema["name"]
+    description = action_schema["description"]
+    parameters = json_schema_to_model(action_schema["parameters"])
+    appName = action_schema["appName"]
+    print(parameters.schema())
+    return StructuredTool.from_function(
+        name=name,
+        description=description,
+        args_schema=parameters,
+        return_schema=True,
+        # TODO use execute action here
+        func = lambda **kwargs: client.execute_action(client.get_action_enum(name, appName), kwargs)
+    )
+
+def ComposioToolset(tool_names: List[App] = [], action_names: List[Action] = []) -> List[StructuredTool]:
+    client = ComposioCore()
+    actions = client.sdk.get_list_of_actions(tool_names, action_names)
+    return [ComposioTool(client, action) for action in actions["items"]]
