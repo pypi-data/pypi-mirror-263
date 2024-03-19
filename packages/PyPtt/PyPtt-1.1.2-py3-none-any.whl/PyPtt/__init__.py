2e99a25f5af4596e31c0c0f5d5e74c903e1ff801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 16:03:31 +0000

    Add package PyPtt/1.1.2/PyPtt-1.1.2-py3-none-any.whl

---
+++ packages/PyPtt/PyPtt-1.1.2-py3-none-any.whl/PyPtt/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '1.1.2'
+
+from SingleLog import LogLevel
+
+from .PTT import API
+from .data_type import *
+from .exceptions import *
+from .service import Service
+
+LOG_LEVEL = LogLevel
+
+_main_version = '1.1'
