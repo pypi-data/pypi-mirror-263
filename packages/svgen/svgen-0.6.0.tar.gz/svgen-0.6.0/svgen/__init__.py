85c8691adc92763c9f541697b68e81c72fc8324e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 15:03:11 +0000

    Add package svgen/0.6.0/svgen-0.6.0.tar.gz

---
+++ packages/svgen/svgen-0.6.0.tar.gz/svgen-0.6.0/svgen/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.4
+# hash=7a4e96b7d373059f6a53ea55c383909e
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A tool for working with scalable vector graphics."
+PKG_NAME = "svgen"
+VERSION = "0.6.0"
