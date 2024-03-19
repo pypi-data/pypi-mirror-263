0e436d841a3219223875c2710099ee2eb55b7d4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:03:37 +0000

    Add package t-office-365/0.1.1/t_office_365-0.1.1.tar.gz

---
+++ packages/t-office-365/t_office_365-0.1.1.tar.gz/t_office_365-0.1.1/t_office_365/decorators.py
@@ -0,0 +1,16 @@
+"""Custom decorators."""
+from retry import retry
+
+from t_office_365.exceptions import BadRequestError, UnexpectedError
+
+
+def retry_if_exception(func):
+    tries = 3
+    backoff = 2
+    delay = 1
+
+    @retry(exceptions=(BadRequestError, UnexpectedError), tries=tries, delay=delay, backoff=backoff)
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+
+    return wrapper
