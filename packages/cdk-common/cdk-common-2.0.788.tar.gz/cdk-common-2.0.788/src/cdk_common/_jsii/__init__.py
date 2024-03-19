ee6396b054e12cd94410ebf1098d63a501679c6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 14:03:15 +0000

    Add package cdk-common/2.0.788/cdk-common-2.0.788.tar.gz

---
+++ packages/cdk-common/cdk-common-2.0.788.tar.gz/cdk-common-2.0.788/src/cdk_common/_jsii/__init__.py
@@ -0,0 +1,24 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-common", "2.0.788", __name__[0:-6], "cdk-common@2.0.788.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
