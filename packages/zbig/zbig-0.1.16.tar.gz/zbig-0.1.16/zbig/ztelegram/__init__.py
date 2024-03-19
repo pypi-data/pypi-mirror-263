e2c11cf0bd1f415b6cd4c7d5863f397bb1b710ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 15:03:34 +0000

    Add package zbig/0.1.16/zbig-0.1.16.tar.gz

---
+++ packages/zbig/zbig-0.1.16.tar.gz/zbig-0.1.16/zbig/ztelegram/__init__.py
@@ -0,0 +1,9 @@
+"""
+wrap telegram bot
+need put .env file in the same folder and set TEL_TOKEN CHAT_ID
+"""
+from .send_photo import send_photo
+from .send_message import send_message
+
+__version__ = "0.1.0"
+__author__ = 'Big Zhu'
