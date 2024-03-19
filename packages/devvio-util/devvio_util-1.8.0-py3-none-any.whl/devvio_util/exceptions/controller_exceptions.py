cd58b5be962baada2e84d1deb9c339b246dd78aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 04:02:58 +0000

    Add package devvio-util/1.8.0/devvio_util-1.8.0-py3-none-any.whl

---
+++ packages/devvio-util/devvio_util-1.8.0-py3-none-any.whl/devvio_util/exceptions/controller_exceptions.py
@@ -0,0 +1,22 @@
+from .devv_error import DevvError
+
+
+class InvalidEndpointError(DevvError):
+    pass
+
+
+class InvalidValueError(DevvError):
+    pass
+
+
+class DuplicateRecipeError(InvalidValueError):
+    def __init__(self):
+        super().__init__("This recipe already exists.", "name", 1010)
+
+
+class TXFailedError(DevvError):
+    pass
+
+
+class MintFailedError(TXFailedError):
+    pass
