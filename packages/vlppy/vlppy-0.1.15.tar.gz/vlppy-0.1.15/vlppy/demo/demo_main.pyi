e2aebe256afc4e6c6902e0b333b6ad08f79a28e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 16:03:06 +0000

    Add package vlppy/0.1.15/vlppy-0.1.15.tar.gz

---
+++ packages/vlppy/vlppy-0.1.15.tar.gz/vlppy-0.1.15/vlppy/demo/demo_main.pyi
@@ -0,0 +1,12 @@
+from ..io import IO as IO
+from ..setting import Settings as Settings
+from .vlp_model import VLP_Model as VLP_Model
+from _typeshed import Incomplete
+
+io: Incomplete
+Args: Incomplete
+save_excel_path: str
+save_npz_path: str
+
+def main() -> None: ...
+def test_func() -> None: ...
