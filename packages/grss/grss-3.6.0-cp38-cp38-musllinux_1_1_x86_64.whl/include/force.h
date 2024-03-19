cfcafc01e9f63cb70a667146677b90d24374c1bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 15:03:03 +0000

    Add package grss/3.6.0/grss-3.6.0-cp38-cp38-musllinux_1_1_x86_64.whl

---
+++ packages/grss/grss-3.6.0-cp38-cp38-musllinux_1_1_x86_64.whl/include/force.h
@@ -0,0 +1,21 @@
+#ifndef FORCE_H
+#define FORCE_H
+
+#include "stm.h"
+
+std::vector<real> get_state_der(const real &t, const std::vector<real> &xInteg,
+                                propSimulation *propSim);
+void force_newton(const propSimulation *propSim, std::vector<real> &accInteg,
+                  std::vector<STMParameters> &allSTMs);
+void force_ppn_simple(const propSimulation *propSim,
+                      std::vector<real> &accInteg,
+                      std::vector<STMParameters> &allSTMs);
+void force_ppn_eih(const propSimulation *propSim, std::vector<real> &accInteg,
+                   std::vector<STMParameters> &allSTMs);
+void force_J2(const propSimulation *propSim, std::vector<real> &accInteg,
+              std::vector<STMParameters> &allSTMs);
+void force_nongrav(const propSimulation *propSim, std::vector<real> &accInteg,
+                   std::vector<STMParameters> &allSTMs);
+void force_thruster(const propSimulation *propSim, std::vector<real> &accInteg);
+
+#endif
