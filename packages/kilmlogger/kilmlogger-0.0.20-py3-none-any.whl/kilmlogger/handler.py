800e09cadc3ebb2a71ce4c13dfc90be278a874b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 15:03:14 +0000

    Add package kilmlogger/0.0.20/kilmlogger-0.0.20-py3-none-any.whl

---
+++ packages/kilmlogger/kilmlogger-0.0.20-py3-none-any.whl/kilmlogger/handler.py
@@ -0,0 +1,29 @@
+import sys
+
+from logging import Handler, LogRecord
+
+from kilmlogger.services.scribe.client import ScribeBaseClient, DefaultScribeClient
+from kilmlogger.constants import ACCEPTED_LEVELS
+
+
+class GRPCEventStreamingHandler(Handler):
+    def __init__(self, client: ScribeBaseClient = DefaultScribeClient()):
+        """
+        Initialize the handler.
+
+        If stream is not specified, sys.stderr is used.
+        """
+        Handler.__init__(self)
+        self._client = client
+
+    def emit(self, record: LogRecord) -> None:
+        # NOTE: Only sent log to scribe and DP if the log level is in ACCEPTED_LEVELS
+        if record.levelname not in ACCEPTED_LEVELS:
+            return
+
+        try:
+            self._client.log(
+                msg=record.msg,
+            )
+        except Exception:
+            sys.stdout.write("--- Logging error ---\n")
