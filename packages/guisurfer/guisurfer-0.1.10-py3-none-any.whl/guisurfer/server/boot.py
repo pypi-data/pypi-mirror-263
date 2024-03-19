03c181c0862a4aa56caf191006142dc5932aae0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 14:02:59 +0000

    Add package guisurfer/0.1.10/guisurfer-0.1.10-py3-none-any.whl

---
+++ packages/guisurfer/guisurfer-0.1.10-py3-none-any.whl/guisurfer/server/boot.py
@@ -0,0 +1,33 @@
+from guisurfer.agent.types import AgentType
+from guisurfer.server.models import EnvVarOptModel
+
+
+def ensure_common_models():
+    try:
+        AgentType(
+            name="SurfDino",
+            description="A GUI surfer which uses Grounding Dino and OCR",
+            image="us-central1-docker.pkg.dev/agentsea-dev/guisurfer/surfdino:latest",
+            supported_runtimes=["gke"],
+            env_opts=[
+                EnvVarOptModel(
+                    name="OPENAI_API_KEY",
+                    description="OpenAI API Key",
+                    required=True,
+                    secret=True,
+                ),
+            ],
+            owner_id="patrick.barker@kentauros.ai",
+            public=True,
+            icon="https://storage.googleapis.com/guisurfer-assets/surf_dino2.webp",
+        )
+    except Exception as e:
+        print(
+            f"failed to create common models: {e} \n this can likely be ignored if they exist"
+        )
+        return
+    print("common models created")
+
+
+def boot_seq():
+    ensure_common_models()
