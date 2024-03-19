2b1c41a1ef441e7493beac40983617ee70d8a7d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 16:04:39 +0000

    Add package SpiderReverser-Modules/1.0.3/SpiderReverser_Modules-1.0.3.tar.gz

---
+++ packages/SpiderReverser-Modules/SpiderReverser_Modules-1.0.3.tar.gz/SpiderReverser_Modules-1.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+# install_requires rely on requirements.txt
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+print(requirements)
+
+setup(
+    name='SpiderReverser_Modules',
+    version='1.0.3',
+    packages=find_packages(),
+    install_requires=requirements,
+    author="SpiderReverser",
+    author_email="spiderreverser@foxmail.com",
+    description="SpiderReverser person modules",
+    url="https://github.com/SpiderReverser/SpiderReverser_Modules",
+)
