6357740a390e59bceee8562b5e4d9983a68ea9ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 15:03:29 +0000

    Add package b3o-fedlearn/0.0.18/b3o-fedlearn-0.0.18.tar.gz

---
+++ packages/b3o-fedlearn/b3o-fedlearn-0.0.18.tar.gz/b3o-fedlearn-0.0.18/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="b3o-fedlearn",
+    version="0.0.18",
+    author="skswjdekdud",
+    author_email="skswjdekdud1103@gmail.com",
+    description="a project for federated learning using AWS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ACS-High-School",
+    project_urls={
+        "Bug Tracker": "https://github.com/ACS-High-School/ML/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    package_data={
+        'b3o_fedlearn': ['client_config.json'],
+    },
+    python_requires=">=3.8",
+)
