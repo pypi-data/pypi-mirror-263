6a4eacb55612f9ac971ed4b4f06c31abcbdba1af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 14:03:11 +0000

    Add package tonic-validate/3.4.0/tonic_validate-3.4.0.tar.gz

---
+++ packages/tonic-validate/tonic_validate-3.4.0.tar.gz/tonic_validate-3.4.0/tonic_validate/metrics/metric.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+from tonic_validate.classes.llm_response import LLMResponse
+from tonic_validate.services.openai_service import OpenAIService
+
+
+class Metric(ABC):
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        """Metric name for the UI"""
+        pass
+
+    @abstractmethod
+    def score(self, llm_response: LLMResponse, openai_service: OpenAIService) -> float:
+        """Calculate the score of the metric"""
+        pass
