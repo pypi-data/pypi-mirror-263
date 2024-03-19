582e03cbdc3431b9f266badae1aeaf97ddc58731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 03:03:24 +0000

    Add package PyBitgetApi/0.2.2/PyBitgetApi-0.2.2.tar.gz

---
+++ packages/PyBitgetApi/PyBitgetApi-0.2.2.tar.gz/PyBitgetApi-0.2.2/PybitgetApi/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.2.2'
+
+from loguru import logger
+from PyBitgetApi.client import Client
+from PyBitgetApi import utils
+from PyBitgetApi import exceptions
+from PyBitgetApi.enums import *
