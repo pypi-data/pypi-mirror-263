429303ef01570337e05e829d18153121d7d74d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 15:03:33 +0000

    Add package req-compile/1.0.0rc12/req-compile-1.0.0rc12.tar.gz

---
+++ packages/req-compile/req-compile-1.0.0rc12.tar.gz/req-compile-1.0.0rc12/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="req-compile",
+    version="1.0.0rc12",
+    author="Spencer Putt",
+    author_email="sputt@alumni.iu.edu",
+    description="Python requirements compiler",
+    long_description=open("CHANGELOG.rst").read() + "\n" + open("README.rst").read(),
+    url="https://github.com/sputt/req-compile",
+    install_requires=open("requirements.in").readlines(),
+    packages=find_packages(include=["req_compile*"]),
+    package_data={"": ["py.typed"]},
+    license="MIT License",
+    entry_points={
+        "console_scripts": [
+            "req-compile = req_compile.cmdline:compile_main",
+            "req-candidates = req_compile.candidates:candidates_main",
+        ],
+    },
+    python_requires=">=3.0, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Topic :: Software Development",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
