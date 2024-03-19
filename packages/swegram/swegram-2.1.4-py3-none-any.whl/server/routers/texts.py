13c777ab765c7743b2fe03f847b40ef334a37865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 14:02:57 +0000

    Add package swegram/2.1.4/swegram-2.1.4-py3-none-any.whl

---
+++ packages/swegram/swegram-2.1.4-py3-none-any.whl/server/routers/texts.py
@@ -0,0 +1,25 @@
+from typing import Any, Dict
+
+from fastapi import APIRouter, Body, Depends, Path
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+
+from server.lib.fetch_data import fetch_data
+from server.routers.database import get_db
+from server.models import Text
+
+
+router = APIRouter()
+
+
+@router.get("/")
+async def read_texts(db: Session = Depends(get_db)) -> JSONResponse:
+    return JSONResponse([item.as_dict() for item in db.query(Text).all()])
+
+
+@router.put("/{language}")
+async def update_texts(
+    language: str = Path(...), data: Dict[str, Any] = Body(...), db: Session = Depends(get_db)
+) -> JSONResponse:
+    texts = db.query(Text).filter(Text.language == language)
+    return JSONResponse(fetch_data(metadata=data, texts=texts))
