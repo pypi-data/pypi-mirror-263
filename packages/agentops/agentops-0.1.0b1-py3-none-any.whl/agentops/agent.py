2344d3f89d06278a246dfe1904469ba6c3f5b6fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 03:03:03 +0000

    Add package agentops/0.1.0b1/agentops-0.1.0b1-py3-none-any.whl

---
+++ packages/agentops/agentops-0.1.0b1-py3-none-any.whl/agentops/agent.py
@@ -0,0 +1,31 @@
+from uuid import uuid4
+from agentops import Client
+from inspect import isclass, isfunction
+
+
+def track_agent(name: str | None):
+    def decorator(obj):
+        obj._agent_ops_agent_name = name or obj.__name__
+
+        if isclass(obj):
+            original_init = obj.__init__
+
+            def new_init(self, *args, **kwargs):
+                self._agent_ops_agent_id = uuid4()
+                ao_client = Client()
+                ao_client.create_agent(self._agent_ops_agent_id, self._agent_ops_agent_name)
+                original_init(self, *args, **kwargs)
+
+            obj.__init__ = new_init
+
+        elif isfunction(obj):
+            obj._agent_ops_agent_id = uuid4()
+            ao_client = Client()
+            ao_client.create_agent(obj._agent_ops_agent_id, obj._agent_ops_agent_name)
+
+        else:
+            raise Exception("Invalid input, 'obj' must be a class or a function")
+
+        return obj
+
+    return decorator
