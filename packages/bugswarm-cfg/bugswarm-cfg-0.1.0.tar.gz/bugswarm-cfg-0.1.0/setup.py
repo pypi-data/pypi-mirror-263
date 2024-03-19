ae797c9eab4d1dd3552349690c2a7a0981c332e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 15:03:19 +0000

    Add package bugswarm-cfg/0.1.0/bugswarm-cfg-0.1.0.tar.gz

---
+++ packages/bugswarm-cfg/bugswarm-cfg-0.1.0.tar.gz/bugswarm-cfg-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bugswarm-cfg',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'bugswarm-cfg=bugswarm_cfg.main:main',
+        ],
+    },
+    install_requires=[
+        'beautifulsoup4',
+        'requests',
+    ],
+    package_data={
+        'bugswarm_cfg': ['*.sh'],
+    },
+    # Meta data
+    author="Apratim Shukla",
+    author_email="apratimshukla6@gmail.com",
+    description="A tool to run BugSwarm Docker containers with sandbox environments for CFG generation using various tools.",
+    license="MIT",
+    keywords="bugswarm cfg",
+    url="https://github.com/apratimshukla6/bugswarm-cfg",   # project home page
+)
