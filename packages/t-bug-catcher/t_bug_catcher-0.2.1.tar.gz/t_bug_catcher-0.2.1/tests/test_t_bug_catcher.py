014cab703ffe041083fff4ef9ae7a4e94fbf60e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 16:04:07 +0000

    Add package t-bug-catcher/0.2.1/t_bug_catcher-0.2.1.tar.gz

---
+++ packages/t-bug-catcher/t_bug_catcher-0.2.1.tar.gz/t_bug_catcher-0.2.1/tests/test_t_bug_catcher.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""Tests for `t_bug_catcher` package."""
+
+
+class TestTBugCatcher:
+    """Smoke tests of the package."""
+
+    def test_example(self):
+        """Smoke test."""
+        assert True
