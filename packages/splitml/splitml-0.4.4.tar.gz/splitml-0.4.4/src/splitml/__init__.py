94b27477de6b1fd308d79a5c9df24e681bc77ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 03:02:48 +0000

    Add package splitml/0.4.4/splitml-0.4.4.tar.gz

---
+++ packages/splitml/splitml-0.4.4.tar.gz/splitml-0.4.4/src/splitml/__init__.py
@@ -0,0 +1,9 @@
+from .splitml import (
+    HTMLSplitter,
+    split_html_str,
+    split_html_file,
+    chunk_html_str,
+    chunk_html_file,
+)
+from .groupers import NodesGrouper
+from .stats import stat_tokens, count_tokens
