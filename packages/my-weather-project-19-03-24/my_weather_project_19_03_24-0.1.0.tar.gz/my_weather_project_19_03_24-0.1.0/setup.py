caeeb9831b191d8fb7fd5ddfc223a8e3a6ba1da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-19 03:03:25 +0000

    Add package my-weather-project-19-03-24/0.1.0/my_weather_project_19_03_24-0.1.0.tar.gz

---
+++ packages/my-weather-project-19-03-24/my_weather_project_19_03_24-0.1.0.tar.gz/my_weather_project_19_03_24-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+        name='my_weather_project_19_03_24',
+        version='0.1.0',
+        author=' MoiseewConstantin2024',
+        author_email='const.moiseew2012@yandex.ru',
+        description='A short weather report on your city',
+        packages=find_packages(),
+        classifiers=[
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            ],
+        python_requires='>=3.6',
+        )
