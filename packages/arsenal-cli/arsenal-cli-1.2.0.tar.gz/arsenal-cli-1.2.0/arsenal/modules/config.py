9aad3fb11f0f522cde58d76370d739670447315e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 16:04:59 +0000

    Add package arsenal-cli/1.2.0/arsenal-cli-1.2.0.tar.gz

---
+++ packages/arsenal-cli/arsenal-cli-1.2.0.tar.gz/arsenal-cli-1.2.0/arsenal/modules/config.py
@@ -0,0 +1,25 @@
+import os
+from os.path import dirname, abspath, expanduser, join
+
+# Base paths
+DATAPATH = join(dirname(dirname(abspath(__file__))), 'data')
+BASEPATH = dirname(dirname(dirname(abspath(__file__))))
+HOMEPATH = expanduser("~")
+FORMATS = ["md", "rst", "yml"]
+EXCLUDE_LIST = ["README.md", "README.rst", "index.rst"]
+FUZZING_DIRS = ["/usr/local/share/wordlists/**/*.txt"]
+
+CHEATS_PATHS = [
+    join(DATAPATH, "cheats"),  # DEFAULT
+    # Additional paths below, add comma to line above
+    join(BASEPATH, "my_cheats"),
+    join(HOMEPATH, ".cheats")
+]
+
+messages_error_missing_arguments = 'Error missing arguments'
+
+# set lower delay to use ESC key (in ms)
+os.environ.setdefault('ESCDELAY', '25')
+os.environ['TERM'] = 'xterm-256color'
+
+savevarfile = join(HOMEPATH, ".arsenal.json")
