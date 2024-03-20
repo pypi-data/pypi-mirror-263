c0bac76ffa6f929fd5d965b839c02e1be6cb09ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 03:03:44 +0000

    Add package gxl-ai-utils/1.3.3/gxl_ai_utils-1.3.3.tar.gz

---
+++ packages/gxl-ai-utils/gxl_ai_utils-1.3.3.tar.gz/gxl_ai_utils-1.3.3/eggs/cats_and_dogs/prepare_data_for_en_cn/shard_from_pachong.py
@@ -0,0 +1,18 @@
+import os.path
+
+from gxl_ai_utils.utils import utils_file
+
+
+def do_handle():
+    """"""
+    data_dir = "/home/work_nfs14/xlgeng/asr_data_shard/pachong_data"
+    data_names = ['shangye_caijing_3', 'shangye_caijing_2', 'shangye_caijing_4','shenghuo_2']
+    all_list = []
+    for data_name in data_names:
+        temp_path = os.path.join(data_dir, data_name,'shards_list.txt')
+        temp_list = utils_file.load_list_file_clean(temp_path)
+        all_list.extend(temp_list)
+    utils_file.write_list_to_file(all_list, os.path.join(data_dir, 'all_shards_list_2.txt'))
+
+if __name__ == '__main__':
+    do_handle()
