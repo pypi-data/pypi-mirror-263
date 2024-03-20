9d43a430a6a720220dae2d6350855c1f79cd5055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 06:03:33 +0000

    Add package MacroQueue/0.1.9/MacroQueue-0.1.9.tar.gz

---
+++ packages/MacroQueue/MacroQueue-0.1.9.tar.gz/MacroQueue-0.1.9/MacroQueue/__init__.py
@@ -0,0 +1,16 @@
+
+from MacroQueue.Dialogs import MyMacroDialog as MacroDialog
+from MacroQueue.Dialogs import MyMacroSettingsDialog as MacroSettingsDialog
+from MacroQueue.Dialogs import MyStartMacroDialog as StartMacroDialog
+from MacroQueue.Dialogs import MyChooseSoftwareDialog as ChooseSoftwareDialog
+from MacroQueue.GUIDesign import MyFrame
+from MacroQueue.GUIDesign import MacroDialog
+from MacroQueue.GUIDesign import MacroSettingsDialog
+from MacroQueue.GUIDesign import StartMacroDialog
+from MacroQueue.GUIDesign import ChooseSoftware
+from MacroQueue.Functions.CreaTec import *
+from MacroQueue.Functions.RHK import *
+from MacroQueue.Functions.SXMRemote import *
+from MacroQueue.Functions.SXM import *
+from MacroQueue.Functions.BField import *
+from MacroQueue.MacroQueue import MacroQueue
