7dc9834374f56a5d21aeb8570033b6c40522be36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 06:03:49 +0000

    Add package cellmap-models/0.0.3/cellmap_models-0.0.3-py3-none-any.whl

---
+++ packages/cellmap-models/cellmap_models-0.0.3-py3-none-any.whl/cellmap_models/pytorch/cellpose/__init__.py
@@ -0,0 +1,19 @@
+from .add_model import add_model
+from .load_model import load_model
+from .get_model import get_model
+from .download_checkpoint import download_checkpoint
+
+models_dict = {
+    "jrc_mus-epididymis-1_nuc_cp": "https://github.com/janelia-cellmap/cellmap-models/releases/download/2024.03.08/jrc_mus-epididymis-1_nuc_cp",
+    "jrc_mus-epididymis-2_nuc_cp": "https://github.com/janelia-cellmap/cellmap-models/releases/download/2024.03.08/jrc_mus-epididymis-2_nuc_cp",
+    "jrc_mus-heart-1_ecs_cp": "https://github.com/janelia-cellmap/cellmap-models/releases/download/2024.03.08/jrc_mus-heart-1_ecs_cp",
+    "jrc_mus-heart-1_nuc_cp": "https://github.com/janelia-cellmap/cellmap-models/releases/download/2024.03.08/jrc_mus-heart-1_nuc_cp",
+    "jrc_mus-hippocampus-1_nuc_cp": "https://github.com/janelia-cellmap/cellmap-models/releases/download/2024.03.08/jrc_mus-hippocampus-1_nuc_cp",
+    "jrc_mus-kidney-3_nuc_cp": "https://github.com/janelia-cellmap/cellmap-models/releases/download/2024.03.08/jrc_mus-kidney-3_nuc_cp",
+    "jrc_mus-liver-3_nuc_cp": "https://github.com/janelia-cellmap/cellmap-models/releases/download/2024.03.08/jrc_mus-liver-3_nuc_cp",
+    "jrc_mus-pancreas-4_nuc_cp": "https://github.com/janelia-cellmap/cellmap-models/releases/download/2024.03.08/jrc_mus-pancreas-4_nuc_cp",
+    "jrc_mus-skin-1_nuc_cp": "https://github.com/janelia-cellmap/cellmap-models/releases/download/2024.03.08/jrc_mus-skin-1_nuc_cp",
+    "jrc_mus-thymus-1_nuc_cp": "https://github.com/janelia-cellmap/cellmap-models/releases/download/2024.03.08/jrc_mus-thymus-1_nuc_cp",
+}
+
+models_list = list(models_dict.keys())
