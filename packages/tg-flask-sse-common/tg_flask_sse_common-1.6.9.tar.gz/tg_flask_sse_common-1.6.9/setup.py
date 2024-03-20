51d30c44964a94fad413299ed916ac5c83cc8442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 15:03:37 +0000

    Add package tg-flask-sse-common/1.6.9/tg_flask_sse_common-1.6.9.tar.gz

---
+++ packages/tg-flask-sse-common/tg_flask_sse_common-1.6.9.tar.gz/tg_flask_sse_common-1.6.9/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    setup.py
+    ~~~~~~~~~~~~~~~~~~~~~~~
+
+    setup.py
+
+    :author: Tangshimin
+    :copyright: (c) 2024, Tungee
+    :date created: 2024-01-31
+
+"""
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='tg_flask_sse_common',
+    version='1.6.9',
+    author='Tangshimin',
+    description='tg_flask_sse_common',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=[
+        'tg_flask_sse_common'
+    ],
+    requires=['redis'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
