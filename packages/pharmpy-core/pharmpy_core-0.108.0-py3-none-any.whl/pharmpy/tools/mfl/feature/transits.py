a8bb4a66f791c4c8d8ebd7c3d3902d78bf862008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 03:03:36 +0000

    Add package pharmpy-core/0.108.0/pharmpy_core-0.108.0-py3-none-any.whl

---
+++ packages/pharmpy-core/pharmpy_core-0.108.0-py3-none-any.whl/pharmpy/tools/mfl/feature/transits.py
@@ -0,0 +1,30 @@
+from functools import partial
+from itertools import product
+from typing import Iterable
+
+from pharmpy.model import Model
+from pharmpy.modeling import set_transit_compartments
+
+from ..statement.feature.symbols import Wildcard
+from ..statement.feature.transits import Transits
+from ..statement.statement import Statement
+from .feature import Feature
+
+
+def features(model: Model, statements: Iterable[Statement]) -> Iterable[Feature]:
+    for statement in statements:
+        if isinstance(statement, Transits):
+            depots = (
+                statement._wildcard if isinstance(statement.depot, Wildcard) else statement.depot
+            )
+            for count, depot in product(statement.counts, depots):
+                if depot.name == 'DEPOT':
+                    yield ('TRANSITS', count, depot.name), partial(
+                        set_transit_compartments, n=count
+                    )
+                elif depot.name == 'NODEPOT':
+                    yield ('TRANSITS', count, depot.name), partial(
+                        set_transit_compartments, n=count + 1, keep_depot=False
+                    )
+                else:
+                    raise ValueError(f'Transit depot {depot} not supported')
