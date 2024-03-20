f09d65376b70487e20a40856b3f36693e8909356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 04:03:18 +0000

    Add package sapphire-renderer/0.0.11/sapphire_renderer-0.0.11-py3-none-any.whl

---
+++ packages/sapphire-renderer/sapphire_renderer-0.0.11-py3-none-any.whl/sapphirerenderer/utility_objects/particle_manager.py
@@ -0,0 +1,51 @@
+import numpy as np
+from ..main import SapphireRenderer
+
+
+class ParticleManager:
+    def __init__(self, parent_object, renderer: SapphireRenderer, hide_parent=True):
+        self.particles = []
+        self.parent_object = parent_object
+        self.renderer = renderer
+
+        if hide_parent:
+            parent_object.hide()
+
+    def add_particle(self, position=np.array([0.0, 0.0, 0.0]), color=(0, 0, 0)):
+        """
+        Add a particle to the particle manager
+        :param position: the position of the particle
+        :param color: the color of the particle
+        :return: the particle object
+        """
+        particle = self.renderer.add_object(str(self.parent_object))
+        particle.move_absolute(position)
+        particle.set_color(color)
+
+        self.particles.append(particle)
+
+        return particle
+
+    def remove_particle(self, particle):
+        """
+        Remove a particle from the particle manager
+        :param particle: the particle to remove
+        """
+        self.particles.remove(particle)
+        self.renderer.remove_object(particle)
+
+    def clear_particles(self):
+        """
+        Remove all particles from the particle manager
+        """
+        for particle in self.particles:
+            self.renderer.remove_object(particle)
+        self.particles = []
+
+    def move_particles(self, movement):
+        """
+        Move all particles by a certain amount
+        :param movement: the amount to move the particles by
+        """
+        for particle in self.particles:
+            particle.move_relative(movement)
