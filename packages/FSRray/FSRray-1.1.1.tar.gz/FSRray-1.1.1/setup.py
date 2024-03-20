1eb1b9065278ce6bbf146f02778aa6d0e112114c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 05:03:46 +0000

    Add package FSRray/1.1.1/FSRray-1.1.1.tar.gz

---
+++ packages/FSRray/FSRray-1.1.1.tar.gz/FSRray-1.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+setup(
+    packages=find_packages(include=['FSRray']),
+    version='1.1.1',
+    name='FSRray',
+    url="https://github.com/Aightech/pyFSRray",
+    description='Python library to communicate with the FSR array',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Alexis Devillard',
+    license='MIT',
+    install_requires=['pyserial'],
+    setup_requires=['pytest-runner', 'pyserial'],
+    tests_require=['pytest==4.4.1', 'pyserial'],
+    test_suite='tests',
+)
