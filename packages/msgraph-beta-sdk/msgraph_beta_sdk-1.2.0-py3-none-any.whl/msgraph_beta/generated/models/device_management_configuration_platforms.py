0b1ffca78c568050ac346231cff1575f881d1a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 16:04:57 +0000

    Add package msgraph-beta-sdk/1.2.0/msgraph_beta_sdk-1.2.0-py3-none-any.whl

---
+++ packages/msgraph-beta-sdk/msgraph_beta_sdk-1.2.0-py3-none-any.whl/msgraph_beta/generated/models/device_management_configuration_platforms.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+class DeviceManagementConfigurationPlatforms(str, Enum):
+    # Default. No platform type specified.
+    None_ = "none",
+    # Settings for Android platform.
+    Android = "android",
+    # Settings for iOS platform.
+    IOS = "iOS",
+    # Settings for MacOS platform.
+    MacOS = "macOS",
+    # Windows 10 X.
+    Windows10X = "windows10X",
+    # Settings for Windows 10 platform.
+    Windows10 = "windows10",
+    # Settings for Linux platform.
+    Linux = "linux",
+    # Evolvable enumeration sentinel value. Do not use.
+    UnknownFutureValue = "unknownFutureValue",
+
