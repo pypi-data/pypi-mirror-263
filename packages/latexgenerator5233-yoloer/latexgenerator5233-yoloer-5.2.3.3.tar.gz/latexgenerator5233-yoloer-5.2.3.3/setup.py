afc64c8aa9482cc3cacb458af4df8f1345a939f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 05:04:11 +0000

    Add package latexgenerator5233-yoloer/5.2.3.3/latexgenerator5233-yoloer-5.2.3.3.tar.gz

---
+++ packages/latexgenerator5233-yoloer/latexgenerator5233-yoloer-5.2.3.3.tar.gz/latexgenerator5233-yoloer-5.2.3.3/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup, find_packages
+
+# Read the contents of your README file
+with open(os.path.join(os.getcwd(), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+setup(
+    name='latexgenerator5233-yoloer',
+    version='5.2.3.3',
+    packages=find_packages(),
+    py_modules=['latex_generator'],
+    install_requires=[],
+    python_requires='>=3.8',
+    author='Wangquanyu',
+    author_email='w1450111904@qq.com',
+    description='A simple LaTeX table and image generator',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='LaTeX, table, image, generator',
+    url='https://github.com/gdnjr5233/Advanced-Python_2024',
+)
