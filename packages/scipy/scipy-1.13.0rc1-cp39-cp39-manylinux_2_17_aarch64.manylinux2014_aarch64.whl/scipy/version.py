2418ef141a273458b6052932f12c971a5712a5c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 05:05:11 +0000

    Add package scipy/1.13.0rc1/scipy-1.13.0rc1-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/scipy/scipy-1.13.0rc1-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/scipy/version.py
@@ -0,0 +1,12 @@
+# THIS FILE IS GENERATED DURING THE SCIPY BUILD
+# See tools/version_utils.py for details
+
+short_version = '1.13.0rc1'
+version = '1.13.0rc1'
+full_version = '1.13.0rc1'
+git_revision = '4db2cc4'
+commit_count = '0'
+release = True
+
+if not release:
+    version = full_version
