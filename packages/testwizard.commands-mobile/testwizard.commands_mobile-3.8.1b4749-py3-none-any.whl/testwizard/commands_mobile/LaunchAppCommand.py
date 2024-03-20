e27fb5f032d7bcb8c24ce81510e784b152a7ff1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 04:04:19 +0000

    Add package testwizard.commands-mobile/3.8.1b4749/testwizard.commands_mobile-3.8.1b4749-py3-none-any.whl

---
+++ packages/testwizard.commands-mobile/testwizard.commands_mobile-3.8.1b4749-py3-none-any.whl/testwizard/commands_mobile/LaunchAppCommand.py
@@ -0,0 +1,21 @@
+
+import sys
+import json
+
+from testwizard.commands_core import CommandBase
+from testwizard.commands_core.SimpleResult import SimpleResult
+
+
+class LaunchAppCommand(CommandBase):
+    def __init__(self, testObject):
+        CommandBase.__init__(self, testObject, "Mobile.LaunchApp")
+
+    def execute(self, bundleId):
+        if bundleId is None:
+            requestObj = []
+        else:
+            requestObj = [bundleId]
+
+        result = self.executeCommand(requestObj)
+
+        return SimpleResult(result, "LaunchApp was successful", "LaunchApp failed")
