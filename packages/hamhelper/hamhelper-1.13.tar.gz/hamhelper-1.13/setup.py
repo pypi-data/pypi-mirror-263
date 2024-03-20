4dbbdbfdbb95679d5abf6e556f0c99bb61168517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 14:04:26 +0000

    Add package hamhelper/1.13/hamhelper-1.13.tar.gz

---
+++ packages/hamhelper/hamhelper-1.13.tar.gz/hamhelper-1.13/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+
+from hamhelper import __version__
+
+# Extra requirements
+extra_maps = [
+    'Basemap',
+]
+
+extra_test = [
+    *extra_maps,
+    'pytest>=4',
+    'coverage',
+]
+
+extra_ci = [
+    *extra_test,
+    'python-coveralls',
+]
+
+extra_dev = [
+    *extra_maps,
+    *extra_test,
+]
+
+setup(
+    name='hamhelper',
+    version=__version__,
+    description='Some helpful tools I use in research and fun. Primarily\
+                 targeted to data visualization and pretty colours.',
+
+    url='https://github.com/Ham0osh/hamHelper',
+    author='Hamish Johnson',
+    author_email='hamish_johnson@sfu.ca',
+
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+
+    packages=find_packages(exclude=['tests', 'tests.*']),
+
+    install_requires=[
+        'numpy',
+        'matplotlib',
+    ],
+
+    extras_require={
+        'map': extra_maps,
+        'dev': extra_dev,
+    },
+
+    classifiers=[
+        'Framework :: Flake8',
+        'Intended Audience :: Science/Research',
+        'License :: Free for non-commercial use',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Scientific/Engineering :: Visualization',
+    ],
+)
---
+++ packages/hamhelper/hamhelper-1.13.tar.gz/hamhelper-1.13/tests/test_init.py
@@ -0,0 +1,10 @@
+"""Test cases for main init"""
+from hamhelper import hello_world
+
+
+def test_hello_world(capsys):
+    """Test hello world function"""
+    hello_world()
+    captured = capsys.readouterr()
+    assert captured.out == 'Found hamHelper!\n'
+    assert captured.err == ''
