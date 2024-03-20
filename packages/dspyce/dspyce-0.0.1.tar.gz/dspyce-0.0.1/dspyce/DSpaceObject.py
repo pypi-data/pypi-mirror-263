0495c135a901ef8e4d840ee84c55b8c3355230dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 16:04:44 +0000

    Add package dspyce/0.0.1/dspyce-0.0.1.tar.gz

---
+++ packages/dspyce/dspyce-0.0.1.tar.gz/dspyce-0.0.1/dspyce/DSpaceObject.py
@@ -0,0 +1,200 @@
+from .metadata import MetaDataList, MetaData
+
+
+def parse_metadata_label(label: str) -> tuple[str, str, str | None]:
+    """
+    Parses a dspace metadata label string from the format <schema>.<element>.<qualifier>
+
+    >>> parse_metadata_label('dc.type')
+    ('dc', 'type', None)
+    >>> parse_metadata_label('dspace.entity.type')
+    ('dspace, 'entity', 'type')
+
+    :param label: The metadata label to parse.
+    :return: A tuple containing prefix, element and qualifier of a label.
+    :raises ValueError: Raises a value-error if the label can not be parsed.
+    :raises AttributeError: Raises a attribute-error if the label isn't a string.
+    """
+    if not isinstance(label, str):
+        raise AttributeError('The label must be from type string.')
+    label = label.split('.')
+    if len(label) == 2:
+        return label[0], label[1], None
+    if len(label) == 3:
+        return label[0], label[1], label[2]
+
+    raise ValueError(f'Could not parse dspace-metadata label "{label}"')
+
+
+class DSpaceObject:
+    """
+    The class DSpaceObject represents an Object in a DSpace repository, such as Items, Collections, Communities.
+    """
+
+    uuid: str
+    """The uuid of the DSpaceObject"""
+    name: str
+    """The name of the DSpaceObject, if existing"""
+    handle: str
+    """The handle of the Object"""
+    metadata: MetaDataList
+    """The metadata provided for the object."""
+    statistic_reports: dict
+    """A dictionary of statistic report objects."""
+
+    def __init__(self, uuid: str = '', handle: str = '', name: str = ''):
+        """
+        Creates a new object of the class DSpaceObject
+
+        :param uuid: The uuid of the DSpaceObject. Default is ''
+        :param handle: The handle of the DSpaceObject. Default is ''
+        :param name: The name of the DSpaceObject, if existing.
+        """
+        self.uuid = uuid
+        self.handle = handle
+        self.name = name
+        self.metadata = MetaDataList([])
+        self.statistic_reports = {}
+
+    def add_dc_value(self, element: str, qualifier: str | None, value: str, language: str = None):
+        """
+        Creates a new dc- metadata field with the given value.
+
+        :param element: Type of the metadata-field. For example 'title'.
+        :param qualifier: The qualifier of the field.
+        :param value: The value of the metadata-field.
+        :param language: The language of the metadata field. Default: None.
+        """
+        self.add_metadata('dc', element, qualifier, value, language)
+
+    def add_metadata(self, prefix: str, element: str, qualifier: str | None, value: str, language: str = None):
+        """
+        Creates a new metadata field with the given value. The schema is specified through the prefix parameter.
+
+        :param element: Type of the metadata-field. For example 'title'.
+        :param value: The value of the metadata-field.
+        :param prefix: The prefix of the schema, which should be used.
+        :param qualifier: The qualifier of the field.
+        :param language: The language of the metadata field.
+        """
+        self.metadata.append(MetaData(prefix, element, qualifier, value, language))
+
+    def remove_metadata(self, prefix: str, element: str, qualifier: str | None, value: str = None):
+        """
+        Remove a specific metadata field from the DSpaceObject. Can either be all values for a field or ony a specific
+        value based on the *value* parameter.
+
+        :param prefix: The prefix of the schema, which should be used.
+        :param element: Type of the metadata-field. For example 'title'.
+        :param qualifier: The qualifier of the field. Or None, if not existing.
+        :param value: The value of the metadata field to delete. Can be used, if only one value in a list of values
+            should be deleted. If None, all values from the given tag will be deleted.
+        """
+        qualifier = '' if qualifier is None else qualifier
+        self.metadata = MetaDataList(filter(
+            lambda m: not (m.schema == prefix and
+                           m.element == element and
+                           m.qualifier == qualifier and
+                           (value is None or m.value == value)),
+            self.metadata))
+
+    def replace_metadata(self, prefix: str, element: str, qualifier: str | None, value: str, language: str = None):
+        """
+        Replaces a specific metadata field from the DSpaceObject. Replaces all values of a given tag. If the tag,
+        doesn't exist, new metadata field will be created.
+
+        :param prefix: The prefix of the schema, which should be used.
+        :param element: Type of the metadata-field. For example 'title'.
+        :param qualifier: The qualifier of the field. Or None, if not existing.
+        :param value: The value of the metadata field to delete. Can be used, if only one value in a list of values
+            should be deleted. If None, all values from the given tag will be deleted.
+        :param language: The language of the metadata value to add.
+        """
+        self.remove_metadata(prefix, element, qualifier)
+        self.add_metadata(prefix, element, qualifier, value, language)
+
+    def get_dspace_object_type(self) -> str:
+        """
+        This Function serves mainly to be overwritten by subclasses to get the type of DSpaceObject.
+        """
+        pass
+
+    def get_identifier(self) -> str | None:
+        """
+        Returns the identifier of this object. Preferably this will be the uuid, but if this does not exist, it uses
+        the handle.
+        :return: The identifier as a string.
+        """
+        if self.uuid != '':
+            return self.uuid
+        if self.handle != '':
+            return self.handle
+        return None
+
+    def __eq__(self, other):
+        if self.uuid == '' and other.uuid == '':
+            raise ValueError('Can not compare objects without a uuid')
+        return self.uuid == other.uuid
+
+    def __str__(self):
+        self.metadata.sort()
+        data = '\n'.join(f'\t{str(m)}' for m in self.metadata)
+        return f'DSpace object with the uuid {self.uuid}:\n{data}'
+
+    def to_dict(self) -> dict:
+        """
+            Converts the current item object to a dictionary object containing all available metadata.
+        """
+        dict_obj = {'uuid': self.uuid, 'handle': self.handle, 'name': self.name}
+        for m in self.metadata:
+            m: MetaData
+            tag = m.get_tag()
+            value = m.value
+            if m.language is not None:
+                value = {m.language: value}
+                if tag in dict_obj.keys():
+                    value.update(dict_obj[tag] if isinstance(dict_obj[tag], dict) else {'': dict_obj[tag]})
+                    # TODO: Correct language implementation in dictionary representations of DSpaceObject.
+            dict_obj[tag] = value
+        return dict_obj
+
+    def get_metadata_values(self, tag: str) -> list | None:
+        """
+        Retrieves the metadata values of a specific tag as a list.
+
+        :param tag: The metadata tag: prefix.element.qualifier
+        :return: The values as a list or None, if the tag doesn't exist.
+        """
+        values = self.metadata.get(tag)
+        if values is None:
+            return None
+        return [v.value for v in (values if isinstance(values, list) else [values])]
+
+    def add_statistic_report(self, report: dict | list[dict] | None):
+        """
+        Adds a new report or list of reports as a dict object to the DSpaceObject
+
+        :param report: The report(s) to add.
+        """
+        if report is None:
+            return
+        report = [report] if isinstance(report, dict) else report
+        for r in report:
+            for k in r.keys():
+                if k not in self.statistic_reports.keys():
+                    self.statistic_reports[k] = r[k]
+                else:
+                    if isinstance(r[k], dict):
+                        self.statistic_reports[k] = (self.statistic_reports[k] +
+                                                     [r[k]]) if isinstance(self.statistic_reports[k],
+                                                                           list) else [self.statistic_reports[k], r[k]]
+                    else:
+                        self.statistic_reports[k] = r[k]
+
+    def has_statistics(self) -> bool:
+        """
+        Checks if statistic reports are available for this object.
+
+        :return: True if there is at least one report.
+        """
+        return len(self.statistic_reports.keys()) > 0
---
+++ packages/dspyce/dspyce-0.0.1.tar.gz/dspyce-0.0.1/dspyce/_testing/dspyceTest.py
@@ -0,0 +1,134 @@
+import unittest
+import dspyce as ds
+from dspyce.DSpaceObject import DSpaceObject
+from dspyce.DSpaceObject import parse_metadata_label
+from dspyce.Item import Item
+from dspyce.Collection import Collection
+from dspyce.Community import Community
+from dspyce.metadata import MetaDataList
+
+
+class DSpaceObjectTest(unittest.TestCase):
+    """
+    Tests for dspyce.DSpaceObject objects.
+    """
+    obj: DSpaceObject = DSpaceObject('123445-123jljl1-234kjj', 'doc/12345', 'test-name')
+
+    def test_parse_metadata_label(self):
+        """
+        Tests function parse_metadata_label from DSpaceObject.py
+        """
+        self.assertEqual(parse_metadata_label('dc.identifier.uri'), ('dc', 'identifier', 'uri'))
+        self.assertEqual(parse_metadata_label('dc.type'), ('dc', 'type', None))
+        self.assertRaises(ValueError, parse_metadata_label, 'dc-title')
+        self.assertRaises(ValueError, parse_metadata_label, 'dc.identifier.uri.url')
+        self.assertRaises(AttributeError, parse_metadata_label, 134)
+
+    def test_init_object(self):
+        """
+        Tests object initialization from DSpaceObject class.
+        """
+        self.assertEqual(self.obj, DSpaceObject('123445-123jljl1-234kjj'))
+        self.assertEqual(self.obj.uuid, '123445-123jljl1-234kjj')
+        self.assertEqual(self.obj.handle, 'doc/12345')
+        self.assertEqual(self.obj.name, 'test-name')
+        self.assertNotEqual(self.obj, DSpaceObject('dddds2-123jljl1-234kjj'))
+        self.assertIsInstance(self.obj, DSpaceObject)
+
+    def test_add_metadata(self):
+        """
+        Tests add_metadata method from DSpaceObject
+        """
+        self.obj.add_dc_value('title', None, 'hello', 'en')
+        self.assertEqual(list(filter(lambda x: x.schema == 'dc' and x.element == 'title', self.obj.metadata))[0].value,
+                         'hello')
+        self.assertEqual(list(filter(lambda x: x.schema == 'dc' and x.element == 'title',
+                                     self.obj.metadata))[0].language, 'en')
+        self.obj.add_metadata('relation', 'isAuthorOfPublication', 'latestForDiscovery',
+                              'kidll88-uuid999-duwkke1222', 'en')
+        self.assertEqual(self.obj.get_metadata_values('relation.isAuthorOfPublication.latestForDiscovery')[0],
+                         'kidll88-uuid999-duwkke1222')
+        self.obj.metadata = MetaDataList([])
+
+    def test_remove_metadata(self):
+        self.obj.add_dc_value('title', None, 'hello', 'en')
+        self.assertEqual(['hello'], self.obj.get_metadata_values('dc.title'))
+        self.obj.remove_metadata('dc', 'title', None)
+        self.assertIsNone(self.obj.get_metadata_values('dc.title'))
+        self.obj.add_dc_value('title', None, 'hello', 'en')
+        self.obj.add_dc_value('title', None, 'hallo', 'de')
+        self.assertEqual(['hello', 'hallo'], self.obj.get_metadata_values('dc.title'))
+        self.obj.remove_metadata('dc', 'title', None, 'hallo')
+        self.assertEqual(['hello'], self.obj.get_metadata_values('dc.title'))
+        self.obj.remove_metadata('dc', 'title', None)
+
+    def test_replace_metadata(self):
+        self.obj.add_dc_value('title', None, 'hello', 'en')
+        self.assertEqual(['hello'], self.obj.get_metadata_values('dc.title'))
+        self.obj.replace_metadata('dc', 'title', None, 'salut')
+        self.assertEqual(['salut'], self.obj.get_metadata_values('dc.title'))
+        self.obj.remove_metadata('dc', 'title', None)
+
+    def test_object_type(self):
+        """
+        Test object_type method from DSpaceObject, Item and Community classes.
+        """
+        self.assertIsNone(self.obj.get_dspace_object_type())
+        self.assertEqual(Item().get_dspace_object_type(), 'Item')
+        self.assertEqual(Collection().get_dspace_object_type(), 'Collection')
+        self.assertEqual(Community().get_dspace_object_type(), 'Community')
+
+    def test_get_identifier(self):
+        """
+        Tests get_identifier method from DSpaceObject
+        """
+        self.assertEqual(self.obj.get_identifier(), '123445-123jljl1-234kjj')
+        self.assertEqual(Item(handle='doc/12').get_identifier(), 'doc/12')
+        self.assertIsNone(Collection().get_identifier())
+
+    def test_equality(self):
+        """
+        Tests __eq__ method from DSpaceObject
+        """
+        self.assertTrue(self.obj == DSpaceObject('123445-123jljl1-234kjj'))
+        self.assertFalse(self.obj == DSpaceObject('12dsf3445-234kjj'))
+
+    def test_to_dict(self):
+        """
+        Tests to_dict method from DSpaceObject.
+        """
+        self.assertEqual({'uuid': '123445-123jljl1-234kjj', 'handle': 'doc/12345', 'name': 'test-name'},
+                         self.obj.to_dict())
+
+    def test_from_dict(self):
+        """
+        Tests from_dict function from dspyce.__init__()
+        """
+        dict_obj = {'uuid': '123445-123jljl1-234kjj', 'handle': 'doc/12345', 'name': 'test-name',
+                    'dc.title': [{'value': 'test-title', 'language': 'en'}, {'value': 'Test-Titel', 'language': 'de'}]}
+        obj = ds.from_dict(dict_obj)
+        self.assertEqual('123445-123jljl1-234kjj', obj.uuid)
+        self.assertEqual('doc/12345', obj.handle)
+        self.assertEqual('test-name', obj.name)
+        self.assertTrue(obj.get_metadata_values('dc.title'), ['test-title', 'Test-Titel'])
+        self.assertEqual('en', obj.metadata[0].language)
+        self.assertEqual('de', obj.metadata[1].language)
+        self.assertRaises(TypeError, ds.from_dict, dict_obj, 'test')
+
+    def test_statistics(self):
+        """
+        Test add_statistic_report and has_statistics methods from DSpaceObject
+        """
+        self.assertFalse(self.obj.has_statistics())
+        self.obj.add_statistic_report({'TotalViews': 0})
+        self.obj.add_statistic_report(None)
+        self.obj.add_statistic_report({"TotalDownloads": {'uuid': 'lkjlkjl', "views": 12}})
+        self.obj.add_statistic_report({"TotalDownloads": {'uuid': '12345', "views": 12}})
+        self.obj.add_statistic_report({"TotalDownloads": {'uuid': 'sd3x33', "views": 15}})
+        self.assertEqual(2, len(self.obj.statistic_reports))
+        self.assertTrue(isinstance(self.obj.statistic_reports['TotalDownloads'], list))
+        self.assertTrue(self.obj.has_statistics())
+
+
+if __name__ == '__main__':
+    unittest.main()
