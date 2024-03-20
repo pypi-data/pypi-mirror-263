0675b63f068c560f52f329ccc1a45263bd941ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 05:06:25 +0000

    Add package codemodder/0.86.0/codemodder-0.86.0-py3-none-any.whl

---
+++ packages/codemodder/codemodder-0.86.0-py3-none-any.whl/codemodder/project_analysis/file_parsers/requirements_txt_file_parser.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+import chardet
+
+from codemodder.logging import logger
+from codemodder.project_analysis.file_parsers.package_store import (
+    FileType,
+    PackageStore,
+)
+
+from .base_parser import BaseParser
+
+
+class RequirementsTxtParser(BaseParser):
+    @property
+    def file_type(self):
+        return FileType.REQ_TXT
+
+    def _parse_file(self, file: Path) -> PackageStore | None:
+        with open(file, "rb") as f:
+            whole_file = f.read()
+
+        enc = chardet.detect(whole_file)
+        if enc["confidence"] > 0.9:
+            encoding = enc.get("encoding")
+            decoded = whole_file.decode(encoding.lower()) if encoding else ""
+            lines = decoded.splitlines() if decoded else []
+        else:
+            logger.debug("Unknown encoding for file: %s", file)
+            return None
+
+        dependencies = set(
+            line.split("#")[0].strip() for line in lines if not line.startswith("#")
+        )
+
+        return PackageStore(
+            type=self.file_type,
+            file=file,
+            dependencies=dependencies,
+            # requirements.txt files do not declare py versions explicitly
+            # though we could create a heuristic by analyzing each dependency
+            # and extracting py versions from them.
+            py_versions=[],
+        )
