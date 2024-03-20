dee27539063856db34faf832f3c893022d8872a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 03:04:21 +0000

    Add package shscan/0.1.9/shscan-0.1.9.tar.gz

---
+++ packages/shscan/shscan-0.1.9.tar.gz/shscan-0.1.9/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shscan',  
+    version='0.1.9',  
+    packages=find_packages(),  
+    install_requires=['requests'],  
+    entry_points={
+        'console_scripts': [
+            'shscan = shscan.main:main'
+        ]
+    },
+    author='boyinf',   
+    description='A package for checking the security headers of a URL.',  
+     long_description="The shscan tool was developed with the purpose of assisting in checking the security headers enabled on specific or internal websites. Created to simplify this process, the tool is notable for its simplicity and efficiency, representing the result in an intuitive way.\n\nFor more information, visit [GitHub](https://github.com/boyinf/shscan).\n\n![Image](https://raw.githubusercontent.com/Boyinf/shscan/main/Owasp-juice.png)",
+    url='https://github.com/boyinf/shscan',  
+    license='MIT',  
+    classifiers=[  
+        'Programming Language :: Python :: 3',  
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',  
+    ],
+)
