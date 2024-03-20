9044c4c8697cf200c6f568b1213a534165abff2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 07:03:55 +0000

    Add package starkinfra/0.11.0/starkinfra-0.11.0.tar.gz

---
+++ packages/starkinfra/starkinfra-0.11.0.tar.gz/starkinfra-0.11.0/starkinfra/utils/rest.py
@@ -0,0 +1,15 @@
+from .relay import set_relay
+from starkcore.utils import rest
+
+
+get_page = set_relay(rest.get_page)
+get_stream = set_relay(rest.get_stream)
+get_id = set_relay(rest.get_id)
+get_content = set_relay(rest.get_content)
+get_sub_resource = set_relay(rest.get_sub_resource)
+get_sub_resources = set_relay(rest.get_sub_resources)
+post_multi = set_relay(rest.post_multi)
+post_single = set_relay(rest.post_single)
+delete_id = set_relay(rest.delete_id)
+patch_id = set_relay(rest.patch_id)
+get_raw = set_relay(rest.get_raw)
