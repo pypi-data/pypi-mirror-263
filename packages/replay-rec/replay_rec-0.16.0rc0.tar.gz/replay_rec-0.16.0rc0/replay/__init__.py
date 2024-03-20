f84359b2580f012e99c5097cb4c5a94b72752e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 16:04:48 +0000

    Add package replay-rec/0.16.0rc0/replay_rec-0.16.0rc0.tar.gz

---
+++ packages/replay-rec/replay_rec-0.16.0rc0.tar.gz/replay_rec-0.16.0rc0/replay/__init__.py
@@ -0,0 +1,2 @@
+""" RecSys library """
+__version__ = "0.16.0.preview"
---
+++ packages/replay-rec/replay_rec-0.16.0rc0.tar.gz/replay_rec-0.16.0rc0/replay/models/nn/sequential/callbacks/__init__.py
@@ -0,0 +1,9 @@
+from .prediction_callbacks import (
+    BasePredictionCallback,
+    PandasPredictionCallback,
+    PolarsPredictionCallback,
+    SparkPredictionCallback,
+    TorchPredictionCallback,
+    QueryEmbeddingsPredictionCallback
+)
+from .validation_callback import ValidationMetricsCallback
