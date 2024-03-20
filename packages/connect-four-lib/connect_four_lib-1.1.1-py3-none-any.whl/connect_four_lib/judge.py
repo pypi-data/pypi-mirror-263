3b3f47445d2cd589e511db00c993194c04805123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 03:03:42 +0000

    Add package connect-four-lib/1.1.1/connect_four_lib-1.1.1-py3-none-any.whl

---
+++ packages/connect-four-lib/connect_four_lib-1.1.1-py3-none-any.whl/connect_four_lib/judge.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+from connect_four_lib.game_state import GameState
+
+
+class Judge(ABC):
+    """
+    An abstract class for validating moves and generating debug infos of a game.
+    """
+
+    @abstractmethod
+    def validate(self, move: str) -> GameState:
+        """
+        Validates a move, does not change internal states.
+
+        Args:
+            move (str): Move to be validated.
+
+        Returns:
+            GameState: An integer describing game state.
+        """
+
+    @abstractmethod
+    def add_move(self, move: str) -> Any:
+        """
+        Adds a move and updates internal data structures based on game.
+
+        Args:
+            move (str): The move to be added.
+
+        Returns:
+            Any: Additional information of added more when needed.
+        """
+
+    @abstractmethod
+    def get_debug_info(self) -> str:
+        """
+        Returns debug info such as visual board state, player advantages, etc.
+
+        Returns:
+            str: Debug info.
+        """
+
+    @abstractmethod
+    def get_all_moves(self) -> list[str]:
+        """
+        Returns all moves as string, formatted based on game.
+
+        Returns:
+            list[str]: List of moves.
+        """
