93e0276fdde42dc488f26422b388e53d76e6b18c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 05:07:12 +0000

    Add package pjsinghs-pypi-package/0.1.0/pjsinghs-pypi-package-0.1.0.tar.gz

---
+++ packages/pjsinghs-pypi-package/pjsinghs-pypi-package-0.1.0.tar.gz/pjsinghs-pypi-package-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pjsinghs-pypi-package",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A sample PyPI package",
+    author="Prabhot Singh",
+    author_email="pjsingh@email.com",
+    url="https://github.com/yourusername/sample-pypi-package",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
