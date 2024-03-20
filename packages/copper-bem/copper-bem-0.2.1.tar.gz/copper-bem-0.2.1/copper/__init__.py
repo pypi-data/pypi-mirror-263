93d2c1730df111aebaebc78ca1ad294fc6b95951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 14:04:38 +0000

    Add package copper-bem/0.2.1/copper-bem-0.2.1.tar.gz

---
+++ packages/copper-bem/copper-bem-0.2.1.tar.gz/copper-bem-0.2.1/copper/__init__.py
@@ -0,0 +1,6 @@
+from copper.chiller import *
+from copper.schema import *
+from copper.constants import LOGGING_FORMAT
+import sys
+
+logging.basicConfig(format=LOGGING_FORMAT, stream=sys.stdout, level=logging.INFO)
