90f516c15bd82b0f5caa39a9e047fd36cedc2a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 14:04:32 +0000

    Add package marque/0.1.1/marque-0.1.1.tar.gz

---
+++ packages/marque/marque-0.1.1.tar.gz/marque-0.1.1/marque/scope.py
@@ -0,0 +1,94 @@
+import datetime
+import typing as t
+from dataclasses import dataclass
+from datetime import timedelta
+
+from pydantic import TypeAdapter
+
+
+@dataclass
+class Tag:
+    content: str
+    value: float | None = None
+
+    def __repr__(self) -> str:
+        return f"<Tag content={self.content} value={self.value}>"
+
+
+@dataclass
+class Artifact:
+    name: str
+    value: dict[str, t.Any]
+
+    def __repr__(self) -> str:
+        return f"<Artifact name={self.name}>"
+
+
+class Scope:
+    def __init__(self, id_: str):
+        self.id = id_
+        self.tags: list[Tag] = []
+        self.artifacts: list[Artifact] = []
+        self.error: str | None = None
+        self.duration: timedelta | None = None
+
+    def __repr__(self) -> str:
+        return f"<Scope id={self.id} tag_count={len(self.tags)} artifact_count={len(self.artifacts)}>"
+
+    # TODO: This could probably get pulled out to the utils
+    # module and rework these as BaseModels or dataclasses
+
+    def to_json(self) -> dict[str, t.Any]:
+        artifact_adapter = TypeAdapter(Artifact)
+        tag_adapter = TypeAdapter(Tag)
+
+        return {
+            "id": self.id,
+            "tags": [tag_adapter.dump_python(tag) for tag in self.tags],
+            "artifacts": [artifact_adapter.dump_python(artifact) for artifact in self.artifacts],
+            "error": self.error,
+            "duration": self.duration.microseconds if self.duration else None,
+        }
+
+    @classmethod
+    def from_json(cls, data: dict[str, t.Any]) -> "Scope":
+        artifact_adapter = TypeAdapter(Artifact)
+        tag_adapter = TypeAdapter(Tag)
+
+        scope = cls(data["id"])
+        scope.tags = [tag_adapter.validate_python(tag) for tag in data["tags"]]
+        scope.artifacts = [artifact_adapter.validate_python(artifact) for artifact in data["artifacts"]]
+        scope.error = data["error"]
+        scope.duration = datetime.timedelta(microseconds=data["duration"]) if data["duration"] else None
+
+        return scope
+
+    def keep(self, name: str, value: dict[str, t.Any], overwrite: bool = False) -> "Scope":
+        if not isinstance(value, dict):
+            raise TypeError(f"{value} is not a dict.")
+
+        for existing in self.artifacts:
+            if existing.name == name:
+                if overwrite:
+                    existing.value = value
+                    break
+                else:
+                    raise KeyError(f"{name} already exists.")
+        else:
+            self.artifacts.append(Artifact(name, value))
+
+        return self
+
+    def recall(self, name: str) -> dict[str, t.Any]:
+        for existing in self.artifacts:
+            if existing.name == name:
+                return existing.value
+        raise KeyError(name)
+
+    def tag(self, content: str, value: float | None = None) -> "Scope":
+        # Should we force uniqueness?
+        # for mname, _ in self.tags:
+        #     if mname == name:
+        #         raise KeyError(f"{name} marker already stored.")
+        self.tags.append(Tag(content, value))
+        return self
---
+++ packages/marque/marque-0.1.1.tar.gz/marque-0.1.1/marque/util.py
@@ -0,0 +1,62 @@
+import builtins
+import typing as t
+from contextlib import contextmanager
+from datetime import timedelta
+
+import orjson
+from pydantic import BaseModel
+
+
+def json_serialize(obj: t.Any) -> str:
+    def custom_serializer(obj: t.Any) -> t.Any:
+        if isinstance(obj, BaseModel):
+            return obj.dict()
+        return obj
+
+    return orjson.dumps(obj, default=custom_serializer).decode()
+
+
+def json_deserialize(obj: str) -> t.Any:
+    return orjson.loads(obj)
+
+
+# https://stackoverflow.com/questions/538666/format-timedelta-to-string
+def format_timedelta(td_object: timedelta) -> str:
+    # Convert the entire timedelta to milliseconds
+    total_milliseconds = (
+        td_object.days * 24 * 60 * 60 * 1000 + td_object.seconds * 1000 + td_object.microseconds // 1000
+    )
+
+    periods = [
+        ("yr", 60 * 60 * 24 * 365 * 1000),
+        ("mo", 60 * 60 * 24 * 30 * 1000),
+        ("d", 60 * 60 * 24 * 1000),
+        ("hr", 60 * 60 * 1000),
+        ("m", 60 * 1000),
+        ("s", 1000),
+        ("ms", 1),
+    ]
+
+    strings = []
+    for period_name, period_milliseconds in periods:
+        if total_milliseconds >= period_milliseconds:
+            period_value, total_milliseconds = divmod(total_milliseconds, period_milliseconds)
+            strings.append(f"{period_value}{period_name}")
+
+    return " ".join(strings) if strings else "~0ms"
+
+
+@contextmanager
+def PrintHook(log_func: t.Callable[[str], None]) -> t.Iterator[None]:
+    original_print = builtins.print
+
+    def custom_print(*args: t.Any, **kwargs: t.Any) -> None:
+        log_message = " ".join(str(arg) for arg in args)
+        log_func(log_message)
+
+    builtins.print = custom_print
+
+    try:
+        yield
+    finally:
+        builtins.print = original_print
