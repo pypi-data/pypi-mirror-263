bc4bd776e5623eb20496950de8eea7b6518cecd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 14:04:40 +0000

    Add package metricflow/0.205.0.dev0/metricflow-0.205.0.dev0.tar.gz

---
+++ packages/metricflow/metricflow-0.205.0.dev0.tar.gz/metricflow-0.205.0.dev0/metricflow/test/dataflow/test_sql_table.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import pytest
+
+from metricflow.sql.sql_table import SqlTable
+
+
+def test_sql_table() -> None:  # noqa: D
+    sql_table = SqlTable(schema_name="foo", table_name="bar")
+
+    assert sql_table.sql == "foo.bar"
+    assert SqlTable.from_string("foo.bar") == sql_table
+
+
+def test_sql_table_with_db() -> None:  # noqa: D
+    sql_table = SqlTable(db_name="db", schema_name="foo", table_name="bar")
+
+    assert sql_table.sql == "db.foo.bar"
+    assert SqlTable.from_string("db.foo.bar") == sql_table
+
+
+def test_invalid_sql_table() -> None:  # noqa: D
+    with pytest.raises(RuntimeError):
+        SqlTable.from_string("foo")
