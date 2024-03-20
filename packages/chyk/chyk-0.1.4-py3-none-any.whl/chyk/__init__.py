49ca8e6ec17582d0cd37c563097b0a3cc73d7930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 14:04:26 +0000

    Add package chyk/0.1.4/chyk-0.1.4-py3-none-any.whl

---
+++ packages/chyk/chyk-0.1.4-py3-none-any.whl/chyk/__init__.py
@@ -0,0 +1,21 @@
+import handcalcs.render
+import forallpeople as u
+u.environment('default')
+from IPython.display import Latex
+from math import sqrt, pi, cos, sin, tan, log10, log2, log, e, atan2, acos, asin
+import numpy as np
+sqrt = np.emath.sqrt
+from matplotlib import pyplot as plt
+from scipy.integrate import quad
+from latex2sympy2 import latex2sympy, latex2latex
+from scipy.optimize import fsolve
+import sympy as sym
+from sympy import roots
+from sympy.abc import x, a, b, c
+from sympy import integrate
+from sympy import lambdify
+from sympy import Symbol
+from sympy.solvers import solve
+from tabulate import tabulate
+import pandas as pd
+from fgmkr import fgmk, fgmk2, fgmk_help
