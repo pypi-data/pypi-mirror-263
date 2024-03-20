b2ec5b5e074a2a1549a2d6f2fe3ca472dec66300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 15:03:29 +0000

    Add package proalgotrader-core/0.3.7/proalgotrader_core-0.3.7.tar.gz

---
+++ packages/proalgotrader-core/proalgotrader_core-0.3.7.tar.gz/proalgotrader_core-0.3.7/proalgotrader_core/token_managers/angel_one_token_manager.pyi
@@ -0,0 +1,22 @@
+from SmartApi import SmartConnect
+from SmartApi.smartWebSocketV2 import SmartWebSocketV2
+from _typeshed import Incomplete
+from proalgotrader_core.helpers.get_totp import get_totp as get_totp
+from proalgotrader_core.token_managers.base_token_manager import BaseTokenManager as BaseTokenManager
+from typing import Any
+
+class AngelOneTokenManager(BaseTokenManager):
+    token: Incomplete
+    client_id: Incomplete
+    totp_key: Incomplete
+    mpin: Incomplete
+    api_key: Incomplete
+    api_secret: Incomplete
+    redirect_url: Incomplete
+    http_client: Incomplete
+    def __init__(self, client_id: str, totp_key: str, mpin: str, api_key: str, api_secret: str, redirect_url: str) -> None: ...
+    def set_token(self, token: str) -> None: ...
+    def get_token(self) -> str: ...
+    def get_http_client(self) -> SmartConnect: ...
+    def get_session(self) -> Any: ...
+    def get_ws_client(self) -> SmartWebSocketV2: ...
