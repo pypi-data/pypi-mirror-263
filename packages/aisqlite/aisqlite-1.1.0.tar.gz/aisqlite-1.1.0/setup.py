c4ccaf652f5d6d5b77bfcb46dd59baf8105a2e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 16:05:02 +0000

    Add package aisqlite/1.1.0/aisqlite-1.1.0.tar.gz

---
+++ packages/aisqlite/aisqlite-1.1.0.tar.gz/aisqlite-1.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aisqlite',
+    version='1.1.0',
+    author='Ray Del Vecchio',
+    author_email='ray@cerebralvalley.ai',
+    description='AI enhanced SQLite',
+    url='https://github.com/raydelvecchio/aisql',
+    packages=find_packages(),
+    install_requires=[
+        'setuptools', 
+        'twine',
+        'openai',
+        'wheel',
+    ],
+)
