d629000e948a5c6a5b960eafa9fe356bbede61a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 16:04:50 +0000

    Add package itsscom/0.1/itsscom-0.1.tar.gz

---
+++ packages/itsscom/itsscom-0.1.tar.gz/itsscom-0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+ name='itsscom',
+ version='0.1',
+ author="Nithingowda A M",
+ author_email="nithingowdaam0123@gmail.com",
+ description="itss demo package",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 2",
+ "Programming Language :: Python :: 2.7",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
