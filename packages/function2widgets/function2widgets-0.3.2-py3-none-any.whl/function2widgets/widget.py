d5fa99d02316256ee3ea817d4c18259971a0ea88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 05:06:28 +0000

    Add package function2widgets/0.3.2/function2widgets-0.3.2-py3-none-any.whl

---
+++ packages/function2widgets/function2widgets-0.3.2-py3-none-any.whl/function2widgets/widget.py
@@ -0,0 +1,65 @@
+import abc
+from typing import Any
+
+from PyQt6.QtWidgets import QWidget
+
+
+class InvalidValueError(ValueError):
+    pass
+
+
+class BaseParameterWidget(QWidget):
+
+    SET_DEFAULT_ON_INIT: bool = False
+
+    def __init__(self, default: Any, stylesheet: str | None, parent: QWidget | None):
+        super().__init__(parent)
+        self._default = default
+        self._parameter_name: str | None = None
+
+        if stylesheet is not None:
+            self.setStyleSheet(stylesheet)
+
+    @property
+    def parameter_name(self) -> str | None:
+        return self._parameter_name
+
+    @parameter_name.setter
+    def parameter_name(self, parameter_name: str | None):
+        self._parameter_name = parameter_name
+
+    @abc.abstractmethod
+    def get_value(self, *args, **kwargs) -> Any:
+        pass
+
+    @abc.abstractmethod
+    def set_value(self, value: Any, *args, **kwargs):
+        pass
+
+    @property
+    def default(self) -> Any:
+        return self._default
+
+    @abc.abstractmethod
+    def set_label(self, label_text: str):
+        pass
+
+    @abc.abstractmethod
+    def get_label(self, label_text) -> str:
+        pass
+
+    @abc.abstractmethod
+    def set_docstring(self, docstring: str):
+        pass
+
+    @abc.abstractmethod
+    def get_docstring(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    def show_label(self, show: bool):
+        pass
+
+    @abc.abstractmethod
+    def show_docstring(self, show: bool):
+        pass
