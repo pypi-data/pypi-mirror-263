f456e1b4d1c41391371df7e722ea59591e240550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 04:04:32 +0000

    Add package ixmp4/0.8.0/ixmp4-0.8.0.tar.gz

---
+++ packages/ixmp4/ixmp4-0.8.0.tar.gz/ixmp4-0.8.0/ixmp4/core/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+from .iamc.variable import Variable as Variable
+from .model import Model as Model
+from .optimization.indexset import IndexSet as IndexSet
+from .optimization.scalar import Scalar as Scalar
+from .platform import Platform as Platform
+from .region import Region as Region
+from .run import Run as Run
+from .scenario import Scenario as Scenario
+from .unit import Unit as Unit
