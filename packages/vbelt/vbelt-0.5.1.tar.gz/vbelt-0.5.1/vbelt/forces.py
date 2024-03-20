c8d12af4783c1e80a7b1e1785a57b6759a4786a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 16:04:35 +0000

    Add package vbelt/0.5.1/vbelt-0.5.1.tar.gz

---
+++ packages/vbelt/vbelt-0.5.1.tar.gz/vbelt-0.5.1/vbelt/forces.py
@@ -0,0 +1,61 @@
+# vbelt: The VASP user toolbelt.
+# Copyright (C) 2023  Théo Cavignac
+# Licensed under EUPL
+from itertools import islice
+import numpy as np
+
+from .outcar_utils import get_species, get_array, get_float
+
+
+def read_forces(file):
+    for line in file:
+        if "POTCAR" in line:
+            break
+    else:
+        raise ValueError("Could not find the list of potential.")
+
+    nb_specs = 1
+    for line in file:
+        if "POTCAR" not in line:
+            break
+        nb_specs += 1
+
+    species = [get_species(file) for _ in range(nb_specs)]
+
+    nb_atoms = get_array(
+        file,
+        "ions per type",
+        expect_equal=True,
+        map_=int,
+    )
+
+    nb_tot = sum(nb_atoms)
+
+    assert len(nb_atoms) == nb_specs
+
+    tol = get_float(file, "EDIFFG", expect_equal=True, after="stop")
+
+    assert tol is not None
+
+    if tol > 0:
+        tol *= 10.0
+    else:
+        tol *= -1.0
+
+    raw = None
+
+    while True:
+        for line in file:
+            if "TOTAL-FORCE" in line:
+                break
+        else:
+            break  # EOF
+
+        raw = list(islice(file, 1, 1 + nb_tot))
+
+    if raw is None:
+        raise ValueError("Forces not found.")
+
+    forces = np.array([line.split()[3:] for line in raw], dtype=float)
+
+    return list(zip(species, nb_atoms)), forces, tol
