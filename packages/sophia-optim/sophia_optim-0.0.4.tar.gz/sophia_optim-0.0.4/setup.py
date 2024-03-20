6569d63ca14d965093fe4417b8789a5e7ee2c60b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 15:03:32 +0000

    Add package sophia-optim/0.0.4/sophia_optim-0.0.4.tar.gz

---
+++ packages/sophia-optim/sophia_optim-0.0.4.tar.gz/sophia_optim-0.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='sophia_optim',
+    version='0.0.4',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'pytorch >= 2.0.0'
+    ]
+)
