df3477dcf2a1f7abb609a07a8af2ad47b3cbfac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 07:03:54 +0000

    Add package ChatGskBot/2.0.0/ChatGskBot-2.0.0.tar.gz

---
+++ packages/ChatGskBot/ChatGskBot-2.0.0.tar.gz/ChatGskBot-2.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ChatGskBot',
+      version='2.0.0',
+      description='Chatgpt GSK',
+      packages=['ChatGskBot'],
+      author_email='doneld528@gmail.com',
+      zip_safe=False)
