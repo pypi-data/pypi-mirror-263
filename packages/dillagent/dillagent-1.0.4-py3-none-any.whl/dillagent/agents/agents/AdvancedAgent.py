274c670de2d49f90664b43ed0653039414568012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 05:03:09 +0000

    Add package dillagent/1.0.4/dillagent-1.0.4-py3-none-any.whl

---
+++ packages/dillagent/dillagent-1.0.4-py3-none-any.whl/dillagent/agents/agents/AdvancedAgent.py
@@ -0,0 +1,27 @@
+from tools import Tool
+from .BaseAgent import BaseAgent
+from ...dependencies.prompts.SysPrompt import SysPrompt
+from typing import List
+from ...llm.LLM import LLM
+
+
+class AdvancedAgent(BaseAgent):
+    def __init__(self, llm: LLM, tools: List, sys_prompt: SysPrompt):
+        """
+        Initialize the BaseAgent with a list of tools and an optional initial prompt.
+
+        Parameters:
+        - tools: A list of Tool instances representing the available tools for the agent.
+        - initial_prompt: An optional initial prompt for the agent. If not provided, a prompt will be generated.
+
+        Returns:
+        None
+        """
+        self.llm = llm
+        self.tools = tools
+        self.sys_prompt = sys_prompt
+        self.sys_prompt.generate_prompt(tools)
+        self.llm.add_sys_prompt(self.sys_prompt.prompt_str)
+
+    def run(self, prompt):
+        return self.llm.run(prompt)
