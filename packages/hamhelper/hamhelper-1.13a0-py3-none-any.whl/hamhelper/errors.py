982bf99e9823c78715221690966bfd94dcca1111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 14:03:58 +0000

    Add package hamhelper/1.13a0/hamhelper-1.13a0-py3-none-any.whl

---
+++ packages/hamhelper/hamhelper-1.13a0-py3-none-any.whl/hamhelper/errors.py
@@ -0,0 +1,11 @@
+"""Functions to help with error catching and handling."""
+import numpy as np
+
+
+def assert_error(expression, error):
+    """Assert function which raises a specified error when the expression evaluates False."""
+    if not isinstance(expression, (np.bool_, bool)):
+        raise RuntimeError('"expression" has to be of type bool')
+    if not expression:
+        raise error
+    return
