c097917edc6624482c56c4e142936be18ccbe3dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 04:04:06 +0000

    Add package area-calculator-se/0.0.2/area_calculator_se-0.0.2-py3-none-any.whl

---
+++ packages/area-calculator-se/area_calculator_se-0.0.2-py3-none-any.whl/area_calculator_se.py
@@ -0,0 +1,21 @@
+from math import pi
+
+
+def welcome():
+    print('Hello, welcome to area calculator package!')
+
+
+def add(num1: int, num2: int) -> int:
+    return num1 + num2
+
+
+def subtract(num1: int, num2: int) -> int:
+    return num1 - num2
+
+
+def multiply(num1: int, num2: int) -> int:
+    return num1 * num2
+
+
+def square(a):
+    return a * a
