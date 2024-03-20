1706e574d482dad5d71e1323ac3e73064ddf43ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 15:03:31 +0000

    Add package backlink-checker/0.0.3/backlink_checker-0.0.3.tar.gz

---
+++ packages/backlink-checker/backlink_checker-0.0.3.tar.gz/backlink_checker-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='backlink_checker',
+  version='0.0.3',
+  description='backlink checker',
+  long_description=open('README.md').read(),
+  long_description_content_type='text/markdown',
+  url='',  
+  author='mr11robot',
+  author_email='taha.youssef.fares@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='backlinks', 
+  packages=find_packages(),
+  install_requires=['openpyxl','pandas','beautifulsoup4','requests','google-oauth2-tool','google-api-python-client'], 
+)
