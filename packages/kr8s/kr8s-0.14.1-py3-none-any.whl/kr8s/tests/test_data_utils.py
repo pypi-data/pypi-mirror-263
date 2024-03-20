124b513054006b120b884e3397e1b89492b4c72b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 06:04:47 +0000

    Add package kr8s/0.14.1/kr8s-0.14.1-py3-none-any.whl

---
+++ packages/kr8s/kr8s-0.14.1-py3-none-any.whl/kr8s/tests/test_data_utils.py
@@ -0,0 +1,46 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023-2024, Kr8s Developers (See LICENSE for list)
+# SPDX-License-Identifier: BSD 3-Clause License
+import pytest
+
+from kr8s._data_utils import (
+    dict_to_selector,
+    dot_to_nested_dict,
+    list_dict_unpack,
+    xdict,
+)
+
+
+def test_list_dict_unpack():
+    data = [{"key": "hello", "value": "world"}]
+    assert list_dict_unpack(data) == {"hello": "world"}
+
+
+def test_dot_to_nested_dict():
+    assert dot_to_nested_dict("hello", "world") == {"hello": "world"}
+    assert dot_to_nested_dict("hello.world", "value") == {"hello": {"world": "value"}}
+    assert dot_to_nested_dict("foo.bar.baz", "value") == {
+        "foo": {
+            "bar": {"baz": "value"},
+        }
+    }
+
+
+def test_dict_to_selector():
+    assert dict_to_selector({"foo": "bar"}) == "foo=bar"
+    assert dict_to_selector({"foo": "bar", "baz": "qux"}) == "foo=bar,baz=qux"
+
+
+def test_xdict():
+    assert xdict(foo="bar") == {"foo": "bar"}
+    assert xdict(foo="bar", baz=None) == {"foo": "bar"}
+    assert xdict(foo="bar", baz="qux") == {"foo": "bar", "baz": "qux"}
+    assert xdict(foo="bar", baz="qux", quux=None) == {"foo": "bar", "baz": "qux"}
+    assert xdict({"foo": "bar", "baz": "qux"}) == {"foo": "bar", "baz": "qux"}
+    assert xdict({"foo": "bar", "baz": None}) == {"foo": "bar"}
+    with pytest.raises(ValueError):
+        xdict({}, {})
+    with pytest.raises(ValueError):
+        assert xdict({}, quux=None) == {
+            "foo": "bar",
+            "baz": "qux",
+        }
