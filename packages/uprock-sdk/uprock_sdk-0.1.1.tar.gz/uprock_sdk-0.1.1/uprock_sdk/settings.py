a06ed1de213b397c3b22fcc0fa66e56152632afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 14:04:27 +0000

    Add package uprock-sdk/0.1.1/uprock_sdk-0.1.1.tar.gz

---
+++ packages/uprock-sdk/uprock_sdk-0.1.1.tar.gz/uprock_sdk-0.1.1/uprock_sdk/settings.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    API_URL: str = "http://localhost"
+
+    NAMESPACE_ID: Optional[int] = None
+
+    TERMS_DEFAULT_LANGUAGE: str = "ru"
+    TERMS_TTL_S: int = 60
+
+    def update(self, data: dict) -> "Settings":
+        update = self.dict()
+        update.update(data)
+
+        for k, v in self.validate(update).dict(exclude_defaults=True).items():
+            setattr(self, k, v)
+        return self
+
+
+GLOBAL_SETTINGS = Settings()
