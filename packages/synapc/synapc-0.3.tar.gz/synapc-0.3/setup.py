6df000eab257bfdf791910ce246caca7737557b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 16:04:54 +0000

    Add package synapc/0.3/synapc-0.3.tar.gz

---
+++ packages/synapc/synapc-0.3.tar.gz/synapc-0.3/setup.py
@@ -0,0 +1,46 @@
+
+from setuptools import setup, find_packages , Extension
+ 
+c_src_files=[
+    'src/activation.c',
+    'src/loss.c',
+    'src/ndarray.c',
+    'src/metric.c',
+    'src/nn.c',
+    'src/num.c',
+    'src/dense_layer.c',
+    'synap.c'
+]
+header_files=[
+    'header_files/activation.h',
+    'header_files/loss.h',
+    'header_files/ndarray.h',
+    'header_files/metric.h',
+    'header_files/nn.h',
+    'header_files/num.h',
+    'header_files/dense_layer.h',
+]
+
+extensions=[Extension('synapc',c_src_files,include_dirs=header_files)]
+
+setup(
+    name='synapc', 
+    version='0.3',  
+    author='Nagaraj Poojari',
+    author_email='np137270@email.com',
+    description='A minimalistic neural network library in C',
+    long_description='',
+    long_description_content_type='text/markdown',
+    ext_modules=extensions,
+    url='https://github.com/nagarajRPoojari/SynapC', 
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
