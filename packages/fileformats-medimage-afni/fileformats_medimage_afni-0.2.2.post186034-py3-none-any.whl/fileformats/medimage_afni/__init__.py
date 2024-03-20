5314a4a4f1d0570533879db996d1f3621e150cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 14:04:01 +0000

    Add package fileformats-medimage-afni/0.2.2.post186034/fileformats_medimage_afni-0.2.2.post186034-py3-none-any.whl

---
+++ packages/fileformats-medimage-afni/fileformats_medimage_afni-0.2.2.post186034-py3-none-any.whl/fileformats/medimage_afni/__init__.py
@@ -0,0 +1,33 @@
+from ._version import __version__  # noqa: F401
+from fileformats.generic import File
+
+
+class Head(File):
+    ext = ".HEAD"
+    binary = True
+
+
+class All1(File):
+    ext = ".all1"
+    binary = True
+
+
+class OneD(File):
+    ext = ".1D"
+    binary = True
+
+
+class R1(File):
+    ext = ".r1"
+    binary = True
+
+
+class ThreeD(File):
+    ext = ".3D"
+    binary = True
+    alternate_exts = (".3d",)
+
+
+class Dset(File):
+    ext = ".dset"
+    binary = True
