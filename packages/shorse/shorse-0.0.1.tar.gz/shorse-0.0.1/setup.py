f829adcf54100b852d51dcf025683317cad33160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 03:03:09 +0000

    Add package shorse/0.0.1/shorse-0.0.1.tar.gz

---
+++ packages/shorse/shorse-0.0.1.tar.gz/shorse-0.0.1/setup.py
@@ -0,0 +1,25 @@
+# a setup.py file for my package named shorse to upload to pypi
+
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='shorse',
+    version='0.0.1',
+    description='A package to display a shorse',
+    url='',
+    author='Shorse',
+    author_email='yusufy2004@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=requirements,
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
