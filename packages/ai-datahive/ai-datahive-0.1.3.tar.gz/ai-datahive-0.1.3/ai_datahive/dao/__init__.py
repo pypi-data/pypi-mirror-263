00d1a7edbaafbf72df6cbd559d1a52801e4d0f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 05:06:51 +0000

    Add package ai-datahive/0.1.3/ai-datahive-0.1.3.tar.gz

---
+++ packages/ai-datahive/ai-datahive-0.1.3.tar.gz/ai-datahive-0.1.3/ai_datahive/dao/__init__.py
@@ -0,0 +1,3 @@
+from .base_dao import BaseDAO, T, Filter
+from .supabase_dao import SupabaseDAO
+from .dao_factory import dao_factory
---
+++ packages/ai-datahive/ai-datahive-0.1.3.tar.gz/ai-datahive-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+def read_requirements():
+    with open('requirements.txt') as f:
+        return [line.strip() for line in f.readlines() if not line.startswith('#')]
+
+
+setup(
+    name='ai-datahive',
+    version='0.1.3',
+    packages=find_packages(),
+    package_data={'ai_datahive': ['*.html']},
+    description='ETL Package for crawling data, transform it with AI and store it in a datastorage.',
+    author='Danny Gerst',
+    author_email='d.gerst@bizrock.de',
+    install_requires=read_requirements(),
+)
