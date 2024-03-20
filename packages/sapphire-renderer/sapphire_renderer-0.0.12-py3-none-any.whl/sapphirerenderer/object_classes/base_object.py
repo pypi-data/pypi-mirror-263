09f50dee61d635ac99f48699a5c2ce2ab5b3a479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 14:04:21 +0000

    Add package sapphire-renderer/0.0.12/sapphire_renderer-0.0.12-py3-none-any.whl

---
+++ packages/sapphire-renderer/sapphire_renderer-0.0.12-py3-none-any.whl/sapphirerenderer/object_classes/base_object.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+
+class Object:
+    def __init__(
+        self,
+        position=np.array([0, 0, 0]),
+        rotation=np.array((0, 0, 0)),
+        scale=1,
+        color=(0, 0, 0),
+    ):
+        self.position = position
+        self.rotation = rotation
+        self.scale = scale
+        self.hidden = True
+        self.color = color
+
+    def is_hidden(self):
+        return self.hidden
+
+    def hide(self):
+        self.hidden = True
+        return self
+
+    def show(self):
+        self.hidden = False
+        return self
+
+    def set_position(self, position):
+        self.position = position
+
+    def set_rotation(self, rotation):
+        self.rotation = rotation
+
+    def set_scale(self, scale):
+        self.scale = scale
+
+    def set_color(self, color):
+        self.color = color
+
+    def get_position(self):
+        return self.position
+
+    def get_rotation(self):
+        return self.rotation
+
+    def get_scale(self):
+        return self.scale
+
+    def get_color(self):
+        return self.color
+
+    def update(self):
+        """
+        empty update for child classes to override
+        :return:
+        """
+        pass
+
+    def __str__(self):
+        return self.__class__.__name__
