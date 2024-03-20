5ab3f6b55f8c095ed08fbd200d5e62914e8719b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 15:03:35 +0000

    Add package poetry-patches/0.1.6/poetry_patches-0.1.6.tar.gz

---
+++ packages/poetry-patches/poetry_patches-0.1.6.tar.gz/poetry_patches-0.1.6/poetry_patches/commands.py
@@ -0,0 +1,22 @@
+from poetry.console.commands.group_command import GroupCommand
+
+from poetry_patches.patcher import PoetryPatcher
+from poetry_patches.state.backup import Backup
+
+
+class PatchesApplyCommand(GroupCommand):
+    name = "patches apply"
+    description = "Apply the patches."
+
+    def handle(self) -> int:
+        PoetryPatcher(self.poetry, self.io, Backup.get()).apply()
+        return 0
+
+
+class PatchesRevertCommand(GroupCommand):
+    name = "patches revert"
+    description = "Revert the patches."
+
+    def handle(self) -> int:
+        Backup.get().revert()
+        return 0
