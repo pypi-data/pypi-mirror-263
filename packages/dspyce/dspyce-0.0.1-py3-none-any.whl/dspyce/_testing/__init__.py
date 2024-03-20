b4961e0c0fffc11330f741fde7e388c6b609b76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 16:04:44 +0000

    Add package dspyce/0.0.1/dspyce-0.0.1-py3-none-any.whl

---
+++ packages/dspyce/dspyce-0.0.1-py3-none-any.whl/dspyce/_testing/__init__.py
@@ -0,0 +1,17 @@
+import unittest
+
+from dspyce._testing.dspyceTest import DSpaceObjectTest
+from dspyce._testing.statisticsTest import StatisticTest
+from dspyce._testing.metadataTests import MetadataTest
+from dspyce._testing.itemTests import ItemTest
+from dspyce._testing.relationTest import RelationTest
+from dspyce._testing.bitstreamTest import BitstreamsTest
+
+
+class InitTests(unittest.TestCase):
+    """Dummy test class"""
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
