7db65926d34c4b6e8848f623938b73a58a3b000f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 05:06:31 +0000

    Add package copyfolder/0.1.5/copyfolder-0.1.5.tar.gz

---
+++ packages/copyfolder/copyfolder-0.1.5.tar.gz/copyfolder-0.1.5/src/__main__.py
@@ -0,0 +1,34 @@
+from argparse import Namespace, ArgumentParser
+import os
+import shutil
+from rich import Console
+
+console = Console()
+
+def move():
+    parser = ArgumentParser(description='Copies the files from the source folder to the destination folder')
+
+    parser.add_argument('source', type=str, help='Enter the source folder', action='store_true')
+    parser.add_argument('destination', type=str, help='Enter the destination folder', action='store_true')
+
+    args: Namespace = parser.parse_args()
+
+    if not os.path.isdir(args.source):
+        console.print(f"Source directory {args.source} does not exist.")
+        return
+
+    if not os.path.isdir(args.destination):
+        console.print(f"Destination directory {args.destination} does not exist.")
+        return
+
+    for file in os.listdir(args.source):
+        file_path = os.path.join(args.source, file)
+        if os.path.isdir(file_path):
+            shutil.copytree(file_path, os.path.join(args.destination, file))
+        elif os.path.isfile(file_path):
+            shutil.copy(file_path, args.destination)
+
+    console.print(f'Copying files from {args.source} to {args.destination}')
+
+if __name__ == "__main__":
+    move()
