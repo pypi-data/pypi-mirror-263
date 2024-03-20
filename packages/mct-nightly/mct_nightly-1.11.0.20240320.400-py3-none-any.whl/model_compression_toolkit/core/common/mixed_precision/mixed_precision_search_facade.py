66262144fe75c5d54956cd469cc8f1fde937784f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 14:04:07 +0000

    Add package mct-nightly/1.11.0.20240320.400/mct_nightly-1.11.0.20240320.400-py3-none-any.whl

---
+++ packages/mct-nightly/mct_nightly-1.11.0.20240320.400-py3-none-any.whl/model_compression_toolkit/core/common/mixed_precision/mixed_precision_search_facade.py
@@ -0,0 +1,131 @@
+# Copyright 2021 Sony Semiconductor Israel, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import copy
+from enum import Enum
+import numpy as np
+from typing import List, Callable, Dict
+
+from model_compression_toolkit.core import MixedPrecisionQuantizationConfig
+from model_compression_toolkit.core.common import Graph
+from model_compression_toolkit.core.common.hessian import HessianInfoService
+from model_compression_toolkit.core.common.mixed_precision.kpi_tools.kpi import KPI, KPITarget
+from model_compression_toolkit.core.common.mixed_precision.kpi_tools.kpi_functions_mapping import kpi_functions_mapping
+from model_compression_toolkit.core.common.framework_implementation import FrameworkImplementation
+from model_compression_toolkit.core.common.mixed_precision.mixed_precision_search_manager import MixedPrecisionSearchManager
+from model_compression_toolkit.core.common.mixed_precision.search_methods.linear_programming import \
+    mp_integer_programming_search
+from model_compression_toolkit.core.common.framework_info import FrameworkInfo
+from model_compression_toolkit.core.common.mixed_precision.solution_refinement_procedure import \
+    greedy_solution_refinement_procedure
+from model_compression_toolkit.core.common.substitutions.apply_substitutions import substitute
+from model_compression_toolkit.logger import Logger
+
+
+class BitWidthSearchMethod(Enum):
+    # When adding a new search_methods MP configuration method, these enum and factory dictionary
+    # should be updated with it's kind and a search_method implementation.
+    INTEGER_PROGRAMMING = 0
+
+
+search_methods = {
+    BitWidthSearchMethod.INTEGER_PROGRAMMING: mp_integer_programming_search}
+
+
+def search_bit_width(graph_to_search_cfg: Graph,
+                     fw_info: FrameworkInfo,
+                     fw_impl: FrameworkImplementation,
+                     target_kpi: KPI,
+                     mp_config: MixedPrecisionQuantizationConfig,
+                     representative_data_gen: Callable,
+                     search_method: BitWidthSearchMethod = BitWidthSearchMethod.INTEGER_PROGRAMMING,
+                     hessian_info_service: HessianInfoService=None) -> List[int]:
+    """
+    Search for an MP configuration for a given graph. Given a search_method method (by default, it's linear
+    programming), we use the sensitivity_evaluator object that provides a function to compute an
+    evaluation for the expected sensitivity for a bit-width configuration.
+    Then, and after computing the KPI for each node in the graph for each bit-width in the search space,
+    we search for the optimal solution, given some target_kpi, the solution should fit.
+    target_kpi have to be passed. If it was not passed, the facade is not supposed to get here by now.
+
+    Args:
+        graph_to_search_cfg: Graph to search a MP configuration for.
+        fw_info: FrameworkInfo object about the specific framework (e.g., attributes of different layers' weights to quantize).
+        fw_impl: FrameworkImplementation object with specific framework methods implementation.
+        target_kpi: Target KPI to bound our feasible solution space s.t the configuration does not violate it.
+        mp_config: Mixed-precision quantization configuration.
+        representative_data_gen: Dataset to use for retrieving images for the models inputs.
+        search_method: BitWidthSearchMethod to define which searching method to use.
+        hessian_info_service: HessianInfoService to fetch Hessian traces approximations.
+
+    Returns:
+        A MP configuration for the graph (list of integers, where the index in the list, is the node's
+        index in the graph, when the graph is topology sorted, and the value in this index is the
+        bit-width index on the node).
+
+    """
+
+    # target_kpi have to be passed. If it was not passed, the facade is not supposed to get here by now.
+    if target_kpi is None:
+        Logger.critical('Target KPI have to be passed for search_methods bit-width configuration')  # pragma: no cover
+
+    # Set graph for MP search
+    graph = copy.deepcopy(graph_to_search_cfg)  # Copy graph before searching
+    if target_kpi.bops < np.inf:
+        # Since Bit-operations count target KPI is set, we need to reconstruct the graph for the MP search
+        graph = substitute(graph, fw_impl.get_substitutions_virtual_weights_activation_coupling())
+
+    # If we only run weights compression with MP than no need to consider activation quantization when computing the
+    # MP metric (it adds noise to the computation)
+    disable_activation_for_metric = (target_kpi.weights_memory < np.inf and
+                                    (target_kpi.activation_memory == np.inf and
+                                     target_kpi.total_memory == np.inf and
+                                     target_kpi.bops == np.inf)) or graph_to_search_cfg.is_single_activation_cfg()
+
+    # Set Sensitivity Evaluator for MP search. It should always work with the original MP graph,
+    # even if a virtual graph was created (and is used only for BOPS KPI computation purposes)
+    se = fw_impl.get_sensitivity_evaluator(
+        graph_to_search_cfg,
+        mp_config,
+        representative_data_gen=representative_data_gen,
+        fw_info=fw_info,
+        disable_activation_for_metric=disable_activation_for_metric,
+        hessian_info_service=hessian_info_service)
+
+    # Each pair of (KPI method, KPI aggregation) should match to a specific provided kpi target
+    kpi_functions = kpi_functions_mapping
+
+    # Instantiate a manager object
+    search_manager = MixedPrecisionSearchManager(graph,
+                                                 fw_info,
+                                                 fw_impl,
+                                                 se,
+                                                 kpi_functions,
+                                                 target_kpi,
+                                                 original_graph=graph_to_search_cfg)
+
+    if search_method in search_methods:  # Get a specific search function
+        search_method_fn = search_methods.get(search_method)
+    else:
+        raise NotImplemented  # pragma: no cover
+
+    # Search for the desired mixed-precision configuration
+    result_bit_cfg = search_method_fn(search_manager,
+                                      target_kpi)
+
+    if mp_config.refine_mp_solution:
+        result_bit_cfg = greedy_solution_refinement_procedure(result_bit_cfg, search_manager, target_kpi)
+
+    return result_bit_cfg
---
+++ packages/mct-nightly/mct_nightly-1.11.0.20240320.400-py3-none-any.whl/model_compression_toolkit/exporter/model_wrapper/keras/builder/fully_quantized_model_builder.py
@@ -0,0 +1,97 @@
+# Copyright 2022 Sony Semiconductor Israel, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from typing import Tuple, Callable
+from model_compression_toolkit.core import common
+from model_compression_toolkit.core.common import Graph
+from model_compression_toolkit.constants import FOUND_TF
+from model_compression_toolkit.core.common.user_info import UserInformation
+from model_compression_toolkit.logger import Logger
+import model_compression_toolkit.core as C
+
+if FOUND_TF:
+    import tensorflow as tf
+    from tensorflow.keras.layers import Layer
+    from model_compression_toolkit.core.keras.back2framework.keras_model_builder import KerasModelBuilder
+    from mct_quantizers import KerasQuantizationWrapper
+    from mct_quantizers import KerasActivationQuantizationHolder
+
+    def _get_wrapper(node: common.BaseNode,
+                     layer: Layer,
+                     fw_impl=None) -> Layer:
+        """
+        A function which takes a computational graph node and a keras layer and perform the quantization wrapping
+        Args:
+            node: A node of mct graph.
+            layer: A keras layer
+
+        Returns: Wrapped layer with weights quantizers and activation quantizers
+
+        """
+        weights_quantizers, _ = fw_impl.get_inferable_quantizers(node)
+        if len(weights_quantizers) > 0:
+            return KerasQuantizationWrapper(layer,
+                                            weights_quantizers)
+        return layer
+
+
+    def get_activation_quantizer_holder(node: common.BaseNode, fw_impl) -> Callable:
+        """
+        Retrieve a ActivationQuantizationHolder layer to use for activation quantization for a node.
+
+        Args:
+            node: Node to get ActivationQuantizationHolder to attach in its output.
+
+        Returns:
+            A ActivationQuantizationHolder layer for the node activation quantization.
+        """
+        _, activation_quantizers = fw_impl.get_inferable_quantizers(node)
+
+        # Holder by definition uses a single quantizer for the activation quantization
+        # thus we make sure this is the only possible case (unless it's a node with no activation
+        # quantization, which in this case has an empty list).
+        if len(activation_quantizers) == 1:
+            return KerasActivationQuantizationHolder(activation_quantizers[0])
+
+        Logger.error(
+            f'ActivationQuantizationHolder supports a single quantizer but {len(activation_quantizers)} quantizers '
+            f'were found for node {node}')
+
+    def get_exportable_keras_model(graph: Graph) -> Tuple[tf.keras.models.Model, UserInformation]:
+        """
+        Convert graph to an exportable Keras model (model with all quantization parameters).
+        An exportable model can then be exported using model_exporter, to retrieve the
+        final exported model.
+
+        Args:
+            graph: Graph to convert to an exportable Keras model.
+
+        Returns:
+            Exportable Keras model and user information.
+        """
+        exportable_model, user_info = KerasModelBuilder(graph=graph,
+                                                        wrapper=lambda n, kn:
+                                                        _get_wrapper(n, kn,
+                                                                     fw_impl=C.keras.keras_implementation.KerasImplementation()),
+                                                        get_activation_quantizer_holder_fn=lambda n:
+                                                        get_activation_quantizer_holder(n,
+                                                                                        fw_impl=C.keras.keras_implementation.KerasImplementation())).build_model()
+        exportable_model.trainable = False
+        return exportable_model, user_info
+else:
+    def get_exportable_keras_model(*args, **kwargs):  # pragma: no cover
+        Logger.error('Installing tensorflow is mandatory '
+                     'when using get_exportable_keras_model. '
+                     'Could not find Tensorflow package.')
