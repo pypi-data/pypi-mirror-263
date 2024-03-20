855fdc3baf6add978bcc712449367e8ea0327142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 05:05:38 +0000

    Add package blacknoise/0.0.3/blacknoise-0.0.3.tar.gz

---
+++ packages/blacknoise/blacknoise-0.0.3.tar.gz/blacknoise-0.0.3/README.md
@@ -0,0 +1,34 @@
+# blacknoise
+
+[![PyPI - Version](https://img.shields.io/pypi/v/blacknoise.svg)](https://pypi.org/project/blacknoise)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/blacknoise.svg)](https://pypi.org/project/blacknoise)
+
+-----
+
+**Table of Contents**
+
+- [Installation](#installation)
+- [License](#license)
+
+## Installation
+
+```console
+pip install blacknoise
+```
+
+### Using blacknoise with Django to serve static files
+
+```python
+from blacknoise import BlackNoise
+from django.core.asgi import get_asgi_application
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+
+application = BlackNoise(get_asgi_application())
+application.add(BASE / "static", "/static")
+```
+
+## License
+
+`blacknoise` is distributed under the terms of the [MIT](https://spdx.org/licenses/MIT.html) license.
---
+++ packages/blacknoise/blacknoise-0.0.3.tar.gz/blacknoise-0.0.3/tests/test_blacknoise.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+
+import httpx
+import pytest
+from starlette.responses import PlainTextResponse
+
+from blacknoise._impl import BlackNoise
+
+
+async def app(scope, receive, send):
+    response = PlainTextResponse(f"Hello from {scope['path']}")
+    await response(scope, receive, send)
+
+
+@pytest.fixture
+def bn():
+    this = Path(__file__).parent
+    blacknoise = BlackNoise(app)
+    blacknoise.add(this / "static", "/static/")
+    return blacknoise
+
+
+def test_files_contents(bn):
+    this = Path(__file__).parent
+
+    assert "/static/hello.txt" in bn._files
+    assert "/static/hello.txt.gz" not in bn._files
+    assert "/static/hello2.txt.gz" in bn._files
+    assert "/static/hello3.txt" in bn._files
+    assert "/static/foo" not in bn._files
+
+    assert bn._files["/static/hello.txt"] == str(this / "static" / "hello.txt")
+
+
+@pytest.mark.asyncio
+async def test_static_file_serving(bn):
+    transport = httpx.ASGITransport(app=bn)
+    async with httpx.AsyncClient(
+        transport=transport, base_url="http://testserver"
+    ) as client:
+        r = await client.get("/static/hello.txt")
+        assert r.status_code == 200
+        assert r.text == "world\n"
+        assert r.headers["content-encoding"] == "gzip"
+
+        r = await client.get("/static/hello2.txt.gz")
+        assert r.status_code == 200
+        assert r.text == "world2\n"
+        assert r.headers["content-encoding"] == "gzip"
+
+        r = await client.get("/static/hello3.txt")
+        assert r.status_code == 200
+        assert r.text == "world3\n"
+        assert "content-encoding" not in r.headers
+
+        r = await client.post("/static/hello.txt")
+        assert r.status_code == 405
+
+        r = await client.get("/static/foo")
+        assert r.status_code == 404
+
+        r = await client.get("/blub")
+        assert r.status_code == 200
+        assert r.text == "Hello from /blub"
