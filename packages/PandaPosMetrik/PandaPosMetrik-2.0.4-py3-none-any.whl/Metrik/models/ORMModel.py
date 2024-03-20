9d67cc6b9626fc9638804eb541edd63dc74bfa60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 03:03:58 +0000

    Add package PandaPosMetrik/2.0.4/PandaPosMetrik-2.0.4-py3-none-any.whl

---
+++ packages/PandaPosMetrik/PandaPosMetrik-2.0.4-py3-none-any.whl/Metrik/models/ORMModel.py
@@ -0,0 +1,32 @@
+from Metrik.models.OrmManager import OrmManager
+from typing import ClassVar, TypeVar, get_type_hints
+import datetime
+from typing_extensions import Self
+T = TypeVar('T', bound='Model')
+
+class Model(object):
+    objects: ClassVar[OrmManager[Self]]
+    
+    
+    
+    def __get__(self, instance, owner):
+        self.owner = owner
+        self.objects = OrmManager()
+        return self
+    
+    def __init__(self, **kwargs) -> None:
+        self.__dict__.update(kwargs)
+        
+        hints = get_type_hints(self)
+        # Sonra tarih tipindekileri dönüştürün
+        for key, hint_type in hints.items():
+            if hint_type == datetime.datetime:
+                attr_value = getattr(self, key, None)  # Varsayılan olarak None dön
+                if attr_value and isinstance(attr_value, str):  # Değer string ve atanmışsa
+                    try:
+                        # Tarih formatı kontrolü ve dönüştürme
+                        setattr(self, key, datetime.datetime.fromisoformat(attr_value))
+                    except ValueError:
+                        # Geçersiz tarih formatı, uygun bir hata yönetimi ekleyin
+                        pass
+            
