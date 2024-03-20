17c625c41a8b63607aca5c1313e88b671e964199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 04:04:42 +0000

    Add package querky/0.1.2/querky-0.1.2.tar.gz

---
+++ packages/querky/querky-0.1.2.tar.gz/querky-0.1.2/querky/type_constructors/__init__.py
@@ -0,0 +1,10 @@
+from .dataclass import DataclassConstructor
+from .typeddict import TypedDictConstructor
+from .namedtuple import NamedTupleTypeConstructor
+
+
+__all__ = [
+    "DataclassConstructor",
+    "TypedDictConstructor",
+    "NamedTupleTypeConstructor"
+]
