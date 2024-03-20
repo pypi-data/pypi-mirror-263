fdb861da61e54876f4ee271a99183634b109611c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 04:04:10 +0000

    Add package vectice/24.1.8.1/vectice-24.1.8.1.tar.gz

---
+++ packages/vectice/vectice-24.1.8.1.tar.gz/vectice-24.1.8.1/src/vectice/models/resource/metadata/pyspark_typing.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from pyspark.sql import Column
+    from pyspark.sql._typing import ColumnOrName  # pyright: ignore[reportPrivateUsage]
+
+
+def expr(str: str) -> Column:
+    ...
+
+
+def avg(col: ColumnOrName) -> Column:
+    ...
