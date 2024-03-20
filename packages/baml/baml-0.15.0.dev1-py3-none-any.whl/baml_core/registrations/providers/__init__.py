96bd21ea3d97e05033736b4c6144bb00f932bbfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 14:04:21 +0000

    Add package baml/0.15.0.dev1/baml-0.15.0.dev1-py3-none-any.whl

---
+++ packages/baml/baml-0.15.0.dev1-py3-none-any.whl/baml_core/registrations/providers/__init__.py
@@ -0,0 +1,22 @@
+# type: ignore
+from .anthropic_provider import AnthropicProvider
+from .anthropic_chat_provider import AnthropicChatProvider
+import openai
+from packaging.version import parse as parse_version
+from .fallback_provider import FallbackProvider
+
+if parse_version(version=openai.__version__) < parse_version("1.0.0"):
+    from .openai_chat_provider import OpenAIChatProvider
+    from .openai_completion_provider import OpenAICompletionProvider
+else:
+    from .openai_chat_provider_1 import OpenAIChatProvider
+    from .openai_completion_provider_1 import OpenAICompletionProvider
+
+
+__all__ = [
+    "FallbackProvider",
+    "AnthropicProvider",
+    "AnthropicChatProvider",
+    "OpenAIChatProvider",
+    "OpenAICompletionProvider",
+]
