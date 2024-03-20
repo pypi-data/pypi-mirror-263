2ef3049b79c8a80b98f879b17c6c999987c7f4de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 06:03:59 +0000

    Add package mdsconnector/1.8.1/mdsconnector-1.8.1.tar.gz

---
+++ packages/mdsconnector/mdsconnector-1.8.1.tar.gz/mdsconnector-1.8.1/src/__init__.py
@@ -0,0 +1,10 @@
+def _import(name, level=1):
+    try:
+        if not __package__:
+            return __import__(name, globals())
+        return __import__(name, globals(), level=level)
+    except:
+        return __import__(name, globals())
+
+
+mdsConnector = _import("mdsconnector").mdsConnector
