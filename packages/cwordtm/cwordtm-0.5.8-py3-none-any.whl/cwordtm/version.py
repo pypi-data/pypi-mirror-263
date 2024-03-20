dd6c2652a283496517099295c430c1f1857c62db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 16:04:38 +0000

    Add package cwordtm/0.5.8/cwordtm-0.5.8-py3-none-any.whl

---
+++ packages/cwordtm/cwordtm-0.5.8-py3-none-any.whl/cwordtm/version.py
@@ -0,0 +1,38 @@
+# version.py
+#    
+# Version information of the module CWordTM
+#
+# Copyright (c) 2024 - NLP 2024 <nlp202403@gmail.com>
+#
+# Updated: 20 March 2024
+#
+# URL: https://github.com/nlp202403/cwordtm.git
+# For license information, see LICENSE.TXT
+
+__author__ = "????"
+"""Name of the author of the package
+"""
+
+__copyright__ = "Copyright (c) 2024 - ????"
+"""Copyright information
+"""
+
+__credits__ = ["Jehovah, the Lord"]
+"""Credit information
+"""
+
+__version__ = "0.5.8"
+"""Version information
+"""
+
+__email__ = "nlp202403@gmail.com"
+"""Author's email address
+"""
+
+__url__ = "https://github.com/nlp202403/cwordtm.git"
+"""GitHub repository for the package
+"""
+
+__docs__ = "https://cwordtm.readthedocs.io"
+"""Package documentation on "Read the Docs" website
+"""
