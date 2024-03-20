84ed087e177b66c09a62284912eea4afba606fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 04:03:52 +0000

    Add package btc-address-dump/0.5.0/btc_address_dump-0.5.0-py2.py3-none-any.whl

---
+++ packages/btc-address-dump/btc_address_dump-0.5.0-py2.py3-none-any.whl/btc_address_dump/p2tr_util.py
@@ -0,0 +1,80 @@
+import hashlib
+import os
+import sys
+from typing import Optional
+
+import ecdsa
+from ecdsa.ellipticcurve import PointJacobi, Point
+
+file_path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.abspath(file_path))
+
+import common_util
+
+
+def bytes_from_int(x: int) -> bytes:
+    return x.to_bytes(32, byteorder="big")
+
+
+def int_from_bytes(b: bytes) -> int:
+    return int.from_bytes(b, byteorder="big")
+
+
+def tagged_hash(tag: str, msg: bytes) -> bytes:
+    tag_hash = hashlib.sha256(tag.encode()).digest()
+    return hashlib.sha256(tag_hash + tag_hash + msg).digest()
+
+
+def lift_x(x: bytes) -> Optional[PointJacobi]:
+    """
+    Define in BIP 340, see https://github.com/bitcoin/bips/blob/master/bip-0340.mediawiki
+
+    :param x: an integer in range 0..p-1,
+    :return: the point P for which P.x() = x and has_even_y(P), or None if no such point exists.
+    """
+    p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F
+    x = int_from_bytes(x)
+    if x >= p:
+        return None
+    y_sq = (pow(x, 3, p) + 7) % p
+    y = pow(y_sq, (p + 1) // 4, p)
+    if pow(y, 2, p) != y_sq:
+        return None
+    P: PointJacobi = PointJacobi.from_affine(Point(ecdsa.curves.SECP256k1.curve, x, y if y % 2 == 0 else p - y))
+    return P
+
+
+def has_even_y(P: PointJacobi) -> bool:
+    return common_util.get_y(P) % 2 == 0
+
+
+def taproot_tweak_seckey(seckey0: bytes) -> bytes:
+    """
+    See https://github.com/bitcoin/bips/blob/master/bip-0341.mediawiki
+    :param seckey0: the private key
+    :return: tweaked private key
+    """
+    P = int_from_bytes(seckey0) * ecdsa.curves.SECP256k1.generator
+    seckey = int_from_bytes(seckey0) if has_even_y(P) else ecdsa.curves.SECP256k1.order - int_from_bytes(seckey0)
+    t = int_from_bytes(tagged_hash("TapTweak", bytes_from_int(common_util.get_x(P))))
+    if t >= ecdsa.curves.SECP256k1.order:
+        raise ValueError
+    return bytes_from_int(int((seckey + t) % ecdsa.curves.SECP256k1.order))
+
+
+def public_key_x_coordinate_to_taproot_tweaked_pubkey(public_key_x_coordinate: bytes) -> bytes:
+    """
+    See https://github.com/bitcoin/bips/blob/master/bip-0086.mediawiki
+
+    P:                  lift_x(public_key_x_coordinate)
+    internal_key:       P.x()
+    Q:                  P + int(HashTapTweak(bytes(P.x()))) * G
+    32_byte_output_key: bytes(Q.x())
+    """
+    P: PointJacobi = lift_x(public_key_x_coordinate)
+    assert P is not None
+    t = int_from_bytes(tagged_hash("TapTweak", bytes_from_int(common_util.get_x(P))))
+    if t >= ecdsa.curves.SECP256k1.order:
+        raise ValueError
+    Q: PointJacobi = P + t * ecdsa.curves.SECP256k1.generator
+    return bytes_from_int(common_util.get_x(Q))
---
+++ packages/btc-address-dump/btc_address_dump-0.5.0-py2.py3-none-any.whl/btc_address_dump/wif_util.py
@@ -0,0 +1,112 @@
+import hashlib
+import groestlcoin_hash2
+import base58
+import os
+import sys
+from typing import Union
+
+file_path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.abspath(file_path))
+
+import base58grs
+
+
+def scrub_input(hex_str_or_bytes: Union[str, bytes]) -> bytes:
+    if isinstance(hex_str_or_bytes, str):
+        hex_str_or_bytes = bytes.fromhex(hex_str_or_bytes)
+    return hex_str_or_bytes
+
+
+# wallet import format key - base58 encoded format
+# https://bitcoin.stackexchange.com/questions/9244/private-key-to-wif
+def encode_wif(private_key: Union[str, bytes], version: bytes, compressed_wif: bool = False) -> bytes:
+    """ Encode wif format private key """
+    private_key = scrub_input(private_key)
+
+    # prepended version byte to private key
+    private_key_with_version = version + private_key
+    if compressed_wif:
+        private_key_with_version = version + private_key + b'\x01'
+    # perform SHA-256 hash on the mainnet_private_key
+    sha256 = hashlib.sha256()
+    sha256.update(private_key_with_version)
+    hash_bytes = sha256.digest()
+
+    # perform SHA-256 on the previous SHA-256 hash
+    sha256 = hashlib.sha256()
+    sha256.update(hash_bytes)
+    hash_bytes = sha256.digest()
+
+    # create a checksum using the first 4 bytes of the previous SHA-256 hash
+    # append the 4 checksum bytes to the mainnet_private_key
+    checksum = hash_bytes[:4]
+
+    # print('checksum', checksum.hex())
+    hash_bytes = private_key_with_version + checksum
+    # print('hash', hash_bytes.hex())
+
+    # convert private_key_with_version + checksum into base58 encoded string
+    return base58.b58encode(hash_bytes)
+
+
+def grs_encode_wif(private_key: Union[str, bytes], version: bytes, compressed_wif: bool = False) -> bytes:
+    """ Encode wif format private key """
+    private_key = scrub_input(private_key)
+
+    # prepended version byte to private key
+    private_key_with_version = version + private_key
+    if compressed_wif:
+        private_key_with_version = version + private_key + b'\x01'
+    # perform DOUBLE GROESTL-512 hash on the mainnet_private_key
+    hash_bytes = groestlcoin_hash2.groestl_hash(private_key_with_version)
+
+    # create a checksum using the first 4 bytes of the GROESTL-512 hash
+    # append the 4 checksum bytes to the mainnet_private_key
+    checksum = hash_bytes[:4]
+
+    # print('checksum', checksum.hex())
+    hash_bytes = private_key_with_version + checksum
+    # print('hash', hash_bytes.hex())
+
+    # convert private_key_with_version + checksum into base58 encoded string
+    return base58grs.b58encode(hash_bytes)
+
+
+def decode_wif(wif: str, compressed_wif: bool = False) -> bytes:
+    """ Decode wif format private key """
+    decoded = base58.b58decode(wif)
+    if len(decoded) == 38 or len(decoded) == 37:
+        private_key = decoded[1:33]  # WIF-compressed: [0x80 xxxx 0x01 checksum] or WIF: [0x80 xxxx checksum]
+    else:
+        raise ValueError("invalid WIF")
+    return private_key
+
+
+def grs_decode_wif(wif: str, compressed_wif: bool = False) -> bytes:
+    """ Decode wif format private key """
+    decoded = base58grs.b58decode(wif)
+    if len(decoded) == 38 or len(decoded) == 37:
+        private_key = decoded[1:33]  # WIF-compressed: [0x80 xxxx 0x01 checksum] or WIF: [0x80 xxxx checksum]
+    else:
+        raise ValueError("invalid WIF")
+    return private_key
+
+
+def is_valid_wif(wif: str, chain: str) -> bool:
+    """ Check if wif is valid"""
+    try:
+        if chain == "grs" or chain == "grs_testnet":
+            result = base58grs.b58decode(wif)
+            #print(result)
+            result, check = result[:-4], result[-4:]
+            # DOUBLE GROESTL-512 hash
+            digest = groestlcoin_hash2.groestl_hash(result)
+        else:
+            result = base58.b58decode(wif)
+            #print(result)
+            result, check = result[:-4], result[-4:]
+            # DOUBLE sha256 hash
+            digest = hashlib.sha256(hashlib.sha256(result).digest()).digest()
+        return check == digest[:4]
+    except ValueError:
+        return False
