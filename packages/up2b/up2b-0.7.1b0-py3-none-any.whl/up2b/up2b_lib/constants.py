759bbc65bf4fc3f3f8ea3f419dee72c5aeb1d986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 15:03:16 +0000

    Add package up2b/0.7.1b0/up2b-0.7.1b0-py3-none-any.whl

---
+++ packages/up2b/up2b-0.7.1b0-py3-none-any.whl/up2b/up2b_lib/constants.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Author:      thepoy
+# @Email:       thepoy@163.com
+# @File Name:   constants.py
+# @Created At:  2021-02-13 09:17:07
+# @Modified At: 2023-04-19 14:42:31
+# @Modified By: thepoy
+
+import sys
+import os
+
+from enum import IntEnum
+from pathlib import Path
+from tempfile import gettempdir
+
+PYTHON_VERSION = sys.version_info
+
+
+class ImageBedCode(IntEnum):
+    SM_MS = 0
+    IMGTU = 1
+    GITHUB = 2
+    # CODING = 3 # 废弃
+    IMGTG = 4
+
+    def __repr__(self) -> str:
+        return str(self.value)
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+
+DEFAULT_TIMEOUT = 10.0
+
+# fmt: off
+IMAGE_BEDS_CODE = {
+    "sm.ms":      ImageBedCode.SM_MS,
+    "imgse.com":  ImageBedCode.IMGTU,
+    "github.com": ImageBedCode.GITHUB,
+    "imgtg.com": ImageBedCode.IMGTG,
+}
+# fmt: on
+
+
+IMAGE_BEDS_NAME = {v: k for k, v in IMAGE_BEDS_CODE.items()}
+
+IMAGE_BEDS_HELP_MESSAGE = "\n\n".join([f"{v}: {k}" for k, v in IMAGE_BEDS_CODE.items()])
+
+
+IS_WINDOWS = sys.platform == "win32"
+IS_MACOS = sys.platform == "darwin"
+
+CONFIG_FOLDER_PATH = (
+    Path(os.environ["APPDATA"]) / "up2b"
+    if IS_WINDOWS
+    else Path(os.environ["HOME"]) / ".config" / "up2b"
+)
+
+if not CONFIG_FOLDER_PATH.exists():
+    os.makedirs(CONFIG_FOLDER_PATH / "conf", 0o755)
+
+CONF_FILE = CONFIG_FOLDER_PATH / "conf" / "conf.up2b.json"
+
+if not CONF_FILE.parent.exists():
+    CONF_FILE.parent.mkdir(parents=True)
+
+CACHE_PATH = Path(gettempdir()) / "up2b"
+CACHE_DATABASE = CONFIG_FOLDER_PATH / "cache.db"
---
+++ packages/up2b/up2b-0.7.1b0-py3-none-any.whl/up2b/up2b_lib/errors.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Author:      thepoy
+# @Email:       thepoy@163.com
+# @File Name:   errors.py
+# @Created At:  2021-02-08 17:04:25
+# @Modified At: 2023-02-21 12:41:08
+# @Modified By: thepoy
+
+
+class UploadFailed(Exception):
+    pass
+
+
+class OverSizeError(ValueError):
+    pass
+
+
+class NotLogin(NotImplementedError):
+    pass
+
+
+class UnsupportedType(TypeError):
+    pass
+
+
+class MissingAuth(ValueError):
+    pass
+
+
+class Timeout(Exception):
+    pass
