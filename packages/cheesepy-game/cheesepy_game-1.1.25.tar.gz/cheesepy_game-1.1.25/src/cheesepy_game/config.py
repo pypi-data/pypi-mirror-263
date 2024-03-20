934f9854df26c110e282696cc9d14deacde4cae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 06:04:53 +0000

    Add package cheesepy-game/1.1.25/cheesepy_game-1.1.25.tar.gz

---
+++ packages/cheesepy-game/cheesepy_game-1.1.25.tar.gz/cheesepy_game-1.1.25/src/cheesepy_game/config.py
@@ -0,0 +1,12 @@
+screen = None
+GRID_ROWS = None
+GRID_COLS = None
+DEFAULT_CELL_SIDE = 50
+FRAMES_PER_SECOND = 25
+show_grid_borders = True
+def check_victory(): return False
+
+
+cell_side = 50
+
+verbose = True
