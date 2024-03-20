f91fcadd400e275e952444c9bc5a13da108c0800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 05:03:31 +0000

    Add package langchain_chinese/0.2.25/langchain_chinese-0.2.25-py3-none-any.whl

---
+++ packages/langchain_chinese/langchain_chinese-0.2.25-py3-none-any.whl/langchain_chinese/__init__.py
@@ -0,0 +1,28 @@
+from langchain_chinese.document_loaders.base import LocalFilesLoader
+from langchain_chinese.retrievers.base import (
+    AskDocumentTool,
+    create_qa_chain,
+    create_qa_toolkits
+)
+
+from langchain_chinese.agents.base import (
+    PROMPT_REACT,
+    PROMPT_COT,
+    create_reason_agent,
+)
+
+from langchain_chinese.memory import WithMemoryBinding, MemoryManager
+
+from langchain_chinese.__version__ import __version__
+
+__all__ = [
+    "LocalFilesLoader",
+    "WithMemoryBinding",
+    "AskDocumentTool",
+    "create_qa_chain",
+    "create_qa_toolkits",
+    
+    "PROMPT_REACT",
+    "PROMPT_COT",
+    "create_reason_agent",
+]
