0024b1926136fe905270fb1ecd607228bd44d9fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 04:04:04 +0000

    Add package vistutils/0.1.41/vistutils-0.1.41-py3-none-any.whl

---
+++ packages/vistutils/vistutils-0.1.41-py3-none-any.whl/vistutils/metas/__init__.py
@@ -0,0 +1,9 @@
+"""Extension of vistutils metas package"""
+#  MIT Licence
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
+
+from ._type_names import Bases, Namespace
+from ._abstract_namespace import AbstractNamespace
+from ._base_namespace import BaseNamespace
+from ._abstract_metaclass import AbstractMetaclass
