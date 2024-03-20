0f7fee630b500064eed7b704903f7a696eee80dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 16:04:54 +0000

    Add package nithinitsscbe/0.1/nithinitsscbe-0.1.tar.gz

---
+++ packages/nithinitsscbe/nithinitsscbe-0.1.tar.gz/nithinitsscbe-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='nithinitsscbe', 
+ version='0.1',
+ author="Nithingowda A M",
+ author_email="AMNithingowda@itssglobal.com",
+ description="package check in server",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 2.7",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
