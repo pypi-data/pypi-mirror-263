45bb745b51cb0536aab7427faa1cda2989f2366b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 03:03:50 +0000

    Add package coghotswap/0.0.2/coghotswap-0.0.2.tar.gz

---
+++ packages/coghotswap/coghotswap-0.0.2.tar.gz/coghotswap-0.0.2/coghotswap/__version__.py
@@ -0,0 +1,13 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+Information about the current version of the coghotswap package.
+"""
+
+__title__ = "coghotswap"
+__description__ = "Allow hot reloading of extensions for Pycord a fork of discord.py"
+__version__ = "0.0.2"
+__author__ = "TropicalShadow"
+__author_email__ = "me@tesseract.club"
+__license__ = "The MIT License (MIT)"
+__url__ = "https://github.com/TropicalShadow/coghotswap"
