163c373a181686338c88ea88b4c48c5c184ecec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 06:04:55 +0000

    Add package maap-algorithms-jupyter-extension/0.3.0/maap_algorithms_jupyter_extension-0.3.0-py3-none-any.whl

---
+++ packages/maap-algorithms-jupyter-extension/maap_algorithms_jupyter_extension-0.3.0-py3-none-any.whl/maap_algorithms_jupyter_extension-0.3.0.data/data/share/jupyter/labextensions/maap_algorithms_jupyter_extension/static/style_index_js.9e64cb67940c31604447.js
@@ -0,0 +1,73 @@
+"use strict";
+(self["webpackChunkmaap_algorithms_jupyter_extension"] = self["webpackChunkmaap_algorithms_jupyter_extension"] || []).push([["style_index_js"],{
+
+/***/ "./node_modules/css-loader/dist/cjs.js!./style/base.css":
+/*!**************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./style/base.css ***!
+  \**************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/cssWithMappingToString.js */ "./node_modules/css-loader/dist/runtime/cssWithMappingToString.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+// Imports
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default()));
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, ":root {\n    --ADE-white: white;\n    --ADE-light-grey: #F8F9FA;\n    --ADE-dark-grey: #DEE2E6;\n    --ADE-hover: #E7F1FF;\n    --ADE-contexts-success: #198754;\n    --ADE-contexts-danger: #DC3545;\n    ;\n\n    --ADE-input-width: 500px;\n}\n\n.jl-ReactAppWidget {\n    overflow: auto;\n}\n\n.Pane {\n    overflow: auto;\n  }\n\n.Resizer {\n    background: #000;\n    opacity: 0.2;\n    z-index: 1;\n    -moz-box-sizing: border-box;\n    -webkit-box-sizing: border-box;\n    box-sizing: border-box;\n    -moz-background-clip: padding;\n    -webkit-background-clip: padding;\n    background-clip: padding-box;\n  }\n   \n.Resizer:hover {\n    -webkit-transition: all 2s ease;\n    transition: all 2s ease;\n}\n   \n.Resizer.horizontal {\n    height: 11px;\n    margin: -5px 0;\n    border-top: 5px solid rgba(255, 255, 255, 0);\n    border-bottom: 5px solid rgba(255, 255, 255, 0);\n    cursor: row-resize;\n    width: 100%;\n}\n\n.Resizer.horizontal:hover {\n    border-top: 5px solid rgba(0, 0, 0, 0.5);\n    border-bottom: 5px solid rgba(0, 0, 0, 0.5);\n}\n\n.pagination {\n    display: flex;\n    justify-content: space-between;\n    align-items: baseline;\n    margin: 0 1rem;\n}\n\n.pagination-footer {\n    position: absolute;\n    bottom: 10px;\n}\n\n.relative {\n    position: relative !important;\n}\n\nform {\n    margin: 1rem;\n}\n\n.flex {\n    display: flex;\n    align-items: center;\n    gap: 0.5rem;\n}\n\ntable {\n    border: 1px solid;\n    --bs-table-bg: inherit !important;\n}\n\n.hover > tbody tr:hover {\n    background-color: var(--ADE-hover);\n}\n\ntd {\n    align-items: center;\n}\n\nthead td {\n    font-weight: bold;\n}\n\nthead>tr:hover {\n    background-color: var(--bs-table-bg) !important;\n}\n\ntbody tr:nth-child(even) {\n    background-color: var(--ADE-light-grey);\n}\n  \ntbody tr:nth-child(odd) {\n    background-color: var(--ADE-white);\n}\n\n/* tbody tr:hover {\n    background-color: var(--ADE-hover);\n} */\n\n.form-table {\n    width: auto !important;\n    vertical-align: middle !important;\n}\n\n/* .form-table td {\n    padding: 0.5rem;\n} */\n\n.form-table input {\n    width: var(--ADE-input-width);\n}\n\n.content-padding, .section-padding {\n    padding: 1rem 0\n}\n  \n.hide-content {\n    overflow: hidden;\n    max-width: 80vw;\n    text-overflow: ellipsis;\n}\n\n.success-icon {\n    color: var(--ADE-contexts-success) !important;\n}\n\n.danger-icon {\n    color: var(--ADE-contexts-danger) !important;\n}\n\n.inputs-table > thead td {\n    padding: 0.5rem 1rem;\n}\n\n.input-row td {\n    padding: 0.5rem 1rem;\n    vertical-align: middle;\n}\n\n.input-row td:last-of-type {\n    padding: inherit;\n}\n\n.show {\n    display: inherit;\n}\n\n.hide {\n    display: none;\n}\n\n.empty-row {\n    text-align: center;\n    font-style: italic;\n    background-color: var(--ADE-light-grey) !important;\n}\n\nsvg:hover {\n    cursor: pointer;\n}\n\n.input-types {\n    display: flex;\n}\n\n.input-types > * {\n    margin-right: 1rem;\n    margin-top: 2rem;\n}\n\n.center-align {\n    text-align: center;\n}\n\n.row-actions > *{\n    margin: 0 0.5rem;\n}\n\n.alert-box {\n    width: max-content;\n}\n\n.alert-box > svg {\n    margin-right: 1rem;\n}\n\n.margin-right-1 {\n    margin-right: 1rem;\n}\n\n\n", "",{"version":3,"sources":["webpack://./style/base.css"],"names":[],"mappings":"AAAA;IACI,kBAAkB;IAClB,yBAAyB;IACzB,wBAAwB;IACxB,oBAAoB;IACpB,+BAA+B;IAC/B,8BAA8B;;;IAG9B,wBAAwB;AAC5B;;AAEA;IACI,cAAc;AAClB;;AAEA;IACI,cAAc;EAChB;;AAEF;IACI,gBAAgB;IAChB,YAAY;IACZ,UAAU;IACV,2BAA2B;IAC3B,8BAA8B;IAC9B,sBAAsB;IACtB,6BAA6B;IAC7B,gCAAgC;IAChC,4BAA4B;EAC9B;;AAEF;IACI,+BAA+B;IAC/B,uBAAuB;AAC3B;;AAEA;IACI,YAAY;IACZ,cAAc;IACd,4CAA4C;IAC5C,+CAA+C;IAC/C,kBAAkB;IAClB,WAAW;AACf;;AAEA;IACI,wCAAwC;IACxC,2CAA2C;AAC/C;;AAEA;IACI,aAAa;IACb,8BAA8B;IAC9B,qBAAqB;IACrB,cAAc;AAClB;;AAEA;IACI,kBAAkB;IAClB,YAAY;AAChB;;AAEA;IACI,6BAA6B;AACjC;;AAEA;IACI,YAAY;AAChB;;AAEA;IACI,aAAa;IACb,mBAAmB;IACnB,WAAW;AACf;;AAEA;IACI,iBAAiB;IACjB,iCAAiC;AACrC;;AAEA;IACI,kCAAkC;AACtC;;AAEA;IACI,mBAAmB;AACvB;;AAEA;IACI,iBAAiB;AACrB;;AAEA;IACI,+CAA+C;AACnD;;AAEA;IACI,uCAAuC;AAC3C;;AAEA;IACI,kCAAkC;AACtC;;AAEA;;GAEG;;AAEH;IACI,sBAAsB;IACtB,iCAAiC;AACrC;;AAEA;;GAEG;;AAEH;IACI,6BAA6B;AACjC;;AAEA;IACI;AACJ;;AAEA;IACI,gBAAgB;IAChB,eAAe;IACf,uBAAuB;AAC3B;;AAEA;IACI,6CAA6C;AACjD;;AAEA;IACI,4CAA4C;AAChD;;AAEA;IACI,oBAAoB;AACxB;;AAEA;IACI,oBAAoB;IACpB,sBAAsB;AAC1B;;AAEA;IACI,gBAAgB;AACpB;;AAEA;IACI,gBAAgB;AACpB;;AAEA;IACI,aAAa;AACjB;;AAEA;IACI,kBAAkB;IAClB,kBAAkB;IAClB,kDAAkD;AACtD;;AAEA;IACI,eAAe;AACnB;;AAEA;IACI,aAAa;AACjB;;AAEA;IACI,kBAAkB;IAClB,gBAAgB;AACpB;;AAEA;IACI,kBAAkB;AACtB;;AAEA;IACI,gBAAgB;AACpB;;AAEA;IACI,kBAAkB;AACtB;;AAEA;IACI,kBAAkB;AACtB;;AAEA;IACI,kBAAkB;AACtB","sourcesContent":[":root {\n    --ADE-white: white;\n    --ADE-light-grey: #F8F9FA;\n    --ADE-dark-grey: #DEE2E6;\n    --ADE-hover: #E7F1FF;\n    --ADE-contexts-success: #198754;\n    --ADE-contexts-danger: #DC3545;\n    ;\n\n    --ADE-input-width: 500px;\n}\n\n.jl-ReactAppWidget {\n    overflow: auto;\n}\n\n.Pane {\n    overflow: auto;\n  }\n\n.Resizer {\n    background: #000;\n    opacity: 0.2;\n    z-index: 1;\n    -moz-box-sizing: border-box;\n    -webkit-box-sizing: border-box;\n    box-sizing: border-box;\n    -moz-background-clip: padding;\n    -webkit-background-clip: padding;\n    background-clip: padding-box;\n  }\n   \n.Resizer:hover {\n    -webkit-transition: all 2s ease;\n    transition: all 2s ease;\n}\n   \n.Resizer.horizontal {\n    height: 11px;\n    margin: -5px 0;\n    border-top: 5px solid rgba(255, 255, 255, 0);\n    border-bottom: 5px solid rgba(255, 255, 255, 0);\n    cursor: row-resize;\n    width: 100%;\n}\n\n.Resizer.horizontal:hover {\n    border-top: 5px solid rgba(0, 0, 0, 0.5);\n    border-bottom: 5px solid rgba(0, 0, 0, 0.5);\n}\n\n.pagination {\n    display: flex;\n    justify-content: space-between;\n    align-items: baseline;\n    margin: 0 1rem;\n}\n\n.pagination-footer {\n    position: absolute;\n    bottom: 10px;\n}\n\n.relative {\n    position: relative !important;\n}\n\nform {\n    margin: 1rem;\n}\n\n.flex {\n    display: flex;\n    align-items: center;\n    gap: 0.5rem;\n}\n\ntable {\n    border: 1px solid;\n    --bs-table-bg: inherit !important;\n}\n\n.hover > tbody tr:hover {\n    background-color: var(--ADE-hover);\n}\n\ntd {\n    align-items: center;\n}\n\nthead td {\n    font-weight: bold;\n}\n\nthead>tr:hover {\n    background-color: var(--bs-table-bg) !important;\n}\n\ntbody tr:nth-child(even) {\n    background-color: var(--ADE-light-grey);\n}\n  \ntbody tr:nth-child(odd) {\n    background-color: var(--ADE-white);\n}\n\n/* tbody tr:hover {\n    background-color: var(--ADE-hover);\n} */\n\n.form-table {\n    width: auto !important;\n    vertical-align: middle !important;\n}\n\n/* .form-table td {\n    padding: 0.5rem;\n} */\n\n.form-table input {\n    width: var(--ADE-input-width);\n}\n\n.content-padding, .section-padding {\n    padding: 1rem 0\n}\n  \n.hide-content {\n    overflow: hidden;\n    max-width: 80vw;\n    text-overflow: ellipsis;\n}\n\n.success-icon {\n    color: var(--ADE-contexts-success) !important;\n}\n\n.danger-icon {\n    color: var(--ADE-contexts-danger) !important;\n}\n\n.inputs-table > thead td {\n    padding: 0.5rem 1rem;\n}\n\n.input-row td {\n    padding: 0.5rem 1rem;\n    vertical-align: middle;\n}\n\n.input-row td:last-of-type {\n    padding: inherit;\n}\n\n.show {\n    display: inherit;\n}\n\n.hide {\n    display: none;\n}\n\n.empty-row {\n    text-align: center;\n    font-style: italic;\n    background-color: var(--ADE-light-grey) !important;\n}\n\nsvg:hover {\n    cursor: pointer;\n}\n\n.input-types {\n    display: flex;\n}\n\n.input-types > * {\n    margin-right: 1rem;\n    margin-top: 2rem;\n}\n\n.center-align {\n    text-align: center;\n}\n\n.row-actions > *{\n    margin: 0 0.5rem;\n}\n\n.alert-box {\n    width: max-content;\n}\n\n.alert-box > svg {\n    margin-right: 1rem;\n}\n\n.margin-right-1 {\n    margin-right: 1rem;\n}\n\n\n"],"sourceRoot":""}]);
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+
+/***/ "./style/index.js":
+/*!************************!*\
+  !*** ./style/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _base_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./base.css */ "./style/base.css");
+
+
+
+/***/ }),
+
+/***/ "./style/base.css":
+/*!************************!*\
+  !*** ./style/base.css ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ "./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js");
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !!../node_modules/css-loader/dist/cjs.js!./base.css */ "./node_modules/css-loader/dist/cjs.js!./style/base.css");
+
+            
+
+var options = {};
+
+options.insert = "head";
+options.singleton = false;
+
+var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__["default"], options);
+
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__["default"].locals || {});
+
+/***/ })
+
+}]);
+//# sourceMappingURL=style_index_js.9e64cb67940c31604447.js.map
