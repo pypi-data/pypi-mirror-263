cbec0fb663fbaa58451683afaea5f021b854c9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 03:04:12 +0000

    Add package aps-toolkit/0.3.8/aps_toolkit-0.3.8-py3-none-any.whl

---
+++ packages/aps-toolkit/aps_toolkit-0.3.8-py3-none-any.whl/test/test_prop_reader.py
@@ -0,0 +1,35 @@
+﻿from unittest import TestCase
+import os
+from .context import PropReader
+from .context import Auth
+
+
+class TestPropDbReader(TestCase):
+    def setUp(self):
+        self.urn = "dXJuOmFkc2sud2lwcHJvZDpmcy5maWxlOnZmLlhUOFFRSk53UXhpTFE2VE1QbmZRTkE_dmVyc2lvbj0x"
+        self.token = Auth().auth2leg()
+        self.prop_reader = PropReader(self.urn, self.token)
+
+    def test_enumerate_properties(self):
+        properties = self.prop_reader.enumerate_properties(1)
+        self.assertNotEquals(properties, 0)
+
+    def test_get_properties(self):
+        properties = self.prop_reader.get_properties(1)
+        self.assertNotEquals(properties, 0)
+
+    def test_get_all_properties(self):
+        properties = self.prop_reader.get_all_properties(1)
+        self.assertNotEquals(properties, 0)
+
+    def test_get_instance(self):
+        instance = self.prop_reader.get_instance(1)
+        self.assertEquals(len(instance), 0)
+
+    def test_get_children(self):
+        children = self.prop_reader.get_children(1)
+        self.assertNotEquals(len(children), 0)
+
+    def test_get_parent(self):
+        parent = self.prop_reader.get_parent(1)
+        self.assertEquals(len(parent), 0)
