926185362519500357ba27190669a350d68abe73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 07:03:51 +0000

    Add package mosaicml-cli/0.6.16/mosaicml-cli-0.6.16.tar.gz

---
+++ packages/mosaicml-cli/mosaicml-cli-0.6.16.tar.gz/mosaicml-cli-0.6.16/mcli/cli/m_diff/__init__.py
@@ -0,0 +1,4 @@
+"""Re-export cli getters"""
+# Re-exporting to make it easier to import in one place
+# pylint: disable=useless-import-alias
+from mcli.cli.m_diff.diff_runs import diff_runs as diff_runs
