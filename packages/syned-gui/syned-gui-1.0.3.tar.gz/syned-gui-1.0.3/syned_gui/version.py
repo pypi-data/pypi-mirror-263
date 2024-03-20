383d1a836c6363aeb8b5d5e3697143b591f9e9fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 06:05:15 +0000

    Add package syned-gui/1.0.3/syned-gui-1.0.3.tar.gz

---
+++ packages/syned-gui/syned-gui-1.0.3.tar.gz/syned-gui-1.0.3/syned_gui/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM syned SETUP.PY
+short_version = '1.0.3'
+version = '1.0.3'
+full_version = '1.0.3'
+git_revision = ''
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
