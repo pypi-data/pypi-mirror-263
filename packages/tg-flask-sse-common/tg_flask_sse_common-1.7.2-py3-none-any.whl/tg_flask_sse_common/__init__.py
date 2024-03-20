5b26fd822d5c8708addeaa9a78403b09244f89da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 16:04:38 +0000

    Add package tg-flask-sse-common/1.7.2/tg_flask_sse_common-1.7.2-py3-none-any.whl

---
+++ packages/tg-flask-sse-common/tg_flask_sse_common-1.7.2-py3-none-any.whl/tg_flask_sse_common/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+    __init__.py.py
+    ~~~~~~~
+
+    Description.
+
+    :author: Tangshimin
+    :copyright: (c) 2024, Tungee
+    :date created: 2024/02/01
+    :python version: 3.7
+"""
+
+NAME = 'tg_flask_sse_common'
+
+VERSION = '1.7.2'
