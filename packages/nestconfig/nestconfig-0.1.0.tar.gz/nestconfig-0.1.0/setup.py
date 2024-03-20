71a6b9bb1972c9c5eda8d31fb283c15fee8fdc55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 04:04:26 +0000

    Add package nestconfig/0.1.0/nestconfig-0.1.0.tar.gz

---
+++ packages/nestconfig/nestconfig-0.1.0.tar.gz/nestconfig-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nestconfig",
+    version="0.1.0",
+    author="Zeyun Zhong",
+    author_email="zeyun.zhong@kit.edu",
+    description="Type-safe configuration management with support for nested dataclasses",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/zeyun-zhong/nestconfig",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "nestconfig"},
+    packages = find_packages(where="nestconfig"),
+    python_requires='>=3.7',
+)
