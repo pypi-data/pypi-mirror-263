b1983fbfc5ef75db33e9280d9816555aa3199d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 15:03:27 +0000

    Add package ring-attention-pytorch/0.2.8/ring-attention-pytorch-0.2.8.tar.gz

---
+++ packages/ring-attention-pytorch/ring-attention-pytorch-0.2.8.tar.gz/ring-attention-pytorch-0.2.8/ring_attention_pytorch/__init__.py
@@ -0,0 +1,11 @@
+from ring_attention_pytorch.ring_attention import (
+    RingAttention,
+    RingTransformer,
+    RingRotaryEmbedding,
+    apply_rotary_pos_emb
+)
+
+from ring_attention_pytorch.ring_flash_attention import (
+    ring_flash_attn,
+    ring_flash_attn_
+)
