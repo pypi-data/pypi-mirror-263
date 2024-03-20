77103a04e0bdd04c64ce776bb534152e03814463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 16:04:26 +0000

    Add package LdShell/1.3/LdShell-1.3.tar.gz

---
+++ packages/LdShell/LdShell-1.3.tar.gz/LdShell-1.3/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(
+    name='LdShell',
+    version='1.3',
+    description='LdShell',
+    author='Me',
+    author_email='me@example.com',
+    packages=setuptools.find_packages(),
+    install_requires=[]
+)
