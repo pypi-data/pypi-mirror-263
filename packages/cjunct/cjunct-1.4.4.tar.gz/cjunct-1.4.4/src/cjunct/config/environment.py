7300bf33b7714c45481866a1f73c72b8b081c372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 16:04:35 +0000

    Add package cjunct/1.4.4/cjunct-1.4.4.tar.gz

---
+++ packages/cjunct/cjunct-1.4.4.tar.gz/cjunct-1.4.4/src/cjunct/config/environment.py
@@ -0,0 +1,31 @@
+"""Separate environment-centric module"""
+
+import typing as t
+
+from named_env import (
+    EnvironmentNamespace,
+    OptionalString,
+    OptionalTernary,
+    OptionalBoolean,
+    OptionalList,
+)
+
+__all__ = [
+    "Env",
+]
+
+
+class Env(EnvironmentNamespace):
+    """Environment variables"""
+
+    CJUNCT_LOG_LEVEL: str = OptionalString("")
+    CJUNCT_CONTEXT_DIRECTORY: str = OptionalString("")
+    CJUNCT_ACTIONS_SOURCE_FILE: str = OptionalString("")
+    CJUNCT_CONFIG_LOADER_SOURCE_FILE: str = OptionalString("")
+    CJUNCT_DISPLAY_SOURCE_FILE: str = OptionalString("")
+    CJUNCT_STRATEGY_NAME: str = OptionalString("")
+    CJUNCT_FORCE_COLOR: t.Optional[bool] = OptionalTernary(None)  # type: ignore
+    CJUNCT_SHELL_INJECT_YIELD_FUNCTION: bool = OptionalBoolean(True)  # type: ignore
+    CJUNCT_EXTERNAL_MODULES_PATHS: t.List[str] = OptionalList([])
+    CJUNCT_ACTIONS_CLASS_DEFINITIONS_DIRECTORY: t.List[str] = OptionalList([])
+    CJUNCT_STRICT_OUTCOMES_RENDERING: bool = OptionalBoolean(True)  # type: ignore
