34bbde63de1f4d94aea254876b33d53fc9a3d470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 06:04:53 +0000

    Add package p1radup/2.2.5/p1radup-2.2.5.tar.gz

---
+++ packages/p1radup/p1radup-2.2.5.tar.gz/p1radup-2.2.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='p1radup',
+    version='2.2.5',
+    packages=find_packages(),
+    install_requires=[
+        'termcolor',
+        'validators',
+    ],
+    entry_points={
+        'console_scripts': [
+            'p1radup = p1radup.p1radup:main',
+        ],
+    },
+    author='Tarek Bouali',
+    author_email='contact@tarekbouali.com',
+    description='This tool is designed to process a list of URLs from an input file, remove duplicate query parameters, and save the modified URLs to an output file.',
+    url='https://github.com/iambouali/p1radup',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
