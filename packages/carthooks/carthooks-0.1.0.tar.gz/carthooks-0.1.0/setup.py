f295ddbab58238a0e8b7a4064688243213866ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 15:03:22 +0000

    Add package carthooks/0.1.0/carthooks-0.1.0.tar.gz

---
+++ packages/carthooks/carthooks-0.1.0.tar.gz/carthooks-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='carthooks',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A simple example package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Carthooks',
+    author_email='developer@carthooks.com',
+    license='MIT',
+    install_requires=[
+        # 列出你的包的依赖，例如
+        'requests>=2.23.0',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
