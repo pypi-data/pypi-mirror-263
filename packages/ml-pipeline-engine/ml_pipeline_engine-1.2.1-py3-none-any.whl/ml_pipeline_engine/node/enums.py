a60c4dad3b9a15f20c8e32388982396685279988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 16:05:09 +0000

    Add package ml-pipeline-engine/1.2.1/ml_pipeline_engine-1.2.1-py3-none-any.whl

---
+++ packages/ml-pipeline-engine/ml_pipeline_engine-1.2.1-py3-none-any.whl/ml_pipeline_engine/node/enums.py
@@ -0,0 +1,19 @@
+import enum
+
+
+class SerializationNodeKind(str, enum.Enum):
+    """
+    Поддерживаемые типы сериализации узлов между
+    """
+
+    cloudpickle = 'cloudpickle'
+
+
+class NodeTag(str, enum.Enum):
+    """
+    Поддерживаемые теги узлов
+    """
+
+    process = 'process'
+    thread = 'thread'
+    non_async = 'non_async'
