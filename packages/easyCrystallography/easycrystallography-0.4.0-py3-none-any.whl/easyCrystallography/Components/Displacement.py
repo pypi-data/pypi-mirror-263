75b496c84554a4f15ac7823f3a390dc37d76ef7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-20 03:03:05 +0000

    Add package easyCrystallography/0.4.0/easycrystallography-0.4.0-py3-none-any.whl

---
+++ packages/easyCrystallography/easycrystallography-0.4.0-py3-none-any.whl/easyCrystallography/Components/Displacement.py
@@ -0,0 +1,7 @@
+#  SPDX-FileCopyrightText: 2023 easyCrystallography contributors <crystallography@easyscience.software>
+#  SPDX-License-Identifier: BSD-3-Clause
+#  © 2022-2023  Contributors to the easyCore project <https://github.com/easyScience/easyCrystallography>
+
+
+__author__ = 'github.com/wardsimon'
+__version__ = '0.1.0'
---
+++ packages/easyCrystallography/easycrystallography-0.4.0-py3-none-any.whl/easyCrystallography/io/cif/__init__.py
@@ -0,0 +1,13 @@
+__author__ = 'github.com/wardsimon'
+__version__ = '0.0.1'
+
+
+#  SPDX-FileCopyrightText: 2023 easyCrystallography contributors <crystallography@easyscience.software>
+#  SPDX-License-Identifier: BSD-3-Clause
+#  © 2022-2023  Contributors to the easyCore project <https://github.com/easyScience/easyCrystallography>
+
+from .atoms import Atoms
+from .lattice import Lattice
+from .spacegroup import SpaceGroup
+
+__all__ = ['Atoms', 'Lattice', 'SpaceGroup']
