8dab74a74a1af8fb80228958eae0fd31eeec16c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 17:04:17 +0000

    Add package nccapy/0.0.7/nccapy-0.0.7-py3-none-any.whl

---
+++ packages/nccapy/nccapy-0.0.7-py3-none-any.whl/nccapy/Geo/BaseMesh.pyi
@@ -0,0 +1,13 @@
+from typing import Any
+
+class Face:
+    vert: list
+    uv: list
+    normal: list
+
+class BaseMesh:
+    verts: list
+    normals: list
+    uv: list
+    faces: list
+    def __init__(self) -> None: ...
