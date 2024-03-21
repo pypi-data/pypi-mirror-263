62d8e3ed60db63f25ae8bbd8f2dccdbe79389033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 06:04:43 +0000

    Add package anaconda-cloud-internal-permission-client/0.0.1/anaconda-cloud-internal-permission-client-0.0.1.tar.gz

---
+++ packages/anaconda-cloud-internal-permission-client/anaconda-cloud-internal-permission-client-0.0.1.tar.gz/anaconda-cloud-internal-permission-client-0.0.1/README.md
@@ -0,0 +1,4 @@
+# pypi_stub_client
+
+A fake package to prevent name-squatting attacks. This is meant to be uploaded to pypi, so if any code tries to download
+permission_client, it will fail
---
+++ packages/anaconda-cloud-internal-permission-client/anaconda-cloud-internal-permission-client-0.0.1.tar.gz/anaconda-cloud-internal-permission-client-0.0.1/permission_client/__init__.py
@@ -0,0 +1,6 @@
+msg = (
+    "This is the stub package from PyPI. You should not get this version of the package, "
+    "unless you are unable to reach our private channel. Please check your anaconda.org "
+    "download token."
+)
+raise ImportError(msg)
