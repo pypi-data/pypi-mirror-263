a0e2c31f2396bab7eaec36ced462cbd8138af97a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 04:04:21 +0000

    Add package BrightnessBooster/1.0.1/BrightnessBooster-1.0.1.tar.gz

---
+++ packages/BrightnessBooster/BrightnessBooster-1.0.1.tar.gz/BrightnessBooster-1.0.1/BrightnessBooster/__init__.py
@@ -0,0 +1,3 @@
+from .brightnessbooster import (beta_parameters_of_image, \
+                                calculate_brightness_and_contrast_from_parameters,\
+                                modify_brightness_distribution)
