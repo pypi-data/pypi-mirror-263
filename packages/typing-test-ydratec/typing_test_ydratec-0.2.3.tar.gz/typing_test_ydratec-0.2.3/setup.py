d4e33d9ee100185e4b6ba33839c8a6fdf7065b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 04:05:31 +0000

    Add package typing-test-ydratec/0.2.3/typing_test_ydratec-0.2.3.tar.gz

---
+++ packages/typing-test-ydratec/typing_test_ydratec-0.2.3.tar.gz/typing_test_ydratec-0.2.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+# setup(
+#     name='typing_test_ydratec',
+#     version='0.2',
+#     author='Ydratec (Clement TARDY)',
+#     author_email='clementardy@icloud.com',
+#     description='A simple typing test in a terminal',
+#     py_modules=['typing_test_ydratec'],
+#     keywords='typing test',
+#     install_requires= find_packages(),
+# )
+
+setup()
+
