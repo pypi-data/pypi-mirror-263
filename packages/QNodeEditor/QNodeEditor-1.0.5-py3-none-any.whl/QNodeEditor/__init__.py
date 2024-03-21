29e3e021a859e5f76df2f181a997ae3b433d3d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 14:04:17 +0000

    Add package QNodeEditor/1.0.5/QNodeEditor-1.0.5-py3-none-any.whl

---
+++ packages/QNodeEditor/QNodeEditor-1.0.5-py3-none-any.whl/QNodeEditor/__init__.py
@@ -0,0 +1,9 @@
+"""Module containing all elements for a node editor in PyQt5"""
+__version__ = "1.0.5"
+from .scene import NodeScene
+from .node import Node
+from .entry import Entry
+from .edge import Edge
+from .graphics.view import NodeView
+from .editor import NodeEditor
+from .dialog import NodeEditorDialog
