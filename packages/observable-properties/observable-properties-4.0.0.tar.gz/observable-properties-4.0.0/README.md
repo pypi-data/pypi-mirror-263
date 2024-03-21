0e05b9d587be6017a22b2810e6b5a72e703ca82d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 03:05:14 +0000

    Add package observable-properties/4.0.0/observable-properties-4.0.0.tar.gz

---
+++ packages/observable-properties/observable-properties-4.0.0.tar.gz/observable-properties-4.0.0/README.md
@@ -0,0 +1,173 @@
+# Observable managed attributes (also known as "observable properties")
+
+In summary:
+
+- Declare observable properties using the `@observable` decorator instead of `@property`.
+- Subscribe/unsubscribe any number of callback functions to observable properties.
+- If the value of an observable property changes, all subscribed callbacks are executed.
+
+## How to use
+
+### Declare observable properties in your class
+
+Use the `@observable` decorator as you do with `@property`:
+
+```python
+from observable_properties import observable
+class Test():
+    def __init__(self):
+        self._value = 0
+    @observable
+    def value(self):
+        return self._value
+    @value.setter
+    def value(self, value):
+        self._value = value
+```
+
+Needless to say, observable properties are also regular properties and they work just the same:
+
+```python
+t = Test()
+t.value = 1000
+print(f"Current value of t: {t.value}")
+```
+
+### Subscribe to property changes
+
+In the context of this library, an **observer** is a callback function to be executed
+when the value of a property is overwritten in a given object. For example:
+
+```python
+def observer(instance,property_name,new_value):
+    print(f"{instance.__class__.__name__}.{property_name} = {new_value}")
+```
+
+To start observing a property, call `subscribe()` passing the instance and property name to observe along with the callback function:
+
+```python
+from observable_properties import subscribe
+
+subscribe(observer,t,"value")
+```
+
+Which means "subscribe `observer` to `t.value`".
+Now, the observer is executed after the value of the observed property changes at the observed object:
+
+```python
+t.value = 2000
+```
+
+prints:
+
+```text
+Test.value = 2000
+```
+
+Additionally:
+
+- Observers **are not allowed** to change the value of the observed property.
+  `ObservablePropertyError` is raised in such a case.
+- The same observer can subscribe to many objects and properties.
+- Any number of observers can subscribe to the same object and property.
+  All of them will run. The execution order depends on subscription order.
+
+### Unsubscribe
+
+To stop observing a property, call `unsubscribe()` with the same parameters that were given to `subscribe()`:
+
+```python
+from observable_properties import unsubscribe
+
+unsubscribe(observer,t,"value")
+```
+
+### Syntactic sugar
+
+To speed things up, derive your class from the `Observable` class (**note the capital letter**).
+The declaration itself does not differ too much from the previous example:
+
+```python
+from observable_properties import Observable
+class ObservableTest(Observable):
+    def __init__(self):
+        self._value = 0
+    @observable
+    def value(self):
+        return self._value
+    @value.setter
+    def value(self, value):
+        self._value = value
+```
+
+Observable classes offer `subscribe()` and `unsubscribe()` **methods** with a slightly different
+syntax to their function counterparts:
+
+```python
+ot = ObservableTest()
+ot.subscribe("value",observer)
+ot.value = 900
+ot.unsubscribe("value", observer)
+ot.value = 500
+```
+
+prints:
+
+```text
+ObservableTest.value = 900
+```
+
+#### Observable changes triggered from *non-setter* methods
+
+A public *setter* is **not mandatory** for observable properties of an `Observable` instance.
+This is a common use case for computed properties.
+
+Any method can notify changes in observable properties in two ways:
+
+- Compute the new value inside the context returned by `self._observable()`.
+  The new value is automatically notified to all subscribers on exit.
+  As an example, see `Product.set()` below.
+- Explicitly call `self._observable_notify()` after the computation is finished.
+  As an example, see `Product.set_alternate()` below.
+
+```python
+class Product(Observable):
+    def __init__(self):
+        self._a = 0
+        self._b = 0
+    @observable
+    def value(self):
+        return self._a * self._b
+    def set(self, a, b):
+        with self._observable("value"):
+            self._a = a
+            self._b = b
+    def set_alternate(self, a, b):
+        changed = (a!=self._a) or (b!=self._b)
+        self._a = a
+        self._b = b
+        if (changed):
+            self._observable_notify("value")
+mult = Product()
+mult.subscribe("value", observer)
+mult.set(3, 5)
+mult.set_alternate(4,3)
+```
+
+prints:
+
+```text
+Product.value = 15
+Product.value = 12
+```
+
+## Other notes
+
+- `subscribe()`, `unsubscribe()`, `_observable()` and `_observable_notify()`
+  raise `ObservablePropertyError` on non-observable or non-existing properties.
+- Subscribing twice to the same object and property has no effect.
+- Unsubscribing to non-subscribed properties has no effect, but `unsubscribe()` returns False.
+- Objects hold strong references to observers. Deleting an observer does not
+  prevent it from running. Unsubscribe first.
+- Coroutines are accepted as observers. They are executed by the means of
+  [asyncio.run()](https://docs.python.org/3/library/asyncio-runner.html#asyncio.run)
---
+++ packages/observable-properties/observable-properties-4.0.0.tar.gz/observable-properties-4.0.0/tests/tests.py
@@ -0,0 +1,202 @@
+# ****************************************************************************
+# @file tests.py
+#
+# @author Ángel Fernández Pineda. Madrid. Spain.
+# @date 2024-01-26
+# @brief Observable managed attributes
+# @copyright 2024. Ángel Fernández Pineda. Madrid. Spain.
+# @license Licensed under the EUPL
+# *****************************************************************************
+
+from observable_properties import *
+from typing import Any
+from asyncio import sleep
+
+print("----------------------------------------")
+print("observable_properties: Automated test")
+print("----------------------------------------")
+
+
+class Test(Observable):
+    def __init__(self):
+        self._value = 0
+        self._name = "test"
+
+    @observable
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    def indirect(self, value):
+        self._value = value
+        self._observable_notify("value")
+
+    def indirect_failure(self, value):
+        self._observable_notify("name")
+
+    def indirect_with(self,value):
+        with self._observable("value"):
+            self._value = value
+
+
+def assert_obs1(expected):
+    global __obs1
+    if expected != __obs1:
+        print(f"Failure at observer 1: expected {expected}, found {__obs1}")
+
+
+def assert_obs2(expected):
+    global __obs2
+    if expected != __obs2:
+        print(f"Failure at observer 2: expected {expected}, found {__obs1}")
+
+
+item = Test()
+not_used = Test()
+
+
+def observer1(instance: object, property_name: str, value: Any):
+    global __obs1
+    __obs1 = value
+    if instance != item:
+        print("Failure at observer_before: passed instance is not 'item'")
+    if property_name != "value":
+        print("Failure at observer_before: observable property name is not 'value'")
+
+
+def observer2(instance: object, property_name: str, value: Any):
+    global __obs2
+    __obs2 = value
+    if instance != item:
+        print("Failure at observer_before: passed instance is not 'item'")
+    if property_name != "value":
+        print("Failure at observer_before: observable property name is not 'value'")
+
+
+def invalid_observer(instance, name, value):
+    item.value = value + 1
+
+
+async def async_observer(instance: object, property_name: str, value: Any):
+    await sleep(1)
+    print(f"--   async observer finished: value={value}")
+
+
+def test_reset():
+    global __obs1
+    global __obs2
+    __obs1 = None
+    __obs2 = None
+    item.unsubscribe("value", observer1)
+    unsubscribe(observer2, item, "value")
+    unsubscribe(invalid_observer, item, "value")
+    unsubscribe(async_observer, item, "value")
+
+
+print("-- Subscribing to non-observable property")
+try:
+    item.subscribe("name", observer1)
+    print("Failure")
+except ObservablePropertyError:
+    pass
+
+print("-- Subscribing to non-existing property")
+try:
+    subscribe(observer1, item, "foo")
+    print("Failure")
+except ObservablePropertyError:
+    pass
+
+print("-- Subscribe first observer and assign value")
+test_reset()
+item.subscribe("value", observer1)
+item.value = 1
+assert_obs1(1)
+assert_obs2(None)
+
+print("-- Subscribe second observer and assign value")
+subscribe(observer2, item, "value")
+item.value = 2
+assert_obs1(2)
+assert_obs2(2)
+
+print("-- Unsubscribe first observer and assign value")
+__obs1 = None
+__obs2 = None
+unsubscribe(observer1,item,"value")
+item.value = 3
+assert_obs1(None)
+assert_obs2(3)
+
+print("-- Unsubscribe second observer and assign value")
+__obs1 = None
+__obs2 = None
+item.unsubscribe("value",observer2)
+item.value = 3
+assert_obs1(None)
+assert_obs2(None)
+
+print("-- Unsubscribing second observer again")
+if unsubscribe(observer2, item, "value"):
+    print("Failure.")
+
+
+print("-- Unsubscribing to non-observable property")
+try:
+    unsubscribe(observer1, item, "name")
+    print("Failure.")
+except ObservablePropertyError:
+    pass
+
+print("-- Unsubscribing to non-existing property")
+try:
+    unsubscribe(observer1, item, "foo")
+    print("Failure.")
+except ObservablePropertyError:
+    pass
+
+
+print("-- Testing invalid observer (infinite loop?)")
+test_reset()
+item.subscribe("value", invalid_observer)
+try:
+    item.value = 4
+except ObservablePropertyError:
+    pass
+except RecursionError:
+    print("Failure")
+
+print("-- Testing async observer (printing for eye-review)")
+test_reset()
+subscribe(async_observer, item, "value")
+item.value = 7
+
+print("-- Testing in-class invalid indirect change")
+test_reset()
+subscribe(observer1, item, "value")
+try:
+    item.indirect_failure(1000)
+    print("Failure.")
+except ObservablePropertyError:
+    pass
+
+print("-- Testing in-class indirect change")
+item.indirect(2)
+assert_obs1(2)
+
+print("-- Testing in-class indirect change with context manager")
+item.indirect_with(3)
+assert_obs1(3)
+
+print("--- END ---")
