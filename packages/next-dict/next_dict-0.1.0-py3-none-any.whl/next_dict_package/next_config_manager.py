98062ad77bea05de421fe60f4db0f015eb71f906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 04:04:08 +0000

    Add package next-dict/0.1.0/next_dict-0.1.0-py3-none-any.whl

---
+++ packages/next-dict/next_dict-0.1.0-py3-none-any.whl/next_dict_package/next_config_manager.py
@@ -0,0 +1,29 @@
+import atexit
+import threading
+
+from next_dict_package.next_dict_obj import NextDict
+
+
+class NextConfigManager:
+
+    def __init__(self, file_path, instant_save=False):
+        self.file_path = file_path
+        self.instant_save = instant_save
+        self.lock = threading.Lock()
+
+        atexit.register(lambda: self.save())
+
+        def on_set(key, value):
+            if self.instant_save:
+                self.save()
+
+        try:
+            with open(file_path, 'r') as file:
+                self.config = NextDict(file.read(), on_set)
+        except FileNotFoundError:
+            self.config = NextDict({}, on_set)
+
+    def save(self):
+        with self.lock:
+            with open(self.file_path, 'w') as file:
+                file.write(str(self.config))
