1c43d226d6cff15c73441ac7868089f9e8a4a26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 15:04:09 +0000

    Add package tokive/0.8/tokive-0.8.tar.gz

---
+++ packages/tokive/tokive-0.8.tar.gz/tokive-0.8/tokhelper/__init__.py
@@ -0,0 +1,8 @@
+from tokhelper.tiktok_main import solve_captcha_auto
+
+
+class TokHelper():
+    def __init__(self):
+        print("init")
+    def solve_auto_helper(self,id,room):
+        solve_captcha_auto(id,room)
