86ef4427ec8ba404a20cdf04d4157e410e414a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 04:04:56 +0000

    Add package CamAgeTestThree/0.0.1/CamAgeTestThree-0.0.1.tar.gz

---
+++ packages/CamAgeTestThree/CamAgeTestThree-0.0.1.tar.gz/CamAgeTestThree-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Package for performing image segmentation'
+
+# Setting up
+setup(
+    name="CamAgeTestThree",
+    version=VERSION,
+    author="Sarthak Tyagi",
+    author_email="<sarthak20540@iiitd.ac.in>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['keras', 'pandas', 'scipy', 'tensorflow'],
+    keywords=['python', 'image', 'segmentation', 'image segmentation'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
