5584cc035206804ae8725a3415d3e2e4f0804e55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 04:04:31 +0000

    Add package InvokeAI/4.0.0rc4/InvokeAI-4.0.0rc4-py3-none-any.whl

---
+++ packages/InvokeAI/InvokeAI-4.0.0rc4-py3-none-any.whl/invokeai/frontend/web/dist/assets/ThemeLocaleProvider-CoUVkOK-.js
@@ -0,0 +1,1 @@
+import{h as s,A as n,ks as i,kt as c,C as o,ku as u,kv as d,kw as m}from"./index-DLyq0nLP.js";function x({children:t}){const{i18n:r}=n(),e=r.dir(),a=s.useMemo(()=>i({...c,direction:e}),[e]);return s.useEffect(()=>{document.body.dir=e},[e]),o.jsx(u,{theme:a,toastOptions:d,children:o.jsx(m,{children:t})})}const h=s.memo(x);export{h as default};
