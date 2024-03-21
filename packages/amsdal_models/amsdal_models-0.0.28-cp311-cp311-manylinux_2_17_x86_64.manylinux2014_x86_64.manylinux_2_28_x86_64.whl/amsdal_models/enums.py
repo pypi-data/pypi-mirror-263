0c40a77dfcd5647ffe0a05ed2ad8cc1c1c7359c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 03:04:47 +0000

    Add package amsdal_models/0.0.28/amsdal_models-0.0.28-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/amsdal_models/amsdal_models-0.0.28-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl/amsdal_models/enums.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class MetaClasses(str, Enum):
+    TYPE = 'TypeMeta'
+    CLASS_OBJECT = 'ClassObject'
+
+
+class BaseClasses(str, Enum):
+    OBJECT = 'Object'
+    CLASS_OBJECT = 'ClassObject'
