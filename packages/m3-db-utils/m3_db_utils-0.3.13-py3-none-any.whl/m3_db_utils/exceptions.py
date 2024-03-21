ebbb24db4674ad8531ef6fc694eb0c6ad9459eb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 04:05:16 +0000

    Add package m3-db-utils/0.3.13/m3_db_utils-0.3.13-py3-none-any.whl

---
+++ packages/m3-db-utils/m3_db_utils-0.3.13-py3-none-any.whl/m3_db_utils/exceptions.py
@@ -0,0 +1,5 @@
+class RequiredFieldEmptyModelEnumValueError(RuntimeError):
+    """
+    Исключение возникающее при установленном пустом (None) значении или его отсутствия у поля значения
+    модели-перечисления
+    """
