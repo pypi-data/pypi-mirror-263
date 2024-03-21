f2965b5e2eacdf1b1c96bd3956488ee34a8e0e8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 16:07:30 +0000

    Add package django-log-lens/0.1.1/django-log-lens-0.1.1.tar.gz

---
+++ packages/django-log-lens/django-log-lens-0.1.1.tar.gz/django-log-lens-0.1.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-log-lens",
+    version="0.1.1",
+    author="Martin Broede",
+    author_email="martin.broede@gmail.com",
+    description="A lightweight Django app for viewing and managing log data conveniently",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/martinbroede/django-log-lens",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires='>=3.9',
+)
