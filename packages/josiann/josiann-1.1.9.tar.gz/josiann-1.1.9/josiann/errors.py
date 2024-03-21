a821f32122b9f32a9db911db121c43f5d5b8b7a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 14:04:07 +0000

    Add package josiann/1.1.9/josiann-1.1.9.tar.gz

---
+++ packages/josiann/josiann-1.1.9.tar.gz/josiann-1.1.9/josiann/errors.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+# Created on 26/07/2021 11:55
+# Author : matteo
+
+"""
+Defines custom errors.
+"""
+
+# ====================================================
+# imports
+
+
+# ====================================================
+# code
+class ShapeError(Exception):
+    """
+    Custom error for shape mismatches.
+    """
