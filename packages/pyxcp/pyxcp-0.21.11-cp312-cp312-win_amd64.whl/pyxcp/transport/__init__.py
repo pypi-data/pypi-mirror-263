24c621255b3d1013adce98df3e88068240b93d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 16:07:48 +0000

    Add package pyxcp/0.21.11/pyxcp-0.21.11-cp312-cp312-win_amd64.whl

---
+++ packages/pyxcp/pyxcp-0.21.11-cp312-cp312-win_amd64.whl/pyxcp/transport/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from .base import FrameAcquisitionPolicy
+from .base import FrameRecorderPolicy
+from .base import LegacyFrameAcquisitionPolicy
+from .base import StdoutPolicy
+from .can import Can
+from .eth import Eth
+from .sxi import SxI
+from .usb_transport import Usb
