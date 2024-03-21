a385748fd28b48feac4d307880d94770eecc98c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 14:04:10 +0000

    Add package Promethee/0.3/Promethee-0.3.tar.gz

---
+++ packages/Promethee/Promethee-0.3.tar.gz/Promethee-0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Promethee",
+    version="0.3",
+    author="Eduardo Teles, Wilson Freita, Vinícius Oliveira, Gabriel Monteiro, Josef Jaeger",
+    description="Promethee algorithm",
+    long_description="Multi-decision criteria algorithm Promethee",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=['pandas',]
+)
