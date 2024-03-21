35cb35290dda1093a91c3a223f49a5f8ebe27bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 16:07:21 +0000

    Add package cloudproof-anonymization/0.1.3/cloudproof_anonymization-0.1.3-cp37-abi3-macosx_10_12_x86_64.whl

---
+++ packages/cloudproof-anonymization/cloudproof_anonymization-0.1.3-cp37-abi3-macosx_10_12_x86_64.whl/cloudproof_anonymization/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from .cloudproof_anonymization import *
+
+__doc__ = cloudproof_anonymization.__doc__
+if hasattr(cloudproof_anonymization, '__all__'):
+    __all__ = cloudproof_anonymization.__all__
