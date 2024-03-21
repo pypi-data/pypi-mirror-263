2af1935bc55e55b140807afd3f09c5171d8b451b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 05:04:17 +0000

    Add package tfs-client/0.1.5/tfs_client-0.1.5-py3-none-any.whl

---
+++ packages/tfs-client/tfs_client-0.1.5-py3-none-any.whl/tfs_client/util/postprocessing.py
@@ -0,0 +1,18 @@
+import haskellian as hk
+from ..http import SamplePreds, predict, Params
+
+def transpose(sample: SamplePreds) -> list[tuple[str, float]]:
+  return list(zip(sample.preds, sample.logprobs))
+
+async def multi_predict(b64_multibatch: list[tuple[str, ...]], **params: Params) -> list[list[list[tuple[str, float]]]]:
+    """Returns an array of shape `BATCH x PLAYERS x TOP_PREDS` of `(pred, logprob)`"""
+    if len(b64_multibatch) == 0:
+        return []
+    num_players = len(b64_multibatch[0])
+    flatbatch = list(hk.flatten(b64_multibatch))
+    flatpreds = await predict(flatbatch, **params)
+    return hk.vpipe(
+        hk.map(transpose, flatpreds),
+        hk.batch(num_players),
+        list
+    )
