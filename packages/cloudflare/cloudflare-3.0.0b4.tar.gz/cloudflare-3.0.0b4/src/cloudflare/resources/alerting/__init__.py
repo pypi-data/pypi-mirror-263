1bebc078a2da5822109c842fdfc0894891e41121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 07:05:02 +0000

    Add package cloudflare/3.0.0b4/cloudflare-3.0.0b4.tar.gz

---
+++ packages/cloudflare/cloudflare-3.0.0b4.tar.gz/cloudflare-3.0.0b4/src/cloudflare/resources/alerting/__init__.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .v3 import (
+    V3,
+    AsyncV3,
+    V3WithRawResponse,
+    AsyncV3WithRawResponse,
+    V3WithStreamingResponse,
+    AsyncV3WithStreamingResponse,
+)
+from .alerting import (
+    Alerting,
+    AsyncAlerting,
+    AlertingWithRawResponse,
+    AsyncAlertingWithRawResponse,
+    AlertingWithStreamingResponse,
+    AsyncAlertingWithStreamingResponse,
+)
+
+__all__ = [
+    "V3",
+    "AsyncV3",
+    "V3WithRawResponse",
+    "AsyncV3WithRawResponse",
+    "V3WithStreamingResponse",
+    "AsyncV3WithStreamingResponse",
+    "Alerting",
+    "AsyncAlerting",
+    "AlertingWithRawResponse",
+    "AsyncAlertingWithRawResponse",
+    "AlertingWithStreamingResponse",
+    "AsyncAlertingWithStreamingResponse",
+]
---
+++ packages/cloudflare/cloudflare-3.0.0b4.tar.gz/cloudflare-3.0.0b4/src/cloudflare/types/intel/indicator_feeds/permission_list_response.py
@@ -0,0 +1,9 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List
+
+from .intel_permission_list_item import IntelPermissionListItem
+
+__all__ = ["PermissionListResponse"]
+
+PermissionListResponse = List[IntelPermissionListItem]
