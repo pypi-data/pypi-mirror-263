4c970d574724479045ef121f5b7da13a42fe7340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 14:04:11 +0000

    Add package jp2rt/0.1.0a16/jp2rt-0.1.0a16.tar.gz

---
+++ packages/jp2rt/jp2rt-0.1.0a16.tar.gz/jp2rt-0.1.0a16/java/src/main/java/it/unimi/di/jp2rt/package-info.java
@@ -0,0 +1,10 @@
+/** 
+ * 
+ * Some javadoc... 
+ * 
+ * <p>This is the overview page for the Java API of the {@code jp²rt} package.
+ *
+ * <p>For more info, please refer to the <a href="https://mapio.github.io/jp2rt/">package documentation</a>.</p>
+ *  
+ */
+package it.unimi.di.jp2rt;
