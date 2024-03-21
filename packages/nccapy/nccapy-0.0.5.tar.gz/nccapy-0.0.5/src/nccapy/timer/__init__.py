c350a735a30656e522c3da1e10899506014e244a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 05:04:05 +0000

    Add package nccapy/0.0.5/nccapy-0.0.5.tar.gz

---
+++ packages/nccapy/nccapy-0.0.5.tar.gz/nccapy-0.0.5/src/nccapy/timer/__init__.py
@@ -0,0 +1,9 @@
+"""
+A simple timer class for timing code execution. I use this in my examples to show how long
+certain operations take to execute. It is not a high precision timer and should not be used
+
+
+
+"""
+
+from .Timer import Timer
