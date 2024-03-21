00583b3b3b8c0c4934bba938e7c4c093c34fffbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 03:04:31 +0000

    Add package flowlite/0.0.2/flowlite-0.0.2.tar.gz

---
+++ packages/flowlite/flowlite-0.0.2.tar.gz/flowlite-0.0.2/flowlite/data/data_transforms.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+class Transform:
+    def __call__(self, x):
+        raise NotImplementedError
+
+
+class RandomFlipHorizontal(Transform):
+    def __init__(self, p = 0.5):
+        self.p = p
+
+    def __call__(self, img):
+        """
+        Horizonally flip an image, specified as an H x W x C NDArray.
+        Args:
+            img: H x W x C NDArray of an image
+        Returns:
+            H x W x C ndarray corresponding to image flipped with probability self.p
+        Note: use the provided code to provide randomness, for easier testing
+        """
+        flip_img = np.random.rand() < self.p
+        if flip_img:
+            return img[:, ::-1, :]
+            # return np.flip(img, axis=1)
+        else:
+            return img    
+        
+
+
+class RandomCrop(Transform):
+    def __init__(self, padding=3):
+        self.padding = padding
+
+    def __call__(self, img):
+        """ Zero pad and then randomly crop an image.
+        Args:
+             img: H x W x C NDArray of an image
+        Return 
+            H x W x C NAArray of cliped image
+        Note: generate the image shifted by shift_x, shift_y specified below
+        """
+        shift_x, shift_y = np.random.randint(low=-self.padding, high=self.padding+1, size=2)
+        padded_img = np.pad(img, ((self.padding, self.padding), (self.padding, self.padding), (0, 0)), 'constant')  
+        H, W, _ = padded_img.shape
+        return padded_img[self.padding + shift_x: H - self.padding + shift_x, self.padding + shift_y: W - self.padding + shift_y, :]
