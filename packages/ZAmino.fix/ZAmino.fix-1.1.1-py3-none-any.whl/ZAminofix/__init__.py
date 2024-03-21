af37507b8cd013a0b14e105665bfa7bf8b806bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 15:04:14 +0000

    Add package ZAmino.fix/1.1.1/ZAmino.fix-1.1.1-py3-none-any.whl

---
+++ packages/ZAmino.fix/ZAmino.fix-1.1.1-py3-none-any.whl/ZAminofix/__init__.py
@@ -0,0 +1,9 @@
+
+from .acm import ACM
+from .client import Client
+from .sub_client import SubClient
+from .lib.util import exceptions, helpers, objects, headers
+from .asyncfix import acm, client, sub_client, socket
+from .socket import Callbacks, SocketHandler
+from requests import get
+from json import loads
