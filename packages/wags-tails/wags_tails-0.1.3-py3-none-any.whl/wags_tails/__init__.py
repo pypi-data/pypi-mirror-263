31d6d2a1551b983f3a71861f1ac0eb741f33782d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 06:05:10 +0000

    Add package wags-tails/0.1.3/wags_tails-0.1.3-py3-none-any.whl

---
+++ packages/wags-tails/wags_tails-0.1.3-py3-none-any.whl/wags_tails/__init__.py
@@ -0,0 +1,44 @@
+"""Data acquisition tools for Wagnerds."""
+from .base_source import DataSource, RemoteDataError
+from .chembl import ChemblData
+from .chemidplus import ChemIDplusData
+from .custom import CustomData
+from .do import DoData
+from .drugbank import DrugBankData
+from .drugsatfda import DrugsAtFdaData
+from .ensembl import EnsemblData
+from .ensembl_transcript_mappings import EnsemblTranscriptMappingData
+from .guide_to_pharmacology import GToPLigandData
+from .hemonc import HemOncData
+from .hgnc import HgncData
+from .mondo import MondoData
+from .ncbi import NcbiGeneData, NcbiGenomeData
+from .ncbi_lrg_refseqgene import NcbiLrgRefSeqGeneData
+from .ncbi_mane_summary import NcbiManeSummaryData
+from .ncit import NcitData
+from .oncotree import OncoTreeData
+from .rxnorm import RxNormData
+
+__all__ = [
+    "DataSource",
+    "RemoteDataError",
+    "ChemblData",
+    "ChemIDplusData",
+    "CustomData",
+    "DoData",
+    "DrugBankData",
+    "DrugsAtFdaData",
+    "EnsemblData",
+    "EnsemblTranscriptMappingData",
+    "GToPLigandData",
+    "HemOncData",
+    "HgncData",
+    "MondoData",
+    "NcbiGeneData",
+    "NcbiGenomeData",
+    "NcbiManeSummaryData",
+    "NcbiLrgRefSeqGeneData",
+    "NcitData",
+    "OncoTreeData",
+    "RxNormData",
+]
