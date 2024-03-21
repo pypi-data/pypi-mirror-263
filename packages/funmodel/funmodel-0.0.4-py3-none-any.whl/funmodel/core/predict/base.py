e645355e4000dded76f11b637560bfcfa78698f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 03:04:14 +0000

    Add package funmodel/0.0.4/funmodel-0.0.4-py3-none-any.whl

---
+++ packages/funmodel/funmodel-0.0.4-py3-none-any.whl/funmodel/core/predict/base.py
@@ -0,0 +1,9 @@
+class PredictModel:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def load(self, *args, **kwargs):
+        pass
+
+    def predict(self, *args, **kwargs):
+        pass
