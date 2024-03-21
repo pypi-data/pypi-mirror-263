8f076835f4cecf44547a0b70e01e28f7fded9e41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 04:05:33 +0000

    Add package lsst-ctrl-bps-parsl/26.0.2/lsst-ctrl-bps-parsl-26.0.2.tar.gz

---
+++ packages/lsst-ctrl-bps-parsl/lsst-ctrl-bps-parsl-26.0.2.tar.gz/lsst-ctrl-bps-parsl-26.0.2/python/lsst/ctrl/bps/parsl/sites/local.py
@@ -0,0 +1,44 @@
+from typing import TYPE_CHECKING
+
+from parsl.executors import HighThroughputExecutor
+from parsl.executors.base import ParslExecutor
+from parsl.providers import LocalProvider
+
+from ..configuration import get_bps_config_value
+from ..site import SiteConfig
+
+if TYPE_CHECKING:
+    from ..job import ParslJob
+
+__all__ = ("Local",)
+
+
+class Local(SiteConfig):
+    """Configuration for running jobs on the local machine
+
+    The number of cores to use is specified in the site configuration, under
+    ``site.<computeSite>.cores`` (`int`).
+    """
+
+    def get_executors(self) -> list[ParslExecutor]:
+        """Get a list of executors to be used in processing
+
+        Each executor should have a unique ``label``.
+        """
+        cores = get_bps_config_value(self.site, "cores", int, required=True)
+        return [HighThroughputExecutor("local", provider=LocalProvider(), max_workers=cores)]
+
+    def select_executor(self, job: "ParslJob") -> str:
+        """Get the ``label`` of the executor to use to execute a job
+
+        Parameters
+        ----------
+        job : `ParslJob`
+            Job to be executed.
+
+        Returns
+        -------
+        label : `str`
+            Label of executor to use to execute ``job``.
+        """
+        return "local"
