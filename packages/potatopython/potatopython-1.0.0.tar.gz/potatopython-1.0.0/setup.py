2e7f5278636171761de57aab1bfc0285cb39edd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 15:04:11 +0000

    Add package potatopython/1.0.0/potatopython-1.0.0.tar.gz

---
+++ packages/potatopython/potatopython-1.0.0.tar.gz/potatopython-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='potatopython',
+    version='1.0.0',
+    description='A simple Template for create python project',
+    author='ks lim',
+    author_email='kianseng.lim@sony.com',
+    packages=['python'],
+)
