4de6f98d841622cf651c32ac2c2b9ffaa23dcd2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 14:04:08 +0000

    Add package NEURON-nightly/9.0a1.dev178/NEURON_nightly-9.0a1.dev178-cp38-cp38-macosx_11_0_x86_64.whl

---
+++ packages/NEURON-nightly/NEURON_nightly-9.0a1.dev178-cp38-cp38-macosx_11_0_x86_64.whl/neuron/.data/include/md2redef.h
@@ -0,0 +1,23 @@
+#ifndef nmodl2_redef_h
+#define nmodl2_redef_h
+
+#undef v
+#undef area
+#undef thisnode
+#undef GC
+#undef EC
+#undef extnode
+#undef xain
+#undef xbout
+#undef i
+#undef sec
+
+#undef nodelist
+#undef nodeindices
+#undef pdata
+#undef prop
+#undef nodecount
+
+#undef id
+
+#endif
