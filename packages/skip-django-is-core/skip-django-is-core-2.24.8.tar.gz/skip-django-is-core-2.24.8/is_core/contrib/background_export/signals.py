b23f586f207915ae4ef21f3f9d81db2a5f7c9a17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 03:04:16 +0000

    Add package skip-django-is-core/2.24.8/skip-django-is-core-2.24.8.tar.gz

---
+++ packages/skip-django-is-core/skip-django-is-core-2.24.8.tar.gz/skip-django-is-core-2.24.8/is_core/contrib/background_export/signals.py
@@ -0,0 +1,5 @@
+import django.dispatch
+
+
+export_success = django.dispatch.Signal(providing_args=['exported_file'])
+export_failure = django.dispatch.Signal(providing_args=['exception'])
---
+++ packages/skip-django-is-core/skip-django-is-core-2.24.8.tar.gz/skip-django-is-core-2.24.8/is_core/version.py
@@ -0,0 +1,5 @@
+VERSION = (2, 24, 8)
+
+
+def get_version():
+    return '.'.join(map(str, VERSION))
