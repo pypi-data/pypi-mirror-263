892b686877902e502e6b57896b9eef5555f4d099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 14:04:04 +0000

    Add package Velkoz/0.0.24/Velkoz-0.0.24-py3-none-any.whl

---
+++ packages/Velkoz/Velkoz-0.0.24-py3-none-any.whl/Velkoz/__init__.py
@@ -0,0 +1,24 @@
+
+from .velkoz import(
+        get_summoner
+
+        )
+
+
+
+
+
+from .tentacles import (
+        AccountApi,
+        ChampionMasteryApi,
+        RiotApiService,
+        api_key,
+        MatchApi,
+        SummonerApi
+        )
+
+from .eye import (
+        Summoner,
+        Match,
+        Participant
+        )
