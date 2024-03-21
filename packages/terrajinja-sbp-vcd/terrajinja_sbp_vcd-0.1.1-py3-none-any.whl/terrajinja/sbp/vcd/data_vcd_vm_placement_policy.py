52d278654627707a90b8a17eeb3627e68445e30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 03:04:33 +0000

    Add package terrajinja-sbp-vcd/0.1.1/terrajinja_sbp_vcd-0.1.1-py3-none-any.whl

---
+++ packages/terrajinja-sbp-vcd/terrajinja_sbp_vcd-0.1.1-py3-none-any.whl/terrajinja/sbp/vcd/data_vcd_vm_placement_policy.py
@@ -0,0 +1,30 @@
+from constructs import Construct
+
+from terrajinja.imports.vcd.data_vcd_vm_placement_policy import DataVcdVmPlacementPolicy
+from .decorators import run_once
+
+
+@run_once(parameter_match=["name", "urn"])
+class SbpDataVcdVmPlacementPolicy(DataVcdVmPlacementPolicy):
+    """Extends the original vcd.data_vcd_vm_placement_policy class
+       to ensure that it only gets called once"""
+
+    def __init__(self, scope: Construct, id_: str, name: str, urn: str):
+        """Enhances the original vcd.data_vcd_vm_placement_policy
+           to ensure that it only gets called once
+
+        Args:
+            scope (Construct): Cdktf App
+            id_ (str): uniq name of the resource
+            name (str): name of the placement policy
+            urn (str): the urn of the provider vcd id
+
+        Original:
+            https://registry.terraform.io/providers/vmware/vcd/3.10.0/docs/data-sources/catalog_vapp_template
+        """
+        super().__init__(
+            scope=scope,
+            id_=f'sbp_vcd_catalog_vapp_template_{name}',
+            name=name,
+            provider_vdc_id=urn
+        )
