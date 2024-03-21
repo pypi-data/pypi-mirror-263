c6cbab380b868e90fdfac79f751168ed94212a9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 04:04:17 +0000

    Add package ansys-units/0.3.2/ansys_units-0.3.2.tar.gz

---
+++ packages/ansys-units/ansys_units-0.3.2.tar.gz/ansys_units-0.3.2/src/ansys/units/_constants.py
@@ -0,0 +1,33 @@
+"""Provides ``QuantityType`` class."""
+
+import os
+
+import yaml
+
+
+class _QuantityType:
+    composite = "Composite"
+    derived = "Derived"
+    no_type = "No Type"
+    temperature = "Temperature"
+    temperature_difference = "Temperature Difference"
+
+
+# Single import of static tables
+
+file_dir = os.path.dirname(__file__)
+qc_path = os.path.join(file_dir, "cfg.yaml")
+
+with open(qc_path, "r") as qc_yaml:
+    qc_data = yaml.safe_load(qc_yaml)
+
+_multipliers: dict = qc_data["multipliers"]
+_unit_systems: dict = qc_data["unit_systems"]
+_base_units: dict = qc_data["base_units"]
+_derived_units: dict = qc_data["derived_units"]
+
+table_path = os.path.join(file_dir, "quantity_tables/si_table.yaml")
+
+with open(table_path, "r") as table:
+    table_data = yaml.safe_load(table)
+_quantity_units_table: dict = table_data["quantity_units_table"]
---
+++ packages/ansys-units/ansys_units-0.3.2.tar.gz/ansys_units-0.3.2/src/ansys/units/unit_registry.py
@@ -0,0 +1,73 @@
+"""Provides the ``UnitRegistry`` class."""
+
+import os
+
+import yaml
+
+from ansys.units import Unit
+
+
+class UnitRegistry:
+    """
+    A representation of valid ``Unit`` instances.
+
+    All base and derived units loaded from the configuration file, `cfg.yaml`,
+    on package initialization are provided by default.
+
+    Parameters
+    ----------
+    config: str, optional
+        Path of a ``YAML`` configuration file, which can be a custom file, and
+        defaults to the provided file, ``cfg.yaml``. Custom configuration files
+        must match the format of the default configuration file.
+    other: dict, optional
+        Dictionary for additional units.
+
+    Examples
+    --------
+    >>> from ansys.units import UnitRegistry, Unit
+    >>> ureg = UnitRegistry()
+    >>> assert ureg.kg == Unit(units="kg")
+    >>> fps = Unit("ft s^-1")
+    >>> ureg.foot_per_sec = fps
+    """
+
+    def __init__(self, config="cfg.yaml", other: dict = None):
+        unitdict = other or {}
+
+        if config:
+            file_dir = os.path.dirname(__file__)
+            qc_path = os.path.join(file_dir, config)
+
+            with open(qc_path, "r") as qc_yaml:
+                qc_data = yaml.safe_load(qc_yaml)
+                _base_units: dict = qc_data["base_units"]
+                _derived_units: dict = qc_data["derived_units"]
+
+            unitdict.update(**_base_units, **_derived_units)
+
+        for unit in unitdict:
+            setattr(self, unit, Unit(unit, unitdict[unit]))
+
+    def __str__(self):
+        returned_string = ""
+        attrs = self.__dict__
+        for key in attrs:
+            returned_string += f"{key}, "
+        return returned_string
+
+    def __setattr__(self, __name: str, unit: any) -> None:
+        if hasattr(self, __name):
+            raise UnitAlreadyRegistered(__name)
+        self.__dict__[__name] = unit
+
+    def __iter__(self):
+        for item in self.__dict__:
+            yield getattr(self, item)
+
+
+class UnitAlreadyRegistered(ValueError):
+    """Raised when a unit has previously been registered."""
+
+    def __init__(self, name: str):
+        super().__init__(f"Unable to override `{name}` it has already been registered.")
