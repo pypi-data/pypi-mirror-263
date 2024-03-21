71ceb363f5d9c265413af3fb4d85ae222d22b546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 16:07:40 +0000

    Add package alignai/0.2.0/alignai-0.2.0.tar.gz

---
+++ packages/alignai/alignai-0.2.0.tar.gz/alignai-0.2.0/alignai/constants.py
@@ -0,0 +1,19 @@
+class EventTypes:
+    SESSION_OPEN = "session_open"
+    SESSION_CLOSE = "session_close"
+    USER_RECOGNIZE = "user_recognize"
+    MESSAGE_CREATE = "message_create"
+
+
+ROLE_USER = "user"
+ROLE_ASSISTANT = "assistant"
+DEFAULT_ASSISTANT_ID = "DEFAULT"
+
+
+FLUSH_INTERVAL_MS = 3000
+FLUSH_BATCH_SIZE = 50
+FLUSH_TIMEOUT_SECONDS = 5
+MAX_BUFFER_SIZE = 200
+API_MAX_RETIRES = 3
+
+SERVER_BASE_URL = "https://api.impaction.ai"
