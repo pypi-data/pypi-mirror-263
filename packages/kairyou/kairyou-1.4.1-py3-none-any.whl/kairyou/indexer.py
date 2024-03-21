f1e4ee2e9e9683284ac60f94992adca03fef97d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 16:07:20 +0000

    Add package kairyou/1.4.1/kairyou-1.4.1-py3-none-any.whl

---
+++ packages/kairyou/kairyou-1.4.1-py3-none-any.whl/kairyou/indexer.py
@@ -0,0 +1,316 @@
+## built-in libraries
+import os
+import typing
+import json
+import time
+
+## third-party libraries
+import spacy
+
+## custom modules
+from .util import _validate_replacement_json, _get_elapsed_time
+from .katakana_util import KatakanaUtil
+from .types import NameAndOccurrence
+
+class Indexer:
+
+    """
+
+    The global Indexer client for indexing names.
+
+    """
+
+    replacement_json:dict
+
+    json_type:typing.Literal["kudasai", "fukuin"]
+
+    text_to_index:str
+
+    indexing_log = ""
+
+    knowledge_base:typing.List[str] = []
+
+    blacklisted_names:typing.List[str] = []
+
+    ## dict of entity labels and their occurrences
+    entity_occurrences:dict = {}
+
+    ner = spacy.load("ja_core_news_lg")
+
+##-------------------start-of-load_static_data()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+    @staticmethod
+    def _load_static_data(text_to_index:str, knowledge_base:str, replacement_json:typing.Union[str, dict]) -> None:
+        
+        """
+        
+        Loads static data into the Indexer class.
+
+        """
+
+        ## text_to_index can be sent in a path to a text file, or just the text itself
+        if(os.path.exists(text_to_index)):
+            with open(text_to_index, "r", encoding="utf-8") as file:
+                Indexer.text_to_index = file.read()
+
+        else:
+            Indexer.text_to_index = text_to_index
+
+        ## knowledge_base can be sent in a path to a directory containing text files, a path to a text file, or just the text itself            
+        if(os.path.exists(knowledge_base)):
+            if(os.path.isdir(knowledge_base)):
+                for file in os.listdir(knowledge_base):
+                    if(file.endswith(".txt")):
+                      with open(os.path.join(knowledge_base, file), "r", encoding="utf-8") as file:
+                          Indexer.knowledge_base.append(file.read())
+            else:
+                with open(knowledge_base, "r", encoding="utf-8") as file:
+                    Indexer.knowledge_base.append(file.read())
+
+        else:
+            Indexer.knowledge_base.append(knowledge_base)
+
+        ## replacement_json can be sent in a path to a json, or as the json itself
+        if(isinstance(replacement_json, str)):
+
+            with open(replacement_json, "r", encoding="utf-8") as file:
+
+                Indexer.replacement_json = json.load(file)
+
+        else:
+
+            Indexer.replacement_json = replacement_json
+                        
+##-------------------start-of-_get_names_from_replacement_json()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    @staticmethod
+    def _get_names_from_replacement_json():
+
+        """
+        
+        Fetches all names from the replacement_json and returns them as a list.
+
+        """
+
+        entries = []
+
+        Indexer.json_type, _ = _validate_replacement_json(Indexer.replacement_json)
+
+        if(Indexer.json_type == "kudasai"):
+
+            key_to_fetch_from = ["single_names", "full_names"]
+
+        else:
+            
+            key_to_fetch_from = ["names", "single-names", "full-names"]
+
+        for key in key_to_fetch_from:
+
+            ## entries can sometimes look like ("Yamanaka Ikuko": ["山中","郁子"])
+            ## so we need to split the japanese names and add them to the list
+
+            entry = Indexer.replacement_json.get(key, [])
+
+            if(isinstance(entry, tuple) or isinstance(entry, list)):
+
+                for name in entry:
+
+                    entries.append(name)
+
+            elif(isinstance(entry, dict)):
+                
+                for name in entry.values():
+
+                    entries.extend(name if isinstance(name, list) else [name])
+
+            else:
+
+                entries.append(entry)
+
+        return list(set(entries))        
+
+##-------------------start-of-_get_names_from_all_sources()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    @staticmethod
+    def _get_names_from_all_sources() -> typing.Tuple[typing.List[NameAndOccurrence], typing.List[NameAndOccurrence], typing.List[NameAndOccurrence]]:
+        
+        """
+        
+        Fetches all names from the knowledge_base, text_to_index, and replacement_json and returns them as a list.
+
+        """
+
+        names_in_knowledge_base = []
+        names_in_text_to_index = []
+        names_in_replacement_json = [NameAndOccurrence(name, 1) for name in Indexer._get_names_from_replacement_json()]
+
+        name_occurrences = {}
+        for entry in Indexer.knowledge_base:
+            entry = entry.split("\n")
+            for line in entry:
+                sentence = Indexer.ner(line)
+                for entity in sentence.ents:
+
+                    if(entity.text in Indexer.blacklisted_names):
+                        continue
+
+                    ## log label and occurrence
+                    Indexer.entity_occurrences[entity.label_] = Indexer.entity_occurrences.get(entity.label_, 0) + 1
+
+                    if(entity.label_ == "PERSON"):
+                        name_occurrences[entity.text] = name_occurrences.get(entity.text, 0) + 1
+                        names_in_knowledge_base.append(NameAndOccurrence(entity.text, name_occurrences[entity.text]))
+
+        name_occurrences = {}
+        for entry in Indexer.text_to_index.split("\n"):
+            sentence = Indexer.ner(entry)
+            for entity in sentence.ents:
+
+                if(entity.text in Indexer.blacklisted_names):
+                    continue
+
+                ## log label and occurrence
+                Indexer.entity_occurrences[entity.label_] = Indexer.entity_occurrences.get(entity.label_, 0) + 1
+
+                if(entity.label_ == "PERSON"):
+                    name_occurrences[entity.text] = name_occurrences.get(entity.text, 0) + 1
+                    names_in_text_to_index.append(NameAndOccurrence(entity.text, name_occurrences[entity.text]))
+
+        return names_in_knowledge_base, names_in_text_to_index, names_in_replacement_json
+    
+##-------------------start-of-_perform_further_elimination()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+    @staticmethod
+    def _perform_further_elimination(names_in_knowledge_base:typing.List[NameAndOccurrence], 
+                                    names_in_text_to_index:typing.List[NameAndOccurrence], 
+                                    names_in_replacement_json:typing.List[NameAndOccurrence]) -> typing.Tuple[typing.List[NameAndOccurrence], typing.List[NameAndOccurrence], typing.List[NameAndOccurrence]]:
+        
+        """
+        
+        Performs further elimination of names.
+
+        Elimination Criteria:
+        1. "Names" that consist of more punctuation than letters.
+        2. "Names" that are actual katakana words. See words.py
+        3. "Names" that are partialy english. (Contains at least one english letter)
+        4. "Names" that appear to be onomatopoeia. (Repeating character sequences)
+
+        """
+
+        names_in_knowledge_base = [name for name in names_in_knowledge_base if not (KatakanaUtil.is_more_punctuation_than_japanese(name.name) or 
+                                                                                    KatakanaUtil.is_actual_word(name.name) or 
+                                                                                    KatakanaUtil.is_partially_english(name.name) or
+                                                                                    KatakanaUtil.is_repeating_sequence(name.name))]
+        
+        names_in_text_to_index = [name for name in names_in_text_to_index if not (KatakanaUtil.is_more_punctuation_than_japanese(name.name) or 
+                                                                                  KatakanaUtil.is_actual_word(name.name) or 
+                                                                                    KatakanaUtil.is_partially_english(name.name) or
+                                                                                  KatakanaUtil.is_repeating_sequence(name.name))]
+        
+        names_in_replacement_json = [name for name in names_in_replacement_json if not (KatakanaUtil.is_more_punctuation_than_japanese(name.name) or 
+                                                                                        KatakanaUtil.is_actual_word(name.name) or 
+                                                                                        KatakanaUtil.is_partially_english(name.name) or
+                                                                                        KatakanaUtil.is_repeating_sequence(name.name))]
+
+
+        return names_in_knowledge_base, names_in_text_to_index, names_in_replacement_json
+    
+##-------------------start-of-trim_honorifics()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+    @staticmethod
+    def _trim_honorifics(names_in_knowledge_base:typing.List[NameAndOccurrence], 
+                        names_in_text_to_index:typing.List[NameAndOccurrence], 
+                        names_in_replacement_json:typing.List[NameAndOccurrence]) -> typing.Tuple[typing.List[NameAndOccurrence], typing.List[NameAndOccurrence], typing.List[NameAndOccurrence]]:
+        
+        """
+        
+        Trims honorifics from names.
+
+        """
+
+        honorifics = Indexer.replacement_json.get('honorifics', [])
+
+        for honorific in honorifics:
+            names_in_knowledge_base = [NameAndOccurrence(name.name.replace(honorific, ""), name.occurrence) for name in names_in_knowledge_base]
+            names_in_text_to_index = [NameAndOccurrence(name.name.replace(honorific, ""), name.occurrence) for name in names_in_text_to_index]
+            names_in_replacement_json = [NameAndOccurrence(name.name.replace(honorific, ""), name.occurrence) for name in names_in_replacement_json]
+
+        return names_in_knowledge_base, names_in_text_to_index, names_in_replacement_json
+    
+##-------------------start-of-is_name_in_other_sources()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    @staticmethod
+    def _is_name_in_other_sources(name: str, all_names: typing.Set[str]) -> bool:
+
+        """
+
+        Checks if a name is in the knowledge_base or replacement_json.
+
+        """
+
+
+        return any(other_name in name for other_name in all_names)
+    
+##-------------------start-of-index()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    @staticmethod
+    def index(text_to_index:str, 
+              knowledge_base:str, 
+              replacement_json:typing.Union[str, dict],
+              blacklist:typing.List[str] = []
+              ) -> typing.Tuple[typing.List[NameAndOccurrence], str]:
+
+        """
+        
+        Determines which names in the text_to_index are not in the knowledge_base or replacement_json and returns them as a list.
+
+        Returns a tuple of tuples. That tuple has the name itself and the occurrence of the name that was flagged.
+
+        Parameters:
+        text_to_index (str): The text to index. Can be a path to a text file, or just the text itself.
+        knowledge_base (str): The knowledge base. Can be a path to a directory containing text files, a path to a text file, or just the text itself.
+        replacement_json (str): The replacement json. Can be a path to a json, or as the json itself.
+        blacklist (list): A list of strings to ignore.
+
+        Returns:
+        new_names (NameAndOccurrence): A list of names that are not in the knowledge_base or replacement_json. (NameAndOccurrence is a named tuple with the fields name and occurrence).
+        Indexer.indexing_log (str): Log of the indexing process (names that were flagged as unique 'names' and which occurrence they were flagged at).
+        
+        """
+
+        time_start = time.time()
+
+        if(len(blacklist) > 0):
+            Indexer.blacklisted_names = blacklist
+
+        new_names:typing.List[NameAndOccurrence] = []
+
+        Indexer._load_static_data(text_to_index, knowledge_base, replacement_json)
+
+        names_in_knowledge_base, names_in_text_to_index, names_in_replacement_json = Indexer._get_names_from_all_sources()
+
+        names_in_knowledge_base, names_in_text_to_index, names_in_replacement_json = Indexer._perform_further_elimination(names_in_knowledge_base, names_in_text_to_index, names_in_replacement_json)
+
+        if(replacement_json):
+            names_in_knowledge_base, names_in_text_to_index, names_in_replacement_json = Indexer._trim_honorifics(names_in_knowledge_base, names_in_text_to_index, names_in_replacement_json)
+
+        names_in_knowledge_base = set(name.name for name in names_in_knowledge_base)
+        names_in_replacement_json = set(name.name for name in names_in_replacement_json)
+
+        all_names = names_in_knowledge_base | names_in_replacement_json
+
+        for name in names_in_text_to_index:
+            if(not Indexer._is_name_in_other_sources(name.name, all_names)):
+                new_names.append(name)
+                Indexer.indexing_log += (f"Name: {name.name} Occurrence: {name.occurrence} was flagged as a unique 'name'\n")
+
+        time_end = time.time()
+
+        Indexer.indexing_log += "\nIgnored Strings: " + str(Indexer.blacklisted_names)
+
+        Indexer.indexing_log += "\nTotal Unique 'Names'  : " + \
+            str(len(new_names))
+        Indexer.indexing_log += "\nTime Elapsed : " + \
+            _get_elapsed_time(time_start, time_end)
+
+        return new_names, Indexer.indexing_log
---
+++ packages/kairyou/kairyou-1.4.1-py3-none-any.whl/kairyou/katakana_util.py
@@ -0,0 +1,192 @@
+## built-in libraries
+import string
+import typing
+
+## custom modules
+from .util import Name
+from .words import katakana_words as words
+
+##--------------------start-of-KatakanaUtil------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class KatakanaUtil:
+
+    """
+    
+    Contains helper functions for katakana handling.
+
+    """
+
+    katakana_words = words.split("\n")
+
+    ## https://en.wikipedia.org/wiki/Katakana_(Unicode_block)
+    KATAKANA_CHARSET = {
+    '゠','ァ','ア','ィ','イ','ゥ','ウ','ェ','エ','ォ','オ','カ','ガ','キ','ギ','ク',
+    'グ','ケ','ゲ','コ','ゴ','サ','ザ','シ','ジ','ス','ズ','セ','ゼ','ソ','ゾ','タ',
+    'ダ','チ','ヂ','ッ','ツ','ヅ','テ','デ','ト','ド','ナ','ニ','ヌ','ネ','ノ','ハ',
+    'バ','パ','ヒ','ビ','ピ','フ','ブ','プ','ヘ','ベ','ペ','ホ','ボ','ポ','マ','ミ',
+    'ム','メ','モ','ャ','ヤ','ュ','ユ','ョ','ヨ','ラ','リ','ル','レ','ロ','ヮ','ワ',
+    'ヰ','ヱ','ヲ','ン','ヴ','ヵ','ヶ','ヷ','ヸ','ヹ','ヺ','・','ー','ヽ','ヾ'
+    }
+
+    ## Punctuation unicode ranges:
+    ## https://kairozu.github.io/updates/cleaning-jp-text
+    PUNCTUATION_CHARSET = {
+    '　','、','。','〃','〄','々','〆','〇','〈','〉','《','》','「','」','『','』',
+    '【','】','〒','〓','〔','〕','〖','〗','〘','〙','〚','〛','〜','〝','〞','〟',
+    '〠','〡','〢','〣','〤','〥','〦','〧','〨','〩','〪','〫','〬','〭','〮','〯',
+    '〰','〱','〲','〳','〴','〵','〶','〷','〸','〹','〺','〻','〼','〽','〾','〿',
+    '！','＂','＃','＄','％','＆','＇','（','）','＊','＋','，','－','．','／','：',
+    '；','＜','＝','＞','？','［','＼','］','＾','＿','｀','｛','｜','｝','～','｟',
+    '｠','｡','｢','｣','､','･','ー','※',' ',' ',' ',' ',"«", "»","_",
+    ' ',' ',' ',' ',' ',' ',' ',
+    '​','‌','‍','‎','‏','‐','‑','‒','–','—',
+    '―','‖','‗','‘','’','‚','‛','“','”','„','‟','†','‡','•','‣','․','‥','…','‧',
+    ' ',' ','‪','‫','‬','‭','‮',
+    ' ','‰','‱','′','″','‴','‵','‶','‷','‸','‹','›','※','‼','‽','‾','‿',
+    '⁀','⁁','⁂','⁃','⁄','⁅','⁆','⁇','⁈','⁉','⁊','⁋','⁌','⁍','⁎','⁏','⁐','⁑','⁒',
+    '⁓','⁔','⁕','⁖','⁗','⁘','⁙','⁚','⁛','⁜','⁝','⁞',' ','⁠',
+    '⁦','⁧','⁨','⁩','«','»','×',"△","▼"
+    } | set(string.punctuation) ## EN punctuation set
+
+##--------------------start-of-is_katakana_only()------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    @staticmethod
+    def is_katakana_only(string:str) -> bool:
+
+        """
+
+        Checks if the string is only katakana.
+        
+        Parameters:
+        string (str) : the string to check.
+
+        Returns:
+        bool : True if the word is only katakana, False otherwise.
+
+        """
+
+        return all([char in KatakanaUtil.KATAKANA_CHARSET for char in string])
+
+##--------------------start-of-_get_katakana_entities()------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    @staticmethod
+    def _get_katakana_entities(names:dict) -> typing.List[Name]:
+
+        """
+
+        Gets the katakana entities from the names dictionary.
+
+        Returns:
+        list (object - Name) : a list of Name objects.
+
+        """
+
+        return [Name(jap=j, eng=e) for e, j in names.items() if KatakanaUtil.is_katakana_only(j)]
+    
+##--------------------start-of-is_actual_word()------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    @staticmethod
+    def is_actual_word(jap:str) -> bool:
+
+        """
+        
+        Checks if the given japanese is an actual katakana word.
+
+        Parameters:
+        jap (str) : the katakana word to check.
+ 
+        Returns:
+        bool : True if the word is an actual katakana word, False otherwise.
+
+        """
+
+        if(jap in KatakanaUtil.katakana_words):
+            return True
+        
+        else:
+            return False
+        
+##--------------------start-of-is_punctuation()------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    @staticmethod
+    def is_punctuation(string:str):
+
+        """
+        
+        Checks if the given string is all punctuation.
+
+        Parameters:
+        string (str) : the string to check.
+
+        Returns:
+        bool : True if the word is all punctuation, False otherwise.
+
+        """
+
+        return all([char in KatakanaUtil.PUNCTUATION_CHARSET for char in string])
+
+##--------------------start-of-is_repeating_sequence()------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+    @staticmethod
+    def is_repeating_sequence(word:str) -> bool:
+
+        """
+
+        Checks if the given word has a repeating sequence.
+
+        Parameters:
+        word (str) : the word to check.
+
+        Returns:
+        bool : True if the word has a repeating sequence, False otherwise.
+
+        """
+
+        for i in range(1, len(word)//2 + 1):  # Only need to iterate to half the word length
+            ## Check every possible subsequence size
+            for ii in range(len(word) - i):
+                if word[ii:ii+i] == word[ii+i:ii+2*i]:
+                    return True
+                
+        return False
+    
+##--------------------start-of-more_punctuation_than_japanese()------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    @staticmethod
+    def is_more_punctuation_than_japanese(text:str) -> bool:
+
+        """
+
+        Checks if the given text has more punctuation than Japanese characters.
+
+        Parameters:
+        text (str) : the text to check.
+
+        Returns:
+        bool : True if the text has more punctuation than Japanese characters, False otherwise.
+
+        """
+
+        # Count non-punctuation (assumed to be Japanese) and punctuation characters
+        non_punctuation_count = sum(1 for char in text if char not in KatakanaUtil.PUNCTUATION_CHARSET)
+        punctuation_count = sum(1 for char in text if char in KatakanaUtil.PUNCTUATION_CHARSET)
+        return punctuation_count > non_punctuation_count
+    
+##--------------------start-of-is_partially_english()------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+    @staticmethod
+    def is_partially_english(text:str) -> bool:
+        
+        """
+        
+        Checks if the given text is partially English.
+        
+        Parameters:
+        text (str) : the text to check.
+        
+        Returns:
+        bool : True if the text is partially English, False otherwise.
+        
+        """
+        
+        return any([char in string.ascii_letters for char in text])
