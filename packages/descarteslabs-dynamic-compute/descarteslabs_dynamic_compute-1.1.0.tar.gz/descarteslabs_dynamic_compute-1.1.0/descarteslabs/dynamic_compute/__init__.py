7c897163bcaf2a4abaedb3731d722f5d5dd26665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 05:03:53 +0000

    Add package descarteslabs-dynamic-compute/1.1.0/descarteslabs_dynamic_compute-1.1.0.tar.gz

---
+++ packages/descarteslabs-dynamic-compute/descarteslabs_dynamic_compute-1.1.0.tar.gz/descarteslabs_dynamic_compute-1.1.0/descarteslabs/dynamic_compute/__init__.py
@@ -0,0 +1,51 @@
+from importlib.metadata import version
+
+__version__ = version("descarteslabs-dynamic-compute")
+
+from .compute_map import (
+    arccos,
+    arcsin,
+    arctan,
+    as_compute_map,
+    cos,
+    e,
+    log,
+    log10,
+    pi,
+    resolution,
+    sin,
+    sqrt,
+    tan,
+)
+from .dot import dot
+from .groupby import ImageStackGroupBy
+from .image_stack import ImageStack
+from .interactive import map
+from .mosaic import Mosaic
+
+from . import catalog  # isort: skip
+
+__all__ = [
+    "as_compute_map",
+    "resolution",
+    "catalog",
+    "share_catalog_blob",
+    "list_catalog_blobs",
+    "delete_blob_in_catalog",
+    "dot",
+    "e",
+    "pi",
+    "sqrt",
+    "sin",
+    "cos",
+    "tan",
+    "arccos",
+    "arcsin",
+    "arctan",
+    "log",
+    "log10",
+    "Mosaic",
+    "ImageStack",
+    "ImageStackGroupBy",
+    "map",
+]
