9500da7cf2d64e0edc46110be5186e4fd4b75138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 03:05:30 +0000

    Add package ozon-api-client/0.0.1/ozon_api_client-0.0.1.tar.gz

---
+++ packages/ozon-api-client/ozon_api_client-0.0.1.tar.gz/ozon_api_client-0.0.1/ozon_api_client/api/__init__.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.analytics_api_api import AnalyticsAPIApi
+from swagger_client.api.barcode_api_api import BarcodeAPIApi
+from swagger_client.api.brand_api_api import BrandAPIApi
+from swagger_client.api.cancellation_api_api import CancellationAPIApi
+from swagger_client.api.category_api_api import CategoryAPIApi
+from swagger_client.api.certification_api_api import CertificationAPIApi
+from swagger_client.api.chat_api_api import ChatAPIApi
+from swagger_client.api.fbo_api import FBOApi
+from swagger_client.api.fbs_api import FBSApi
+from swagger_client.api.fbsr_fbs_marks_api import FBSrFBSMarksApi
+from swagger_client.api.finance_api_api import FinanceAPIApi
+from swagger_client.api.polygon_api_api import PolygonAPIApi
+from swagger_client.api.prices_stocks_api_api import PricesStocksAPIApi
+from swagger_client.api.pricing_strategy_api_api import PricingStrategyAPIApi
+from swagger_client.api.product_api_api import ProductAPIApi
+from swagger_client.api.promos_api import PromosApi
+from swagger_client.api.rfbs_returns_api_api import RFBSReturnsAPIApi
+from swagger_client.api.report_api_api import ReportAPIApi
+from swagger_client.api.return_api_api import ReturnAPIApi
+from swagger_client.api.returns_api_api import ReturnsAPIApi
+from swagger_client.api.seller_rating_api import SellerRatingApi
+from swagger_client.api.supplier_api_api import SupplierAPIApi
+from swagger_client.api.warehouse_api_api import WarehouseAPIApi
