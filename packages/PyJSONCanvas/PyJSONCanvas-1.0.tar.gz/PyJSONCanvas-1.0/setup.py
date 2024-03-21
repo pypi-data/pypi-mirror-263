e26b7c3846f027a3e3ae5f42a9332bea51b5aa42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 15:04:14 +0000

    Add package PyJSONCanvas/1.0/PyJSONCanvas-1.0.tar.gz

---
+++ packages/PyJSONCanvas/PyJSONCanvas-1.0.tar.gz/PyJSONCanvas-1.0/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name="PyJSONCanvas",
+    version="1.0",
+    description="A simple library for working with JSON Canvas (previously known as Obsidian Canvas) files.",
+    author="Chaitanya Sharma",
+    url="https://github.com/CheeksTheGeek/PyJSONCanvas",
+    license="MIT",
+    py_modules=["pyjsoncanvas"],
+)
