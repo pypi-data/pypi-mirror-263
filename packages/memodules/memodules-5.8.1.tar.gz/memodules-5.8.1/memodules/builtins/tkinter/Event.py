ffdd895b512a822ed7bd942ab226e605ffa1d09d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 15:03:55 +0000

    Add package memodules/5.8.1/memodules-5.8.1.tar.gz

---
+++ packages/memodules/memodules-5.8.1.tar.gz/memodules-5.8.1/memodules/builtins/tkinter/Event.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from typing import Literal
+
+
+@dataclass(frozen=True)
+class Window:
+    Resize: Literal["<Configure>"] = "<Configure>"
+    """Example:
+        my_window.bind(Resize, lambda event: print(event))"""
+
+
+@dataclass(frozen=True)
+class TextWidget:
+    PressEnter: Literal["<Return>"] = "<Return>"
+    "Events when the Enter key is pressed"
+    AnyKey: Literal["<Key>"] = "<Key>"
+    "Events when any key is pressed"
+
+
+@dataclass(frozen=True)
+class ListWidget:
+    ItemSelect: Literal['<<ListboxSelect>>'] = '<<ListboxSelect>>'
+    "Events when the any item is selected"
+
+
+Tk = Window()
+Toplevel = Window()
+Text = TextWidget()
+Listbox = ListWidget()
