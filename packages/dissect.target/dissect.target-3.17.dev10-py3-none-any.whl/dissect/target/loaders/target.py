b6a75679ca4cb7b08d7a88d46592ca207630c197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 17:04:19 +0000

    Add package dissect.target/3.17.dev10/dissect.target-3.17.dev10-py3-none-any.whl

---
+++ packages/dissect.target/dissect.target-3.17.dev10-py3-none-any.whl/dissect/target/loaders/target.py
@@ -0,0 +1,27 @@
+try:
+    import yaml
+except ImportError:
+    raise ImportError("Missing PyYAML dependency")
+
+from dissect.target import container
+from dissect.target.loader import Loader
+
+
+class TargetLoader(Loader):
+    """Load target files."""
+
+    def __init__(self, path, **kwargs):
+        super().__init__(path)
+        self.base_dir = path.parent
+        self.definition = yaml.safe_load(path.open("rb"))
+
+    @staticmethod
+    def detect(path):
+        return path.suffix.lower() == ".target"
+
+    def map(self, target):
+        for disk in self.definition["disks"]:
+            target.disks.add(container.open(disk))
+
+    def open(self, path):
+        return self.base_dir.joinpath(path).open("rb")
