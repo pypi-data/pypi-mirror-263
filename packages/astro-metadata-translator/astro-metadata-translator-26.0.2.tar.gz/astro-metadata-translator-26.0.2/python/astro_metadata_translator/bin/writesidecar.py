a76198caeea457763f5b6cd69075fdea1d95f841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 04:05:41 +0000

    Add package astro-metadata-translator/26.0.2/astro-metadata-translator-26.0.2.tar.gz

---
+++ packages/astro-metadata-translator/astro-metadata-translator-26.0.2.tar.gz/astro-metadata-translator-26.0.2/python/astro_metadata_translator/bin/writesidecar.py
@@ -0,0 +1,168 @@
+# This file is part of astro_metadata_translator.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the LICENSE file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# Use of this source code is governed by a 3-clause BSD-style
+# license that can be found in the LICENSE file.
+
+from __future__ import annotations
+
+__all__ = ("write_sidecar_files", "write_sidecar_file")
+
+import os
+import sys
+import traceback
+from collections.abc import Sequence
+from typing import IO
+
+from ..file_helpers import find_files, read_file_info
+
+
+def _split_ext(file: str) -> tuple[str, str]:
+    """Split the extension from the file name and return it and the root.
+
+    Special case handling of .gz and other compression extensions.
+    """
+    special = {".gz", ".bz2", ".xz", ".fz"}
+
+    root, ext = os.path.splitext(file)
+
+    if ext in special:
+        root, second_ext = os.path.splitext(root)
+        ext = second_ext + ext
+
+    return root, ext
+
+
+def write_sidecar_file(
+    file: str,
+    hdrnum: int,
+    content_mode: str,
+    print_trace: bool,
+    outstream: IO = sys.stdout,
+    errstream: IO = sys.stderr,
+) -> bool:
+    """Write JSON summary to sidecar file.
+
+    Parameters
+    ----------
+    file : `str`
+        The file from which the header is to be read.
+    hdrnum : `int`
+        The HDU number to read. The primary header is always read and
+        merged with the header from this HDU.
+    content_mode : `str`
+        Content mode to use when writing JSON to sidecar. Options are:
+        ``metadata`` to write the unmodified header;
+        ``translated`` to write the translated ObservationInfo.
+    print_trace : `bool`
+        If there is an error reading the file and this parameter is `True`,
+        a full traceback of the exception will be reported. If `False` prints
+        a one line summary of the error condition. If `None` the exception
+        will be allowed.
+    outstream : `io.StringIO`, optional
+        Output stream to use for standard messages. Defaults to `sys.stdout`.
+    errstream : `io.StringIO`, optional
+        Stream to send messages that would normally be sent to standard
+        error. Defaults to `sys.stderr`.
+
+    Returns
+    -------
+    success : `bool`
+        `True` if the file was handled successfully, `False` if the file
+        could not be processed.
+    """
+    if content_mode not in ("metadata", "translated"):
+        raise ValueError(f"Specified content mode '{content_mode}' is not understood.")
+
+    try:
+        # Calculate the JSON from the file
+        json_str = read_file_info(
+            file,
+            hdrnum,
+            content_mode=content_mode,
+            content_type="json",
+            print_trace=print_trace,
+            outstream=outstream,
+            errstream=errstream,
+        )
+        if json_str is None:
+            return False
+
+        # Calculate sidecar file name derived from this file.
+        # Match the ButlerURI behavior in that .fits.gz should be replaced
+        # with .json, and not resulting in .fits.json.
+        root, ext = _split_ext(file)
+        newfile = root + ".json"
+        with open(newfile, "w") as fd:
+            print(json_str, file=fd)
+
+    except Exception as e:
+        if print_trace is None:
+            raise e
+        if print_trace:
+            traceback.print_exc(file=outstream)
+        else:
+            print(repr(e), file=outstream)
+        return False
+    return True
+
+
+def write_sidecar_files(
+    files: Sequence[str],
+    regex: str,
+    hdrnum: int,
+    content_mode: str,
+    print_trace: bool,
+    outstream: IO = sys.stdout,
+    errstream: IO = sys.stderr,
+) -> tuple[list[str], list[str]]:
+    """Process each file and create sidecar file.
+
+    Parameters
+    ----------
+    files : iterable of `str`
+        The files or directories from which the headers are to be read.
+    regex : `str`
+        Regular expression string used to filter files when a directory is
+        scanned.
+    hdrnum : `int`
+        The HDU number to read. The primary header is always read and
+    content_mode : `str`
+        Content mode to use when writing JSON to sidecar. Options are:
+        ``metadata`` to write the unmodified header;
+        ``translated`` to write the translated ObservationInfo.
+    print_trace : `bool`
+        If there is an error reading the file and this parameter is `True`,
+        a full traceback of the exception will be reported. If `False` prints
+        a one line summary of the error condition.
+    outstream : `io.StringIO`, optional
+        Output stream to use for standard messages. Defaults to `sys.stdout`.
+    errstream : `io.StringIO`, optional
+        Stream to send messages that would normally be sent to standard
+        error. Defaults to `sys.stderr`.
+
+    Returns
+    -------
+    okay : `list` of `str`
+        All the files that were processed successfully.
+    failed : `list` of `str`
+        All the files that could not be processed.
+    """
+    found_files = find_files(files, regex)
+
+    # Process each file
+    failed = []
+    okay = []
+    for path in sorted(found_files):
+        isok = write_sidecar_file(path, hdrnum, content_mode, print_trace, outstream, errstream)
+        if isok:
+            okay.append(path)
+        else:
+            failed.append(path)
+
+    return okay, failed
---
+++ packages/astro-metadata-translator/astro-metadata-translator-26.0.2.tar.gz/astro-metadata-translator-26.0.2/tests/test_translate_header.py
@@ -0,0 +1,171 @@
+# This file is part of astro_metadata_translator.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the LICENSE file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# Use of this source code is governed by a 3-clause BSD-style
+# license that can be found in the LICENSE file.
+
+import io
+import os.path
+import unittest
+
+from astro_metadata_translator.bin.translateheader import process_files
+
+TESTDIR = os.path.abspath(os.path.dirname(__file__))
+TESTDATA = os.path.join(TESTDIR, "data")
+
+
+class TestTranslateHeader(unittest.TestCase):
+    """Test that the translate_header CLI logic works."""
+
+    def _readlines(self, stream):
+        """Return the lines written to the stream.
+
+        Parameters
+        ----------
+        stream : `io.StringIO`
+            The stream to read.
+
+        Returns
+        -------
+        lines : `list` of `str`
+            The lines contained in the stream.
+        """
+        stream.seek(0)
+        return [ll.rstrip() for ll in stream.readlines()]
+
+    def test_translate_header(self):
+        """Translate some header files."""
+        with io.StringIO() as out:
+            with io.StringIO() as err:
+                okay, failed = process_files(
+                    [TESTDATA],
+                    r"^fitsheader.*yaml$",
+                    0,
+                    False,
+                    outstream=out,
+                    errstream=err,
+                    output_mode="none",
+                )
+                self.assertEqual(self._readlines(out), [])
+                lines = self._readlines(err)
+                self.assertEqual(len(lines), 10)
+                self.assertTrue(lines[0].startswith("Analyzing"), f"Line: '{lines[0]}'")
+
+        self.assertEqual(len(okay), 10)
+        self.assertEqual(len(failed), 0)
+
+    def test_translate_header_table(self):
+        """Translate some header files with table output."""
+        with io.StringIO() as out:
+            with io.StringIO() as err:
+                okay, failed = process_files(
+                    [TESTDATA], r"^fitsheader.*yaml$", 0, False, outstream=out, errstream=err
+                )
+                output = self._readlines(out)
+                self.assertTrue(output[0].startswith("ObsId"))
+                self.assertTrue(output[1].startswith("-------"))
+                self.assertEqual(len(output), 12)
+                errlines = self._readlines(err)
+                self.assertEqual(len(errlines), 0)
+
+        self.assertEqual(len(okay), 10)
+        self.assertEqual(len(failed), 0)
+
+    def test_translate_header_fails(self):
+        """Translate some header files that fail."""
+        with io.StringIO() as out:
+            with io.StringIO() as err:
+                okay, failed = process_files(
+                    [TESTDATA], r"^.*yaml$", 0, False, outstream=out, errstream=err, output_mode="none"
+                )
+
+                lines = self._readlines(out)
+                self.assertEqual(len(lines), len(failed))
+                self.assertTrue(lines[0].startswith("Failure processing"), f"Line: '{lines[0]}'")
+                self.assertIn("not a mapping", lines[0], f"Line: '{lines[0]}'")
+
+                lines = self._readlines(err)
+                self.assertEqual(len(lines), 13)
+                self.assertTrue(lines[0].startswith("Analyzing"), f"Line: '{lines[0]}'")
+
+        self.assertEqual(len(okay), 10)
+        self.assertEqual(len(failed), 3)
+
+    def test_translate_header_traceback(self):
+        """Translate some header files that fail and trigger traceback."""
+        with io.StringIO() as out:
+            with io.StringIO() as err:
+                okay, failed = process_files(
+                    [TESTDATA], r"^.*yaml$", 0, True, outstream=out, errstream=err, output_mode="none"
+                )
+
+                lines = self._readlines(out)
+                self.assertGreaterEqual(len(lines), 22, "\n".join(lines))
+                self.assertTrue(lines[0].startswith("Traceback"), f"Line '{lines[0]}'")
+
+                lines = self._readlines(err)
+                self.assertGreaterEqual(len(lines), 13, "\n".join(lines))
+                self.assertTrue(lines[0].startswith("Analyzing"), f"Line: '{lines[0]}'")
+
+        self.assertEqual(len(okay), 10)
+        self.assertEqual(len(failed), 3)
+
+    def test_translate_header_dump(self):
+        """Check that a header is dumped."""
+        with io.StringIO() as out:
+            with io.StringIO() as err:
+                okay, failed = process_files(
+                    [os.path.join(TESTDATA, "fitsheader-decam.yaml")],
+                    r"^fitsheader.*yaml$",
+                    0,
+                    False,
+                    outstream=out,
+                    errstream=err,
+                    output_mode="yaml",
+                )
+
+                lines = self._readlines(out)
+                # Look for a DECam header in the output
+                header = "\n".join(lines)
+                self.assertIn("DTINSTRU", header)
+
+                lines = self._readlines(err)
+                self.assertEqual(len(lines), 1)
+                self.assertTrue(lines[0], "Analyzing tests/data/fitsheader-decam.yaml...")
+
+        self.assertEqual(len(okay), 1)
+        self.assertEqual(len(failed), 0)
+
+    def test_translate_header_loud(self):
+        """Check that ObservationInfo content is displayed."""
+        with io.StringIO() as out:
+            with io.StringIO() as err:
+                okay, failed = process_files(
+                    [os.path.join(TESTDATA, "fitsheader-decam.yaml")],
+                    r"^fitsheader.*yaml$",
+                    0,
+                    False,
+                    outstream=out,
+                    errstream=err,
+                    output_mode="verbose",
+                )
+
+                lines = self._readlines(out)
+                # Look for the translated DECam header in the output
+                self.assertEqual(lines[2], "datetime_begin: 2013-09-01T06:02:55.754")
+
+                lines = self._readlines(err)
+                self.assertEqual(len(lines), 1)
+                self.assertTrue(lines[0], "Analyzing tests/data/fitsheader-decam.yaml...")
+
+        self.assertEqual(len(okay), 1)
+        self.assertEqual(len(failed), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
