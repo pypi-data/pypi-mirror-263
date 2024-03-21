ee96be4e732bf933eddd323d8dd77803eaeda442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 04:05:02 +0000

    Add package gnsmql/0.1.9/gnsmql-0.1.9.tar.gz

---
+++ packages/gnsmql/gnsmql-0.1.9.tar.gz/gnsmql-0.1.9/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'gnsmql',
+  packages = ['gnsmql'],
+  version = '0.1.9',
+  license='MIT',
+  description = 'Database for sleepy people.',
+  long_description=open('README.md').read(),
+  long_description_content_type='text/markdown',
+  author = 'Simon Renggli',
+  author_email = 'simon_renggli1@sluz.ch',
+  url = 'https://github.com/SimonRenggli1/GNSMQL',
+  download_url = 'https://github.com/SimonRenggli1/GNSMQL/archive/refs/tags/latest.tar.gz',
+  keywords = ['database', 'sleepy', 'tired', 'zzz'],
+  classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
