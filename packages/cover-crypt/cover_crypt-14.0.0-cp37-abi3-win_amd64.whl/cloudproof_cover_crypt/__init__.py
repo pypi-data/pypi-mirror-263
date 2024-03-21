31842730c23f0a95d87c8ba6565b3277aa807bb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 16:07:21 +0000

    Add package cover-crypt/14.0.0/cover_crypt-14.0.0-cp37-abi3-win_amd64.whl

---
+++ packages/cover-crypt/cover_crypt-14.0.0-cp37-abi3-win_amd64.whl/cloudproof_cover_crypt/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from .cloudproof_cover_crypt import *
+
+__doc__ = cloudproof_cover_crypt.__doc__
+if hasattr(cloudproof_cover_crypt, '__all__'):
+    __all__ = cloudproof_cover_crypt.__all__
