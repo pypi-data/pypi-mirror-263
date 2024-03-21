1fce628c83443b3aae1c3b75ffab6e1e367999c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 16:07:18 +0000

    Add package NOMTrainer/1.1.4/NOMTrainer-1.1.4.tar.gz

---
+++ packages/NOMTrainer/NOMTrainer-1.1.4.tar.gz/NOMTrainer-1.1.4/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name = "NOMTrainer",
+    version = "1.1.4",
+    author = "CSSA",
+    author_email="ladder.cssa@gmail.com",
+    description="A Trainer for Neural Object Model (NOM) in Tensorflow.",
+    long_description="long_description",
+    long_description_content_type="text/markdown",
+    url="https://github.com/LadderCSSA/NOMTrainer",            
+    packages=setuptools.find_packages(),     
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9'
+)
