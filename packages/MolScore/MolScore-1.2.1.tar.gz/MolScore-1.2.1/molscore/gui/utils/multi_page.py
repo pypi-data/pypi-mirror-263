db5f498cec17f224d369a63982c9d1a8ca9f28c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 17:04:16 +0000

    Add package MolScore/1.2.1/MolScore-1.2.1.tar.gz

---
+++ packages/MolScore/MolScore-1.2.1.tar.gz/MolScore-1.2.1/molscore/gui/utils/multi_page.py
@@ -0,0 +1,23 @@
+from itertools import cycle
+
+import streamlit as st
+from molscore.gui.utils import utils
+
+
+def multi_plot(main_df, SS):
+    """ Show multiple plots """
+
+    col1, col2 = st.columns([3, 1])
+    y_variables = col1.multiselect('y-axis', [c for c in main_df.columns.tolist() if c not in SS.exclude_params], default=['valid', 'unique', 'occurrences'])
+    valid_only = col2.checkbox(label='Valid only')
+    unique_only = col2.checkbox(label='Unique only')
+
+    tdf = main_df
+    if valid_only:
+        tdf = tdf.loc[tdf.valid == 'true', :]
+    if unique_only:
+        tdf = tdf.loc[tdf.unique == True, :]
+
+    for y, col in zip(y_variables, cycle(st.columns(3))):
+        sub_fig = utils.plotly_plot(y, tdf, size=(250, 200))
+        col.plotly_chart(sub_fig)
