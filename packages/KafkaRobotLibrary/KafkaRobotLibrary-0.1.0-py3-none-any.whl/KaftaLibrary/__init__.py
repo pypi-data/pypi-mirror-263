a2c82fdb05670e6d5519dffb04d3fd06714c96ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 06:05:39 +0000

    Add package KafkaRobotLibrary/0.1.0/KafkaRobotLibrary-0.1.0-py3-none-any.whl

---
+++ packages/KafkaRobotLibrary/KafkaRobotLibrary-0.1.0-py3-none-any.whl/KaftaLibrary/__init__.py
@@ -0,0 +1,10 @@
+from .keywords import KafkaKeywords
+
+
+class KafkaLibrary(KafkaKeywords):
+    """
+    KafkaLibrary é uma biblioteca do Robot Framework para interação com o Apache Kafka.
+
+    Para mais informações, veja a documentação das keywords.
+    """
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
