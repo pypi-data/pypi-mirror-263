b08e3bd2ed6a1538e2b6ec460ef50fdc8db860a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 07:04:09 +0000

    Add package gender-based-ml/0.8.2/gender_based_ml-0.8.2.tar.gz

---
+++ packages/gender-based-ml/gender_based_ml-0.8.2.tar.gz/gender_based_ml-0.8.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name="gender_based_ml",
+    version="0.8.2",
+    author="Shruthipriya",
+    author_email="priya@shavik.ai",
+    description="ML based project to determine the gender of a speaker in an audio file",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+      package_data={
+        "gender_based_ml": ["*.*"],
+    },
+    include_package_data=True,
+    install_requires=["tensorflow>=2.7.0", "keras>=2.7.0", "torch>=0.8.1"],
+)
