6273fd033ca5285c2196a96e224020f3a2764d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 15:04:06 +0000

    Add package jijzept/1.18.0rc1/jijzept-1.18.0rc1-cp311-cp311-win_amd64.whl

---
+++ packages/jijzept/jijzept-1.18.0rc1-cp311-cp311-win_amd64.whl/jijzept/sampler/thirdparty/__init__.py
@@ -0,0 +1,32 @@
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)
+
+import jijzept.sampler.thirdparty.digitalannealer as digitalannealer
+import jijzept.sampler.thirdparty.dwaveleaphybridcqm as dwaveleaphybridcqm
+import jijzept.sampler.thirdparty.fixstars_amplify as fixstars_amplify
+
+from jijzept.sampler.thirdparty.digitalannealer import (
+    JijDA4Sampler,
+    JijDA4SolverParameters,
+)
+from jijzept.sampler.thirdparty.dwaveleaphybridcqm import (
+    JijLeapHybridCQMParameters,
+    JijLeapHybridCQMSampler,
+)
+from jijzept.sampler.thirdparty.fixstars_amplify import (
+    JijFixstarsAmplifyParameters,
+    JijFixstarsAmplifySampler,
+)
+
+__all__ = [
+    "digitalannealer",
+    "dwaveleaphybridcqm",
+    "fixstars_amplify",
+    "JijLeapHybridCQMSampler",
+    "JijLeapHybridCQMParameters",
+    "JijFixstarsAmplifySampler",
+    "JijFixstarsAmplifyParameters",
+    "JijDA4Sampler",
+    "JijDA4SolverParameters",
+]
