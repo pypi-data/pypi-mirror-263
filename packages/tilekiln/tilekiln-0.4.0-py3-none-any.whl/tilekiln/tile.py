7538e11c6037e6f2cf23fd560e88c1e40e5f93f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 05:04:00 +0000

    Add package tilekiln/0.4.0/tilekiln-0.4.0-py3-none-any.whl

---
+++ packages/tilekiln/tilekiln-0.4.0-py3-none-any.whl/tilekiln/tile.py
@@ -0,0 +1,28 @@
+# TODO: Add dataclass
+# TODO: __slots__?
+class Tile:
+    def __init__(self, zoom: int, x: int, y: int):
+        '''Creates a tile object, with x, y, and zoom
+        '''
+        assert zoom >= 0
+        assert x >= 0
+        assert x < 2**zoom
+        assert y >= 0
+        assert y < 2**zoom
+
+        self.zoom = zoom
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"Tile({self.zoom},{self.x},{self.y})"
+
+    @classmethod
+    def from_string(cls, tile):
+        fragments = tile.split("/")
+        return cls(int(fragments[0]), int(fragments[1]), int(fragments[2]))
+
+    def bbox(self, buffer):
+        '''Returns the bounding box for a tile
+        '''
+        return f'''ST_TileEnvelope({self.zoom}, {self.x}, {self.y}, margin=>{buffer})'''
