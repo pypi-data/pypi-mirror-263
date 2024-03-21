d751f82db64e304d80e261f75ee9902df9ce1870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 05:03:51 +0000

    Add package atproto/0.0.46/atproto-0.0.46-py3-none-any.whl

---
+++ packages/atproto/atproto-0.0.46-py3-none-any.whl/atproto_client/models/com/atproto/identity/submit_plc_operation.py
@@ -0,0 +1,24 @@
+##################################################################
+# THIS IS THE AUTO-GENERATED CODE. DON'T EDIT IT BY HANDS!
+# Copyright (C) 2023 Ilya (Marshal) <https://github.com/MarshalX>.
+# This file is part of Python atproto SDK. Licenced under MIT.
+##################################################################
+
+
+import typing as t
+
+import typing_extensions as te
+
+if t.TYPE_CHECKING:
+    from atproto_client.models.unknown_type import UnknownInputType
+from atproto_client.models import base
+
+
+class Data(base.DataModelBase):
+    """Input data model for :obj:`com.atproto.identity.submitPlcOperation`."""
+
+    operation: 'UnknownInputType'  #: Operation.
+
+
+class DataDict(te.TypedDict):
+    operation: 'UnknownInputType'  #: Operation.
