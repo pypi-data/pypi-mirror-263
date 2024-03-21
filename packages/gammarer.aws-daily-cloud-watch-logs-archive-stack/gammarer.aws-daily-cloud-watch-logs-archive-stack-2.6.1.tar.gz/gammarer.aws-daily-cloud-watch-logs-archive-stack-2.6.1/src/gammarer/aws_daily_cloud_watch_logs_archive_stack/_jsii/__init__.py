97f9de226ae5375ba51c5c1ef6ed7af917883e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 06:04:30 +0000

    Add package gammarer.aws-daily-cloud-watch-logs-archive-stack/2.6.1/gammarer.aws-daily-cloud-watch-logs-archive-stack-2.6.1.tar.gz

---
+++ packages/gammarer.aws-daily-cloud-watch-logs-archive-stack/gammarer.aws-daily-cloud-watch-logs-archive-stack-2.6.1.tar.gz/gammarer.aws-daily-cloud-watch-logs-archive-stack-2.6.1/src/gammarer/aws_daily_cloud_watch_logs_archive_stack/_jsii/__init__.py
@@ -0,0 +1,29 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+import gammarer.aws_secure_bucket._jsii
+import gammarer.aws_secure_log_bucket._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@gammarer/aws-daily-cloud-watch-logs-archive-stack",
+    "2.6.1",
+    __name__[0:-6],
+    "aws-daily-cloud-watch-logs-archive-stack@2.6.1.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
