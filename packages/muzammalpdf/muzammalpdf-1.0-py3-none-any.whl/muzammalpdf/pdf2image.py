a5c2e437c7b5045c4d07262a1c459057444d0e02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 06:05:10 +0000

    Add package muzammalpdf/1.0/muzammalpdf-1.0-py3-none-any.whl

---
+++ packages/muzammalpdf/muzammalpdf-1.0-py3-none-any.whl/muzammalpdf/pdf2image.py
@@ -0,0 +1,14 @@
+""" This module takes a pdf file and convert it to an image. """
+
+
+class Convertor:
+    """ This class takes a pdf file and converts it to an image"""
+    def convert(self, path):
+        """ Convert the given PDF to an image.
+        parameter:
+        path (string): the path to the pdf file.
+
+        Returns:
+            string: the content of the pdf file.
+        """
+        print("pdf2image")
---
+++ packages/muzammalpdf/muzammalpdf-1.0-py3-none-any.whl/muzammalpdf/pdf2text.py
@@ -0,0 +1,14 @@
+""" This module takes a pdf file and convert it to text. """
+
+
+class Convertor:
+    """ This class takes a pdf file and converts it to text. """
+    def convert(self, path):
+        """ Convert the given PDF to text.
+        parameter:
+        path (string): the path to the pdf file.
+
+        Returns:
+            string: the content of the pdf file.
+        """
+        print("pdf2text")
