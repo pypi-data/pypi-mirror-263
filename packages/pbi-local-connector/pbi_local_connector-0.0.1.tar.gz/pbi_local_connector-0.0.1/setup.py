aea1df6e8bbd3a8008b78b0d2e7f4185124e4469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 06:05:38 +0000

    Add package pbi-local-connector/0.0.1/pbi_local_connector-0.0.1.tar.gz

---
+++ packages/pbi-local-connector/pbi_local_connector-0.0.1.tar.gz/pbi_local_connector-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pbi_local_connector",
+    version="0.0.1",
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    description = "Provides utilities to read data from PowerBI dataset open locally",
+    author="ARM",
+    url="https://github.com/Alexandre-RM/pbi_local_connector",
+    install_requires=["pandas", "pyadomd"]
+)
+
+#py setup.py sdist bdist_wheel
+#twine upload --skip-existing dist/*
+
