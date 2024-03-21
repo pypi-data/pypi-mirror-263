099b2a5d051a3c9125fa2a273a2a885bf3acfda1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 03:04:08 +0000

    Add package NearBeach/0.31.6/nearbeach-0.31.6.tar.gz

---
+++ packages/NearBeach/nearbeach-0.31.6.tar.gz/nearbeach-0.31.6/src/js/components/permissions/NewPermissionSet.vue
@@ -0,0 +1,93 @@
+<template>
+	<n-config-provider :theme="getTheme(theme)">
+		<div class="card">
+			<div class="card-body">
+				<h1>New Permission Set</h1>
+				<hr/>
+
+				<div class="row">
+					<div class="col-md-4">
+						<strong>New Permission Set</strong>
+						<p class="text-instructions">
+							Please enter in a unique permission set name. Please
+							note - all values are default permission level of 0.
+						</p>
+					</div>
+					<div class="col-md-8">
+						<label>Permission Set Name</label>
+						<input
+							type="text"
+							v-model="permissionSetNameModel"
+							class="form-control"
+						/>
+					</div>
+				</div>
+
+				<!-- Submit Button -->
+				<hr/>
+				<div class="row submit-row">
+					<div class="col-md-12">
+						<a
+							href="javascript:void(0)"
+							class="btn btn-primary save-changes"
+							v-on:click="addNewPermissionSet"
+						>Create new Permission Set</a
+						>
+					</div>
+				</div>
+			</div>
+		</div>
+	</n-config-provider>
+</template>
+
+<script>
+//Import mixins
+import getThemeMixin from "../../mixins/getThemeMixin";
+
+export default {
+	name: "NewPermissionSet",
+	props: {
+		rootUrl: {
+			type: String,
+			default: "/",
+		},
+		theme: {
+			type: String,
+			default: "",
+		},
+	},
+	data() {
+		return {
+			permissionSetNameModel: "",
+		};
+	},
+	mixins: [getThemeMixin],
+	methods: {
+		addNewPermissionSet() {
+			//Data to send
+			const data_to_send = new FormData();
+			data_to_send.set(
+				"permission_set_name",
+				this.permissionSetNameModel
+			);
+
+			this.axios.post(
+				`${this.rootUrl}new_permission_set/save/`,
+				data_to_send
+			).then((response) => {
+				//Go to the new location
+				window.location.href = response.data;
+			}).catch((error) => {
+				this.$store.dispatch("newToast", {
+					header: "Error adding permission set",
+					message: `Sorry, we could not save the permission set. Error -> ${error}`,
+					extra_classes: "bg-danger",
+					delay: 0,
+				});
+			});
+		},
+	},
+};
+</script>
+
+<style scoped></style>
