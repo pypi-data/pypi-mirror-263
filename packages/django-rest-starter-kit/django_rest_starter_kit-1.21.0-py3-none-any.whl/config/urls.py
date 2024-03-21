bf548421b3ebc6c88a0cbebf3982a3b2751322bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 16:07:44 +0000

    Add package django-rest-starter-kit/1.21.0/django_rest_starter_kit-1.21.0-py3-none-any.whl

---
+++ packages/django-rest-starter-kit/django_rest_starter_kit-1.21.0-py3-none-any.whl/config/urls.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from django.urls import path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="hossain.chisty11@gmail.com"),
+        license=openapi.License(name="Apache-2.0 license"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("api/v1/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/v1/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/v1/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+]
