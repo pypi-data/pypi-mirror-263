56817528cb3e2fb5752410069d8b38a41c801b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 05:04:18 +0000

    Add package sample-738/0.0.1/sample_738-0.0.1.tar.gz

---
+++ packages/sample-738/sample_738-0.0.1.tar.gz/sample_738-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="sample_738",
+    version="0.0.1",
+    author="AST-LW",
+    description="This is a sample package",
+    packages=find_packages()
+)
