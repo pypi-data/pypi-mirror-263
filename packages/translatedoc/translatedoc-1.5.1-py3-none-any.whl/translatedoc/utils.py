7b62bde1b89fcf99323ad92d182ed91db11369be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 16:07:27 +0000

    Add package translatedoc/1.5.1/translatedoc-1.5.1-py3-none-any.whl

---
+++ packages/translatedoc/translatedoc-1.5.1-py3-none-any.whl/translatedoc/utils.py
@@ -0,0 +1,36 @@
+"""ユーティリティ関数置き場。"""
+
+import logging
+import pathlib
+import sys
+
+import tqdm
+
+
+def check_overwrite(output_path: pathlib.Path, force: bool) -> bool:
+    """上書き確認。"""
+    if output_path.exists() and not force:
+        with tqdm.tqdm.external_write_mode():
+            print(f"Output path already exists: {output_path}", file=sys.stderr)
+            try:
+                input_ = input("Overwrite? [y/N] ")
+            except EOFError:
+                input_ = ""
+            if input_ != "y":
+                print("Skipped.", file=sys.stderr)
+                return False
+    return True
+
+
+def set_verbose(verbose: bool) -> None:
+    """ログレベルをDEBUGに設定する。"""
+    if verbose:
+        logging.getLogger("translatedoc").setLevel(logging.DEBUG)
+
+
+class TqdmLoggingHandler(logging.StreamHandler):
+    """tqdm対応のStreamHandler。"""
+
+    def emit(self, record):
+        with tqdm.tqdm.external_write_mode(file=self.stream):
+            super().emit(record)
