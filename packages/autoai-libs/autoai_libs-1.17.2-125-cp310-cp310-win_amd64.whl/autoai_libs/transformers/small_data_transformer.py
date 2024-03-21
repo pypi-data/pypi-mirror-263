a7e4d20db5369f8f407be11ddfe7efa7e80effc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 03:05:21 +0000

    Add package autoai-libs/1.17.2/autoai_libs-1.17.2-125-cp310-cp310-win_amd64.whl

---
+++ packages/autoai-libs/autoai_libs-1.17.2-125-cp310-cp310-win_amd64.whl/autoai_libs/transformers/small_data_transformer.py
@@ -0,0 +1,30 @@
+################################################################################
+# IBM Confidential
+# OCO Source Materials
+# 5737-H76, 5725-W78, 5900-A1R, 5737-L65
+# (c) Copyright IBM Corp. 2021-2024. All Rights Reserved.
+# The source code for this program is not published or otherwise divested of its trade secrets,
+# irrespective of what has been deposited with the U.S. Copyright Office.
+################################################################################
+import numpy as np
+import logging
+
+from .general_transformer import AutoAITransformer
+
+
+class SmallDataTransformer(AutoAITransformer):
+    def __init__(self):
+        super().__init__()
+
+    def is_valid(self, X: np.ndarray) -> bool:
+        try:
+            self.fit_transform(X)
+            return True
+        except Exception as e:
+            logging.error(
+                "Transfomer {0}, error in fit_transform, type(X): {1}, error={2!s}".format(
+                    self.__class__.__name__, type(X), e
+                ),
+                exc_info=True,
+            )
+            return False
