1e99fc46749c55d9be2c08489efee62d46571072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 03:04:35 +0000

    Add package chellow/1710934022.0.0/chellow-1710934022.0.0-py3-none-any.whl

---
+++ packages/chellow/chellow-1710934022.0.0-py3-none-any.whl/chellow/reports/report_csv_llfcs.py
@@ -0,0 +1,66 @@
+import csv
+import os
+import threading
+import traceback
+
+from flask import g
+
+from sqlalchemy.orm import joinedload
+
+import chellow.dloads
+from chellow.models import Llfc, Session
+from chellow.utils import hh_format
+from chellow.views import chellow_redirect
+
+
+def content(user):
+    f = writer = None
+    try:
+        with Session() as sess:
+            running_name, finished_name = chellow.dloads.make_names("llfcs.csv", user)
+            f = open(running_name, mode="w", newline="")
+            writer = csv.writer(f, lineterminator="\n")
+            writer.writerow(
+                (
+                    "Chellow Id",
+                    "DNO Code",
+                    "Code",
+                    "Description",
+                    "Voltage Level",
+                    "Is Substation?",
+                    "Is Import?",
+                    "Valid From",
+                    "Valid To",
+                )
+            )
+
+            for llfc in (
+                sess.query(Llfc)
+                .order_by(Llfc.id)
+                .options(joinedload(Llfc.dno), joinedload(Llfc.voltage_level))
+            ):
+                writer.writerow(
+                    (
+                        str(llfc.id),
+                        llfc.dno.dno_code,
+                        llfc.code,
+                        llfc.description,
+                        llfc.voltage_level.code,
+                        llfc.is_substation,
+                        llfc.is_import,
+                        hh_format(llfc.valid_from),
+                        hh_format(llfc.valid_to),
+                    )
+                )
+    except BaseException:
+        writer.writerow([traceback.format_exc()])
+    finally:
+        if f is not None:
+            f.close()
+            os.rename(running_name, finished_name)
+
+
+def do_get(sess):
+    args = (g.user,)
+    threading.Thread(target=content, args=args).start()
+    return chellow_redirect("/downloads", 303)
