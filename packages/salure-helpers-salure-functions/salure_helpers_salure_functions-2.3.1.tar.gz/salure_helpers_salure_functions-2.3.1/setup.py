cd15f53d68312dc1510cc9aed312b2f38a41cc0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 04:05:26 +0000

    Add package salure-helpers-salure-functions/2.3.1/salure_helpers_salure_functions-2.3.1.tar.gz

---
+++ packages/salure-helpers-salure-functions/salure_helpers_salure_functions-2.3.1.tar.gz/salure_helpers_salure_functions-2.3.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name='salure_helpers_salure_functions',
+    version='2.3.1',
+    description='Helpful functions from Salure',
+    long_description='Helpful functions from Salure',
+    author='D&A Salure',
+    author_email='support@salureconnnect.com',
+    packages=["salure_helpers.salure_functions"],
+    license='Salure License',
+    install_requires=[
+        'pandas>=1,<3',
+        'requests>=2,<=3',
+        'pyarrow>=10,<11'
+    ],
+    zip_safe=False,
+)
