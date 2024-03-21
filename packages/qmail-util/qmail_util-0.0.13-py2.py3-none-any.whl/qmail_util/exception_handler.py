36900fa77f0f791b1279397c3799a5fe31a88fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 17:04:17 +0000

    Add package qmail-util/0.0.13/qmail_util-0.0.13-py2.py3-none-any.whl

---
+++ packages/qmail-util/qmail_util-0.0.13-py2.py3-none-any.whl/qmail_util/exception_handler.py
@@ -0,0 +1,52 @@
+"""
+  ehandler
+
+  well-behaved exception handler for python cli commands
+
+"""
+
+import sys
+from traceback import format_exception
+
+
+def exception_handler(
+    exception_type,
+    exception,
+    traceback,
+    debug_hook=sys.excepthook,
+):
+    logger = ExceptionHandler.logger
+
+    elist = format_exception(exception)
+    traceback_msg = "".join(elist[:-1]).rstrip("\n")
+    error_msg = elist[-1].rstrip("\n")
+
+    if logger:
+        logger.debug(traceback_msg)
+        logger.error(error_msg)
+
+    if ExceptionHandler.debug:
+        debug_hook(
+            exception_type,
+            exception,
+            traceback,
+        )
+
+    elif not logger:
+        sys.stderr.write(error_msg + "\n")
+        sys.stderr.flush()
+
+    sys.exit(-1)
+
+
+class ExceptionHandler:
+    installed = False
+    debug = None
+    logger = None
+
+    def __init__(self, debug=False, logger=False):
+        self.__class__.debug = debug
+        self.__class__.logger = logger
+        if not self.installed:
+            sys.excepthook = exception_handler
+            self.__class__.installed = True
---
+++ packages/qmail-util/qmail_util-0.0.13-py2.py3-none-any.whl/qmail_util/version.py
@@ -0,0 +1,4 @@
+__version__ = "0.0.13"
+__author__ = "Matt Krueger"
+__email__ = "mkrueger@rstms.net"
+__timestamp__ = "2024-03-21 04:07:13-06:00"
