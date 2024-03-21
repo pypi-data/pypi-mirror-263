f6775878d89c9ceec89c8d90904b7641b8b27506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 15:03:57 +0000

    Add package unstract-sdk/0.15.2/unstract_sdk-0.15.2-py3-none-any.whl

---
+++ packages/unstract-sdk/unstract_sdk-0.15.2-py3-none-any.whl/unstract/sdk/exceptions.py
@@ -0,0 +1,10 @@
+class SdkError(Exception):
+    DEFAULT_MESSAGE = "Something went wrong"
+
+    def __init__(self, message: str = DEFAULT_MESSAGE):
+        super().__init__(message)
+        # Make it user friendly wherever possible
+        self.message = message
+
+    def __str__(self) -> str:
+        return self.message
