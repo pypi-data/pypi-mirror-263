a3ff448d0b1efe1952d2f656077637ad7dbc3373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 15:04:00 +0000

    Add package terminal-notifier/0.2/terminal_notifier-0.2.tar.gz

---
+++ packages/terminal-notifier/terminal_notifier-0.2.tar.gz/terminal_notifier-0.2/README.md
@@ -0,0 +1,18 @@
+# terminal-notifier
+  A command-line tool that provides multi-channel notifications (desktop, phone, email, popup) based on command outcomes (success, failure, user input required, crash).
+
+
+## Installation
+
+To install Command Notifier, you can use pip:
+
+```bash
+pip todecide
+
+
+# for DEV
+- to build and install
+ python .\setup.py sdist bdist_wheel
+
+ pip uninstall terminal-notifier
+ pip install . 
---
+++ packages/terminal-notifier/terminal_notifier-0.2.tar.gz/terminal_notifier-0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="terminal_notifier",  # Replace with your package name
+    version="0.2",  # Replace with your package version
+    author="Anish Tipnis",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "plyer",
+        "playsound"
+    ],
+    entry_points={
+        "console_scripts": [
+            "terminal_notifier=terminal_notifier.__main__:main"
+        ]
+    },
+
+    include_package_data=True,  # Include package data specified by MANIFEST.in
+    package_data={
+        "": ["*.mp3"],  # Include all mp3 files in the package
+    }
+)
