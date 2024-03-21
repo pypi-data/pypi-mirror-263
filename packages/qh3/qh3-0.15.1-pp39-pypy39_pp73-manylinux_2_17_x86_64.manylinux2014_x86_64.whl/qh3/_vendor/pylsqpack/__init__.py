f33f06c6091e64a75c77aa7b183b6e6029d90593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 16:07:18 +0000

    Add package qh3/0.15.1/qh3-0.15.1-pp39-pypy39_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/qh3/qh3-0.15.1-pp39-pypy39_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/qh3/_vendor/pylsqpack/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+
+from ._binding import (
+    Decoder,
+    DecoderStreamError,
+    DecompressionFailed,
+    Encoder,
+    EncoderStreamError,
+    StreamBlocked,
+)
+
+__version__ = "1.0.3"
