0442988a8f286d5a3518bfdc9b336dae9eab4c97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 04:04:40 +0000

    Add package py4jps/1.41.1a0/py4jps-1.41.1a0.tar.gz

---
+++ packages/py4jps/py4jps-1.41.1a0.tar.gz/py4jps-1.41.1a0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='py4jps',
+    version='1.41.1a',
+    author='Daniel Nurkowski',
+    author_email='danieln@cmclinnovations.com',
+    license='MIT',
+    python_requires='>=3.5',
+    description="Py4jps is a thin Python wrapper for the TheWorldAvatar project.",
+    url="https://github.com/cambridge-cares/TheWorldAvatar/tree/develop/JPS_BASE_LIB/python_wrapper",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=find_namespace_packages(exclude=['tests', 'tests.*']),
+    install_requires=['py4j==0.10.9.1','docopt','concurrent_log_handler', 'pydantic', 'rdflib==7.0.0', 'requests'],
+    include_package_data= True,
+    entry_points={
+        'console_scripts': [
+            'jpsrm = py4jps.resRegistry.resManager:start',
+        ]
+     },
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Programming Language :: Python :: 3.9"
+    ]
+)
