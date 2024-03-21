65dc1f83f451d9712dff2faf4354a6dd0cc6983c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 05:04:26 +0000

    Add package qsharp/1.2.3.dev0/qsharp-1.2.3.dev0-cp37-abi3-manylinux_2_31_aarch64.whl

---
+++ packages/qsharp/qsharp-1.2.3.dev0-cp37-abi3-manylinux_2_31_aarch64.whl/qsharp/__init__.py
@@ -0,0 +1,45 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+from ._qsharp import (
+    init,
+    eval,
+    run,
+    compile,
+    estimate,
+    set_quantum_seed,
+    set_classical_seed,
+    dump_machine,
+    StateDump,
+    ShotResult,
+)
+
+from ._native import Result, Pauli, QSharpError, TargetProfile
+
+# IPython notebook specific features
+try:
+    if __IPYTHON__:  # type: ignore
+        from ._ipython import register_magic, enable_classic_notebook_codemirror_mode
+
+        register_magic()
+        enable_classic_notebook_codemirror_mode()
+except NameError:
+    pass
+
+
+__all__ = [
+    "init",
+    "eval",
+    "run",
+    "set_quantum_seed",
+    "set_classical_seed",
+    "dump_machine",
+    "compile",
+    "estimate",
+    "Result",
+    "Pauli",
+    "QSharpError",
+    "TargetProfile",
+    "StateDump",
+    "ShotResult",
+]
