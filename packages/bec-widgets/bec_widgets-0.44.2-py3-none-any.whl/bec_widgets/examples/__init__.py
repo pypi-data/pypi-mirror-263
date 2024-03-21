e7537af6953dfeb18d0f0eafeecb2fea3ef409b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 03:04:39 +0000

    Add package bec-widgets/0.44.2/bec_widgets-0.44.2-py3-none-any.whl

---
+++ packages/bec-widgets/bec_widgets-0.44.2-py3-none-any.whl/bec_widgets/examples/__init__.py
@@ -0,0 +1,9 @@
+from .motor_movement import (
+    MotorControlApp,
+    MotorControlMap,
+    MotorControlPanel,
+    MotorControlPanelRelative,
+    MotorControlPanelAbsolute,
+    MotorCoordinateTable,
+    MotorThread,
+)
