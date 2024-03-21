1bc5b78de621ec54ff021003b2ad1cc13e213e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 05:04:31 +0000

    Add package nomad-media-pip/0.0.93/nomad_media_pip-0.0.93.tar.gz

---
+++ packages/nomad-media-pip/nomad_media_pip-0.0.93.tar.gz/nomad_media_pip-0.0.93/nomad_media_pip/common/account_authentication/refresh_token.py
@@ -0,0 +1,39 @@
+from nomad_media_pip.exceptions.api_exception_handler import _api_exception_handler
+
+import json, requests
+
+'''
+ * Refresh Token
+ *
+ * @param {string} REFRESH_TOKEN | The refresh token
+ *
+ * @returns {string} Authentication token
+'''
+def _refresh_token(URL, REFRESH_TOKEN, DEBUG):
+    API_URL = f"{URL}/account/refresh-token"
+
+    # Create header for the request
+    HEADERS = {
+        'Content-Type': 'application/json'
+    }
+
+    # Build the payload BODY
+    BODY = {
+        "refreshToken": REFRESH_TOKEN
+    }
+
+    if DEBUG:
+        print(f"API URL: {API_URL}\nMETHOD: POST\nBODY: {json.dumps(BODY, indent=4)}")
+
+    try:
+        # Send the request
+        RESPONSE = requests.post(API_URL, headers= HEADERS, data= json.dumps(BODY))
+
+        if not RESPONSE.ok:
+            raise Exception()
+
+        return RESPONSE.json()
+
+    except:
+        _api_exception_handler(RESPONSE, "Refresh Token failed")
+
