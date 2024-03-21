a6b5628a5fccd227a603d52f23acbe3f1ece8dfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 16:07:22 +0000

    Add package patched-yolo-infer/1.1.0/patched_yolo_infer-1.1.0.tar.gz

---
+++ packages/patched-yolo-infer/patched_yolo_infer-1.1.0.tar.gz/patched_yolo_infer-1.1.0/setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+pwd = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(pwd, "patched_yolo_infer/README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+
+VERSION = '1.1.0'
+DESCRIPTION = '''YOLO-Patch-Based-Inference for detection/segmentation of small objects in images.'''
+
+setup(
+    name="patched_yolo_infer",
+    version=VERSION,
+    license="MIT",
+    url="https://github.com/Koldim2001/YOLO-Patch-Based-Inference",
+    author="Koldim2001",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+        'numpy',
+        'opencv-python',
+        'matplotlib',
+        'torch',
+        'ultralytics'
+    ],
+    keywords=[
+        "python",
+        "yolov8",
+        "yolov9",
+        "rtdetr",
+        "sam",
+        "object detection",
+        "instance segmentation",
+        "patch-based inference",
+        "small object detection",
+        "yolov8-seg",
+        "image patching",
+        "yolo visualization",
+        "slice-based inference",
+        "slicing inference",
+        "inference visualization",
+        "patchify",
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
