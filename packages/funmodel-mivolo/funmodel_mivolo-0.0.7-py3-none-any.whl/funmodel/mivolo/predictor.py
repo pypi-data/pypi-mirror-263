d5ee41de398b0dac3de61a92a66fbcb950661cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 03:05:02 +0000

    Add package funmodel-mivolo/0.0.7/funmodel_mivolo-0.0.7-py3-none-any.whl

---
+++ packages/funmodel-mivolo/funmodel_mivolo-0.0.7-py3-none-any.whl/funmodel/mivolo/predictor.py
@@ -0,0 +1,80 @@
+import numpy as np
+from fundrive.download import simple_download
+from fundrive.drives.oss import public_oss_url
+from funmodel.core.predict.image import ImagePredictModel
+from funmodel.mivolo.model.mi_volo import MiVOLO
+from funmodel.mivolo.model.yolo_detector import Detector
+from funmodel.mivolo.structures import PersonAndFaceResult
+
+
+class MivoloPredictor(ImagePredictModel):
+    def __init__(
+        self,
+        with_persons=False,
+        disable_faces=False,
+        device="cpu",
+        verbose: bool = False,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(model_name="mivolo", *args, **kwargs)
+        self.detector_model = None
+        self.age_gender_model = None
+        self.load(with_persons, disable_faces, device, verbose)
+
+    def load(
+        self,
+        with_persons=False,
+        disable_faces=False,
+        device="cpu",
+        verbose: bool = False,
+        *args,
+        **kwargs,
+    ):
+        checkpoint = f"{self.cache_path}/mivolo_imbd.pth.tar"
+        detector_weights = f"{self.cache_path}/yolov8x_person_face.pt"
+        simple_download(
+            url=public_oss_url(path="models/mivolo/mivolo_imbd.pth.tar"),
+            filepath=checkpoint,
+        )
+        simple_download(
+            url=public_oss_url(path="models/mivolo/yolov8x_person_face.pt"),
+            filepath=detector_weights,
+        )
+
+        self.detector_model = Detector(detector_weights, device, verbose=verbose)
+        self.age_gender_model = MiVOLO(
+            checkpoint,
+            device,
+            half=True,
+            use_persons=with_persons,
+            disable_faces=disable_faces,
+            verbose=verbose,
+        )
+
+    def predict(self, image: np.ndarray, draw=False, *args, **kwargs):
+        detected_objects: PersonAndFaceResult = self.detector_model.predict(image)
+        self.age_gender_model.predict(image, detected_objects)
+
+        out_im = None
+        if draw:
+            out_im = detected_objects.plot()
+        result = []
+        for i in range(detected_objects.n_objects):
+            result.append(
+                {
+                    "age": detected_objects.ages[i],
+                    "gender": detected_objects.genders[i],
+                    "gender_score": detected_objects.gender_scores[i],
+                    "body": detected_objects.yolo_results[i]
+                    .boxes.xywh.cpu()
+                    .numpy()[0]
+                    .tolist(),
+                    "cls": detected_objects.yolo_results[i]
+                    .boxes.cls.cpu()
+                    .numpy()[0]
+                    .tolist(),
+                }
+            )
+        result = [res for res in result if res["cls"] == 1]
+        return result, out_im
