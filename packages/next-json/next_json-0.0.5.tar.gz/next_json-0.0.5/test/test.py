7ffd8900bb4fb473bd88a6ac1e73249d91eb020b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 15:04:05 +0000

    Add package next-json/0.0.5/next_json-0.0.5.tar.gz

---
+++ packages/next-json/next_json-0.0.5.tar.gz/next_json-0.0.5/test/test.py
@@ -0,0 +1,16 @@
+from next_json.next_json_obj import NextJson
+
+foo = NextJson()
+foo.on('set', lambda k, v: print(f'检测到属性修改: key={k} value={v}'))
+foo.once('set', lambda k, v: print(f'检测到属性修改(这个函数只触发一次): key={k} value={v}'))
+foo.on('get', lambda k, v: print(f'检测到属性获取: key={k} default={v}'))
+foo.on('del', lambda k: print(f'检测到属性删除: key={k}'))
+
+foo.a = 1
+foo.c.d = '牛逼'
+foo['hh'] = '非常牛逼'
+del foo.hh
+# del不存在的属性不会报错
+del foo.hh
+
+print(foo)
