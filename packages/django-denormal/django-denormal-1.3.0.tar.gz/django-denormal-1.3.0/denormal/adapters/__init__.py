927d56de0f04ae17f31c933f137f8db89a0932c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 04:05:15 +0000

    Add package django-denormal/1.3.0/django-denormal-1.3.0.tar.gz

---
+++ packages/django-denormal/django-denormal-1.3.0.tar.gz/django-denormal-1.3.0/denormal/adapters/__init__.py
@@ -0,0 +1,22 @@
+from django.utils.module_loading import import_string
+from django import VERSION as DJANGO_VERSION
+
+SUPPORTED_DJANGO_VERSIONS = [
+    (2, 2),
+    (3, 0),
+    (3, 1),
+    (3, 2),
+    (4, 0),
+]
+
+if DJANGO_VERSION[:2] not in SUPPORTED_DJANGO_VERSIONS:
+    raise NotImplementedError(
+        'No working version for django %s.%s yet. Welcome to the land of '
+        'opportunities!' % DJANGO_VERSION[:2]
+    )
+
+
+SpecificDjango = import_string(
+    'denormal.adapters.django%sp%s.SpecificDjango' % DJANGO_VERSION[:2])
+
+this_django = SpecificDjango()
---
+++ packages/django-denormal/django-denormal-1.3.0.tar.gz/django-denormal-1.3.0/setup.py
@@ -0,0 +1,52 @@
+import os
+from setuptools import find_packages, setup
+
+
+README = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.rst')
+
+
+DEPENDENCIES = [
+    'django >=2.2, <4.1',
+]
+
+
+DEPENDENCY_LINKS = [
+    # Add externally hosted packages like so:
+    # e.g. http://github.com/[USERNAME]/[REPO]/tarball/[BRANCH]#egg=[EGG_NAME]
+]
+
+
+setup(
+    name='django-denormal',
+    version='1.3.0',
+    description='Django automatic denormalization toolkit',
+    author='trashnroll',
+    author_email='trashnroll@gmail.com',
+    install_requires=DEPENDENCIES,
+    dependency_links=DEPENDENCY_LINKS,
+    setup_requires=[],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='django denormalization',
+    packages=find_packages()
+)
