f7b897a2733c8a8b9706a1cd3fc58f20452953bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 15:04:14 +0000

    Add package boomtils/1.70/boomtils-1.70.tar.gz

---
+++ packages/boomtils/boomtils-1.70.tar.gz/boomtils-1.70/README.md
@@ -0,0 +1,20 @@
+# Boomtils
+
+please send help
+
+useless package made by real.iusebako on discord
+
+## how does it work?
+
+like this
+```python
+from boomtils.lay_down_the_beat import letslaydownthebeat
+
+letslaydownthebeat(5)
+```
+
+
+## please excuse me for my horrible readme skills
+thank you
+
+### update 1.70 had to be released due to the readme being incorrectly capitalized. whoopsies!
---
+++ packages/boomtils/boomtils-1.70.tar.gz/boomtils-1.70/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="boomtils",
+    version="1.70",
+    author="iusebako",
+    author_email="leaf5ter@tuta.io",
+    description="The most useless package ever created.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/real-kwellercat/boomtils.py",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
