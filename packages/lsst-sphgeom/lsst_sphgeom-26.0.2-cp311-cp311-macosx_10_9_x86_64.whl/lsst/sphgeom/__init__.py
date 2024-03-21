3cfcbd5ead85be07b64282500972d16d24a83b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 04:04:45 +0000

    Add package lsst-sphgeom/26.0.2/lsst_sphgeom-26.0.2-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/lsst-sphgeom/lsst_sphgeom-26.0.2-cp311-cp311-macosx_10_9_x86_64.whl/lsst/sphgeom/__init__.py
@@ -0,0 +1,33 @@
+#
+# LSST Data Management System
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+# See the COPYRIGHT file
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <https://www.lsstcorp.org/LegalNotices/>.
+#
+
+"""lsst.sphgeom
+"""
+
+from ._healpixPixelization import *
+from ._sphgeom import *
+from ._sphgeom import Pixelization
+from ._yaml import *
+from .pixelization_abc import *
+from .version import *
+
+PixelizationABC.register(Pixelization)
