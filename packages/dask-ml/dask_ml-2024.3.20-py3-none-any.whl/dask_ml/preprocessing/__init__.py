5c50b25999264cc2d555f487d9f8e03255d80875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 03:05:30 +0000

    Add package dask-ml/2024.3.20/dask_ml-2024.3.20-py3-none-any.whl

---
+++ packages/dask-ml/dask_ml-2024.3.20-py3-none-any.whl/dask_ml/preprocessing/__init__.py
@@ -0,0 +1,30 @@
+"""Utilties for Preprocessing data.
+"""
+
+from ._block_transformer import BlockTransformer
+from ._encoders import OneHotEncoder
+from .data import (
+    Categorizer,
+    DummyEncoder,
+    MinMaxScaler,
+    OrdinalEncoder,
+    PolynomialFeatures,
+    QuantileTransformer,
+    RobustScaler,
+    StandardScaler,
+)
+from .label import LabelEncoder
+
+__all__ = [
+    "StandardScaler",
+    "MinMaxScaler",
+    "RobustScaler",
+    "QuantileTransformer",
+    "Categorizer",
+    "DummyEncoder",
+    "OrdinalEncoder",
+    "LabelEncoder",
+    "OneHotEncoder",
+    "PolynomialFeatures",
+    "BlockTransformer",
+]
