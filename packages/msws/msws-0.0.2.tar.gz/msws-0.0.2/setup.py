12b98f3d5f1cc3835e32b4482864e0f793527e13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 14:03:48 +0000

    Add package msws/0.0.2/msws-0.0.2.tar.gz

---
+++ packages/msws/msws-0.0.2.tar.gz/msws-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2' 
+DESCRIPTION = 'Python package for generating the random number using Middle Square Weyl sequence'
+LONG_DESCRIPTION = '''The msws package is designed to generate random numbers using the Middle Square Weyl sequence algorithm.
+This algorithm offers a methodical approach to producing pseudo-random numbers with a focus on simplicity and efficiency.
+The module offers functionalities to both generate random numbers and seed values necessary for the process.
+'''
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="msws", 
+        version=VERSION,
+        author="Garv Saxena",
+        author_email="garvsaxena185@gmail.com",
+        url  = "https://github.com/Volcano-Dragon/msws",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[],        
+        keywords=['PRNG', 'MSWS', 'Weyl sequence', 'Middle square'],
+        classifiers= [
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
