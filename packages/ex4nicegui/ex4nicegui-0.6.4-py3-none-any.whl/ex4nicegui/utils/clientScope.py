ed86233d3ee1d97efabd36ac4dc63e922e50dd65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 16:07:23 +0000

    Add package ex4nicegui/0.6.4/ex4nicegui-0.6.4-py3-none-any.whl

---
+++ packages/ex4nicegui/ex4nicegui-0.6.4-py3-none-any.whl/ex4nicegui/utils/clientScope.py
@@ -0,0 +1,43 @@
+from typing import Dict
+from signe.core.scope import ScopeSuite
+from nicegui import Client, context as ng_context
+
+
+_TClientID = str
+
+
+class NgScopeSuite(ScopeSuite):
+    def __init__(self) -> None:
+        super().__init__()
+        self._top_scope = self.scope()
+        self._top_scope.on()
+
+
+class NgClientScopeManager:
+    def __init__(self) -> None:
+        self._client_scope_map: Dict[_TClientID, NgScopeSuite] = {}
+
+    def new_scope(self, detached: bool = False):
+        return self.get_current_scope().scope(detached)
+
+    def get_current_scope(self):
+        # if len(ng_context.get_slot_stack()) <= 0:
+        #     return
+
+        client = ng_context.get_client()
+        # if client.shared:
+        #     return
+
+        if client.id not in self._client_scope_map:
+            self._client_scope_map[client.id] = NgScopeSuite()
+
+            @client.on_disconnect
+            def _(e: Client):
+                if e.id in self._client_scope_map:
+                    self._client_scope_map[e.id]._top_scope.dispose()
+                    del self._client_scope_map[e.id]
+
+        return self._client_scope_map[client.id]
+
+
+_CLIENT_SCOPE_MANAGER = NgClientScopeManager()
