214240cf760c2c6a83369f2b2a3649763b635a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-21 15:04:15 +0000

    Add package composition-vectorizer/0.1.2/composition_vectorizer-0.1.2.tar.gz

---
+++ packages/composition-vectorizer/composition_vectorizer-0.1.2.tar.gz/composition_vectorizer-0.1.2/composition_vectorizer/d1.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pickle
+from importlib.resources import path
+
+
+
+with path('composition_vectorizer.orderings', '1d_orderings.pkl') as path:
+    with open(path, 'rb') as fid:
+        orders_dict = pickle.load(fid)
+
+
+
+
+
+
+def featurizer_1d(comps, order, el_list = []):
+        if order not in ['alphabet','periodic','pettifor','m-pettifor']:
+           raise ValueError("order must be one of ['alphabet','periodic','pettifor','m-pettifor']")
+        
+        comps = pymatgen_comp(comps)
+        if len(el_list) == 0:
+            all_eles = []
+            for c in comps:
+                all_eles += list(c.get_el_amt_dict().keys())
+            eles = np.array(sorted(all_eles,key = lambda x : orders_dict[order].index(x)))
+        else:
+            eles = np.array(sorted(el_list,key = lambda x : orders_dict[order].index(x)))
+          
+        
+        all_vecs = np.zeros([len(comps), len(eles)])
+        for i, c in enumerate(comps):
+            for k, v in c.get_el_amt_dict().items(): 
+                j = np.argwhere(eles == k)
+                all_vecs[i, j] = v
+
+        all_vecs = all_vecs / np.sum(all_vecs, axis=1).reshape(-1, 1)
+        all_vecs = np.array(all_vecs, dtype=np.float32)
+
+        return (all_vecs,eles)
