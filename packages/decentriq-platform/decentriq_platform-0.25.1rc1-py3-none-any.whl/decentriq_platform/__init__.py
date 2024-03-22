a9b94463239a11d9bb5ee4950ce22b140a603d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 05:04:26 +0000

    Add package decentriq-platform/0.25.1rc1/decentriq_platform-0.25.1rc1-py3-none-any.whl

---
+++ packages/decentriq-platform/decentriq_platform-0.25.1rc1-py3-none-any.whl/decentriq_platform/__init__.py
@@ -0,0 +1,98 @@
+"""
+.. include:: ../../decentriq_platform_docs/gcg_getting_started.md
+___
+"""
+__docformat__ = "restructuredtext"
+__pdoc__ = {
+    "api": False,
+    "attestation": True,
+    "authentication": True,
+    "builders": False,
+    "certs": False,
+    "client": False,
+    "compute": False,
+    "config": False,
+    "container": True,
+    "graphql": False,
+    "helpers": False,
+    "node": True,
+    "permission": False,
+    "post": True,
+    "proto": False,
+    "s3_sink": True,
+    "data_source_s3": True,
+    "data_science": True,
+    "lookalike_media": True,
+    "dataset_sink": True,
+    "session": True,
+    "sql": True,
+    "storage": True,
+    "types": True,
+    "verification": False,
+    "data_source_snowflake": True,
+    "google_dv_360_sink": True,
+    "azure_blob_storage": True,
+    "salesforce": True,
+    "data_lab": True,
+    "permutive": True,
+}
+
+from .client import Client, create_client, Session
+from .builders import DataRoomBuilder, DataRoomCommitBuilder, GovernanceProtocol
+from .data_lab import DataLabBuilder
+from .lookalike_media import LookalikeMediaDcrBuilder, LookalikeMediaDcr
+from .compute import Noop, StaticContent
+from .permission import Permissions
+from .storage import Key
+from .attestation import enclave_specifications, EnclaveSpecifications
+
+from .endorsement import Endorser
+from .keychain import Keychain, KeychainEntry
+from .types import DataLabDatasetType
+
+from . import data_science
+from . import lookalike_media
+
+
+__all__ = [
+    "create_client",
+    "Client",
+    "Session",
+    "DataRoomBuilder",
+    "DataRoomCommitBuilder",
+    "DataLabBuilder",
+    "LookalikeMediaDcrBuilder",
+    "LookalikeMediaDcr",
+    "DataLabDatasetType",
+    "Permissions",
+    "GovernanceProtocol",
+    "enclave_specifications",
+    "EnclaveSpecifications",
+    "Key",
+    "KeychainEntry",
+    "StaticContent",
+    "Noop",
+    "post",
+    "sql",
+    "container",
+    "s3_sink",
+    "data_source_s3",
+    "dataset_sink",
+    "meta_sink",
+    "google_dv_360_sink",
+    "data_science",
+    "lookalike_media",
+    "storage",
+    "attestation",
+    "types",
+    "authentication",
+    "session",
+    "node",
+    "Endorser",
+    "Keychain",
+    "data_source_snowflake",
+    "azure_blob_storage",
+    "salesforce",
+    "data_lab",
+    "permutive",
+]
