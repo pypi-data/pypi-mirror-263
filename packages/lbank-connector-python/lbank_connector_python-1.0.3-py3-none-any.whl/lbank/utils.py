c0ee1e7b8648c37755379f89f628a50f052f6120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 03:05:45 +0000

    Add package lbank-connector-python/1.0.3/lbank_connector_python-1.0.3-py3-none-any.whl

---
+++ packages/lbank-connector-python/lbank_connector_python-1.0.3-py3-none-any.whl/lbank/utils.py
@@ -0,0 +1,37 @@
+import hashlib
+import logging
+import random
+import string
+import time
+
+
+def get_timestamp() -> str:
+    return str(int(time.time() * 1000)).split(".", maxsplit=1)[0]
+
+
+def random_str() -> str:
+    num = string.ascii_letters + string.digits
+    return "".join(random.sample(num, 35))
+
+
+def build_md5(payload: dict) -> str:
+    """
+    @param payload: request form
+    @return:
+    """
+    params = [k + '=' + str(payload[k]) for k in sorted(payload.keys())]
+    params = '&'.join(params)
+    msg = hashlib.md5(params.encode("utf8")).hexdigest().upper()
+    return msg
+
+
+def init_logger(level, log_file: str = None):
+    logging.Formatter.converter = time.gmtime  # date time in GMT/UTC
+    logging.basicConfig(
+        level=level,
+        filename=log_file,
+        # format="[%(asctime)s] %(process)d %(thread)d {%(name)s} %(levelname)s [%(module)s:%(lineno)s:%(funcName)s] %("
+        #        "message)s",
+        format="[%(asctime)s.%(msecs)03d] UTC %(levelname)s %(name)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
