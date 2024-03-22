6b827db2c2a39f5a115a86400150e994c910c52f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 03:05:46 +0000

    Add package open-interview/1.0.7/open_interview-1.0.7-py3-none-any.whl

---
+++ packages/open-interview/open_interview-1.0.7-py3-none-any.whl/openinterview/__init__.py
@@ -0,0 +1,16 @@
+from .manager import InterviewManager
+from .models.claude import InterviewClaude
+from .models.gpt import InterviewGPT
+from .utils.doc_manager import DocumentCreator
+from .utils.file_manager import load_file_content
+from .utils.prompter import create_system_prompt, create_base_prompt
+from .modules.voice.google import google_tts, google_stt, save_google_tts
+
+try:
+    from openinterview.modules.voice.openai import openai_tts, openai_stt
+except ImportError:
+    pass
+
+
+__version__ = "1.0.7"
+__author__ = "daniel park <parkminwoo1991@gmail.com>"
