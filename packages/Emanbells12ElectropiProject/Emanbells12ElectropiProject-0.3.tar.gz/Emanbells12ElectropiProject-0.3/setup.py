b3b91c869b800a3d135f382a213b270a1070f558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 06:05:53 +0000

    Add package Emanbells12ElectropiProject/0.3/Emanbells12ElectropiProject-0.3.tar.gz

---
+++ packages/Emanbells12ElectropiProject/Emanbells12ElectropiProject-0.3.tar.gz/Emanbells12ElectropiProject-0.3/setup.py
@@ -0,0 +1,14 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Emanbells12ElectropiProject',
+    version='0.3',
+    packages=find_packages(),
+    description='This is my project for the python course of Electro Pi initiative code is provided in github:https://github.com/emanbell12/dataprepkitElectroPi.git',
+    install_requires=[
+        'pandas',
+        'numpy'
+    ],
+)
