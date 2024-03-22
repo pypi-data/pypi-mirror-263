e059be9173b1fceea49172dca577558806991f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 05:04:14 +0000

    Add package CognexNativePy/1.0.1/CognexNativePy-1.0.1.tar.gz

---
+++ packages/CognexNativePy/CognexNativePy-1.0.1.tar.gz/CognexNativePy-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CognexNativePy',
+    version='1.0.1',
+    description='A Python library for communicating with Cognex In-Sight vision systems. Wrapper of the native commands',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Florian LOBERT',
+    url='https://github.com/FLo-ABB/CognexNativePy',
+    packages=find_packages(where=''),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+    ],
+)
