7905f8a640a59796fb5aa3690db93aca56c47682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 15:05:25 +0000

    Add package tko/0.3.1/tko-0.3.1-py3-none-any.whl

---
+++ packages/tko/tko-0.3.1-py3-none-any.whl/tko/loader.py
@@ -0,0 +1,232 @@
+from __future__ import annotations
+from typing import List, Tuple, Optional
+import re
+import os
+
+from .basic import Unit
+from .pattern import PatternLoader
+
+
+class VplParser:
+    @staticmethod
+    def finish(text):
+        return text if text.endswith("\n") else text + "\n"
+
+    @staticmethod
+    def unwrap(text):
+        while text.endswith("\n"):
+            text = text[:-1]
+        if text.startswith("\"") and text.endswith("\""):
+            text = text[1:-1]
+        return VplParser.finish(text)
+
+    @staticmethod
+    class CaseData:
+        def __init__(self, case="", inp="", outp="", grade: Optional[int] = None):
+            self.case: str = case
+            self.input: str = VplParser.finish(inp)
+            self.output: str = VplParser.unwrap(VplParser.finish(outp))
+            self.grade: Optional[int] = grade
+
+        def __str__(self):
+            return "case=" + self.case + '\n' \
+                   + "input=" + self.input \
+                   + "output=" + self.output \
+                   + "gr=" + str(self.grade)
+
+    regex_vpl_basic = r"case= *([ \S]*) *\n *input *=(.*?)^ *output *=(.*)"
+    regex_vpl_extended = r"case= *([ \S]*) *\n *input *=(.*?)^ *output *=(.*?)^ *grade *reduction *= *(\S*)% *\n?"
+
+    @staticmethod
+    def filter_quotes(x):
+        return x[1:-2] if x.startswith('"') else x
+
+    @staticmethod
+    def split_cases(text: str) -> List[str]:
+        regex = r"^ *[Cc]ase *="
+        subst = "case="
+        text = re.sub(regex, subst, text, 0, re.MULTILINE | re.DOTALL)
+        return ["case=" + t for t in text.split("case=")][1:]
+
+    @staticmethod
+    def extract_extended(text) -> Optional[CaseData]:
+        f = re.match(VplParser.regex_vpl_extended, text, re.MULTILINE | re.DOTALL)
+        if f is None:
+            return None
+        try:
+            gr = int(f.group(4))
+        except ValueError:
+            gr = None
+        return VplParser.CaseData(f.group(1), f.group(2), f.group(3), gr)
+
+    @staticmethod
+    def extract_basic(text) -> Optional[CaseData]:
+        m = re.match(VplParser.regex_vpl_basic, text, re.MULTILINE | re.DOTALL)
+        if m is None:
+            return None
+        return VplParser.CaseData(m.group(1), m.group(2), m.group(3), None)
+
+    @staticmethod
+    def parse_vpl(content: str) -> List[CaseData]:
+        text_cases = VplParser.split_cases(content)
+        seq: List[VplParser.CaseData] = []
+
+        for text in text_cases:
+            case = VplParser.extract_extended(text)
+            if case is not None:
+                seq.append(case)
+                continue
+            case = VplParser.extract_basic(text)
+            if case is not None:
+                seq.append(case)
+                continue
+            print("invalid case: " + text)
+            exit(1)
+        return seq
+
+    @staticmethod
+    def to_vpl(unit: CaseData):
+        text = "case=" + unit.case + "\n"
+        text += "input=" + unit.input
+        text += "output=\"" + unit.output + "\"\n"
+        if unit.grade is not None:
+            text += "grade reduction=" + str(unit.grade) + "%\n"
+        return text
+
+
+class Loader:
+    regex_tio = r"^ *>>>>>>>> *(.*?)\n(.*?)^ *======== *\n(.*?)^ *<<<<<<<< *\n?"
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def parse_cio(text, source, crude_mode=False):
+        unit_list = []
+        text = "\n" + text
+
+        for test_case in text.split("\n#__case")[1:]:
+            unit = Unit()
+            unit.source = source
+            unit.output = test_case
+            unit_list.append(unit)
+
+        for unit in unit_list:
+            test = unit.output
+            if "\n$end" in test:
+                test = test.split("\n$end")[0] + "\n$end"
+
+            lines = test.split("\n")
+            tags = lines[0].strip().split(" ")
+            if tags[-1].endswith("%"):
+                unit.grade = int(tags[-1][0:-1])
+                del tags[-1]
+            else:
+                unit.grade = None
+            unit.case = " ".join(tags)
+            unit.output = "\n".join(lines[1:])
+
+        # concatenando testes contínuos
+        for i in range(len(unit_list)):
+            unit = unit_list[i]
+            if "\n$end" not in unit.output and (i < len(unit_list) - 1):
+                unit_list[i + 1].output = unit.output + '\n' + unit_list[i + 1].output
+                unit.output = unit.output + "\n$end\n"
+
+        for unit in unit_list:
+            lines = unit.output.split('\n')
+            unit.output = ""
+            unit.input = ""
+            # filtrando linhas vazias e comentários
+            for line in lines:
+                if crude_mode:  #
+                    unit.output += line + '\n'
+                    if line == "" or line.startswith("$") or line.startswith("#"):
+                        unit.input += line + '\n'
+                else:
+                    if line != "" and not line.startswith("#"):
+                        unit.output += line + '\n'
+                        if line.startswith("$"):
+                            unit.input += line[1:] + '\n'
+        for unit in unit_list:
+            unit.fromCio = True
+
+        return unit_list
+
+    @staticmethod
+    def parse_tio(text: str, source: str = "") -> List[Unit]:
+
+        # identifica se tem grade e retorna case name e grade
+        def parse_case_grade(value: str) -> Tuple[str, Optional[int]]:
+            if value.endswith("%"):
+                words = value.split(" ")
+                last = value.split(" ")[-1]
+                _case = " ".join(words[:-1])
+                grade_str = last[:-1]           # ultima palavra sem %
+                try:
+                    _grade = int(grade_str)
+                    return _case, _grade
+                except ValueError:
+                    pass
+            return value, None
+
+        matches = re.findall(Loader.regex_tio, text, re.MULTILINE | re.DOTALL)
+        unit_list = []
+        for m in matches:
+            case, grade = parse_case_grade(m[0])
+            unit_list.append(Unit(case, m[1], m[2], grade, source))
+        return unit_list
+
+    @staticmethod
+    def parse_vpl(text: str, source: str = "") -> List[Unit]:
+        data_list = VplParser.parse_vpl(text)
+        output: List[Unit] = []
+        for m in data_list:
+            output.append(Unit(m.case, m.input, m.output, m.grade, source))
+        return output
+
+    @staticmethod
+    def parse_dir(folder) -> List[Unit]:
+        pattern_loader = PatternLoader()
+        files = sorted(os.listdir(folder))
+        matches = pattern_loader.get_file_sources(files)
+
+        unit_list: List[Unit] = []
+        try:
+            for m in matches:
+                unit = Unit()
+                unit.source = os.path.join(folder, m.label)
+                unit.grade = 100
+                with open(os.path.join(folder, m.input_file)) as f:
+                    value = f.read()
+                    unit.input = value + ("" if value.endswith("\n") else "\n")
+                with open(os.path.join(folder, m.output_file)) as f:
+                    value = f.read()
+                    unit.output = value + ("" if value.endswith("\n") else "\n")
+                unit_list.append(unit)
+        except FileNotFoundError as e:
+            print(str(e))
+        return unit_list
+
+    @staticmethod
+    def parse_source(source: str) -> List[Unit]:
+        if os.path.isdir(source):
+            return Loader.parse_dir(source)
+        if os.path.isfile(source):
+            #  if PreScript.exists():
+            #      source = PreScript.process_source(source)
+            with open(source) as f:
+                content = f.read()
+            if source.endswith(".vpl"):
+                return Loader.parse_vpl(content, source)
+            elif source.endswith(".tio"):
+                return Loader.parse_tio(content, source)
+            elif source.endswith(".md"):
+                tests = Loader.parse_tio(content, source)
+                tests += Loader.parse_cio(content, source)
+                return tests
+            else:
+                print("warning: target format do not supported: " + source)  # make this a raise
+        else:
+            raise FileNotFoundError('warning: unable to find: ' + source)
+        return []
