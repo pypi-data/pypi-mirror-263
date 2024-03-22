7dc821a307fd4af54acbea89923205da8ff35ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 14:05:38 +0000

    Add package wetterdienst/0.79.0/wetterdienst-0.79.0-py3-none-any.whl

---
+++ packages/wetterdienst/wetterdienst-0.79.0-py3-none-any.whl/wetterdienst/ui/explorer/util.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2018-2021, earthobservations developers.
+# Distributed under the MIT License. See LICENSE for more info.
+import polars as pl
+
+
+def frame_summary(frame: pl.DataFrame):
+    return f"{len(frame)} records and columns={list(frame.columns)}"
