508f2997ec6a99742635a8dab3e57103b6ddd8a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 05:04:37 +0000

    Add package TOPSIS-Sort-B/1.0.3/TOPSIS-Sort-B-1.0.3.tar.gz

---
+++ packages/TOPSIS-Sort-B/TOPSIS-Sort-B-1.0.3.tar.gz/TOPSIS-Sort-B-1.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages, Extension
+
+VERSION = '1.0.3'
+DESCRIPTION = 'Topsis-Sort-B package'
+
+setup(
+    name="TOPSIS-Sort-B",
+    version=VERSION,
+    author="gilbertomoj",
+    author_email="gibamedeirosgc@gmail.com",
+    description=DESCRIPTION,
+    long_description=open("README.md", 'r', encoding="utf-8").read(),
+    long_description_content_type='text/markdown',
+    packages=["TOPSIS-Sort-B"],
+    install_requires=["numpy"],
+    keywords=['python', 'topsis', 'topsis-sort-b'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
