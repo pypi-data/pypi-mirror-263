e79acacb81ca89e1fa25efb746a82e8f0c4c2a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 14:06:17 +0000

    Add package thirdai/0.7.35/thirdai-0.7.35-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/thirdai/thirdai-0.7.35-cp39-cp39-macosx_11_0_arm64.whl/thirdai/neural_db/__init__.py
@@ -0,0 +1,29 @@
+try:
+    import nltk
+
+    from . import parsing_utils
+    from .constraint_matcher import AnyOf, EqualTo, GreaterThan, InRange, LessThan
+    from .documents import (
+        CSV,
+        DOCX,
+        PDF,
+        URL,
+        Document,
+        InMemoryText,
+        Reference,
+        SalesForce,
+        SentenceLevelDOCX,
+        SentenceLevelPDF,
+        SharePoint,
+        SQLDatabase,
+        Unstructured,
+    )
+    from .model_bazaar import Login, ModelBazaar
+    from .neural_db import CancelState, CheckpointConfig, NeuralDB, Strength, Sup
+
+    nltk.download("punkt")
+except ImportError as error:
+    raise ImportError(
+        "To use thirdai.neural_db, please install the additional dependencies by"
+        " running 'pip install thirdai[neural_db]'"
+    )
