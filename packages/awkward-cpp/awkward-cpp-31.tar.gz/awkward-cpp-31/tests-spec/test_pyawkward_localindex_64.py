0fa55deb4550ada6a322090f291d9b880e917adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 14:06:07 +0000

    Add package awkward-cpp/31/awkward-cpp-31.tar.gz

---
+++ packages/awkward-cpp/awkward-cpp-31.tar.gz/awkward-cpp-31/tests-spec/test_pyawkward_localindex_64.py
@@ -0,0 +1,21 @@
+# AUTO GENERATED ON 2024-03-21 AT 23:08:24
+# DO NOT EDIT BY HAND!
+#
+# To regenerate file, run
+#
+#     python dev/generate-tests.py
+#
+
+# fmt: off
+
+import pytest
+import kernels
+
+def test_pyawkward_localindex_64_1():
+    toindex = [123, 123, 123]
+    length = 3
+    funcPy = getattr(kernels, 'awkward_localindex_64')
+    funcPy(toindex=toindex, length=length)
+    pytest_toindex = [0, 1, 2]
+    assert toindex[:len(pytest_toindex)] == pytest.approx(pytest_toindex)
+
