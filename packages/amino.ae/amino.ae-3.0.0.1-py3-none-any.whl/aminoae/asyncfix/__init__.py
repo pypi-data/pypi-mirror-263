00ef808d782f2f1fa9122350b6da2add416d8dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 06:05:50 +0000

    Add package amino.ae/3.0.0.1/amino.ae-3.0.0.1-py3-none-any.whl

---
+++ packages/amino.ae/amino.ae-3.0.0.1-py3-none-any.whl/aminoae/asyncfix/__init__.py
@@ -0,0 +1,10 @@
+__title__ = 'amino.ae'
+__author__ = 'Reaper'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021-2023 Reaper'
+
+from .acm import ACM
+from .client import Client
+from .sub_client import SubClient
+from .socket import Callbacks, SocketHandler
+
