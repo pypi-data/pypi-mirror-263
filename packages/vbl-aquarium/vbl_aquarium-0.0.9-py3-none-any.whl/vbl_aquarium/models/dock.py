2144b7a8886ed7695af5d777f0e15dd488062759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 06:04:49 +0000

    Add package vbl-aquarium/0.0.9/vbl_aquarium-0.0.9-py3-none-any.whl

---
+++ packages/vbl-aquarium/vbl_aquarium-0.0.9-py3-none-any.whl/vbl_aquarium/models/dock.py
@@ -0,0 +1,26 @@
+from vbl_aquarium.utils.vbl_base_model import VBLBaseModel
+
+# File IO
+
+
+# Models for sending data to dock server
+class BucketModel(VBLBaseModel):
+    token: str
+    bucket: str
+    password: str
+
+
+class UploadModel(VBLBaseModel):
+    data: str
+    password: str
+
+
+# Models for sending save/load messages
+class SaveModel(VBLBaseModel):
+    bucket: str
+    password: str
+
+
+class LoadModel(VBLBaseModel):
+    bucket: str
+    password: str
