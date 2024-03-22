23ac1773ae1ae0b657582039a2c8be2a5e3c94f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 16:07:45 +0000

    Add package ctkchart/2.0.2/ctkchart-2.0.2-py3-none-any.whl

---
+++ packages/ctkchart/ctkchart-2.0.2-py3-none-any.whl/ctkchart/__init__.py
@@ -0,0 +1,14 @@
+from .CTkLineChart import CTkLineChart
+from .CTkLine import CTkLine
+
+ENABLED: str = "enabled"
+DISABLED: str  = "disabled"
+
+NORMAL: str  = "normal"
+DASHED: str  = "dashed"
+DOTTED: str  = "dotted"
+
+TOP: str = "top"
+SIDE: str = "side"
+
+__version__ = "2.0.2"
