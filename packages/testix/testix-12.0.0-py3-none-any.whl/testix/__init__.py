8a9bb54d815bbc57b92627154e695aae9f1adbf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 03:05:39 +0000

    Add package testix/12.0.0/testix-12.0.0-py3-none-any.whl

---
+++ packages/testix/testix-12.0.0-py3-none-any.whl/testix/__init__.py
@@ -0,0 +1,3 @@
+from .patch_module import patch_module
+from .frequentlyused import *
+from . import loop_breaker
---
+++ packages/testix/testix-12.0.0-py3-none-any.whl/testix/frequentlyused.py
@@ -0,0 +1,7 @@
+from testix.fake import Fake
+from testix.fakeobject import FakeObject
+from testix.argumentexpectations import *
+from testix.scenario import *
+from testix.hook import Hook
+from testix import saveargument
+from testix.DSL import *
