7fa166378da9a9616c73096edd69276970c73ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 04:06:03 +0000

    Add package mkdocs-asyncapi-html-plugin/0.2.0/mkdocs-asyncapi-html-plugin-0.2.0.tar.gz

---
+++ packages/mkdocs-asyncapi-html-plugin/mkdocs-asyncapi-html-plugin-0.2.0.tar.gz/mkdocs-asyncapi-html-plugin-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='mkdocs-asyncapi-html-plugin',
+    version='0.2.0',
+    description='generate html file from asyncapi yml file',
+    author='Hani',
+    author_email='hani@regentmarkets.com',
+    packages=['mkdocs-asyncapi-html-plugin'],
+    install_requires=[],  # Add dependencies if any
+)
