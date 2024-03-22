62c5baeaed5b85d1eaab8f02985da059b786177f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 03:05:30 +0000

    Add package speXtra/0.40.0/spextra-0.40.0.tar.gz

---
+++ packages/speXtra/spextra-0.40.0.tar.gz/spextra-0.40.0/spextra/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from . import spextra
+from . import database
+from . import utils
+
+from .spextra import Spextrum, Passband, ExtinctionCurve, get_vega_spectrum
+from .database import spextra_database, DEFAULT_DATA
+from .libraries import SpecLibrary, FilterSystem, ExtCurvesLibrary
+from .configuration import config
+from .exceptions import SpextraError
