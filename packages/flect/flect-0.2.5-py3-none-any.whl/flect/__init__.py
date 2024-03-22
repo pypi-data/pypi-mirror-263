41e22fcb4afaada98fc4d110b2960674df889177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 04:05:31 +0000

    Add package flect/0.2.5/flect-0.2.5-py3-none-any.whl

---
+++ packages/flect/flect-0.2.5-py3-none-any.whl/flect/__init__.py
@@ -0,0 +1,11 @@
+from flect.application import flect
+from flect.response import ActionResponse, PageResponse
+from flect.version import VERSION
+
+__version__ = VERSION
+
+__all__ = [
+    "flect",
+    "PageResponse",
+    "ActionResponse",
+]
