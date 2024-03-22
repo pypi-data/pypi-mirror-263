09f11b77ef2b367b1729adfaae41aea05a9ffd9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 16:07:37 +0000

    Add package pyqqq-cli/0.1.6/pyqqq_cli-0.1.6.tar.gz

---
+++ packages/pyqqq-cli/pyqqq_cli-0.1.6.tar.gz/pyqqq_cli-0.1.6/qupiato/cli/config.py
@@ -0,0 +1,14 @@
+import dotenv
+import os
+
+dotenv.load_dotenv(dotenv.find_dotenv())
+
+
+PYQQQ_API_KEY = os.getenv("PYQQQ_API_KEY")
+
+BUCKET_NAME = 'qupiato-user-artifacts'
+
+DEPLOYER_WS_URL = os.getenv("DEPLOYER_WS_URL", "wss://qupiato.com/deployer/ws")
+
+CREDENTIAL_FILE_PATH = os.path.expanduser("~/.qred")
+
