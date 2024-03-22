454947ef34964084e0a7ba1dc24e425741f0cafc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 04:05:15 +0000

    Add package pedal/2.6.1/pedal-2.6.1-py3-none-any.whl

---
+++ packages/pedal/pedal-2.6.1-py3-none-any.whl/pedal/types/library/__init__.py
@@ -0,0 +1,5 @@
+import pedal.types.library.matplotlib
+import pedal.types.library.cs1_curriculum
+import pedal.types.library.designer
+import pedal.types.library.turtles
+import pedal.types.library.microbit
