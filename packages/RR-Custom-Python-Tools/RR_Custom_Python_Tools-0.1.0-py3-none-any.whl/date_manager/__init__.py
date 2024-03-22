8145859029fae4b780c3145cb4839137330f314a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 05:04:45 +0000

    Add package RR-Custom-Python-Tools/0.1.0/RR_Custom_Python_Tools-0.1.0-py3-none-any.whl

---
+++ packages/RR-Custom-Python-Tools/RR_Custom_Python_Tools-0.1.0-py3-none-any.whl/date_manager/__init__.py
@@ -0,0 +1,10 @@
+# date_manager/__init__.py
+
+"""Frequent date operations using methods provided.
+
+Modules exported by this package:
+
+- `date_manager`: Provide several frequently used datetime operations.
+"""
+
+from date_manager.date_manager import DateManager  # noqa: F401
