197016ed82afcdd538d46fbf8dfbce09ee05a683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 03:06:13 +0000

    Add package takeoff-launcher/0.0.5/takeoff_launcher-0.0.5.tar.gz

---
+++ packages/takeoff-launcher/takeoff_launcher-0.0.5.tar.gz/takeoff_launcher-0.0.5/src/takeoff_launcher/conf_mgr.py
@@ -0,0 +1,42 @@
+"""
+This module implements and instantiates the common configuration class used in the project.
+"""
+
+# ───────────────────────────────────────────────────── imports ────────────────────────────────────────────────────── #
+
+from pathlib import Path
+
+# ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────── #
+#                           specifies all modules that shall be loaded and imported into the                           #
+#                                current namespace when we use 'from package import *'                                 #
+# ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────── #
+
+__all__ = ["conf_mgr"]
+
+
+# ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────── #
+#                                                Configuration Manager                                                 #
+# ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────── #
+
+
+class ConfManager:
+    """Configuration Manager class"""
+
+    # paths
+    path_root: Path = Path(__file__).parent.parent.parent.resolve()  # takeoff_launcher/
+    path_src: Path = path_root / "src"  # takeoff_launcher/src/
+    path_version: Path = path_root / "version.txt"  # takeoff_launcher/version.txt
+
+    # version
+    current_cli_version: str = path_version.read_text().strip()
+    current_takeoff_version: str = "0.13.0"
+
+    # takeoff image
+    image_name: str = "tytn/takeoff"
+    image_tag_gpu: str = current_takeoff_version + "-gpu"  # 0.13.0-gpu
+    image_tag_cpu: str = current_takeoff_version + "-cpu"  # 0.13.0-cpu
+
+
+# ─────────────────────────────────────────────── ConfManager Instance ─────────────────────────────────────────────── #
+
+conf_mgr = ConfManager()
