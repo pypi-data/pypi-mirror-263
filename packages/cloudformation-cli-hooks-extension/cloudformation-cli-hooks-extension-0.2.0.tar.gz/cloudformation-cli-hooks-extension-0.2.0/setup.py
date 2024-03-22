9164eedf5a6154fbc865ee8d0fb0ca1aebf380dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 04:04:22 +0000

    Add package cloudformation-cli-hooks-extension/0.2.0/cloudformation-cli-hooks-extension-0.2.0.tar.gz

---
+++ packages/cloudformation-cli-hooks-extension/cloudformation-cli-hooks-extension-0.2.0.tar.gz/cloudformation-cli-hooks-extension-0.2.0/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+"""Hook extension for the CloudFormation CLI"""
+import os.path
+import re
+from setuptools import setup
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with open(os.path.join(HERE, *parts), "r", encoding="utf-8") as fp:
+        return fp.read()
+
+
+# https://packaging.python.org/guides/single-sourcing-package-version/
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name="cloudformation-cli-hooks-extension",
+    version=find_version("src", "hook_extension", "__init__.py"),
+    description=__doc__,
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="Amazon Web Services",
+    author_email="aws-cloudformation-developers@amazon.com",
+    url="https://github.com/aws-cloudformation/cloudformation-cli-hooks-extension",
+    include_package_data=True,
+    zip_safe=True,
+    python_requires=">=3.6",
+    packages=["hook_extension"],
+    package_dir={"": "src"},
+    install_requires=["cloudformation-cli>=0.2.33"],
+    entry_points={
+        "rpdk.v1.extensions": [
+            "hook = hook_extension.entry:HookExtension",
+        ],
+    },
+    license="Apache License 2.0",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Code Generators",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="Amazon Web Services AWS CloudFormation",
+)
---
+++ packages/cloudformation-cli-hooks-extension/cloudformation-cli-hooks-extension-0.2.0.tar.gz/cloudformation-cli-hooks-extension-0.2.0/src/hook_extension/configure_hook.py
@@ -0,0 +1,83 @@
+"""
+This sub command sets the type configuration of the hook registered in your AWS account.
+"""
+import logging
+from pathlib import Path
+from argparse import Namespace
+
+from botocore.exceptions import ClientError
+
+from rpdk.core.boto_helpers import create_sdk_session
+from rpdk.core.project import Project
+from rpdk.core.exceptions import DownstreamError, InvalidProjectError
+
+LOG = logging.getLogger(__name__)
+
+COMMAND_NAME = "configure"
+
+def _set_type_configuration(cfn_client, type_name: str, type_configuration_json: str) -> None:
+    """
+    Sets Hook type configuration by calling CloudFormation SetTypeConfiguration API with arguments.
+    https://docs.aws.amazon.com/AWSCloudFormation/latest/APIReference/API_SetTypeConfiguration.html
+
+    Parameters:
+        cfn_client: Boto3 session CloudFormation client.
+        type_name (string): The hook type name to call SetTypeConfiguration with.
+        type_configuration_json (string): The json formatted string to use as the Hook's TypeConfiguration
+
+    Returns:
+       dict: The response from the SetTypeConfiguration API.
+
+    Side effect: Type configuration of hook will be updated in AWS account.
+    """
+    LOG.debug("Calling SetTypeConfiguration for %s", type_name)
+    try:
+        response = cfn_client.set_type_configuration(TypeName=type_name, Type="HOOK", Configuration=type_configuration_json)
+        LOG.debug("Successful response from SetTypeConfiguration")
+        return response
+    except cfn_client.exceptions.TypeNotFoundException as e:
+        msg = "Setting type configuration resulted in TypeNotFoundException. Have you registered this hook first?"
+        print("\n" + msg)
+        raise DownstreamError(msg) from e
+    except ClientError as e:
+        raise DownstreamError from e
+
+
+def _configure_hook(args: Namespace) -> None:
+    """
+    Main method for the hook configure command. Uses file path specified to set the type configuration of the Hook in AWS.
+
+    Parameters:
+        args (Namespace): The arguments to use with this command.
+            Required keys in Namespace: 'configuration_path', 'profile', 'endpoint_url', 'region'. All default to None.
+
+    Returns:
+        None.
+
+    Side effect: Configuration Arn printed to system out.
+    """
+    cfn_client = create_sdk_session(args.region, args.profile).client("cloudformation", endpoint_url=args.endpoint_url)
+    project = Project()
+    project.load()
+    type_name = project.type_name
+
+    configuration_file_path = Path(args.configuration_path)
+
+    try:
+        configuration_file = open(configuration_file_path, 'r', encoding="utf-8")
+    except FileNotFoundError as e:
+        raise InvalidProjectError(f"Configuration file {configuration_file_path} not found.") from e
+
+    with configuration_file:
+        configuration_json = configuration_file.read()
+
+    set_type_config_response = _set_type_configuration(cfn_client, type_name, configuration_json)
+    print(f"ConfigurationArn: {set_type_config_response['ConfigurationArn']}")
+
+def setup_parser(parser):
+    configure_subparser = parser.add_parser(COMMAND_NAME, description=__doc__)
+    configure_subparser.set_defaults(command=_configure_hook)
+    configure_subparser.add_argument("--configuration-path", help="Filepath to CloudFormation configuration json to use for the hook.", required=True)
+    configure_subparser.add_argument("--profile", help="AWS profile to use.")
+    configure_subparser.add_argument("--endpoint-url", help="CloudFormation endpoint to use.")
+    configure_subparser.add_argument("--region", help="AWS Region to submit the type.")
