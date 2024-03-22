607ee49370215364a78ffd41e982690c26d57578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 05:04:36 +0000

    Add package xarray-selafin/0.1.5/xarray-selafin-0.1.5.tar.gz

---
+++ packages/xarray-selafin/xarray-selafin-0.1.5.tar.gz/xarray-selafin-0.1.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="xarray-selafin",
+    version="0.1.5",
+    author=["tomsail", "lucduron"],
+    author_email="l.duron@cnr.tm.fr",
+    description="https://github.com/seareport/xarray-selafin",
+    packages=find_packages(),
+    package_data={
+        "xarray_selafin": ["data/*", "variable/*"],
+    },
+    install_requires=[
+        "numpy",
+        "pytest",
+        "scipy",
+        "shapely",
+        "xarray",
+        "netcdf4",
+    ],
+    extras_require={
+        "dask": ["dask"],
+        "dev": [
+            "matplotlib"
+        ],  # Assuming netcdf4 is listed intentionally in both main and dev dependencies
+    },
+    entry_points={
+        "xarray.backends": [
+            "selafin = xarray_selafin.xarray_backend:SelafinBackendEntrypoint",
+        ],
+    },
+    python_requires=">=3.9",
+)
