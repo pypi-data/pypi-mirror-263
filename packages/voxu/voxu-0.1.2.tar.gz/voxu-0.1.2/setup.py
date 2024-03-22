1d1b6ab624d94c68fe018524f2a498b09398d254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 17:05:00 +0000

    Add package voxu/0.1.2/voxu-0.1.2.tar.gz

---
+++ packages/voxu/voxu-0.1.2.tar.gz/voxu-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='voxu',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'sqlalchemy',
+        'flask',
+        'psycopg2-binary'
+    ],
+)
