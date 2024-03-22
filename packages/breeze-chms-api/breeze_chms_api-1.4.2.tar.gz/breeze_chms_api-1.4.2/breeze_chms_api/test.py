83374e1bcbff4c219d617321733d7cd48c735bec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 04:04:36 +0000

    Add package breeze-chms-api/1.4.2/breeze_chms_api-1.4.2.tar.gz

---
+++ packages/breeze-chms-api/breeze_chms_api-1.4.2.tar.gz/breeze_chms_api-1.4.2/breeze_chms_api/test.py
@@ -0,0 +1,17 @@
+# import breeze_chms_api.breeze
+# from breeze import ENDPOINTS
+
+# api = breeze_chms_api.breeze.breeze_api()
+#    def _request(self,
+#                  endpoint: ENDPOINTS,
+#                  command: str = '',
+#                  params: Mapping[str, Union[str, int, float, Mapping, Sequence]]
+#                  = dict(),
+#                  headers: dict = dict(),
+#                  timeout: int = 60,
+#                  ):
+
+# f = api._request(ENDPOINTS.HISTORY, 'list')
+# f = api.list_people(limit=1)
+#
+# print(f)
