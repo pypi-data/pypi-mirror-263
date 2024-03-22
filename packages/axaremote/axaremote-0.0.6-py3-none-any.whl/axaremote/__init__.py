3d27b93a210b70a4f2cc4027b649319d9420a64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 16:07:42 +0000

    Add package axaremote/0.0.6/axaremote-0.0.6-py3-none-any.whl

---
+++ packages/axaremote/axaremote-0.0.6-py3-none-any.whl/axaremote/__init__.py
@@ -0,0 +1,16 @@
+"""
+Implements the AXA Remote library for controlling AXA Remote window openers.
+
+Created on 12 Nov 2022
+
+@author: Rogier van Staveren
+"""
+
+__version__ = "0.0.6"
+
+from axaremote.axaremote import (
+    AXARemote,
+    AXARemoteError,
+    AXARemoteSerial,
+    AXARemoteTelnet,
+)
