22dcce5342889725ee1a4b6ec261e6c4aac6dd19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 16:07:43 +0000

    Add package pypytimer/1.0.0/pypytimer-1.0.0.tar.gz

---
+++ packages/pypytimer/pypytimer-1.0.0.tar.gz/pypytimer-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="pypytimer",
+      version="1.0.0",
+      description="Helps to measure the time it takes for a function or method to run",
+      package=["pypytimer"],
+      author="Samuel Oyewoade",
+      author_email="samueladebare1010@gmail.com",
+      zip_safe=False
+      )
