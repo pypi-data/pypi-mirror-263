2a3c9c9677aa38222a2a9defde6e1f867fa6b1b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 06:04:56 +0000

    Add package pytracertool/2.0.3/pytracertool-2.0.3.tar.gz

---
+++ packages/pytracertool/pytracertool-2.0.3.tar.gz/pytracertool-2.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+setup(
+
+    name='pytracertool',
+    version='2.0.3',
+    author='Darshan Lakshman',
+    author_email='darshan.lakshman@icloud.com',
+    description='A versatile tool for tracing code execution, capturing variable changes, and logging print statements.',
+    long_description=long_description,
+    url='https://github.com/DarshanLakshman/PyTracerTool.git',
+    packages=find_packages(),
+    install_requires=['tabulate', 'typing'],
+    keywords=["trace", "debugging", "tracing", "execution", "visualisation"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ]
+)
+
