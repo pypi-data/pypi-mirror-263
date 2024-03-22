1bbc6e90ff3ad0354c4f86f5ea53dac1180414c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 04:04:33 +0000

    Add package hdxcli/1.0rc51/hdxcli-1.0rc51.tar.gz

---
+++ packages/hdxcli/hdxcli-1.0rc51.tar.gz/hdxcli-1.0rc51/src/hdx_cli/cli_interface/profile/commands.py
@@ -0,0 +1,96 @@
+import click
+import toml
+
+from ...library_api.utility.decorators import report_error_and_exit
+from ...library_api.common.config_constants import PROFILE_CONFIG_FILE
+from ...library_api.common.profile import save_profile, get_profile_data_from_standard_input
+from ...library_api.common.logging import get_logger
+
+logger = get_logger()
+
+
+@click.group(help="Profile-related operations")
+@click.pass_context
+def profile(ctx: click.Context):
+    ctx.obj = {'usercontext': ctx.parent.obj['usercontext']}
+
+
+@click.command(help='Show profile')
+@click.argument('profile_name', default=None, required=False)
+@click.pass_context
+@report_error_and_exit(exctype=Exception)
+def profile_show(ctx: click.Context,
+                 profile_name):
+    profilename = ctx.parent.obj['usercontext'].profilename if not profile_name else profile_name
+    logger.info(f'Showing [{profilename}]')
+    logger.info('-' * 100)
+    with open(PROFILE_CONFIG_FILE, 'r', encoding='utf-8') as config_file:
+        cfg_dict = toml.load(config_file)
+        for prof_name, prof_val in cfg_dict.items():
+            if prof_name == profilename:
+                logger.info(f"Profile: {prof_name}")
+                for cfg_key, cfg_val in prof_val.items():
+                    logger.info(f"{cfg_key}: {cfg_val}")
+
+
+@click.command(help='List profiles')
+@click.pass_context
+@report_error_and_exit(exctype=Exception)
+def profile_list(ctx: click.Context):
+    with open(PROFILE_CONFIG_FILE, 'r', encoding='utf-8') as config_file:
+        cfg_dict = toml.load(config_file)
+        for cfg_name in cfg_dict:
+            logger.info(cfg_name)
+
+
+@click.command(help='Edit profile')
+@click.argument('profile_name')
+@click.pass_context
+@report_error_and_exit(exctype=Exception)
+def profile_edit(ctx: click.Context,
+                 profile_name: str):
+    with open(PROFILE_CONFIG_FILE, 'r', encoding='utf-8') as config_file:
+        cfg_dict = toml.load(config_file)
+    profile_to_edit = cfg_dict[profile_name]
+    logger.info(f'Editing [{profile_name}]')
+    logger.info(f'-' * 100)
+    username, hostname, scheme = (profile_to_edit['username'],
+                                  profile_to_edit['hostname'],
+                                  profile_to_edit['scheme'])
+
+    edit_profile_data = get_profile_data_from_standard_input(hostname=hostname,
+                                                             username=username,
+                                                             http_scheme=scheme)
+    save_profile(username=edit_profile_data.username,
+                 hostname=edit_profile_data.hostname,
+                 profile_config_file=PROFILE_CONFIG_FILE,
+                 profilename=profile_name,
+                 scheme=edit_profile_data.scheme,
+                 initial_profile=cfg_dict)
+
+
+@click.command(help='Add profile')
+@click.argument('profile_name')
+@click.pass_context
+@report_error_and_exit(exctype=Exception)
+def profile_add(ctx: click.Context,
+                profile_name: str):
+    with open(PROFILE_CONFIG_FILE, 'r', encoding='utf-8') as config_file:
+        cfg_dict = toml.load(config_file)
+    if cfg_dict.get(profile_name):
+        logger.info(f"Profile '{profile_name}' already exists.")
+        return
+
+    edit_profile_data = get_profile_data_from_standard_input()
+    save_profile(username=edit_profile_data.username,
+                 hostname=edit_profile_data.hostname,
+                 profile_config_file=PROFILE_CONFIG_FILE,
+                 profilename=profile_name,
+                 scheme=edit_profile_data.scheme,
+                 initial_profile=cfg_dict)
+
+
+profile.add_command(profile_list, name='list')
+profile.add_command(profile_show, name='show')
+profile.add_command(profile_edit, name='edit')
+profile.add_command(profile_add, name='add')
---
+++ packages/hdxcli/hdxcli-1.0rc51.tar.gz/hdxcli-1.0rc51/src/hdx_cli/library_api/common/exceptions.py
@@ -0,0 +1,92 @@
+
+
+class HdxCliException(Exception):
+   'Base class for all exceptions'
+
+
+class HttpException(HdxCliException):
+    'Exception thrown from problems in Http calls to the config API'
+    def __init__(self, error_code, message):
+        self.error_code = error_code
+        self.message = message
+
+
+class CommandLineException(HdxCliException):
+    pass
+
+
+class BadFileNameConventionException(HdxCliException):
+    pass
+
+
+class NotImplementedException(HdxCliException):
+    pass
+
+
+class NotSupportedException(HdxCliException):
+    pass
+
+
+class TokenExpiredException(HdxCliException):
+    pass
+
+
+class LoginException(HdxCliException):
+    pass
+
+
+class CacheFileNotFoundException(HdxCliException, FileNotFoundError):
+    pass
+
+
+class ProfileNotFoundException(HdxCliException):
+    pass
+
+
+# A resource like a project, transform, etc. was not found
+class ResourceNotFoundException(HdxCliException):
+    pass
+
+
+class ProjectNotFoundException(ResourceNotFoundException):
+    pass
+
+
+class TableNotFoundException(ResourceNotFoundException):
+    pass
+
+
+class TransformNotFoundException(ResourceNotFoundException):
+    pass
+
+
+class TransformFileNotFoundException(ResourceNotFoundException):
+    pass
+
+
+class LogicException(HdxCliException):
+    pass
+
+
+class InvalidFormatFileException(HdxCliException):
+    pass
+
+
+class MissingSettingsException(InvalidFormatFileException):
+    pass
+
+
+class InvalidRoleException(HdxCliException):
+    pass
+
+
+class InvalidDataException(HdxCliException):
+    pass
+
+
+class InvalidEmailException(InvalidDataException):
+    pass
+
+
+class QueryOptionNotFound(HdxCliException):
+    pass
