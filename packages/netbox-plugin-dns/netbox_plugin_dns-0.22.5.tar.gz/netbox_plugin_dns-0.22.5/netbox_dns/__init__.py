d944f7707eb45e0262c7d51fac60db633975a388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 04:05:59 +0000

    Add package netbox-plugin-dns/0.22.5/netbox_plugin_dns-0.22.5.tar.gz

---
+++ packages/netbox-plugin-dns/netbox_plugin_dns-0.22.5.tar.gz/netbox_plugin_dns-0.22.5/netbox_dns/__init__.py
@@ -0,0 +1,87 @@
+import sys
+
+from extras.plugins import PluginConfig
+from django.db.utils import OperationalError
+
+try:
+    # NetBox 3.5.0 - 3.5.7, 3.5.9+
+    from extras.plugins import get_plugin_config
+except ImportError:
+    # NetBox 3.5.8
+    from extras.plugins.utils import get_plugin_config
+
+__version__ = "0.22.5"
+
+
+class DNSConfig(PluginConfig):
+    name = "netbox_dns"
+    verbose_name = "NetBox DNS"
+    description = "NetBox plugin for DNS data"
+    min_version = "3.5.0"
+    version = __version__
+    author = "Peter Eckel"
+    author_email = "pete@netbox-dns.org"
+    required_settings = []
+    default_settings = {
+        "zone_default_ttl": 86400,
+        "zone_soa_serial_auto": True,
+        "zone_soa_serial": 1,
+        "zone_soa_refresh": 43200,
+        "zone_soa_retry": 7200,
+        "zone_soa_expire": 2419200,
+        "zone_soa_minimum": 3600,
+        "feature_ipam_coupling": False,
+        "tolerate_underscores_in_hostnames": False,
+        "tolerate_leading_underscore_types": [
+            "TXT",
+            "SRV",
+        ],
+        "tolerate_non_rfc1035_types": [],
+        "enable_root_zones": False,
+        "enforce_unique_records": True,
+        "enforce_unique_rrset_ttl": True,
+    }
+    base_url = "netbox-dns"
+
+    def ready(self):
+        #
+        # Check if required custom fields exist for IPAM coupling
+        #
+        if get_plugin_config("netbox_dns", "feature_ipam_coupling"):
+            from extras.models import CustomField
+            from ipam.models import IPAddress
+            from django.contrib.contenttypes.models import ContentType
+
+            try:
+                objtype = ContentType.objects.get_for_model(IPAddress)
+                required_cf = (
+                    "ipaddress_dns_record_name",
+                    "ipaddress_dns_record_ttl",
+                    "ipaddress_dns_record_disable_ptr",
+                    "ipaddress_dns_zone_id",
+                )
+
+                if CustomField.objects.filter(
+                    name__in=required_cf, content_types=objtype
+                ).count() < len(required_cf):
+                    print(
+                        "WARNING: 'feature_ipam_coupling' is enabled, but the required"
+                        " custom fields for IPAM DNS coupling are missing. Please run"
+                        " the Django management command 'setup_coupling' to create the"
+                        " missing custom fields.",
+                        file=sys.stderr,
+                    )
+            except OperationalError as exc:
+                print(
+                    "WARNING: Unable to connect to PostgreSQL, cannot check custom fields"
+                    " for feature_ipam_coupling",
+                    file=sys.stderr,
+                )
+
+        super().ready()
+
+
+#
+# Initialize plugin config
+#
+config = DNSConfig
---
+++ packages/netbox-plugin-dns/netbox_plugin_dns-0.22.5.tar.gz/netbox_plugin_dns-0.22.5/netbox_dns/views/record.py
@@ -0,0 +1,85 @@
+from dns import name as dns_name
+
+from netbox.views import generic
+
+from netbox_dns.filters import RecordFilter
+from netbox_dns.forms import (
+    RecordImportForm,
+    RecordFilterForm,
+    RecordForm,
+    RecordBulkEditForm,
+)
+from netbox_dns.models import Record
+from netbox_dns.tables import RecordTable, ManagedRecordTable
+from netbox_dns.utilities import value_to_unicode
+
+
+class RecordListView(generic.ObjectListView):
+    queryset = Record.objects.filter(managed=False).prefetch_related(
+        "zone", "ptr_record"
+    )
+    filterset = RecordFilter
+    filterset_form = RecordFilterForm
+    table = RecordTable
+
+
+class ManagedRecordListView(generic.ObjectListView):
+    queryset = Record.objects.filter(managed=True).prefetch_related(
+        "zone", "address_record"
+    )
+    filterset = RecordFilter
+    filterset_form = RecordFilterForm
+    table = ManagedRecordTable
+    actions = ("export",)
+    template_name = "netbox_dns/record/managed.html"
+
+
+class RecordView(generic.ObjectView):
+    queryset = Record.objects.all().prefetch_related("zone", "ptr_record")
+
+    def get_extra_context(self, request, instance):
+        context = {}
+
+        name = dns_name.from_text(instance.name, origin=None)
+        if name.to_text() != name.to_unicode():
+            context["unicode_name"] = name.to_unicode()
+
+        unicode_value = value_to_unicode(instance.value)
+        if instance.value != unicode_value:
+            context["unicode_value"] = unicode_value
+
+        return context
+
+
+class RecordEditView(generic.ObjectEditView):
+    queryset = Record.objects.filter(managed=False).prefetch_related(
+        "zone", "ptr_record"
+    )
+    form = RecordForm
+    default_return_url = "plugins:netbox_dns:record_list"
+
+
+class RecordDeleteView(generic.ObjectDeleteView):
+    queryset = Record.objects.filter(managed=False)
+    default_return_url = "plugins:netbox_dns:record_list"
+
+
+class RecordBulkImportView(generic.BulkImportView):
+    queryset = Record.objects.filter(managed=False).prefetch_related(
+        "zone", "ptr_record"
+    )
+    model_form = RecordImportForm
+    table = RecordTable
+    default_return_url = "plugins:netbox_dns:record_list"
+
+
+class RecordBulkEditView(generic.BulkEditView):
+    queryset = Record.objects.filter(managed=False).prefetch_related("zone")
+    filterset = RecordFilter
+    table = RecordTable
+    form = RecordBulkEditForm
+
+
+class RecordBulkDeleteView(generic.BulkDeleteView):
+    queryset = Record.objects.filter(managed=False)
+    table = RecordTable
