82fab6f96a1622e31e81fc654d5708671adde7f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 17:04:58 +0000

    Add package bec-server/1.21.1/bec-server-1.21.1.tar.gz

---
+++ packages/bec-server/bec-server-1.21.1.tar.gz/bec-server-1.21.1/bec_server/launch.py
@@ -0,0 +1,49 @@
+import argparse
+import os
+
+import libtmux
+
+from bec_server.service_handler import ServiceHandler
+
+
+def main():
+    """
+    Launch the BEC server in a tmux session. All services are launched in separate panes.
+    """
+    parser = argparse.ArgumentParser(description="Utility tool managing the BEC server")
+    command = parser.add_subparsers(dest="command")
+    start = command.add_parser("start", help="Start the BEC server")
+    start.add_argument(
+        "--config", type=str, default=None, help="Path to the BEC service config file"
+    )
+    command.add_parser("stop", help="Stop the BEC server")
+    restart = command.add_parser("restart", help="Restart the BEC server")
+    restart.add_argument(
+        "--config", type=str, default=None, help="Path to the BEC service config file"
+    )
+    command.add_parser("attach", help="Open the currently running BEC server session")
+
+    args = parser.parse_args()
+
+    if "config" in args:
+        config = args.config
+    else:
+        config = None
+
+    service_handler = ServiceHandler(
+        bec_path=os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+        config_path=config,
+    )
+    if args.command == "start":
+        service_handler.start()
+    elif args.command == "stop":
+        service_handler.stop()
+    elif args.command == "restart":
+        service_handler.restart()
+    elif args.command == "attach":
+        server = libtmux.Server()
+        session = server.find_where({"session_name": "bec"})
+        if session is None:
+            print("No BEC session found")
+            return
+        session.attach_session()
