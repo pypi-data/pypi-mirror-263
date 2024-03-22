9f4c5ce2c0108e04d719bef191acf9c60a3a2839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 16:07:46 +0000

    Add package flexy-token/0.3/flexy_token-0.3.tar.gz

---
+++ packages/flexy-token/flexy_token-0.3.tar.gz/flexy_token-0.3/flexy_token/api_client.py
@@ -0,0 +1,55 @@
+import requests
+from datetime import timezone
+import jwt
+import psycopg2
+
+class APIClient:
+    def __init__(self, account_sid, api_secret,api_key,identity):
+        self.account_sid = account_sid
+        self.api_secret = api_secret
+        self.api_key= api_key
+        self.identity=identity
+        
+
+def AccessToken(account_sid,api_secret,api_key,identity=""):
+		try:
+		
+			payload = {
+				'account_sid': account_sid,
+				'api_key': api_key,
+				'api_secret': api_secret,
+				'identity': identity,
+			}   
+			token = jwt.encode(payload, 'your_secret_key', algorithm='HS256')
+			
+			print (token,'tokentt')
+
+			try:
+				connection = psycopg2.connect(
+					dbname="macha_flexydial",
+					user="flexydial",
+					password="flexydial",
+					host="10.12.0.84",
+					port="5432"
+				)
+				print("Connected to the database.",connection)
+				mycursor = connection.cursor()
+				query=("INSERT INTO connector_usertoken VALUES (%s,%s);")
+				table_values = (identity,token)
+				mycursor.execute(query,table_values)
+				connection.commit()
+			except psycopg2.Error as e:
+				print("Unable to connect to the database:", e)
+
+			finally:
+				connection.close()
+
+			return token
+
+		except Exception as e:
+			return f"Token unable to generate because of an exception"
+
+			
+
+
+ 
