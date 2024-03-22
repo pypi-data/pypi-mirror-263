d1dcd18b262b2cc26b37023d91c8dba7647fef1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 06:05:06 +0000

    Add package YAPythonObfuscator/0.0.2/YAPythonObfuscator-0.0.2-py3-none-any.whl

---
+++ packages/YAPythonObfuscator/YAPythonObfuscator-0.0.2-py3-none-any.whl/yapo/obfuscator.py
@@ -0,0 +1,11 @@
+import techniques
+
+def obfuscate(code):
+	code = techniques.decomment(code)
+	code = techniques.string_encrypt(code)
+	code = techniques.base64(code, decoder_function = "b64d(%s)")
+	code = """b64 = __import__('base64')
+b64d = lambda c: b64.b64decode(c).decode('utf-8')
+""" + code
+	code = techniques.string_encrypt(code)
+	return code
