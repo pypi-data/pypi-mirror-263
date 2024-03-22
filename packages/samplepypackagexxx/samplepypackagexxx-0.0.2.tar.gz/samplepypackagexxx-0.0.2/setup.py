3eca64d91335a8f5648e5900f8f2a3010aca4daa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 04:05:24 +0000

    Add package samplepypackagexxx/0.0.2/samplepypackagexxx-0.0.2.tar.gz

---
+++ packages/samplepypackagexxx/samplepypackagexxx-0.0.2.tar.gz/samplepypackagexxx-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.2"
+DESCRIPTION = "sample package"
+
+setup(
+    name="samplepypackagexxx",
+    version=VERSION,
+    author="Julian Amin",
+    author_email="julian.amin@infor.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=["samplepackage"],
+    # classifiers=[
+    #     "sample :: sample"
+    # ]
+)
