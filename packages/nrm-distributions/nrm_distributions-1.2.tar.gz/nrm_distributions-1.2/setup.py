dc62e3499997c1eebea25643163d275ae39d0e51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 04:04:19 +0000

    Add package nrm-distributions/1.2/nrm_distributions-1.2.tar.gz

---
+++ packages/nrm-distributions/nrm_distributions-1.2.tar.gz/nrm_distributions-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nrm_distributions',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['nrm_distributions'],
+      author=['Nkanyiso Mantengu'],
+      author_email=['mantengunr@gmail.com'],
+      zip_safe=False)
