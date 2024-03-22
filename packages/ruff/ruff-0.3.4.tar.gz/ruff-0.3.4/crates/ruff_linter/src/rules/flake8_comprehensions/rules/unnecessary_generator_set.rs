df499e61a722d33a44bcac77039e997aee9b7d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 06:05:34 +0000

    Add package ruff/0.3.4/ruff-0.3.4.tar.gz

---
+++ packages/ruff/ruff-0.3.4.tar.gz/ruff-0.3.4/crates/ruff_linter/src/rules/flake8_comprehensions/rules/unnecessary_generator_set.rs
@@ -0,0 +1,84 @@
+use ruff_diagnostics::{AlwaysFixableViolation, Diagnostic, Edit, Fix};
+use ruff_macros::{derive_message_formats, violation};
+use ruff_python_ast as ast;
+use ruff_text_size::{Ranged, TextSize};
+
+use crate::checkers::ast::Checker;
+use crate::rules::flake8_comprehensions::fixes::{pad_end, pad_start};
+
+use super::helpers;
+
+/// ## What it does
+/// Checks for unnecessary generators that can be rewritten as `set`
+/// comprehensions.
+///
+/// ## Why is this bad?
+/// It is unnecessary to use `set` around a generator expression, since
+/// there are equivalent comprehensions for these types. Using a
+/// comprehension is clearer and more idiomatic.
+///
+/// ## Examples
+/// ```python
+/// set(f(x) for x in foo)
+/// ```
+///
+/// Use instead:
+/// ```python
+/// {f(x) for x in foo}
+/// ```
+///
+/// ## Fix safety
+/// This rule's fix is marked as unsafe, as it may occasionally drop comments
+/// when rewriting the call. In most cases, though, comments will be preserved.
+#[violation]
+pub struct UnnecessaryGeneratorSet;
+
+impl AlwaysFixableViolation for UnnecessaryGeneratorSet {
+    #[derive_message_formats]
+    fn message(&self) -> String {
+        format!("Unnecessary generator (rewrite as a `set` comprehension)")
+    }
+
+    fn fix_title(&self) -> String {
+        "Rewrite as a `set` comprehension".to_string()
+    }
+}
+
+/// C401 (`set(generator)`)
+pub(crate) fn unnecessary_generator_set(checker: &mut Checker, call: &ast::ExprCall) {
+    let Some(argument) = helpers::exactly_one_argument_with_matching_function(
+        "set",
+        &call.func,
+        &call.arguments.args,
+        &call.arguments.keywords,
+    ) else {
+        return;
+    };
+    if !checker.semantic().is_builtin("set") {
+        return;
+    }
+    if argument.is_generator_expr() {
+        let mut diagnostic = Diagnostic::new(UnnecessaryGeneratorSet, call.range());
+
+        // Convert `set(x for x in y)` to `{x for x in y}`.
+        diagnostic.set_fix({
+            // Replace `set(` with `}`.
+            let call_start = Edit::replacement(
+                pad_start("{", call.range(), checker.locator(), checker.semantic()),
+                call.start(),
+                call.arguments.start() + TextSize::from(1),
+            );
+
+            // Replace `)` with `}`.
+            let call_end = Edit::replacement(
+                pad_end("}", call.range(), checker.locator(), checker.semantic()),
+                call.arguments.end() - TextSize::from(1),
+                call.end(),
+            );
+
+            Fix::unsafe_edits(call_start, [call_end])
+        });
+
+        checker.diagnostics.push(diagnostic);
+    }
+}
---
+++ packages/ruff/ruff-0.3.4.tar.gz/ruff-0.3.4/crates/ruff_linter/src/rules/flake8_pyi/rules/unaliased_collections_abc_set_import.rs
@@ -0,0 +1,93 @@
+use ruff_diagnostics::{Applicability, Diagnostic, Fix, FixAvailability, Violation};
+use ruff_macros::{derive_message_formats, violation};
+use ruff_python_semantic::Imported;
+use ruff_python_semantic::{Binding, BindingKind};
+use ruff_text_size::Ranged;
+
+use crate::checkers::ast::Checker;
+
+use crate::renamer::Renamer;
+
+/// ## What it does
+/// Checks for `from collections.abc import Set` imports that do not alias
+/// `Set` to `AbstractSet`.
+///
+/// ## Why is this bad?
+/// The `Set` type in `collections.abc` is an abstract base class for set-like types.
+/// It is easily confused with, and not equivalent to, the `set` builtin.
+///
+/// To avoid confusion, `Set` should be aliased to `AbstractSet` when imported. This
+/// makes it clear that the imported type is an abstract base class, and not the
+/// `set` builtin.
+///
+/// ## Example
+/// ```python
+/// from collections.abc import Set
+/// ```
+///
+/// Use instead:
+/// ```python
+/// from collections.abc import Set as AbstractSet
+/// ```
+///
+/// ## Fix safety
+/// This rule's fix is marked as unsafe for `Set` imports defined at the
+/// top-level of a module. Top-level symbols are implicitly exported by the
+/// module, and so renaming a top-level symbol may break downstream modules
+/// that import it.
+#[violation]
+pub struct UnaliasedCollectionsAbcSetImport;
+
+impl Violation for UnaliasedCollectionsAbcSetImport {
+    const FIX_AVAILABILITY: FixAvailability = FixAvailability::Sometimes;
+
+    #[derive_message_formats]
+    fn message(&self) -> String {
+        format!(
+            "Use `from collections.abc import Set as AbstractSet` to avoid confusion with the `set` builtin"
+        )
+    }
+
+    fn fix_title(&self) -> Option<String> {
+        Some(format!("Alias `Set` to `AbstractSet`"))
+    }
+}
+
+/// PYI025
+pub(crate) fn unaliased_collections_abc_set_import(
+    checker: &Checker,
+    binding: &Binding,
+) -> Option<Diagnostic> {
+    let BindingKind::FromImport(import) = &binding.kind else {
+        return None;
+    };
+    if !matches!(
+        import.qualified_name().segments(),
+        ["collections", "abc", "Set"]
+    ) {
+        return None;
+    }
+
+    let name = binding.name(checker.locator());
+    if name == "AbstractSet" {
+        return None;
+    }
+
+    let mut diagnostic = Diagnostic::new(UnaliasedCollectionsAbcSetImport, binding.range());
+    if checker.semantic().is_available("AbstractSet") {
+        diagnostic.try_set_fix(|| {
+            let scope = &checker.semantic().scopes[binding.scope];
+            let (edit, rest) = Renamer::rename(name, "AbstractSet", scope, checker.semantic())?;
+            Ok(Fix::applicable_edits(
+                edit,
+                rest,
+                if scope.kind.is_module() {
+                    Applicability::Unsafe
+                } else {
+                    Applicability::Safe
+                },
+            ))
+        });
+    }
+    Some(diagnostic)
+}
