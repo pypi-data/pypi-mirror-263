ddbf9d2833058f0c862d97daca9a282d37fd401f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 17:05:00 +0000

    Add package flexy-token/0.8/flexy_token-0.8-py3-none-any.whl

---
+++ packages/flexy-token/flexy_token-0.8-py3-none-any.whl/flexy_token/api_client.py
@@ -0,0 +1,52 @@
+import requests
+from datetime import timezone
+import jwt
+from get_url import url
+
+class APIClient:
+	def __init__(self, account_sid, api_secret,api_key,identity):
+		self.account_sid = account_sid
+		self.api_secret = api_secret
+		self.api_key= api_key
+		self.identity=identity
+		
+
+def AccessToken(account_sid,api_secret,api_key,identity=""):
+		try:
+		
+			payload = {
+				'account_sid': account_sid,
+				'api_key': api_key,
+				'api_secret': api_secret,
+				'identity': identity,
+			}   
+			token = jwt.encode(payload, 'your_secret_key', algorithm='HS256')
+			
+			print (token,'tokentt')
+
+			return token
+
+		except Exception as e:
+			return f"Token unable to generate because of an exception"
+
+
+ 
+def CreateUser(token,username,password,email):
+	try:
+		payload = {
+			'token': token,
+			'username': username,
+			'password': password,
+			'email': email
+		}
+		send_url = f"{url}/api/create-third-party-user/"
+
+		response = requests.post(send_url,headers='',json=payload)
+
+		if response.status_code == 201:
+			return print("User created succesfully")
+		else:
+			return print("User already exists")
+	
+	except Exception as e:
+		return print("Invalid data")
