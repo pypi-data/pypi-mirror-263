f043e3a2ace855785d6300b44f5b65c764fa1beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 03:05:26 +0000

    Add package Promethee/0.5/Promethee-0.5.tar.gz

---
+++ packages/Promethee/Promethee-0.5.tar.gz/Promethee-0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as fh:
+    longdescription = fh.read()
+
+setup(
+    name="Promethee",
+    version="0.5",
+    author="Eduardo Teles, Wilson Freita, Vinícius Oliveira, Gabriel Monteiro, Josef Jaeger",
+    description="Promethee algorithm",
+    long_description=longdescription,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=['pandas',]
+)
