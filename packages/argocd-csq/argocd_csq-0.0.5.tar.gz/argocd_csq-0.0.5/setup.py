9f503906c20ab6bc417c05ee3ed2b726669413cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 04:04:57 +0000

    Add package argocd-csq/0.0.5/argocd_csq-0.0.5.tar.gz

---
+++ packages/argocd-csq/argocd_csq-0.0.5.tar.gz/argocd_csq-0.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'argocd_csq',
+    version = '0.0.5',
+    description = 'CS tool to manage ArgoCD',
+    packages = find_packages(),
+    entry_points={
+        'console_scripts': [
+            'argocd_csq = argocd_csq.entrypoint_script:main',
+        ],
+    },
+    
+    install_requires = [
+        "colorama==0.4.6",
+        "inquirer==3.2.4",
+        "PyJWT==2.8.0",
+        "Requests==2.31.0",
+        "structlog==24.1.0"
+    ],
+    python_requires = ">=3.10"
+)
