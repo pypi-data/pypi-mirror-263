9ed2cf72156060e61527c1aafbfdca025460d742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 03:05:34 +0000

    Add package mov-cli-films/1.2.8/mov-cli-films-1.2.8.tar.gz

---
+++ packages/mov-cli-films/mov-cli-films-1.2.8.tar.gz/mov-cli-films-1.2.8/mov_cli_films/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from mov_cli.plugins import PluginHookData
+
+from .vadapav import *
+from .vidsrcto import *
+
+plugin: PluginHookData = {
+    "version": 1, 
+    "scrapers": {
+        "DEFAULT": VidSrcToScraper, 
+        "vadapav": VadapavScraper,
+        "vidsrcto": VidSrcToScraper
+    }
+}
+
+__version__ = "1.2.8"
