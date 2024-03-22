12cb128f651045047531e9611f83f8420253c764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 14:06:30 +0000

    Add package abqpy/2018.7.2/abqpy-2018.7.2.tar.gz

---
+++ packages/abqpy/abqpy-2018.7.2.tar.gz/abqpy-2018.7.2/src/sketch.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+
+class FeatureSketch:
+    ...
+
+
+def Sketch(self, sheetSize: float = 200, gridSize: float = 5) -> FeatureSketch:
+    return FeatureSketch()
+
+
+def SketchTransform():
+    ...
+
+
+__all__ = [
+    "Sketch",
+    "SketchTransform",
+]
