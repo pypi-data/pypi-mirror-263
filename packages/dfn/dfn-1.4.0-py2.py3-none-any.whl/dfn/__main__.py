194db4ea37afda25c93d7c4a10f9be38413f1894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 03:05:28 +0000

    Add package dfn/1.4.0/dfn-1.4.0-py2.py3-none-any.whl

---
+++ packages/dfn/dfn-1.4.0-py2.py3-none-any.whl/dfn/__main__.py
@@ -0,0 +1,23 @@
+import argparse as arg
+import os
+
+from .util import download_from_nas
+
+def dfn_parser():
+    parser = arg.ArgumentParser(description     = "download_drom_nas",
+                                formatter_class = arg.ArgumentDefaultsHelpFormatter,
+                                prog            = "python -m dfn")
+    required_group = parser.add_argument_group("required", "required parameters for compilation")
+    required_group.add_argument("--url",
+                                type    = str,
+                                required= True,
+                                help    = "url")
+    required_group.add_argument("--sfyc",
+                                action = "store_true",
+                                help    = "是否是算法一处的网盘")
+    return parser
+if __name__ == "__main__":
+    parser = dfn_parser()
+    a = parser.parse_args()
+    download_from_nas(a.url, a.sfyc)
+    
