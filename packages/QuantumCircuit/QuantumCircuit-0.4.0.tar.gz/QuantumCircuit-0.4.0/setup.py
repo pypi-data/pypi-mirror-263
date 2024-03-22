33c5bcf1f7a8d386c393a46ae443f31454489028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 16:07:44 +0000

    Add package QuantumCircuit/0.4.0/QuantumCircuit-0.4.0.tar.gz

---
+++ packages/QuantumCircuit/QuantumCircuit-0.4.0.tar.gz/QuantumCircuit-0.4.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='QuantumCircuit',
+    version='0.4.0',
+    description='CETC量子模拟器',  # 包描述
+    long_description=long_description,  # 详细描述
+    long_description_content_type='text/markdown',
+    author='CETC',  # 作者姓名
+    author_email='wangzhiqiang@tgqs.net',  # 作者邮箱
+    license='MIT',  # 许可证
+    packages=find_packages(),
+    install_requires=[
+        'numpy==1.24.3',
+        'numba==0.58.1',
+        'matplotlib==3.7.1',
+    ],
+    classifiers=[
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    ],
+)
---
+++ packages/QuantumCircuit/QuantumCircuit-0.4.0.tar.gz/QuantumCircuit-0.4.0/simulator_tgq/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+from .simulator_tgq import *
+
+name = "QuantumCircuit"
