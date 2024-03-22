0c7e647916b454f8b448a09e22fbe62a51de8939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 03:06:17 +0000

    Add package tad-mctc/0.0.6/tad_mctc-0.0.6-py3-none-any.whl

---
+++ packages/tad-mctc/tad_mctc-0.0.6-py3-none-any.whl/tad_mctc/typing/__init__.py
@@ -0,0 +1,29 @@
+# This file is part of tad-mctc.
+#
+# SPDX-Identifier: LGPL-3.0
+# Copyright (C) 2023 Marvin Friede
+#
+# tad-mctc is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# tad_mctc is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with tad-mctc. If not, see <https://www.gnu.org/licenses/>.
+"""
+Typing
+======
+
+This module contains all type annotations for this project.
+
+Since typing still significantly changes across different Python versions,
+all the special cases are handled here as well.
+"""
+from .builtin import *
+from .compat import *
+from .pytorch import *
