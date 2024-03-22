bb979c5a0976da8d89076d6794a17ba715418228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 06:05:24 +0000

    Add package hyperetes/0.0.1/hyperetes-0.0.1-py3-none-any.whl

---
+++ packages/hyperetes/hyperetes-0.0.1-py3-none-any.whl/hyperetes/assistant.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Python
+
+"""Copyright (c) Alexander Fedotov.
+This source code is licensed under the license found in the
+LICENSE file in the root directory of this source tree.
+"""
+if __name__ == "__main__":
+    print('Hyperetes is an Assistant')
