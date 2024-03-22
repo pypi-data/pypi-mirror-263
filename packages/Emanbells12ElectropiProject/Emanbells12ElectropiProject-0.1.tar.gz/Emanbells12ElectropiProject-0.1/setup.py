9f2a7e11780e8c793641cf5d3807a233a137dd57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 06:05:35 +0000

    Add package Emanbells12ElectropiProject/0.1/Emanbells12ElectropiProject-0.1.tar.gz

---
+++ packages/Emanbells12ElectropiProject/Emanbells12ElectropiProject-0.1.tar.gz/Emanbells12ElectropiProject-0.1/setup.py
@@ -0,0 +1,13 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Emanbells12ElectropiProject',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy'
+    ],
+)
