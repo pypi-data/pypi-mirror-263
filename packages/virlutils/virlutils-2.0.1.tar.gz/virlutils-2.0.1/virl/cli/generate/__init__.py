80ebcece185bbfbadb62779f95ddeaa5ae6c0a17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 14:06:24 +0000

    Add package virlutils/2.0.1/virlutils-2.0.1.tar.gz

---
+++ packages/virlutils/virlutils-2.0.1.tar.gz/virlutils-2.0.1/virl/cli/generate/__init__.py
@@ -0,0 +1,32 @@
+import click
+from virl.api import plugin, check_valid_plugin, NoPluginError
+from virl.cli.generate.ansible.commands import ansible
+from virl.cli.generate.pyats.commands import pyats
+from virl.cli.generate.nso.commands import nso
+
+
+@click.group()
+def generate():
+    """
+    generate inv file for various tools
+    """
+    pass
+
+
+def init_generators():
+    generate.add_command(ansible)
+    generate.add_command(pyats)
+    generate.add_command(nso)
+
+    for gen in plugin.Plugin.get_plugins("generator"):
+        try:
+            pl = plugin.GeneratorPlugin(generator=gen)
+        except NoPluginError:
+            continue
+        if not check_valid_plugin(pl, pl.generate, "generate"):
+            click.secho(
+                "ERROR: Malformed plugin for generator {}.  The `generate` method must be static and a click.command".format(gen), fg="red"
+            )
+            plugin.Plugin.remove_plugin("generator", gen)
+        else:
+            generate.add_command(pl.generate, name=gen)
---
+++ packages/virlutils/virlutils-2.0.1.tar.gz/virlutils-2.0.1/virl/cli/telnet/commands.py
@@ -0,0 +1,53 @@
+import click
+from virl.api import VIRLServer
+from subprocess import call
+from virl.helpers import (
+    get_cml_client,
+    get_current_lab,
+    safe_join_existing_lab,
+    get_node_mgmt_ip,
+)
+from virl2_client.exceptions import NodeNotFound
+
+
+@click.command()
+@click.argument("node", nargs=1)
+def telnet(node):
+    """
+    telnet to a node
+    """
+    server = VIRLServer()
+    client = get_cml_client(server)
+
+    current_lab = get_current_lab()
+    if current_lab:
+        lab = safe_join_existing_lab(current_lab, client)
+        if lab:
+            try:
+                node_obj = lab.get_node_by_label(node)
+            except NodeNotFound:
+                click.secho("Node {} was not found in lab {}".format(node, current_lab), fg="red")
+                exit(1)
+
+            if node_obj.is_active():
+                mgmtip = get_node_mgmt_ip(node_obj)
+                if mgmtip:
+                    if "VIRL_TELNET_COMMAND" in server.config:
+                        cmd = server.config["VIRL_TELNET_COMMAND"]
+                        cmd = cmd.format(host=mgmtip)
+                        print("Calling user specified command: {}".format(cmd))
+                        exit(call(cmd.split()))
+                    else:
+                        click.secho("Attemping telnet connection to {} at {}".format(node_obj.label, mgmtip))
+
+                        exit(call(["telnet", mgmtip]))
+                else:
+                    click.secho("Node {} does not have an external management IP".format(node_obj.label))
+            else:
+                click.secho("Node {} is not active".format(node_obj.label), fg="yellow")
+        else:
+            click.secho("Unable to find lab {}".format(current_lab), fg="red")
+            exit(1)
+    else:
+        click.secho("No current lab set", fg="red")
+        exit(1)
