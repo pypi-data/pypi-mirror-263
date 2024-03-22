5a1ae9cdf21387331d6e3361c15bbc3837b7effa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 15:04:43 +0000

    Add package hzh/1.1/hzh-1.1.tar.gz

---
+++ packages/hzh/hzh-1.1.tar.gz/hzh-1.1/hzh/pdf2txt.py
@@ -0,0 +1,76 @@
+# !usr/bin/env python
+# -*- coding:utf-8 -*-
+
+'''
+ Author       : Huang zh
+ Email        : jacob.hzh@qq.com
+ Date         : 2024-03-21 17:42:49
+ LastEditTime : 2024-03-22 10:03:21
+ FilePath     : \\hzh\\hzh\\pdf2txt.py
+ Description  : 
+'''
+
+from nltk.corpus import stopwords
+from collections import Counter
+from nltk.tokenize import word_tokenize
+from pypdf import PdfReader
+from tqdm import trange
+
+def get_pdf_txt(path):
+    all_txt = []
+    reader = PdfReader(path)
+    number_of_pages = len(reader.pages)
+    for i in trange(number_of_pages):
+        page = reader.pages[i]
+        text = page.extract_text()
+        text = text.replace('-\n', '').replace('\n',' ')
+        all_txt.append(text)
+    return ' '.join(all_txt)
+
+def process(text):
+    # nltk.download('stopwords')
+    # nltk.download('punkt')
+
+    # 加载停用词列表
+    stop_words = list(set(stopwords.words('english')))
+    stop_words.extend(['int', 'float', 'llm', 'llms', 'cnn', 'rnn', 'lstm', 'rnns', 'bert', 'gpt', 'gpt1', 'gpt2',
+                        'gpt3', 'chatgpt', 'gpt4', 'elmo', 'TrafficBERT', 'arXiv', 'nlp', 'acm', 'frp','rmspe',
+                        'marre', 'rmsle', 'tnr', 'fnr', 'tpr', 'sft', 'emnlp'])
+    
+    # 分词
+    words = word_tokenize(text)
+
+    # 去除停用词
+    filtered_words = [word for word in words if word.lower() not in stop_words and word.lower().isalpha()]
+    filtered_words = [word for word in filtered_words if len(word) > 2]
+    return filtered_words
+
+
+
+def count_and_sort_words(text):
+    # 使用Counter统计单词出现的次数
+    word_count = Counter(text)
+    # 按照出现频率进行排序
+    sorted_word_count = dict(sorted(word_count.items(), key=lambda item: item[1], reverse=True))
+    return sorted_word_count
+
+def write_word_count_to_file(word_count, output_file, counts=3):
+    all_count = 0
+    with open(output_file, 'w', encoding='utf-8') as file:
+        for word, count in word_count.items():
+            if count >= counts:
+                all_count += 1
+                file.write(f"{word}\n")
+    print(f"共{all_count}个单词")
+
+
+def get_words_from_pdf(pdf_path, output_path):
+    text = get_pdf_txt(pdf_path)
+    process_text = process(text)
+    sort_words = count_and_sort_words(process_text)
+    write_word_count_to_file(sort_words, output_path)
+
+if __name__ == '__main__':
+    pdf_path, output_path = '123.pdf', './cs.txt'
+    get_words_from_pdf(pdf_path, output_path)
+    
---
+++ packages/hzh/hzh-1.1.tar.gz/hzh-1.1/setup.py
@@ -0,0 +1,42 @@
+# !usr/bin/env python
+# -*- coding:utf-8 -*-
+
+'''
+ Author       : Huang zh
+ Email        : jacob.hzh@qq.com
+ Date         : 2024-03-22 09:43:44
+ LastEditTime : 2024-03-22 10:11:35
+ FilePath     : \\hzh\\setup.py
+ Description  : 
+'''
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+def get_install_requires():
+    requires = [
+        'pypdf==4.1.0',
+        'tqdm',
+        'nltk',
+    ]
+    return requires
+setuptools.setup(
+    name="hzh",  # 模块名称
+    version="1.1",  # 当前版本
+    author="leander",  # 作者
+    author_email="1034235826@qq.com",  # 作者邮箱
+    description="Get the high-frequency words in the paper to the local txt file, which is convenient to pass the word book to recite",  # 模块简介
+    long_description=long_description,  # 模块详细介绍
+    # long_description_content_type="text/markdown",  # 模块详细介绍格式
+    # url="https://github.com/wupeiqi/fucker",  # 模块github地址
+    packages=setuptools.find_packages(),  # 自动找到项目中导入的模块
+    # 模块相关的元数据
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # 依赖模块
+    install_requires=get_install_requires(),
+    python_requires='>=3',
+)
