88141d332c0aa10bec8458202fad4223efc7beac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 15:05:00 +0000

    Add package cloudflare/3.0.0b5/cloudflare-3.0.0b5.tar.gz

---
+++ packages/cloudflare/cloudflare-3.0.0b5.tar.gz/cloudflare-3.0.0b5/src/cloudflare/types/queue_list_response.py
@@ -0,0 +1,9 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List
+
+from .workers_queue import WorkersQueue
+
+__all__ = ["QueueListResponse"]
+
+QueueListResponse = List[WorkersQueue]
