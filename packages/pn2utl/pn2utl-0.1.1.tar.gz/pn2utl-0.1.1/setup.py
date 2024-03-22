576d59307ca606a673c67c58f9bd538e5309e436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 03:04:42 +0000

    Add package pn2utl/0.1.1/pn2utl-0.1.1.tar.gz

---
+++ packages/pn2utl/pn2utl-0.1.1.tar.gz/pn2utl-0.1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pn2utl",
+    version="0.1.1",
+    author="leehiking",
+    author_email="gz6201347@163.com",
+    description="test first package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/leehiking/my_pkg_1",
+    packages=setuptools.find_packages(),
+    install_requires=['torch>=1.0', 'numpy>=1.0'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
