33f5f90a6d7111949439bb7f0befc9d65df98d12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 14:05:34 +0000

    Add package andromeda-torch/0.0.9/andromeda_torch-0.0.9-py3-none-any.whl

---
+++ packages/andromeda-torch/andromeda_torch-0.0.9-py3-none-any.whl/andromeda_torch/__init__.py
@@ -0,0 +1,7 @@
+from andromeda_torch.model import Andromeda
+from andromeda_torch.tokenizer import Tokenizer
+
+__all__ = [
+    "Andromeda",
+    "Tokenizer",
+]
---
+++ packages/andromeda-torch/andromeda_torch-0.0.9-py3-none-any.whl/andromeda_torch/configs.py
@@ -0,0 +1,108 @@
+from andromeda_torch.model import Andromeda
+
+Andromeda1Billion = Andromeda(
+    num_tokens=25000,
+    max_seq_len=4192,
+    dim=2048,
+    depth=16,
+    dim_head=128,
+    heads=8,
+    use_abs_pos_emb=False,
+    alibi_pos_bias=True,
+    alibi_num_heads=4,
+    rotary_xpos=True,
+    attn_flash=True,
+    attn_kv_heads=2,
+    qk_norm=True,
+    attn_qk_norm=True,
+    attn_qk_norm_dim_scale=True,
+)
+
+
+Andromeda3Billion = Andromeda(
+    num_tokens=50432,
+    max_seq_len=8192,
+    dim=3072,
+    depth=24,
+    dim_head=128,
+    heads=12,
+    use_abs_pos_emb=False,
+    alibi_pos_bias=True,
+    alibi_num_heads=6,
+    rotary_xpos=True,
+    attn_kv_heads=2,
+    qk_norm=True,
+    attn_qk_norm=True,
+    attn_qk_norm_dim_scale=True,
+)
+
+
+Andromeda7Billion = Andromeda(
+    num_tokens=50432,
+    max_seq_len=8192,
+    dim=4096,
+    depth=32,
+    dim_head=128,
+    heads=16,
+    use_abs_pos_emb=False,
+    alibi_pos_bias=True,
+    alibi_num_heads=8,
+    rotary_xpos=True,
+    attn_kv_heads=2,
+    qk_norm=True,
+    attn_qk_norm=True,
+    attn_qk_norm_dim_scale=True,
+)
+
+Andromeda10Billion = Andromeda(
+    num_tokens=50432,
+    max_seq_len=8192,
+    dim=5120,
+    depth=32,
+    dim_head=128,
+    heads=20,
+    use_abs_pos_emb=False,
+    alibi_pos_bias=True,
+    alibi_num_heads=4,
+    rotary_xpos=True,
+    attn_kv_heads=2,
+    qk_norm=True,
+    attn_qk_norm=True,
+    attn_qk_norm_dim_scale=True,
+)
+
+Andromeda15Billion = Andromeda(
+    num_tokens=50432,
+    max_seq_len=8192,
+    dim=6144,
+    depth=40,
+    dim_head=128,
+    heads=24,
+    use_abs_pos_emb=False,
+    alibi_pos_bias=True,
+    alibi_num_heads=4,
+    rotary_xpos=True,
+    attn_kv_heads=2,
+    qk_norm=True,
+    attn_qk_norm=True,
+    attn_qk_norm_dim_scale=True,
+)
+
+Andromeda20Billion = Andromeda(
+    num_tokens=50432,
+    max_seq_len=8192,
+    dim=7168,
+    depth=48,
+    dim_head=128,
+    heads=28,
+    use_abs_pos_emb=False,
+    alibi_pos_bias=True,
+    alibi_num_heads=4,
+    rotary_xpos=True,
+    attn_kv_heads=2,
+    qk_norm=True,
+    attn_qk_norm=True,
+    attn_qk_norm_dim_scale=True,
+)
+
+# to GPT like 176Billion Parameters 122888 dimension, 96 depth, 96 heads, attn dim head 128
