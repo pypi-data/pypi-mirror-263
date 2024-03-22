46bd3505d47e2fcf2bfb739881ef9596db6ba1e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 04:05:29 +0000

    Add package RuneArtist/1.0.7/RuneArtist-1.0.7.tar.gz

---
+++ packages/RuneArtist/RuneArtist-1.0.7.tar.gz/RuneArtist-1.0.7/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RuneArtist',
+    version='1.0.7',
+    author='kekwak',
+    author_email='kekwak@mail.ru',
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    package_data={
+        'RuneArtist': ['pictures/*'],
+    },
+    install_requires=['requests>=2.25.1'],
+    classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: Microsoft :: Windows',
+    ],
+    keywords='python',
+    python_requires='>=3.7'
+)
