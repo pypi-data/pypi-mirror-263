e60846ee020d52b2c3860cef4366a21008cb47ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 17:04:46 +0000

    Add package mkdocs-asyncapi-html-plugin/0.2.5/mkdocs_asyncapi_html_plugin-0.2.5.tar.gz

---
+++ packages/mkdocs-asyncapi-html-plugin/mkdocs_asyncapi_html_plugin-0.2.5.tar.gz/mkdocs_asyncapi_html_plugin-0.2.5/mkdocs_asyncapi_html_plugin/mkdocs_asyncapi_html_plugin.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import re
+import subprocess
+import mkdocs
+import tempfile
+import os
+import shutil
+
+PATTERN = r'(<iframe\s+.*?asyncapi="([^"]*)".*?src="([^"]*)".*?>)'
+MARKER = re.compile(PATTERN)
+
+class AsyncAPIPlugin(mkdocs.plugins.BasePlugin):
+    def on_page_markdown(self, markdown, page, config, files):
+        match = MARKER.search(markdown)
+        if match is None:
+            return markdown
+        indir = "docs"
+        path = match.group(2)
+        html_path = os.path.splitext(path)[0] + '.html'
+        newfile = os.path.join(indir, html_path)
+
+        print(f'asyncapi path: {path}')
+        print(f'src path: {html_path}')
+
+        if os.path.isfile(newfile):
+            print(f'{newfile} exists')
+            return markdown
+        
+        markdown = re.sub(PATTERN, '', markdown)
+        with tempfile.TemporaryDirectory() as outdir:
+            infile = os.path.join(indir, path)
+            print("Generating HTML from", infile)
+            subprocess.run(
+                [
+                    "ag",
+                    infile,
+                    "@asyncapi/html-template",
+                    "-p",
+                    "singleFile=true"
+                    "--force-write",
+                    "-o",
+                    outdir,
+                ],
+                check=True,
+            )
+            print ("Done generating HTML")
+            outfile = os.path.join(outdir, 'index.html')
+            print("Moving", outfile, "to", newfile)
+
+            shutil.copy(outfile, newfile)
+
+        return markdown
