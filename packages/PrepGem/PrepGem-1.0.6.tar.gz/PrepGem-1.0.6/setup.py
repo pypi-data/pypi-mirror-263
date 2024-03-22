835034bc6aac245fda5f5088a6df9db8de3b97fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 15:05:23 +0000

    Add package PrepGem/1.0.6/PrepGem-1.0.6.tar.gz

---
+++ packages/PrepGem/PrepGem-1.0.6.tar.gz/PrepGem-1.0.6/setup.py
@@ -0,0 +1,37 @@
+from __future__ import division
+try:
+    import setuptools
+except:
+    print ('''
+setuptools not found.
+
+On linux, the package is often called python-setuptools''')
+    from sys import exit
+    exit(1)
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='PrepGem',
+    version='1.0.6',
+    author='Mohamed Edderyouch',
+    author_email='mohamededderyouch5@gmail.com',
+    maintainer= 'Zineb Boughedda',
+    license='Academic Free License',
+    python_requires='>=3.6',
+    packages=setuptools.find_packages(),
+    description='Text Preprocessing package for NLP projects',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['nltk', 'pandas', 'tqdm','SpellChecker','IPython', 'textblob'],
+    url='https://github.com/simoderyouch',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+)
