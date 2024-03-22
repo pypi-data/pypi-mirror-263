b8700cea89073d67b9c0825831cc42790b548dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 06:04:43 +0000

    Add package jupyter-pieces/1.14.0/jupyter_pieces-1.14.0-py3-none-any.whl

---
+++ packages/jupyter-pieces/jupyter_pieces-1.14.0-py3-none-any.whl/jupyter_pieces-1.14.0.data/data/share/jupyter/labextensions/jupyter_pieces/static/_92c0.10433fb690b63ec97044.js
@@ -0,0 +1,14 @@
+(self["webpackChunkjupyter_pieces"] = self["webpackChunkjupyter_pieces"] || []).push([["_92c0"],{
+
+/***/ "?92c0":
+/*!************************!*\
+  !*** crypto (ignored) ***!
+  \************************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+}]);
+//# sourceMappingURL=_92c0.10433fb690b63ec97044.js.map
