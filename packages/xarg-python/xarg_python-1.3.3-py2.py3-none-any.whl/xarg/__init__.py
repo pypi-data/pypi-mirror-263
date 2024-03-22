7ba06945d11e9e58b8fecd3bb86a5c3ba0422325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 03:05:12 +0000

    Add package xarg-python/1.3.3/xarg_python-1.3.3-py2.py3-none-any.whl

---
+++ packages/xarg-python/xarg_python-1.3.3-py2.py3-none-any.whl/xarg/__init__.py
@@ -0,0 +1,17 @@
+# coding:utf-8
+
+from .util import __version__
+from .util import chdir
+from .util import safile
+from .util import singleton
+
+from .actuator import Namespace
+from .actuator import commands
+from .actuator import add_command
+from .actuator import end_command
+from .actuator import pre_command
+from .actuator import run_command
+
+from .parser import argp
+
+from .scanner import scanner
