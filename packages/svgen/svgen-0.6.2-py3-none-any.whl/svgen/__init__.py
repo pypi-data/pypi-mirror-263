675607c1f2fbe66210ebec4bed6be6ef91db2826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 15:04:42 +0000

    Add package svgen/0.6.2/svgen-0.6.2-py3-none-any.whl

---
+++ packages/svgen/svgen-0.6.2-py3-none-any.whl/svgen/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.4
+# hash=94653ca8b39ed4b46cf711ce392361e1
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A tool for working with scalable vector graphics."
+PKG_NAME = "svgen"
+VERSION = "0.6.2"
