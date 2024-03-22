981d9a2c488da008ba2b7f379ff72326d10bc804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 03:05:34 +0000

    Add package dfn/1.5.0/dfn-1.5.0.tar.gz

---
+++ packages/dfn/dfn-1.5.0.tar.gz/dfn-1.5.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='dfn',
+    version='1.5.0',
+    author='yifei.gao',
+    author_email='yifei.gao@sophgo.com',
+    description='download_from_nas',
+    packages=['dfn'],
+    install_requires=["requests","tqdm"]
+)
