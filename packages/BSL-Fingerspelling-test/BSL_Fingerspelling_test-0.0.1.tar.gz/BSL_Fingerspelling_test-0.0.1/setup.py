eaf6de2761dd2111fd07a7bc216bc3f78fa94da5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 04:04:19 +0000

    Add package BSL-Fingerspelling-test/0.0.1/BSL_Fingerspelling_test-0.0.1.tar.gz

---
+++ packages/BSL-Fingerspelling-test/BSL_Fingerspelling_test-0.0.1.tar.gz/BSL_Fingerspelling_test-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    include_package_data=True,
+    name='BSL_Fingerspelling_test',
+    version='0.0.1',
+    description='Test',
+    author="Miguel Garcia Hernandez",
+    author_email="miguelgarher01@gmail.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
