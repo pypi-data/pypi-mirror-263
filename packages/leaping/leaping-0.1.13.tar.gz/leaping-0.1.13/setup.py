a787481ef47e8e47a9e6a5715f6f274b50152e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 16:07:58 +0000

    Add package leaping/0.1.13/leaping-0.1.13.tar.gz

---
+++ packages/leaping/leaping-0.1.13.tar.gz/leaping-0.1.13/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="leaping",
+    version="0.1.13",
+    entry_points={
+        'console_scripts': [
+            'leaping=leaping:main',
+        ],
+    },
+    python_requires='>=3.0',
+    install_requires=[
+        "pytest-leaping==0.1.13",
+        "prompt_toolkit==3.0.20",
+        "openai==1.12.0"
+    ],
+)
