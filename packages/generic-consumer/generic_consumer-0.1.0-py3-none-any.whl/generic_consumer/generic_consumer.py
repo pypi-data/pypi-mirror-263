148ee5a395bda14a197fd20178343a3a1f8109cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 15:05:28 +0000

    Add package generic-consumer/0.1.0/generic_consumer-0.1.0-py3-none-any.whl

---
+++ packages/generic-consumer/generic_consumer-0.1.0-py3-none-any.whl/generic_consumer/generic_consumer.py
@@ -0,0 +1,49 @@
+from abc import abstractmethod
+from typing import final
+
+
+class GenericConsumer:
+    @final
+    def __init__(self, *args, **kwargs):
+        self.payloads: list = None  # type: ignore
+        self.__args = args
+        self.__kwargs = kwargs
+
+        self._initialize(*args, **kwargs)
+
+    @abstractmethod
+    def _initialize(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def _has_payload(self, *args, **kwargs) -> bool:
+        return True
+
+    @abstractmethod
+    def _get_payloads(self, *args, **kwargs) -> list:
+        return []
+
+    @abstractmethod
+    def _run(self, *args, **kwargs):
+        pass
+
+    @final
+    def run(self, *args, **kwargs):
+        if self._has_payload():
+            self.payloads = self._get_payloads(
+                *self.__args,
+                *args,
+                **{
+                    **self.__kwargs,
+                    **kwargs,
+                },
+            )
+
+        return self._run(
+            *self.__args,
+            *args,
+            **{
+                **self.__kwargs,
+                **kwargs,
+            },
+        )
