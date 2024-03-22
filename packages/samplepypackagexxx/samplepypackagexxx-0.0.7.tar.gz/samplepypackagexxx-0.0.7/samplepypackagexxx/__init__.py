df1a3e6fd1f18a22eabc160620a8c5b9313c5b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 04:04:19 +0000

    Add package samplepypackagexxx/0.0.7/samplepypackagexxx-0.0.7.tar.gz

---
+++ packages/samplepypackagexxx/samplepypackagexxx-0.0.7.tar.gz/samplepypackagexxx-0.0.7/samplepypackagexxx/__init__.py
@@ -0,0 +1,1 @@
+from samplepypackagexxx import add, subtract
---
+++ packages/samplepypackagexxx/samplepypackagexxx-0.0.7.tar.gz/samplepypackagexxx-0.0.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.7"
+DESCRIPTION = "sample package"
+
+setup(
+    name="samplepypackagexxx",
+    version=VERSION,
+    author="Julian Amin",
+    author_email="julian.amin@infor.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=["samplepackage"],
+    # classifiers=[
+    #     "sample :: sample"
+    # ]
+)
