4f9f88b68adb12ba06aaf0dd31da42ff2b9f7c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-22 05:04:30 +0000

    Add package telebot-mem/0.1.1/telebot_mem-0.1.1-py3-none-any.whl

---
+++ packages/telebot-mem/telebot_mem-0.1.1-py3-none-any.whl/telebot_mem/mongo_db.py
@@ -0,0 +1,35 @@
+import json
+
+from telebot_mem.db import MemDB
+
+from pymongo import MongoClient
+from telebot.types import Message
+
+
+class MongoMem(MemDB):
+    def __init__(self, connecion_string, db_name):
+        self.client = MongoClient(connecion_string)[db_name]
+
+    def insert_message(self, message: Message):
+        if message and message.json:
+            obj = message.json
+            obj.update({"_id": message.message_id})
+            self.client.messages.insert_one(obj)
+
+    def get_message(self, message_id):
+        return self.client.messages.find_one({"_id": message_id})
+
+    def get_last_message(self):
+        return self.client.messages.find_one(sort=[("_id", -1)])
+
+    def validate(self):
+        # check message collection exists
+        return "messages" in self.client.list_collection_names()
+
+    # decorator methods
+    def memorize(self, func):
+        def wrapper(*args, **kwargs):
+            self.insert_message(args[0] if args else kwargs["message"])
+            func(*args, **kwargs)
+
+        return wrapper
