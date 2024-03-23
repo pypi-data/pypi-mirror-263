9bf0a2e7cc1e0e4f0ed2bb27562fd696b9d9cae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 03:05:17 +0000

    Add package typecastai/0.1.3/typecastai-0.1.3-py3-none-any.whl

---
+++ packages/typecastai/typecastai-0.1.3-py3-none-any.whl/typecastai/actor.py
@@ -0,0 +1,18 @@
+
+class Actors:
+    def __init__(self) -> None:
+        pass
+
+    def get_ssfm_sample_actors(self):
+        return [
+            {
+                'name': 'Dan',
+                'actor_id': '615c6abd9d9ddf11c620ad8e',
+                'model_version': 'v4',
+            },
+            {
+                'name': 'Jodie',
+                'actor_id': '637cc000808a2da802dcb402',
+                'model_version': 'v3',
+            }
+        ]
