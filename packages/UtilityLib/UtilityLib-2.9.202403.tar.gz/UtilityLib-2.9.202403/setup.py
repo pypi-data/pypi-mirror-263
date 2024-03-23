2b0a1a4ad79239b99a4880cf31a4b463c8914114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 16:04:58 +0000

    Add package UtilityLib/2.9.202403/UtilityLib-2.9.202403.tar.gz

---
+++ packages/UtilityLib/UtilityLib-2.9.202403.tar.gz/UtilityLib-2.9.202403/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    packages=find_packages(),
+    description='UtilityLib: Lazy Evaluation Saves Time',
+    url='https://github.com/TheBiomics/UtilityLib',
+)
