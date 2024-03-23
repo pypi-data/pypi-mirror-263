ec5294f6edd58b335bf4cdeed2219f0b1e0159e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 05:05:01 +0000

    Add package winzig/0.2.1/winzig-0.2.1.tar.gz

---
+++ packages/winzig/winzig-0.2.1.tar.gz/winzig-0.2.1/winzig/main.py
@@ -0,0 +1,25 @@
+import asyncio
+import click
+from winzig.config import Config
+from winzig.database import create_db_and_tables, get_engine
+from winzig.commands import crawl_links, fetch_links, search_links, start_tui
+
+
+@click.group()
+@click.pass_context
+def cli(ctx):
+    if ctx.obj is None:
+        ctx.obj = {}
+
+    engine = get_engine(Config().sqlite_url)
+    asyncio.run(create_db_and_tables(engine))
+    ctx.obj["engine"] = engine
+
+
+cli.add_command(crawl_links)
+cli.add_command(fetch_links)
+cli.add_command(search_links)
+cli.add_command(start_tui)
+
+if __name__ == "__main__":
+    cli()
