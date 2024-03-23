4c7b1e1ab777804949d933d8ffd6e4f464247efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 14:04:16 +0000

    Add package JovQueue/0.0.2/jovqueue-0.0.2.tar.gz

---
+++ packages/JovQueue/jovqueue-0.0.2.tar.gz/jovqueue-0.0.2/README.md
@@ -0,0 +1,1 @@
+# JovQueue
---
+++ packages/JovQueue/jovqueue-0.0.2.tar.gz/jovqueue-0.0.2/tests/test_JovQueue.py
@@ -0,0 +1,23 @@
+from JovQueue import JovQueue
+import pytest
+import time
+import threading
+
+def example_test(value):
+    time.sleep(value)
+
+# We should throw an exception if someone attempts to create a 0 thread JovQueue
+def test_no_threads():
+    with pytest.raises(Exception):
+        JovQueue(0, example_test)
+
+# We should also throw an exception if someone attempts to create more than 99 threads
+def test_100_threads():
+    with pytest.raises(Exception):
+        JovQueue(100, example_test)
+
+# We want to make sure the correct number of threads is created when we run an example_test
+def test_correct_thread_counts():
+    jovqueue = JovQueue(8, example_test)
+    # We should then run a bunch of things through the Queue 
+    assert threading.active_count() == 9
