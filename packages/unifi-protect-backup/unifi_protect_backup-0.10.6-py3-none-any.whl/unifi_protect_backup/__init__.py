ecdaa5604993efdf614a18c9b2bee74d307f12c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 14:04:37 +0000

    Add package unifi-protect-backup/0.10.6/unifi_protect_backup-0.10.6-py3-none-any.whl

---
+++ packages/unifi-protect-backup/unifi_protect_backup-0.10.6-py3-none-any.whl/unifi_protect_backup/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level package for Unifi Protect Backup."""
+
+__author__ = """sebastian.goscik"""
+__email__ = 'sebastian@goscik.com'
+__version__ = '0.10.6'
+
+from .downloader import VideoDownloader
+from .event_listener import EventListener
+from .purge import Purge
+from .uploader import VideoUploader
+
+from .missing_event_checker import MissingEventChecker  # isort: skip
