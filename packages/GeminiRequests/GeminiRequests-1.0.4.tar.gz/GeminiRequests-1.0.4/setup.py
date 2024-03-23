b99fdd09fa2e7b8de335fe74602f27c45e5a4d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 16:04:32 +0000

    Add package GeminiRequests/1.0.4/GeminiRequests-1.0.4.tar.gz

---
+++ packages/GeminiRequests/GeminiRequests-1.0.4.tar.gz/GeminiRequests-1.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    version='1.0.4',
+    name='GeminiRequests',
+    packages=find_packages(),
+    license='MIT',
+    description='An example Python package',
+    long_description=open('gemini/README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Promiteus',
+    author_email='sbdt.israel@gmail.com',
+    install_requires=[
+        "requests"
+    ],
+)
