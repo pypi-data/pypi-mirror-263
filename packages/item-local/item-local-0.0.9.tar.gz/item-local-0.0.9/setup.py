f16a1fd2600574db07d379715c1d46159cee4e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 03:05:37 +0000

    Add package item-local/0.0.9/item-local-0.0.9.tar.gz

---
+++ packages/item-local/item-local-0.0.9.tar.gz/item-local-0.0.9/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+PACKAGE_NAME = "item-local"
+package_dir = PACKAGE_NAME.replace("-", "_")
+
+setuptools.setup(
+    name=PACKAGE_NAME,  # https://pypi.org/project/item-local
+    version='0.0.9',
+    author="Circles",
+    author_email="info@circles.life",
+    url=f"https://github.com/circles-zone/{PACKAGE_NAME}-python-package",
+    packages=[package_dir],
+    package_dir={package_dir: f'{package_dir}/src'},
+    package_data={package_dir: ['*.py']},
+    long_description="Item Local",
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "logger-local>=0.0.101",  # https://pypi.org/project/logger-local/
+    ]
+)
