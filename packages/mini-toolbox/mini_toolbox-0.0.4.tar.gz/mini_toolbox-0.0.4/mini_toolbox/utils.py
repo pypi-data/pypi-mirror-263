87092de534ae271537392644e86127a86fda7d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 16:04:48 +0000

    Add package mini-toolbox/0.0.4/mini_toolbox-0.0.4.tar.gz

---
+++ packages/mini-toolbox/mini_toolbox-0.0.4.tar.gz/mini_toolbox-0.0.4/mini_toolbox/utils.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Mail: haha@haha.com
+# @File: /autoosse:/code/mini_toolbox/mini_toolbox/utils.py
+# @Date: 2024-03-19 10:42:12
+# @Desc: 常用工具箱
+
+
+def add_one(number):
+    return number + 1
