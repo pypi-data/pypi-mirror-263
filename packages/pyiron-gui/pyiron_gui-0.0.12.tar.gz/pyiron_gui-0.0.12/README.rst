fc94f69e13a1862ba9ce29579b7d5a6be5f27446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 03:05:21 +0000

    Add package pyiron-gui/0.0.12/pyiron_gui-0.0.12.tar.gz

---
+++ packages/pyiron-gui/pyiron_gui-0.0.12.tar.gz/pyiron_gui-0.0.12/README.rst
@@ -0,0 +1,24 @@
+pyiron_gui
+============
+.. image:: https://coveralls.io/repos/github/pyiron/pyiron_gui/badge.svg?branch=main
+    :target: https://coveralls.io/github/pyiron/pyiron_gui?branch=main
+    :alt: Coverage Status
+
+.. image:: https://anaconda.org/conda-forge/pyiron_gui/badges/latest_release_date.svg
+    :target: https://anaconda.org/conda-forge/pyiron_gui/
+    :alt: Release_Date
+
+.. image:: https://github.com/pyiron/pyiron_gui/workflows/Python%20package/badge.svg
+    :target: https://github.com/pyiron//pyiron_gui/actions
+    :alt: Build Status
+
+    
+This repository is for GUI extensions to the overall pyiron framework.
+
+Getting started:
+----------------
+Test pyiron with mybinder:
+
+.. image:: https://mybinder.org/badge_logo.svg
+     :target: https://mybinder.org/v2/gh/pyiron/pyiron_gui/main
+     :alt: mybinder
---
+++ packages/pyiron-gui/pyiron_gui-0.0.12.tar.gz/pyiron_gui-0.0.12/setup.py
@@ -0,0 +1,45 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+
+import versioneer
+
+setup(
+    name='pyiron_gui',
+    version=versioneer.get_version(),
+    description='Repository for GUI plugins to the pyiron IDE.',
+    long_description='http://pyiron.org',
+
+    url='https://github.com/pyiron/pyiron_gui',
+    author='Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department',
+    author_email='siemer@mpie.de',
+    license='BSD',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
+    ],
+
+    keywords='pyiron',
+    packages=find_packages(exclude=["*tests*", "*docs*", "*binder*", "*conda*", "*notebooks*", "*.ci_support*"]),
+    install_requires=[
+        'pyiron_base>=0.7.7',
+        'pyiron_atomistics>=0.4.17',
+        'ipywidgets>=8.1.2',
+        'matplotlib>=3.8.3',
+        'nbconvert>=7.16.2',
+        'nbformat>=5.10.3',
+        'numpy>=1.26.4',
+        'pandas>=2.2.0',
+    ],
+    cmdclass=versioneer.get_cmdclass(),
+)
