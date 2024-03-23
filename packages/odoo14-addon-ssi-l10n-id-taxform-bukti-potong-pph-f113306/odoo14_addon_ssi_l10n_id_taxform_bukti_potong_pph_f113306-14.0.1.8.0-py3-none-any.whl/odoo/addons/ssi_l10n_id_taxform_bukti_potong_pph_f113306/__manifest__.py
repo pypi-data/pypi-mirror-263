5237961ada72f24f8823db8a68b3c8fecdae078e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 16:04:40 +0000

    Add package odoo14-addon-ssi-l10n-id-taxform-bukti-potong-pph-f113306/14.0.1.8.0/odoo14_addon_ssi_l10n_id_taxform_bukti_potong_pph_f113306-14.0.1.8.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-l10n-id-taxform-bukti-potong-pph-f113306/odoo14_addon_ssi_l10n_id_taxform_bukti_potong_pph_f113306-14.0.1.8.0-py3-none-any.whl/odoo/addons/ssi_l10n_id_taxform_bukti_potong_pph_f113306/__manifest__.py
@@ -0,0 +1,38 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Indonesia - Bukti Potong PPh 23 (f.1.1.33.06)",
+    "version": "14.0.1.8.0",
+    "category": "localization",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "ssi_l10n_id_taxform_bukti_potong_pph_mixin",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule_data.xml",
+        "data/l10n_id_bukti_potong_type.xml",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_data.xml",
+        "data/policy_template_data.xml",
+        "data/approval_template_data.xml",
+        "data/account_journal_data.xml",
+        "views/bukti_potong_pph_type_views.xml",
+        "views/bukti_potong_pph_f113306_in_views.xml",
+        "views/bukti_potong_pph_f113306_out_views.xml",
+    ],
+    "demo": [
+        "demo/account_journal_demo.xml",
+        "demo/account_account_demo.xml",
+        "demo/account_tax_demo.xml",
+        "demo/l10n_id_bukti_potong_type_demo.xml",
+    ],
+}
---
+++ packages/odoo14-addon-ssi-l10n-id-taxform-bukti-potong-pph-f113306/odoo14_addon_ssi_l10n_id_taxform_bukti_potong_pph_f113306-14.0.1.8.0-py3-none-any.whl/odoo/addons/ssi_l10n_id_taxform_bukti_potong_pph_f113306/models/bukti_potong_pph_f113306_in.py
@@ -0,0 +1,42 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import _, api, fields, models
+from odoo.exceptions import Warning as UserError
+
+from odoo.addons.ssi_decorator import ssi_decorator
+
+
+class BuktiPotongPPhf113306In(models.Model):
+    _name = "l10n_id.bukti_potong_pph_f113306_in"
+    _inherit = "l10n_id.bukti_potong_pph_mixin"
+    _description = "Bukti Potong PPh 23 f.1.1.33.06 In"
+
+    def _get_type_id(self):
+        type_id = self.env.ref(
+            "ssi_l10n_id_taxform_bukti_potong_pph_f113306."
+            "bukti_potong_pph_type_f113306_in"
+        )
+        if not type_id.id:
+            err_msg = _("Bukti Potong PPh 23 f113306 In type hasn't defined")
+            raise UserError(err_msg)
+        return type_id.id
+
+    @api.model
+    def _default_type_id(self):
+        return self._get_type_id()
+
+    type_id = fields.Many2one(
+        default=lambda self: self._default_type_id(),
+    )
+
+    line_ids = fields.One2many(
+        comodel_name="l10n_id.bukti_potong_pph_f113306_in_line",
+    )
+
+    @ssi_decorator.insert_on_form_view()
+    def _insert_form_element(self, view_arch):
+        if self._automatically_insert_view_element:
+            view_arch = self._reconfigure_statusbar_visible(view_arch)
+        return view_arch
