5cb2bd905b904e6969cf75d3b4cc3544970130cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 05:05:02 +0000

    Add package udk/0.0.2/udk-0.0.2.tar.gz

---
+++ packages/udk/udk-0.0.2.tar.gz/udk-0.0.2/src/udk/tools.py
@@ -0,0 +1,12 @@
+class tools:
+    def editor(file):
+        # Simple Text editor
+        with open(file, "a") as file:
+            while True:
+                line = input()
+                if line.startswith(":q"):
+                    break
+                else:
+                    file.write(line+'\n')
+
+tools.editor()
