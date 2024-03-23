e3641206a3a1606a3ef1eb563c45b7c2cee484ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 04:05:18 +0000

    Add package pt-pump-up-orms/0.0.1/pt_pump_up_orms-0.0.1.tar.gz

---
+++ packages/pt-pump-up-orms/pt_pump_up_orms-0.0.1.tar.gz/pt_pump_up_orms-0.0.1/src/pt_pump_up_orms/Link.py
@@ -0,0 +1,21 @@
+from pt_pump_up_admin import CRUD
+
+
+class Link(CRUD):
+    def __init__(self,
+                 id: int = None,
+                 email: str = None,
+                 website: str = None,
+                 github_url: str = None,
+                 hugging_face_url: str = None,
+                 papers_with_code_url: str = None,
+                 paper_url: str = None) -> None:
+
+        super().__init__("link",
+                         id=id,
+                         email=email,
+                         website=website,
+                         github_url=github_url,
+                         hugging_face_url=hugging_face_url,
+                         papers_with_code_url=papers_with_code_url,
+                         paper_url=paper_url)
---
+++ packages/pt-pump-up-orms/pt_pump_up_orms-0.0.1.tar.gz/pt_pump_up_orms-0.0.1/src/pt_pump_up_orms/Model.py
@@ -0,0 +1,35 @@
+from pt_pump_up_admin import CRUD
+from requests import Request
+from pt_pump_up_admin.link import Link
+from pt_pump_up_admin.resource_stats import ResourceStats
+
+
+class Model(CRUD):
+    def __init__(self,
+                 id: int = None,
+                 short_name: str = None,
+                 full_name: str = None,
+                 description: str = None,
+                 year: int = None,
+                 authors: list = None,
+                 nlp_tasks: list = None,
+                 link: Link = None,
+                 resource_stats: ResourceStats = None,
+                 results: list = None) -> None:
+
+        super().__init__('machine-learning-model',
+                         id=id,
+                         short_name=short_name,
+                         full_name=full_name,
+                         description=description,
+                         year=year,
+
+                         author_emails=[author.json['link']['email']
+                                        for author in authors] if authors else None,
+
+                         nlp_tasks_short_names=[
+                             nlp_task.json['short_name'] for nlp_task in nlp_tasks] if nlp_tasks else None,
+
+                         link=link.json if link else None,
+                         resource_stats=resource_stats.json if resource_stats else None,
+                         results=[result.json for result in results] if results else None)
