000b72521e0e3975fb83551ab03ae7d0b6492cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 05:05:32 +0000

    Add package abstra/2.5.2/abstra-2.5.2.tar.gz

---
+++ packages/abstra/abstra-2.5.2.tar.gz/abstra-2.5.2/abstra/tables.py
@@ -0,0 +1,14 @@
+from abstra_internals.interface.sdk.tables.api import (
+    run,
+    query_df,
+    TablesExecutionError,
+    select,
+    select_by_id,
+    select_df,
+    select_one,
+    insert,
+    update,
+    delete,
+    update_by_id,
+    delete_by_id,
+)
