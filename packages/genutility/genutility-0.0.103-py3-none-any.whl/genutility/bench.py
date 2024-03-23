39045c96faf569958e6b85398bd9468ef2225f21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 04:06:59 +0000

    Add package genutility/0.0.103/genutility-0.0.103-py3-none-any.whl

---
+++ packages/genutility/genutility-0.0.103-py3-none-any.whl/genutility/bench.py
@@ -0,0 +1,39 @@
+import tracemalloc
+from typing import Optional
+
+
+class MeasureMemory:
+    __slots__ = ("total", "snapshot")
+
+    total: Optional[int]
+
+    def __init__(self) -> None:
+        tracemalloc.start()
+
+        self.total = None
+
+    def _comp(self) -> int:
+        snapshot_now = tracemalloc.take_snapshot()
+        stats = snapshot_now.compare_to(self.snapshot, "lineno")
+        return sum(stat.size for stat in stats)
+
+    def __enter__(self) -> "MeasureMemory":
+        self.snapshot = tracemalloc.take_snapshot()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.total = self._comp()
+
+    def get(self) -> int:
+        if self.total:
+            return self.total
+        else:
+            return self._comp()
+
+    def print(self, name: str) -> None:
+        if self.total:
+            total = self.total
+        else:
+            total = self._comp()
+
+        print(f"{name} uses {total / 1024 / 1024:.3f} MiB of memory")
