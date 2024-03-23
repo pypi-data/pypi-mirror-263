09a7fab0f43228c236ede36ddde2b490d1272980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 05:04:55 +0000

    Add package comicpy/0.1.20/comicpy-0.1.20-py3-none-any.whl

---
+++ packages/comicpy/comicpy-0.1.20-py3-none-any.whl/comicpy/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+ComicPy app
+"""
+
+from comicpy.comicpy import ComicPy
+
+from comicpy.models import (
+    ImageComicData,
+    CurrentFile,
+    CompressorFileData
+)
+
+from comicpy.checkfile import CheckFile
+
+from comicpy.exceptionsClasses import (
+    InvalidFile,
+    EmptyFile,
+    FileExtentionNotMatch,
+    DirectoryPathNotExists,
+    DirectoryEmptyFilesValid,
+    DirectoryFilterEmptyFiles
+)
+
+from comicpy.valid_extentions import ValidExtentions
+
+from comicpy.handlers import (
+    PdfHandler,
+    ZipHandler,
+    RarHandler,
+    ImagesHandler
+)
+
+from comicpy.cli import CliComicPy
