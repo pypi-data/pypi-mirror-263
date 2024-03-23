4f11752d195e7ac72a0e75209eb12e33389a5204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 15:05:17 +0000

    Add package faker-edu/1.1.0/faker_edu-1.1.0-py3-none-any.whl

---
+++ packages/faker-edu/faker_edu-1.1.0-py3-none-any.whl/faker_edu/__init__.py
@@ -0,0 +1,298 @@
+from pickle import FALSE, TRUE
+from faker.generator import Generator
+import faker.providers.address.en_US
+
+INSTITUTIONTYPES = [
+    'College',
+    'Junior College',
+    'State University',
+    'University',
+]
+
+SECONDARYTYPES = [
+    'High',
+    'High School',
+    'Prep',
+    'Preparatory School',
+    'School',
+    'Unified High School',
+]
+
+TOPICS = [
+    'Cloud',
+    'Computing',
+    'Connected',
+    'Credentaliing',
+    'Friendly',
+    'Rainbow',
+    'Salesforce',
+    'Snowflake',
+    'Sunshine',
+    'Supernova',
+    'Synergy',
+    'Worldview',
+]
+
+DEPARTMENTS = [
+    'Accounting Division',
+    'African American and African Diaspora Studies',
+    'Africana Studies',
+    'Anesthesiology',
+    'Anthropology',
+    'Applied Physics and Applied Mathematics',
+    'Archaeology',
+    'Architecture',
+    'Art History',
+    'Asian and Middle Eastern Cultures',
+    'Astronomy and Astrophysics',
+    'Biochemistry and Molecular Biophysics',
+    'Biology',
+    'Biomedical Engineering',
+    'Cardiology',
+    'Chemistry',
+    'Civil Engineering',
+    'Classics',
+    'Classics and Ancient Studies',
+    'Computer Science',
+    'Dance',
+    'Decision, Risk, and Operations Division',
+    'Dermatology',
+    'Digestive and Liver Diseases',
+    'Earth and Environmental Sciences',
+    'East Asian Languages and Cultures',
+    'Ecology, Evolution and Environmental Biology',
+    'Economics',
+    'Electrical Engineering',
+    'Endocrinology',
+    'English and Comparative Literature',
+    'Environmental Health Sciences',
+    'Environmental Science',
+    'Epidemiology',
+    'Film',
+    'Finance and Economics Division',
+    'French',
+    'French and Romance Philology',
+    'General Medicine',
+    'Genetics and Development',
+    'Germanic Languages',
+    'Global Support',
+    'Health Policy and Management',
+    'Hematology',
+    'History',
+    'Industrial Engineering & Operations Research',
+    'Infectious Diseases',
+    'Italian',
+    'Latin American and Iberian Cultures',
+    'Management Division',
+    'Marketing Division',
+    'Mathematics',
+    'Mechanical Engineering',
+    'Medicine',
+    'Microbiology & Immunology',
+    'Middle Eastern, South Asian, and African Studies',
+    'Molecular Medicine',
+    'Music',
+    'Nephrology',
+    'Neurology',
+    'Neuroscience',
+    'Pediatrics',
+    'Pharmacology',
+    'Philosophy',
+    'Physical Education',
+    'Physical Education & Recreation Program',
+    'Physics',
+    'Physiology and Cellular Biophysics',
+    'Political Science',
+    'Population & Family Health',
+    'Psychiatry',
+    'Psychology',
+    'Pulmonary, Allergy and Critical Care Medicine',
+    'Radiation Oncology',
+    'Radiology',
+    'Rehabilitation and Regenerative Medicine',
+    'Religion',
+    'Rheumatology',
+    'Slavic Languages',
+    'Sociology',
+    'Sociomedical Sciences',
+    'Spanish and Latin American Cultures',
+    'Statistics',
+    'Surgery',
+    'Systems Biology',
+    'Theatre',
+    'Visual Arts',
+    'Writing',
+]
+
+FACULTYPOSITIONS = [
+    'Adjunct Professor',
+    'Assistant Professor',
+    'Associate Professor',
+    'Chair',
+    'Instructor',
+    'Lecturer',
+    'Professor',
+    'Professor Emeritus',
+    'Teaching Assistant',
+]
+
+SPORTS = [
+    "Baseball",
+    "Basketball",
+    "Beach Volleyball",
+    "Bowling",
+    "Cricket",
+    "Cross country",
+    "Curling",
+    "Fencing",
+    "Field hockey",
+    "Football",
+    "Golf",
+    "Gymnastics",
+    "Ice hockey",
+    "Indoor Track",
+    "Lacrosse",
+    "Rifle",
+    "Rowing",
+    "Rugby",
+    "Skiing",
+    "Soccer",
+    "Softball",
+    "Swimming & Diving",
+    "Tennis",
+    "Track",
+    "Volleyball",
+    "Water polo",
+    "Wrestling"
+]
+
+FACILITYTYPES = [
+    'Building',
+    'Center',
+    'Hall',
+    'Lab',
+    'Library',
+    'Tower'
+]
+
+ACADEMICDISCIPLINES = [
+    'Agriculture',
+    'Anthropology',
+    'Applied science',
+    'Archaeology',
+    'Architecture and design',
+    'Biology',
+    'Business',
+    'Chemistry',
+    'Computer science',
+    'Divinity',
+    'Earth science',
+    'Economics',
+    'Education',
+    'Engineering and technology',
+    'Environmental studies and forestry',
+    'Family and consumer science',
+    'Formal science',
+    'Geography',
+    'History',
+    'Human physical performance and recreation',
+    'Humanities',
+    'Journalism, media studies and communication',
+    'Languages and literature',
+    'Law',
+    'Library and museum studies',
+    'Mathematics',
+    'Medicine and health',
+    'Military sciences',
+    'Natural science',
+    'Performing arts',
+    'Philosophy',
+    'Physics',
+    'Political science',
+    'Psychology',
+    'Public administration',
+    'Public policy',
+    'Religious Studies',
+    'Social science',
+    'Social work',
+    'Sociology',
+    'Space science',
+    'Theology',
+    'Transportation',
+    'Visual arts'
+]
+
+class Provider(faker.providers.BaseProvider):
+    def institution_name(self):
+        """Fake higher education institution names."""
+        fakeUsAddress = faker.providers.address.en_US.Provider(Generator())
+        fullTopicList = set(TOPICS)
+        usStates = set(fakeUsAddress.states)
+        countryList = set(fakeUsAddress.countries)
+        fullTopicList = fullTopicList.union(usStates)
+        fullTopicList = fullTopicList.union(countryList)
+        suffix = self.random_element(INSTITUTIONTYPES)
+        topic = self.random_element(fullTopicList)
+        return " ".join([topic, suffix]).strip()
+
+    def highschool_name(self):
+        """Generate name of a high school."""
+        # Schools are named for people or places
+        prefix = self.generator.city()
+        if (self.random_element([TRUE, FALSE]) == TRUE):
+            prefix = self.generator.name()
+
+        suffix = self.random_element(SECONDARYTYPES)
+
+        return " ".join([prefix, suffix]).strip()
+
+    def department_name(self):
+        return self.random_element(DEPARTMENTS)
+
+    def faculty_position(self):
+        position = self.random_element(FACULTYPOSITIONS)
+        return position
+
+    def faculty_title(self):
+        position = self.faculty_position()
+        department = self.department_name()
+        return position+" of "+department
+
+    def sport(self):
+        genders = ["Men's", "Women's"]
+        gender = self.random_element(genders)
+        sport = self.random_element(SPORTS)
+        return gender + ' ' + sport
+
+    def facility_name(self):
+        fakeName = faker.providers.person.en_US.Provider(Generator())
+        lasts = set(fakeName.last_names.keys())
+
+        last_name = self.random_element(lasts)
+        discipline = self.random_element(DEPARTMENTS)
+        building = self.random_element(FACILITYTYPES)
+        name = last_name + ' ' + discipline + ' ' + building
+        return name
+
+    def college_name(self):
+        college = self.random_element(['College', 'School'])
+        topic = self.random_element(ACADEMICDISCIPLINES)
+        name = 'The '
+        if (self.random_element([TRUE, FALSE]) == TRUE):
+            fakeName = faker.providers.person.en_US.Provider(Generator())
+            lasts = set(fakeName.last_names.keys())
+            last_name = self.random_element(lasts)
+            """Deciding if the College name will have first and last name, or just last name"""
+            if self.random_element([1, 2]) == 2:
+                female_firsts = set(fakeName.first_names_female.keys())
+                male_firsts = set(fakeName.first_names_male.keys())
+                firsts = female_firsts.union(male_firsts)
+                first_name = self.random_element(firsts)
+                name += first_name + ' '
+            name += last_name + ' '
+        name += college + ' of ' + topic
+        return name
+
+    def academic_discipline(self):
+        """Provides an academic discipline."""
+        return self.random_element(ACADEMICDISCIPLINES)
---
+++ packages/faker-edu/faker_edu-1.1.0-py3-none-any.whl/faker_edu/tests/test_faker_highered.py
@@ -0,0 +1,129 @@
+import unittest
+from faker import Faker, Generator
+import faker_edu
+import faker
+
+class HigherEdProviderTestCase(unittest.TestCase):
+    """Provider test case."""
+
+    def setUp(self):
+        self.fake = Faker()
+        self.fake.add_provider(faker_edu.Provider)
+
+    def test_lists_in_order(self):
+        """Test internal values are in order."""
+        for attr_name, attr in faker_edu.__dict__.items():
+            with self.subTest(attr_name=attr_name):
+                if isinstance(attr, list):
+                    self.assert_list_in_order(attr)
+
+    def assert_list_in_order(self, the_list):
+        """Assert a list is in order."""
+        prev_value = ""
+        for this_value in the_list:
+            self.assertGreaterEqual(this_value, prev_value)
+            prev_value = this_value
+
+    def test_no_duplicates(self):
+        """Test value lists don't contain duplicates."""
+        for attr_name, attr in faker_edu.__dict__.items():
+            with self.subTest(attr_name=attr_name):
+                if isinstance(attr, list):
+                    self.assertEqual(len(attr), len(set(attr)), attr_name + " contains duplicates")
+
+    def test_institution_name(self):
+        """Test that generated institution name is at least two words long."""
+        result = self.fake.institution_name()
+        word_count = len(result.split())
+        self.assertGreaterEqual(word_count, 2)
+
+    def test_high_school_name(self):
+        """Test that generated high school name is at least two words long."""
+        result = self.fake.highschool_name()
+        word_count = len(result.split())
+        self.assertGreaterEqual(word_count, 2)
+
+    def test_department_name(self):
+        """Test that generated department name is from the list."""
+        dept = self.fake.department_name()
+        self.assertIn(dept, faker_edu.DEPARTMENTS,
+                      'Generated department not from list')
+
+    def test_position_name(self):
+        """Test that generated position name is from the list."""
+        pos = self.fake.faculty_position()
+        self.assertIn(pos, faker_edu.FACULTYPOSITIONS,
+                      'Generated faculty position not from list')
+
+    def test_faculty_title(self):
+        """Test that generated position name is from the list."""
+        title = self.fake.faculty_title()
+        self.assertTrue(' of ' in title, 'Missing "of" in title')
+        parts = title.split(' of ')
+        self.assertIn(parts[0], faker_edu.FACULTYPOSITIONS,
+                      'Position segment not from position list.')
+        self.assertIn(parts[1], faker_edu.DEPARTMENTS,
+                      'Position segment not from department list.')
+
+    def test_sports(self):
+        """Test that generated sport is from the list."""
+        # A sampling of 10 seems sufficient.
+        for _ in range(10):
+            sport = self.fake.sport()
+            parts = sport.split()
+            self.assertTrue(len(parts) >= 2, 'Generated sport is less then 2 words: ' + sport)
+            self.assertIn(parts[0], ["Men's", "Women's"],
+                        'Position segment not in gender list')
+            self.assertIn(" ".join(parts[1:]), faker_edu.SPORTS,
+                        'Position segment not in sports list')
+
+    def test_facility_name(self):
+        facility_name = self.fake.facility_name()
+        parts = facility_name.split(' ')
+        fakeName = faker.providers.person.en_US.Provider(Generator())
+        self.assertIn(parts[0], fakeName.last_names,
+                          'Position segment not from name dictionary')
+        self.assertIn(' '.join(parts[1:-1]).strip(), faker_edu.DEPARTMENTS,
+                          'Position segment not from department list')
+        self.assertIn(parts[-1], faker_edu.FACILITYTYPES,
+                          'Position segment not from building list')
+
+    def test_college_name(self):
+        college_name = self.fake.college_name()
+        self.assertTrue('The ' in college_name, 'Missing "The" in title')
+        self.assertTrue(' of ' in college_name, 'Missing "of" in title')
+        parts = college_name.split(' of ')
+        name = parts[0].split()
+        if len(name) == 4:
+            fakeName = faker.providers.person.en_US.Provider(Generator())
+            female_firsts = set(fakeName.first_names_female.keys())
+            male_firsts = set(fakeName.first_names_male.keys())
+            firsts = female_firsts.union(male_firsts)
+            self.assertIn(name[1], firsts,
+                          'Position segment not from first name dictionary')
+            self.assertIn(name[2], fakeName.last_names,
+                          'Position segment not from last name dictionary')
+            self.assertIn(name[3], ['College', 'School'],
+                          'Position segment not from school type list')
+        elif len(name) == 3:
+            fakeName = faker.providers.person.en_US.Provider(Generator())
+            self.assertIn(name[1], fakeName.last_names,
+                          'Position segment not from last name dictionary')
+            self.assertIn(name[2], ['College', 'School'],
+                          'Position segment not from school type list')
+        else:
+            self.assertIn(name[1], ['College', 'School'],
+                          'Position segment not from school type list')
+
+        self.assertIn(parts[1], faker_edu.ACADEMICDISCIPLINES,
+                          'Position segment not from college type list')
+
+    def test_academic_discipline(self):
+        """Test that generated academic discipline is from the list."""
+        for _ in range(10):
+            discipline = self.fake.academic_discipline()
+            self.assertIn(discipline, faker_edu.ACADEMICDISCIPLINES,
+                        'Generated discipline not from list: ' + discipline)
+
+if __name__ == "__main__":
+    unittest.main()
