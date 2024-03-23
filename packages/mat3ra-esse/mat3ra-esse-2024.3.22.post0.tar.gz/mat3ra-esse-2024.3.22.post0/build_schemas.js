5080fe578ffff8fa4908a9f6593829252a268355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 04:05:09 +0000

    Add package mat3ra-esse/2024.3.22.post0/mat3ra-esse-2024.3.22.post0.tar.gz

---
+++ packages/mat3ra-esse/mat3ra-esse-2024.3.22.post0.tar.gz/mat3ra-esse-2024.3.22.post0/build_schemas.js
@@ -0,0 +1,47 @@
+/**
+ * build_schemas uses node API to read all schemas from the FS
+ * at build time and writes them out to a single schemas.js file for
+ * downstream consumption to avoid FS calls in the browser.
+ */
+const fs = require("fs");
+const JSONSchemasGenerator = require("./dist/js/esse/JSONSchemasGenerator").default;
+
+// JS Modules
+
+const generator = new JSONSchemasGenerator();
+const { schemas, wrappedExamples, propertiesManifest, results } = generator;
+
+if (process.env.BUILD_PYTHON_MODULES === "true") {
+    // PY Modules
+    fs.writeFileSync(
+        "./src/py/mat3ra/esse/data/examples.py",
+        ["import json", `EXAMPLES = json.loads(r'''${JSON.stringify(wrappedExamples)}''')`].join(
+            "\n",
+        ),
+        "utf8",
+    );
+    fs.writeFileSync(
+        "./src/py/mat3ra/esse/data/schemas.py",
+        ["import json", `SCHEMAS = json.loads(r'''${JSON.stringify(schemas)}''')`].join("\n"),
+        "utf8",
+    );
+    fs.writeFileSync(
+        "./src/py/mat3ra/esse/data/properties.py",
+        [
+            "import json",
+            `PROPERTIES_MANIFEST = json.loads(r'''${JSON.stringify(propertiesManifest)}''')`,
+            `RESULTS = json.loads(r'''${JSON.stringify(results)}''')`,
+        ].join("\n"),
+        "utf8",
+    );
+}
+
+if (process.env.BUILD_ASSETS !== "true") {
+    process.exit(0);
+}
+
+const subfolder = process.env.BUILD_PATH || "./docs/js/";
+const skipMergeAllOff = process.env.SKIP_MERGE_ALLOF === "true";
+
+generator.writeResolvedSchemas(subfolder, skipMergeAllOff);
+generator.writeResolvedExamples(subfolder);
