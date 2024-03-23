01bfe8e5ca8283c9d9a522f3c063f124604721f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 05:04:16 +0000

    Add package wrts/0.6.1/wrts-0.6.1.tar.gz

---
+++ packages/wrts/wrts-0.6.1.tar.gz/wrts-0.6.1/README.md
@@ -0,0 +1,6 @@
+# WRTS.py (or wrts or wrtsapi)
+*renaming wrts to studygo is like the rebranding of twitter to X*
+
+No docs *yet*.
+
+oh yeah btw disclaimer: not my responsibility if you do something dumb dumb
---
+++ packages/wrts/wrts-0.6.1.tar.gz/wrts-0.6.1/wrts/exceptions.py
@@ -0,0 +1,13 @@
+# TODO: Handle these exceptions properly
+
+class LoginFailure(Exception):
+	pass
+
+class QuestionError(Exception):
+	pass
+
+class UploadError(Exception):
+	pass
+
+class UserError(Exception):
+	pass
