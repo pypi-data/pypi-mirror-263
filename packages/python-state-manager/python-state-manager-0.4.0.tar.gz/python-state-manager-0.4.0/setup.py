26cebf2a11ec1d670b3dd8b2845a29d14228b263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 05:04:20 +0000

    Add package python-state-manager/0.4.0/python-state-manager-0.4.0.tar.gz

---
+++ packages/python-state-manager/python-state-manager-0.4.0.tar.gz/python-state-manager-0.4.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-state-manager',
+    version='0.4.0',
+    packages=find_packages(),
+    install_requires=[],
+    # Additional metadata about your package.
+    author='Chinmay Shrivastava',
+    author_email='cshrivastava99@gmail.com',
+    description='A simple state machine to handle states of programs.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ChinmayShrivastava/pdf-page-annotator',
+    license='GPLv3',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Environment :: MacOS X",
+        "Environment :: Win32 (MS Windows)",
+        "Environment :: X11 Applications",
+    ],
+    entry_points={},
+)
