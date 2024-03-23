8a389fe6eafb9689d98761dee4c6a9a4fead3b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 14:04:32 +0000

    Add package soccerdata/1.6.1/soccerdata-1.6.1.tar.gz

---
+++ packages/soccerdata/soccerdata-1.6.1.tar.gz/soccerdata-1.6.1/soccerdata/__init__.py
@@ -0,0 +1,25 @@
+"""A collection of tools to read and process soccer data from various sources."""
+
+__version__ = '1.6.1'
+
+__all__ = [
+    'ClubElo',
+    'ESPN',
+    'FBref',
+    'FiveThirtyEight',
+    'FotMob',
+    'MatchHistory',
+    'SoFIFA',
+    'Understat',
+    'WhoScored',
+]
+
+from .clubelo import ClubElo
+from .espn import ESPN
+from .fbref import FBref
+from .fivethirtyeight import FiveThirtyEight
+from .fotmob import FotMob
+from .match_history import MatchHistory
+from .sofifa import SoFIFA
+from .understat import Understat
+from .whoscored import WhoScored
