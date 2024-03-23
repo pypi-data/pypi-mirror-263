f2318fd330398691816284c2fbc45c1b5f54ba50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 15:04:53 +0000

    Add package ipdetective/1.0.1/ipdetective-1.0.1.tar.gz

---
+++ packages/ipdetective/ipdetective-1.0.1.tar.gz/ipdetective-1.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='ipdetective',
+    version='1.0.1',
+    description='A Python client for IPDetective API',
+    long_description=long_description,
+    author='IPDetective',
+    author_email='andrew@ipdetective.io',
+    project_urls={
+        'Homepage': 'https://ipdetective.io',
+        'Source': 'https://github.com/AndrewCopeland/ipdetective-client-python'
+    },
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=['requests'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Utilities',
+        'Intended Audience :: Developers'
+    ],
+)
