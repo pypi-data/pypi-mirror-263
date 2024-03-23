ed4ff6c318232a45e1e32e749259896510cc505c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 15:04:47 +0000

    Add package cchdo.hydro/1.0.2.8/cchdo.hydro-1.0.2.8.tar.gz

---
+++ packages/cchdo.hydro/cchdo.hydro-1.0.2.8.tar.gz/cchdo.hydro-1.0.2.8/cchdo/hydro/tests/conftest.py
@@ -0,0 +1,21 @@
+import io
+
+import pytest
+
+from ..exchange import read_exchange
+from ..exchange.helpers import simple_bottle_exchange
+
+
+@pytest.fixture
+def nc_empty():
+    return read_exchange(io.BytesIO(simple_bottle_exchange()))
+
+
+@pytest.fixture
+def nc_placeholder():
+    params = ("OXYGEN", "OXYGEN_FLAG_W", "DELC14", "DELC14_FLAG_W", "C14ERR")
+    units = ("UMOL/KG", "", "/MILLE", "", "/MILLE")
+    data = ("-999", "1", "-999", "1", "-999")
+    return read_exchange(
+        io.BytesIO(simple_bottle_exchange(params=params, units=units, data=data))
+    )
