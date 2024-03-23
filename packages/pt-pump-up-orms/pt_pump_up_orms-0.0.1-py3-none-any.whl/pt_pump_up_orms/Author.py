85a3d32c3385c47b301e658d6b764a341cf7061c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 04:05:18 +0000

    Add package pt-pump-up-orms/0.0.1/pt_pump_up_orms-0.0.1-py3-none-any.whl

---
+++ packages/pt-pump-up-orms/pt_pump_up_orms-0.0.1-py3-none-any.whl/pt_pump_up_orms/Author.py
@@ -0,0 +1,16 @@
+from pt_pump_up_admin import CRUD
+from pt_pump_up_admin.link import Link
+
+
+class Author(CRUD):
+    def __init__(self,
+                 id: int = None,
+                 name: str = None,
+                 institution: str = None,
+                 link: Link = None) -> None:
+
+        super().__init__("author",
+                         id=id,
+                         name=name,
+                         institution=institution,
+                         link=link.json if link else None)
---
+++ packages/pt-pump-up-orms/pt_pump_up_orms-0.0.1-py3-none-any.whl/pt_pump_up_orms/ResourceStats.py
@@ -0,0 +1,17 @@
+from pt_pump_up_admin import CRUD
+
+class ResourceStats(CRUD):
+    def __init__(self,
+                 id: int = None,
+                 standard_format: bool = None,
+                 off_the_shelf: bool = None,
+                 preservation_rating: str = None) -> None:
+
+        preservation_rating = preservation_rating.lower(
+        ) if preservation_rating is not None else None
+
+        super().__init__("resource-stats",
+                         id=id,
+                         standard_format=standard_format,
+                         off_the_shelf=off_the_shelf,
+                         preservation_rating=preservation_rating)
