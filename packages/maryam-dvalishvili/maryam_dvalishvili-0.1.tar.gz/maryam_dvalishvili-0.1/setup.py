6e0992a9e5d562d06c62d8d66b225379a6f4cdd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 05:04:54 +0000

    Add package maryam-dvalishvili/0.1/maryam_dvalishvili-0.1.tar.gz

---
+++ packages/maryam-dvalishvili/maryam_dvalishvili-0.1.tar.gz/maryam_dvalishvili-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='maryam_dvalishvili',
+    version='0.1',
+    packages=find_packages(),
+    description='My Python package for birthday greetings',
+    author='a_bitsadze',
+    author_email='alika.bitsadze@gmail.com',
+)
