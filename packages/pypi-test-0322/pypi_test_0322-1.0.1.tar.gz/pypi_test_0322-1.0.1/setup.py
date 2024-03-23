d0f5feecacb163a362cf4950bbdb68f65e5270c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 04:07:03 +0000

    Add package pypi-test-0322/1.0.1/pypi_test_0322-1.0.1.tar.gz

---
+++ packages/pypi-test-0322/pypi_test_0322-1.0.1.tar.gz/pypi_test_0322-1.0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="pypi_test_0322",
+    version="1.0.1",
+    author="suk-6",
+    author_email="me@suk.kr",
+    description="test",
+    url="https://suk.kr",
+    packages=setuptools.find_packages(include=["pypi_test_0322", "pypi_test_0322.*"]),
+    python_requires=">=3.6",
+    license="MIT",
+)
