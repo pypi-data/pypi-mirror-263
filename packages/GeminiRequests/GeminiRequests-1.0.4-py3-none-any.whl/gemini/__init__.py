1e725a4b49f00a90191d0e59fa235a73c5e0f039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 16:04:32 +0000

    Add package GeminiRequests/1.0.4/GeminiRequests-1.0.4-py3-none-any.whl

---
+++ packages/GeminiRequests/GeminiRequests-1.0.4-py3-none-any.whl/gemini/__init__.py
@@ -0,0 +1,38 @@
+import requests
+
+
+class AI:
+    def __init__(self, history_file, gemini_api_key):
+        self.api_key = gemini_api_key
+
+        # The URL for the API request
+        self.url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=' + self.api_key
+
+        # Headers for the request
+        self.headers = {
+            'Content-Type': 'application/json',
+        }
+        self.history = []
+
+    def update_history(self, data_list):
+        self.history = data_list
+
+    def clear_history(self):
+        self.history = [""]
+
+    def generate_text(self, text: str):
+
+        self.update_history(self.history + [{"role": "user", "parts": [{"text": text}]}])
+
+        data = {
+            "contents": self.history,
+        }
+        response = requests.post(self.url, json=data, headers=self.headers)
+
+        result = response.json()['candidates'][0]['content']['parts'][0]['text']
+        self.update_history(self.history + [{"role": "assistant", "parts": [{"text": result}]}])
+
+        return result
+
+
+
