73586e4828a7ded4dda04d4cbb1f6616550d4d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 04:06:35 +0000

    Add package seaplane.api/0.0.16b1/seaplane.api-0.0.16b1-py3-none-any.whl

---
+++ packages/seaplane.api/seaplane.api-0.0.16b1-py3-none-any.whl/seaplane/gen/carrier/api/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+# import apis into api package
+from seaplane.gen.carrier.api.endpoint_api import EndpointApi
+from seaplane.gen.carrier.api.event_api import EventApi
+from seaplane.gen.carrier.api.flow_api import FlowApi
+from seaplane.gen.carrier.api.key_value_api import KeyValueApi
+from seaplane.gen.carrier.api.object_api import ObjectApi
+from seaplane.gen.carrier.api.stream_api import StreamApi
+from seaplane.gen.carrier.api.webhook_api import WebhookApi
