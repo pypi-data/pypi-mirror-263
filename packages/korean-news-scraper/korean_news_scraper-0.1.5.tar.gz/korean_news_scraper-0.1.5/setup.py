daa8821e43a7bcb21ed7fe6b54ae79a75a6bf5ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 05:05:00 +0000

    Add package korean-news-scraper/0.1.5/korean_news_scraper-0.1.5.tar.gz

---
+++ packages/korean-news-scraper/korean_news_scraper-0.1.5.tar.gz/korean_news_scraper-0.1.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='korean-news-scraper',
+    version='0.0.1',
+    url='https://github.com/kimminsum/korean-news-scraper.git',
+    author='kar7mp5',
+    author_email='tommy1005a@gmail.com',
+    description='A python package to scrape news.',
+    packages=find_packages(),
+    install_requires=['requests', 'beautifulsoup4', 'selenium', 'pandas', 'tqdm'],
+)
