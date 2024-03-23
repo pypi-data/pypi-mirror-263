08f3d8fd720f68e8346c5539f12f68bab2fea26d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 16:04:59 +0000

    Add package ynab-transaction-adjuster/0.3.0/ynab_transaction_adjuster-0.3.0.tar.gz

---
+++ packages/ynab-transaction-adjuster/ynab_transaction_adjuster-0.3.0.tar.gz/ynab_transaction_adjuster-0.3.0/ynabtransactionadjuster/models/originalsubtransaction.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+from ynabtransactionadjuster.models.category import Category
+from ynabtransactionadjuster.models.payee import Payee
+
+
+@dataclass(frozen=True)
+class OriginalSubTransaction:
+	"""Represents an YNAB Subtransaction as part of an existing split transaction
+
+	:ivar payee: The payee of the subtransaction
+	:ivar category: The category of the subtransaction
+	:ivar amount: The amount of the subtransaction in milliunits
+	:ivar memo: The memo of the subtransaction
+	"""
+	payee: Payee
+	category: Category
+	memo: str
+	amount: int
