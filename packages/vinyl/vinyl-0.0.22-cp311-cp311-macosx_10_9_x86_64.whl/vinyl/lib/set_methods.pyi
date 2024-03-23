15cf7399dabcd287093566a6943ba14062569e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 05:05:43 +0000

    Add package vinyl/0.0.22/vinyl-0.0.22-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/vinyl/vinyl-0.0.22-cp311-cp311-macosx_10_9_x86_64.whl/vinyl/lib/set_methods.pyi
@@ -0,0 +1,8 @@
+from typing import TypeAlias
+from vinyl.lib.field import Field as Field
+from vinyl.lib.graph import VinylGraph as VinylGraph
+
+base_join_type: TypeAlias
+
+class JoinNodeInfo: ...
+class JoinHelper: ...
