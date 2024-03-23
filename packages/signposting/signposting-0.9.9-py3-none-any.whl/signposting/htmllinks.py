425e02e72cf47c8efea322e9b0667a30596f8e29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 16:04:44 +0000

    Add package signposting/0.9.9/signposting-0.9.9-py3-none-any.whl

---
+++ packages/signposting/signposting-0.9.9-py3-none-any.whl/signposting/htmllinks.py
@@ -0,0 +1,154 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: 2022 The University of Manchester, UK
+#
+#   Copyright 2022 The University of Manchester, UK
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+"""
+Parse HTML to find <link> elements for signposting.
+"""
+
+from typing import Union
+import warnings
+import requests
+from bs4 import BeautifulSoup,SoupStrainer
+# FIXME: Where can we import this from?
+##from html.parser import HTMLParseError
+from .signpost import SIGNPOSTING,AbsoluteURI,Signpost,Signposting
+
+def find_signposting_html(uri:Union[AbsoluteURI, str], warn_empty:bool=True) -> Signposting:
+    """Parse HTML to find ``<link>`` elements for signposting.
+    
+    HTTP redirects will be followed and any relative paths in links
+    made absolute correspondingly.
+
+    :param uri: An absolute http/https URI, which HTML will be inspected.
+    :param warn_empty: If true, raise warning if no direct signpostings were found
+    :throws ValueError: If the `uri` is invalid
+    :throws IOError: If the network request failed, e.g. connection timeout
+    :throws requests.HTTPError: If the HTTP request failed, e.g. 404 Not Found
+    :throws UnrecognizedContentType: If the HTTP resource was not a recognized HTML/XHTML content type
+    :throws HTMLParseError: If the HTML could not be parsed.
+    :returns: A parsed :class:`Signposting` object (which may be empty)
+    """
+    html = _get_html(AbsoluteURI(uri))
+    signposting = _parse_html(html)
+    if warn_empty and not signposting:
+        warnings.warn("No direct signposting found in HTML from <%s>" % html.requested_url)
+    return signposting
+
+class DownloadedText(str):
+    """Text downloaded from HTTP"""
+
+    content_type: str
+    """The returned Content-Type of the downloaded text"""
+
+    requested_url: AbsoluteURI
+    """The requested URL, before redirection"""
+
+    resolved_url: AbsoluteURI
+    """The resolved URL, after redirection."""
+
+    def __new__(cls, value:str, content_type:str, requested_url:AbsoluteURI, resolved_url:AbsoluteURI):
+        # NOTE: Do not return value if it's already an DownloadedText
+        # instance; it may differ in the other attributes or subclass
+        s = super().__new__(cls, value)
+        # NOTE: content_type is necessarily a signpost.MediaType, 
+        # as this string typically include charset, e.g. 
+        # "text/html; charset=iso-8859-1"
+        s.content_type = content_type
+        s.requested_url = requested_url
+        s.resolved_url = resolved_url
+        return s
+
+class HTML(DownloadedText):
+    """Downloaded HTML document as string"""
+    pass
+
+class XHTML(DownloadedText):
+    """Downloaded XHTML document as a string"""
+    pass
+
+class UnrecognizedContentType(Exception):
+    def __init__(self, content_type:str, uri:AbsoluteURI):
+        super().__init__("Unrecognized content-type %s for <%s>" % (content_type, uri))
+        self.content_type = content_type
+        self.uri = uri
+
+def _get_html(uri:AbsoluteURI) -> Union[HTML,XHTML]:
+    HEADERS = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9"
+    }
+    # Should ideally throw Not Acceptable error if none of the above
+    page = requests.get(uri, headers=HEADERS)
+
+    resolved_url = AbsoluteURI(page.url, uri)
+    
+    # Note: According to HTTP/1.1 updates (Appendix B) in 
+    # https://datatracker.ietf.org/doc/html/rfc7231
+    # then Content-Location should NO LONGER be used for 
+    # resolving relative URI references.
+    ##if "Content-Location" in page.headers:
+    ##    # More specific, e.g. "index.en.html" - parse as relative URI reference
+    ##    resolved_url = AbsoluteURI(page.headers["Content-Location"], resolved_url)
+
+    if page.status_code == 203:
+        warnings.warn("203 Non-Authoritative Information <%s> - Signposting URIs may have been rewritten by proxy" %
+                    resolved_url)
+    elif page.status_code == 410:
+            warnings.warn(
+                "410 Gone <%s> - still processing signposting for thumbstone page" % resolved_url)
+    else:
+        # raise requests.HTTPError for any other 4xx/5xx error
+        page.raise_for_status()
+    
+    ct = page.headers.get("Content-Type", "")
+    if "text/html" in ct:
+        # page.text should get HTTP-level encoding correct,
+        # but will not know about any charset declarations inside.
+        return HTML(page.text, ct, uri, resolved_url)
+    elif "application/xhtml+xml" in ct or "application/xml" in ct or "xhtml" in ct or "+xml" in ct:
+        # Hopefully some XHTML inside.
+        # These typically don't have charset parameter, the below
+        # will guess by detection
+        return XHTML(page.text, ct, uri, resolved_url)
+    else:
+        # HTTP server didn't honor our Accept header, and returned non-HTML.
+        # It may be an image or something else that will crash our HTML parser,
+        # so we'll bail out here.
+        raise UnrecognizedContentType(ct, uri)
+
+def _parse_html(html:Union[HTML,XHTML]) -> Signposting:
+    soup = BeautifulSoup(html, 'html.parser', 
+        # Ignore any other elements to reduce chance of parse errors
+        parse_only=SoupStrainer(["head", "link"]))
+    signposts = []
+    if soup.head: # In case <head> was missing
+        for link in soup.head.find_all("link"):
+            # Ensure all filters are in lower case and known
+            url = link.get("href")
+            if not url:
+                warnings.warn("Invalid <link> element, missing href attribute: %s" % link)
+                continue
+            type = link.get("type")
+            profiles = link.get("profile")
+            rels = set(r.lower() for r in link.get("rel", [])
+                        if r.lower() in SIGNPOSTING or ":" in r)
+            for rel in rels:
+                try:
+                    signpost = Signpost(rel, url, type, profiles, html.resolved_url)
+                except ValueError as e:
+                    warnings.warn("Ignoring invalid signpost from %s: %s" % (html.requested_url, e))
+                    continue
+                signposts.append(signpost)
+    return Signposting(html.resolved_url, signposts)
---
+++ packages/signposting/signposting-0.9.9-py3-none-any.whl/signposting/linkheader.py
@@ -0,0 +1,177 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: 2022 Stian Soiland-Reyes
+#
+#   Copyright 2022 Stian Soiland-Reyes
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+"""
+Parse HTTP headers to find Signposting links
+"""
+
+from typing import List, Tuple, Optional, Union
+from httplink import ParsedLinks, Link, parse_link_header
+from urllib.parse import urljoin
+from .signpost import SIGNPOSTING, Signpost, Signposting, LinkRel, AbsoluteURI
+
+
+def _filter_links_by_rel(parsedLinks: ParsedLinks, *rels: str) -> List[Link]:
+    """Filter links to select from a set of relations.
+
+    The relations must be valid Signposting relation listed in `SIGNPOSTING`.
+
+    :param parsedLinks: The :class:`ParsedLinks` to filter from
+    :param rels: One or more link relations to filter by, e.g. ``item``
+    :return: A list of :class:`Link` which ``rel`` match the given `rels`.
+
+    """
+    if rels:
+        # Ensure all filters are in lower case
+        filterRels = set(r.lower() for r in rels)
+        unknown = filterRels - SIGNPOSTING
+        if unknown:
+            raise ValueError("Unknown FAIR Signposting relations: %s" % unknown)
+    else:
+        # Fallback - all valid signposting relations
+        filterRels = SIGNPOSTING
+    return [l for l in parsedLinks.links if l.rel & filterRels]
+
+
+def _filter_links_uris(parsedLinks: ParsedLinks) -> List[Link]:
+    """Filter links to select those that are extension URIs
+
+    :param parsedLinks: The :class:`ParsedLinks` to filter from
+    :return: A list of :class:`Link` which ``rel`` is an AbsoluteURI
+
+    """
+    extensions = []
+    for l in parsedLinks.links:
+        for rel in l.rel:
+            if ":" in rel:
+                try:
+                    AbsoluteURI(rel)
+                    extensions.append(l)
+                    break
+                except ValueError:
+                    continue
+                    
+    return extensions
+
+def _optional_link(parsedLinks: ParsedLinks, rel: str) -> Optional[Link]:
+    """Look up a single link relation.
+
+    The relation must be a valid Signposting relation listed in `SIGNPOSTING`.
+
+    It is undefined which relation is returned if multiple links of the same
+    relation are found.
+
+    :param parsedLinks: The :class:`ParsedLinks` to look up from
+    :param rel: The link relation to look up by, e.g. ``author``
+    :return: The found ``Link``, or ``None`` if not found.
+    """
+    if not rel.lower() in SIGNPOSTING:
+        raise ValueError("Unknown FAIR Signposting relation: %s" % rel)
+    if rel in parsedLinks:
+        return parsedLinks[rel]
+    return None
+
+def _link_attr(link: Link, key: str) -> Optional[str]:
+    """Look up an optional link attribute with given `key` from a `Link`.
+
+    This is a workaround as :class:`Link` exposes ``__getitem__`` and ``__contains__`` but not ``Dict.get()``
+
+    :param link: :class:`Link` to look up attribute of
+    :param key: Link attribute key to look up
+    :return: The attribute value, or ``None`` if the link attribute was not found.
+    """
+    if key in link:
+        return link[key]
+    return None
+
+def linkToSignpost(link: Link, rel: Union[LinkRel,AbsoluteURI], context_url: str = None) -> Signpost:
+    """Convert from a :class:`Link` to a :class:`Signpost` 
+    object for a given link relation.
+
+    :param link: The :class:`Link` to convert
+    :param rel: The link relation to create a signpost for
+    :param context_url: Optional, the context URL these link describe (unless specifying ``anchor``)
+    :return: The converted :class:`Signpost` object
+    """
+    context = _link_attr(link, "anchor") or context_url
+    return Signpost(rel, link.target,
+        _link_attr(link, "type"),
+        _link_attr(link, "profile"),
+        context, link)
+
+def linksToSignposting(links: List[Link], context: str = None) -> Signposting:
+        """Initialize Signposting object for a given `ParsedLinks`
+        as discovered from the (optional) `context` base URL.
+
+        :param links: A list of :class:`Link` to convert
+        :param context: Optional, the context URL these link describe (unless specifying ``anchor``)
+        :return: The :class:`Signposting` of the converted :class:`Signpost` objects
+        """
+        signposts: List[Signpost] = []
+        for l in links:
+            # TODO: Check if context matches "anchor"
+            for rel in l.rel:
+                if rel in SIGNPOSTING: # Allow URI extensions https://datatracker.ietf.org/doc/html/rfc8288#section-2.1.2
+                    signposts.append(linkToSignpost(l, LinkRel(rel), context))
+                elif ":" in rel:
+                    signposts.append(linkToSignpost(l, AbsoluteURI(rel), context))
+        return Signposting(context, signposts)
+
+def _absolute_attribute(k: str, v: str, baseurl: str) -> Tuple[str, str]:
+    """Ensure link attribute value uses absolute URI, resolving from the baseurl.
+
+    Currently this will mean the `context`_ attribute ``anchor``
+    and ``profile`` will be rewritten.
+
+    .. _context: https://datatracker.ietf.org/doc/html/rfc8288#section-3.2
+    """
+    if k.lower() == "anchor" or k.lower() == "profile":
+        return k, urljoin(baseurl, v)
+    return k, v
+
+
+def find_signposting_http_link(headers: List[str], baseurl: str = None) -> Signposting:
+    """Find signposting among HTTP Link headers.
+
+    Links are discovered according to defined `FAIR`_ `signposting`_ relations.
+
+    :param headers: A list of individual HTTP ``Link`` headers. 
+        The headers should be valid according to `RFC8288`_, 
+        excluding the ``"Link:"`` prefix.
+    :param baseurl: Optional base URL to make relative link targets absolute from
+    :return: A :class:`Signposting` of the collected signposts.
+    :raise ValueError: If the link headers could not be parsed.
+
+    .. _signposting: https://signposting.org/conventions/
+    .. _FAIR: https://signposting.org/FAIR/
+    .. _rfc8288: https://doi.org/10.17487/RFC8288
+
+    """
+    parsed = parse_link_header(", ".join(headers))
+    signposting: List[Link] = []
+    # Ignore non-Signposting relations like "stylesheet"
+    # but include URI extensions
+    for l in (_filter_links_by_rel(parsed) + _filter_links_uris(parsed)):
+        if baseurl is not None:
+            # Make URLs absolute by modifying Link object in-place
+            target = urljoin(baseurl, l.target)
+            attributes = [_absolute_attribute(
+                k, v, baseurl) for k, v in l.attributes]
+            link = Link(target, attributes)
+        else:
+            link = l  # unchanged
+        signposting.append(link)
+    return linksToSignposting(signposting, baseurl)
