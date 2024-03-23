23e491f92bc66f4c10cb3c697895e67e73633c70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 05:04:54 +0000

    Add package rankgun/2.0.9/rankgun-2.0.9.tar.gz

---
+++ packages/rankgun/rankgun-2.0.9.tar.gz/rankgun-2.0.9/README.md
@@ -0,0 +1,9 @@
+# RankGun V2.0.8
+
+
+![Tests](https://github.com/Noah-Haf/RankGunModule/actions/workflows/tests.yml/badge.svg)
+
+An API wrapper made for the RankGun API.
+
+Make for [rankgun](https://rankgun.works), read the [docs](https://docs.rankgun.works)!
+
---
+++ packages/rankgun/rankgun-2.0.9.tar.gz/rankgun-2.0.9/tests/testing.py
@@ -0,0 +1,26 @@
+import rankgun
+import sys
+import asyncio
+
+Api_Token = sys.argv[1]
+
+
+async def main():
+    print("let's go")
+    Workspace = rankgun.RankGun(Api_Token, 1)
+
+    Attempt1 = await Workspace.promote(username="RankGunTests")
+    Attempt2 = await Workspace.demote(username="RankGunTests")
+
+    print(Attempt1)
+    if Attempt1["status_code"] != 200 or Attempt2["status_code"] != 200:
+        raise Exception("Something went wrong")
+    else:
+        print("All went well")
+
+
+async def run_main():
+    await main()
+
+
+asyncio.run(run_main())
