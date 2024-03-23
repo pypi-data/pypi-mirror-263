bc3faec954710ca02c7a0f04e0a4202ebf89a5fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 03:05:18 +0000

    Add package taskiq-dependencies/1.5.3/taskiq_dependencies-1.5.3-py3-none-any.whl

---
+++ packages/taskiq-dependencies/taskiq_dependencies-1.5.3-py3-none-any.whl/taskiq_dependencies/utils.py
@@ -0,0 +1,52 @@
+import inspect
+import sys
+from contextlib import _AsyncGeneratorContextManager, _GeneratorContextManager
+from typing import Any, AsyncContextManager, ContextManager, Optional
+
+if sys.version_info >= (3, 10):
+    from typing import TypeGuard
+else:
+    from typing_extensions import TypeGuard
+
+
+class ParamInfo:
+    """
+    Parameter information.
+
+    This class helps you to get information,
+    about how the current dependency was specified.
+
+    If there's no dependant function, the name will be an empty string
+    and the definition will be None.
+    """
+
+    def __init__(
+        self,
+        name: str,
+        signature: Optional[inspect.Parameter] = None,
+    ) -> None:
+        self.name = name
+        self.definition = signature
+
+    def __repr__(self) -> str:
+        return f"ParamInfo<name={self.name}>"
+
+
+def iscontextmanager(obj: Any) -> TypeGuard[ContextManager[Any]]:
+    """
+    Return true if the object is a sync context manager.
+
+    :param obj: object to check.
+    :return: bool that indicates whether the object is a context manager or not.
+    """
+    return issubclass(obj.__class__, _GeneratorContextManager)
+
+
+def isasynccontextmanager(obj: Any) -> TypeGuard[AsyncContextManager[Any]]:
+    """
+    Return true if the object is a async context manager.
+
+    :param obj: object to check.
+    :return: bool that indicates whether the object is a async context manager or not.
+    """
+    return issubclass(obj.__class__, _AsyncGeneratorContextManager)
