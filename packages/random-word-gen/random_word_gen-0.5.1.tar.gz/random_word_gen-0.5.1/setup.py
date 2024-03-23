6ca5f68abb1ad75d3476108f10a2f03bb8b638f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 15:05:31 +0000

    Add package random-word-gen/0.5.1/random_word_gen-0.5.1.tar.gz

---
+++ packages/random-word-gen/random_word_gen-0.5.1.tar.gz/random_word_gen-0.5.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('C:/Users/student/VSCode/random_word_gen/README.md','r') as f:
+    description = f.read()
+
+
+setup(
+    name='random_word_gen',
+    version="0.5.1",
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
