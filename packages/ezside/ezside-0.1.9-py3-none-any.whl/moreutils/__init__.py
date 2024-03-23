a0fbc530db6e6ed48b9005473a9d37afb5fdf11b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 03:05:33 +0000

    Add package ezside/0.1.9/ezside-0.1.9-py3-none-any.whl

---
+++ packages/ezside/ezside-0.1.9-py3-none-any.whl/moreutils/__init__.py
@@ -0,0 +1,7 @@
+"""The 'moreutils' package provides a collection of utility classes and
+functions. """
+#  MIT Licence
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
+
+from ._int_field import IntField
