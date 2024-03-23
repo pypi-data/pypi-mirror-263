a7d74f989753665d80125f91da3c2e6c353e7b7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 04:05:12 +0000

    Add package puzzle-radicale-auth-ldap/0.0.1/puzzle-radicale-auth-ldap-0.0.1.tar.gz

---
+++ packages/puzzle-radicale-auth-ldap/puzzle-radicale-auth-ldap-0.0.1.tar.gz/puzzle-radicale-auth-ldap-0.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name="puzzle-radicale-auth-ldap",
+    version="0.0.1",
+    description="LDAP Authentication Plugin for Radicale 3",
+    author="Raoul Thill",
+    license="GNU GPL v3",
+    install_requires=["radicale >= 3.0", "ldap3 >= 2.3"],
+    url="https://github.com/puzzle/puzzle-radicale-auth-ldap",
+    download_url="https://github.com/puzzle/puzzle-radicale-auth-ldap/archive/refs/tags/v.0.0.1.tar.gz",
+    packages=["puzzle_radicale_auth_ldap"])
