690767c223aafdbd5d4068be72e8dc085333fbbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 14:04:16 +0000

    Add package bit-common/0.1.2/bit_common-0.1.2-py3-none-any.whl

---
+++ packages/bit-common/bit_common-0.1.2-py3-none-any.whl/bit/network.py
@@ -0,0 +1,36 @@
+from typing import Union
+
+import ipaddress
+from bit import UnsignedInteger
+
+from pydantic_core import core_schema
+
+
+class IPv4Address(UnsignedInteger):
+    LENGTH = 32
+
+    @classmethod
+    def transform(cls, value: Union[str, int, list[int]]) -> int:
+        if isinstance(value, str):
+            return int(ipaddress.IPv4Address(value))
+
+        return super().transform(value)
+
+
+class IPv6Address(IPv4Address):
+    LENGTH = 128
+
+    @classmethod
+    def transform(cls, value: Union[str, int, list[int]]) -> int:
+        if isinstance(value, str):
+            return int(ipaddress.IPv6Address(value))
+
+        return super().transform(value)
+
+
+class IPv4Mask(IPv4Address):
+    pass
+
+
+class IPv6Mask(IPv6Address):
+    pass
