1a026cda6135069a0b4e1e695d741a5fad1e96c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 04:04:55 +0000

    Add package vigi/0.9.1/vigi-0.9.1-py3-none-any.whl

---
+++ packages/vigi/vigi-0.9.1-py3-none-any.whl/vigi/app.py
@@ -0,0 +1,33 @@
+# This is the entry point for the Flask application.
+
+from flask_bootstrap import Bootstrap5
+from flask import Flask, redirect, url_for
+
+from .cache import cache
+from .context_processors import utility_processor
+
+from .routes.live import live_blueprint
+from .routes.camera import camera_blueprint
+from .routes.recordings import recordings_blueprint
+
+# Initialize the Flask app
+app = Flask(__name__, static_url_path='')
+
+# setup the cache
+cache.init_app(app)
+
+# configure context processors for views
+app.context_processor(utility_processor)
+
+# Bootstrap wrapper for Flask
+bootstrap = Bootstrap5(app)
+
+# register the blueprints (sub-apps)
+app.register_blueprint(live_blueprint)
+app.register_blueprint(camera_blueprint)
+app.register_blueprint(recordings_blueprint)
+
+# the root route redirects to the live view
+@app.route('/')
+def index():
+    return redirect(url_for('live.index'))
