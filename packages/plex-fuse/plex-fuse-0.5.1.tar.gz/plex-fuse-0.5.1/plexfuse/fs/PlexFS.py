b7ccc570248f6ee207d4cfa4a7d2f46fcdf8e07b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 16:04:45 +0000

    Add package plex-fuse/0.5.1/plex-fuse-0.5.1.tar.gz

---
+++ packages/plex-fuse/plex-fuse-0.5.1.tar.gz/plex-fuse-0.5.1/plexfuse/fs/PlexFS.py
@@ -0,0 +1,110 @@
+import errno
+from functools import cache
+from pathlib import Path
+from threading import Lock
+
+import fuse
+
+from plexfuse.control.ControlListener import ControlListener
+from plexfuse.fs.PlexDirectory import PlexDirectory
+from plexfuse.fs.PlexFile import PlexFile
+from plexfuse.fs.RefCountedDict import RefCountedDict
+from plexfuse.normalize import normalize
+from plexfuse.plex.PlexApi import PlexApi
+from plexfuse.vfs.Control import Control
+from plexfuse.vfs.entry.DirEntry import DirEntry
+from plexfuse.vfs.PlexVFS import PlexVFS
+
+
+class PlexFS(fuse.Fuse):
+    control: ControlListener
+
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        self.plex = plex = PlexApi()
+        self.vfs = PlexVFS(plex, self)
+        self.cache_path = None
+        self.http_cache = None
+        self.control = None
+        self.control_path = None
+        self.file_map = RefCountedDict()
+        self.iolock = Lock()
+
+    def fsinit(self):
+        # "cache_path" property doesn't get always initialized from options:
+        # https://github.com/libfuse/python-fuse/issues/61#issuecomment-1902472620
+        cache_path = self.cache_path if self.cache_path else PlexApi.CACHE_PATH
+        PlexApi.CACHE_PATH = Path(cache_path).absolute()
+        PlexApi.HTTP_CACHE = self.http_cache
+        print(f"fsinit: CACHE_PATH={PlexApi.CACHE_PATH}")
+        print(f"fsinit: HTTP_CACHE={PlexApi.HTTP_CACHE}")
+        print(f"fsinit: control_path={self.control_path}")
+        if self.control_path:
+            control = Control(self.plex, self, self.vfs)
+            self.control = ControlListener(self.control_path, control).start()
+
+    def fsdestroy(self):
+        if self.control:
+            self.control.stop()
+
+    @cache
+    def getattr(self, path: str):
+        path = normalize(path, is_path=True)
+        try:
+            item = self.vfs[path]
+        except KeyError as e:
+            print(f"ERROR: getattr: Unsupported path: {e}")
+            return -errno.ENOENT
+
+        if isinstance(item, DirEntry):
+            return PlexDirectory(st_nlink=2 + len(item))
+
+        return PlexFile(st_size=item.size, **item.attr)
+
+    @cache
+    def readdir(self, path: str, offset: int):
+        return list(self._readdir(path))
+
+    def _readdir(self, path: str):
+        for r in [".", ".."]:
+            yield fuse.Direntry(r)
+
+        try:
+            it = self.vfs[path]
+        except KeyError as e:
+            print(f"ERROR: readdir({path}): {e}")
+            return
+
+        for r in it:
+            yield fuse.Direntry(r)
+
+    def read(self, path, size, offset):
+        with self.iolock:
+            entry = self.file_map[path]
+
+            return entry.read(offset, size)
+
+    def release(self, path, flags):
+        with self.iolock:
+            try:
+                del self.file_map[path]
+            except KeyError as e:
+                print(f"ERROR: release({path}): {e}")
+                return -errno.EINVAL
+
+            return 0
+
+    def open(self, path, flags):
+        with self.iolock:
+            try:
+                entry = self.vfs[path]
+            except KeyError as e:
+                print(f"ERROR: open({path}): {e}")
+                return -errno.ENOENT
+
+            if isinstance(entry, DirEntry):
+                return -errno.EISDIR
+
+            self.file_map[path] = entry
+
+            return 0
