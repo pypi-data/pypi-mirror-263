4304fa3ab2681e7ea3292dfa5b06860219426051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 03:04:44 +0000

    Add package LMchain/0.2.23/LMchain-0.2.23-py3-none-any.whl

---
+++ packages/LMchain/LMchain-0.2.23-py3-none-any.whl/lmchain/agents/strict_json_agent_wrapper.py
@@ -0,0 +1,15 @@
+
+
+class strictJsonWrapper:
+    def __init__(self,llm):
+        self.llm = llm
+
+    def json_struct(self,describe,json_example):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        pass
+
+
+
+
---
+++ packages/LMchain/LMchain-0.2.23-py3-none-any.whl/lmchain/prompts/__init__.py
@@ -0,0 +1,3 @@
+name = "prompts"
+from .prompt_template import PromptTemplate
+
