9d280120bf416e791987cd78432a39428d0400a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 15:05:04 +0000

    Add package cdktf-cdktf-provider-opentelekomcloud/11.1.4/cdktf_cdktf_provider_opentelekomcloud-11.1.4-py3-none-any.whl

---
+++ packages/cdktf-cdktf-provider-opentelekomcloud/cdktf_cdktf_provider_opentelekomcloud-11.1.4-py3-none-any.whl/cdktf_cdktf_provider_opentelekomcloud/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import cdktf._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@cdktf/provider-opentelekomcloud",
+    "11.1.4",
+    __name__[0:-6],
+    "provider-opentelekomcloud@11.1.4.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
