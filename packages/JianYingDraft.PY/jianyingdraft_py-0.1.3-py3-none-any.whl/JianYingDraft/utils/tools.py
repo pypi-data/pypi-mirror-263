577d3bc6e5c1efd93e9700fa7112c28aa7a81c07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 16:04:50 +0000

    Add package JianYingDraft.PY/0.1.3/jianyingdraft_py-0.1.3-py3-none-any.whl

---
+++ packages/JianYingDraft.PY/jianyingdraft_py-0.1.3-py3-none-any.whl/JianYingDraft/utils/tools.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+import uuid
+import json
+
+from BasicLibrary.io.dirHelper import DirHelper
+
+
+def generate_id():
+    """
+        生成uuid
+    """
+    return str(uuid.uuid4()).upper()
+
+
+def write_json(path, data):
+    with open(path, 'w') as file:
+        json.dump(data, file)
+    pass
+
+
+def read_json(path):
+    with open(path, 'r') as file:
+        return json.load(file)
+    pass
+
+
+def create_folder(folder_path):
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
+    pass
+
+    DirHelper.ensure_exist(folder_path)
+
+
+
+
+
