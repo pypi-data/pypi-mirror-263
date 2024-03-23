0ad2d06b4187bcf0c12f25935a0845db09674106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 04:08:15 +0000

    Add package qtapputils/0.5.1/qtapputils-0.5.1.tar.gz

---
+++ packages/qtapputils/qtapputils-0.5.1.tar.gz/qtapputils-0.5.1/qtapputils/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © QtAppUtils Project Contributors
+# https://github.com/jnsebgosselin/apputils
+#
+# This file is part of QtAppUtils.
+# Licensed under the terms of the MIT License.
+# -----------------------------------------------------------------------------
+
+version_info = (0, 5, 1)
+__version__ = '.'.join(map(str, version_info))
+__date__ = '22/03/2024'
+__project_url__ = "https://github.com/jnsebgosselin/qtapputils"
