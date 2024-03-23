9632cd6d7a6299dbba50faefd35def8ece712f1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 16:04:34 +0000

    Add package pyrepositories/1.0.0/pyrepositories-1.0.0.tar.gz

---
+++ packages/pyrepositories/pyrepositories-1.0.0.tar.gz/pyrepositories-1.0.0/src/repositories/__init__.py
@@ -0,0 +1,5 @@
+from .datatable import DataTable
+from .lib import FilterField, Entity
+from .datasource import DataSource
+from .json_repository import JsonTable
+from .pg_repository import PgTable
