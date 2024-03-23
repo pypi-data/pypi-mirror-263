b23e50e6b93082e623d44766e2d328ed2ceef5d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 04:06:20 +0000

    Add package boilersaas/0.2.2/boilersaas-0.2.2.tar.gz

---
+++ packages/boilersaas/boilersaas-0.2.2.tar.gz/boilersaas-0.2.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="boilersaas",
+    version="0.2.2",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        "Babel==2.14.0",
+        "Flask==3.0.2",
+        "flask-babel==4.0.0",
+        "Flask-Dance==7.1.0",
+        "Flask-Login==0.6.3",
+        "Flask-Mail==0.9.1",
+        "Flask-SQLAlchemy==3.1.1",
+        "Flask-WTF==1.2.1",
+        "itsdangerous==2.1.2",
+        "python-dotenv==1.0.1",
+        "requests==2.31.0",
+        "SQLAlchemy==2.0.28",
+        "Werkzeug==3.0.1",
+        "WTForms==3.1.2"
+    ],
+    author="DS",
+    author_email="hello@davidstern.me",
+    description="A boilerplate for flask SAAS apps, with social sign in, email confirmation, and more.",
+    keywords="flask,boilerplate,saas,app",
+    url="http://example.com/your_package",  # Optional
+)
