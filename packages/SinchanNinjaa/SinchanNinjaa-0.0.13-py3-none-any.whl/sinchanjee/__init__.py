ad8dbe7597b4a2ab1d70defce5b05cbf1806d9df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 05:04:33 +0000

    Add package SinchanNinjaa/0.0.13/SinchanNinjaa-0.0.13-py3-none-any.whl

---
+++ packages/SinchanNinjaa/SinchanNinjaa-0.0.13-py3-none-any.whl/sinchanjee/__init__.py
@@ -0,0 +1,8 @@
+# from vidstream.streaming import StreamingServer
+# from vidstream.streaming import CameraClient
+# from vidstream.streaming import VideoClient
+# from vidstream.streaming import ScreenShareClient
+# from vidstream.audio import AudioSender
+# from vidstream.audio import AudioReceiver
+
+from sinchanjee.sinchanpg1 import sayhello
