d56470449691e091204fe6cefb97589100af4bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 04:05:40 +0000

    Add package pt-pump-up-orms/0.0.3/pt_pump_up_orms-0.0.3.tar.gz

---
+++ packages/pt-pump-up-orms/pt_pump_up_orms-0.0.3.tar.gz/pt_pump_up_orms-0.0.3/src/pt_pump_up_orms/__init__.py
@@ -0,0 +1,7 @@
+from .cruds.Author import Author
+from .cruds.Dataset import Dataset
+from .cruds.Link import Link
+from .cruds.Model import Model
+from .cruds.NLP_Task import NLP_Task
+from .cruds.ResourceStats import ResourceStats
+from .cruds.Result import Result
