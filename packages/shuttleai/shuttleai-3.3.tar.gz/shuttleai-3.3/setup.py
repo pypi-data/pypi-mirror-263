6ac50e781ce9395209a37295d8f1ab1c188ce6c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 04:04:59 +0000

    Add package shuttleai/3.3/shuttleai-3.3.tar.gz

---
+++ packages/shuttleai/shuttleai-3.3.tar.gz/shuttleai-3.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shuttleai', 
+    version='3.3',
+    author='shuttle',
+    author_email='noreply@shuttleai.app',
+    description="Access Shuttle AI's API via a simple and user-friendly lib.",
+    long_description="Access Shuttle AI's API via a simple and user-friendly lib. Dashboard: https://shuttleai.app Discord: https://discord.gg/shuttleai",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'httpx',
+        'aiohttp'
+    ],
+    extras_require={
+        'cli': [
+            'asyncclick',
+            'pystyle'
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'shuttleai = shuttleai.cli:main [cli]'
+        ],
+    },
+)
