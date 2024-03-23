3a52a902c44ae15ef7ac951f2a9c7f973dd6b1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 04:06:35 +0000

    Add package mkdocs-asyncapi-html-plugin/0.3.3/mkdocs_asyncapi_html_plugin-0.3.3.tar.gz

---
+++ packages/mkdocs-asyncapi-html-plugin/mkdocs_asyncapi_html_plugin-0.3.3.tar.gz/mkdocs_asyncapi_html_plugin-0.3.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+setup(
+    name="mkdocs_asyncapi_html_plugin",
+    version="0.3.3",
+    description="mkdocs plugin to generate pages from asyncapi spec files",
+    long_description="mkdocs plugin to generate pages from asyncapi spec files",
+    long_description_content_type="text/markdown",
+    author="hani",
+    author_email="hani@regentmarkets.com",
+    url="https://github.com/hani-deriv/mkdocs-asyncapi-plugin",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords=["mkdocs", "asyncapi", "plugin", "python"],
+    #packages=['mkdocs_asyncapi_html_plugin'],
+    py_modules=["mkdocs_asyncapi_html_plugin"],
+    install_requires=[],
+    entry_points={
+        "mkdocs.plugins": [
+            "asyncapi_html = mkdocs_asyncapi_html_plugin:AsyncAPIPlugin"
+        ]
+    },
+    python_requires=">=3.7",
+)
