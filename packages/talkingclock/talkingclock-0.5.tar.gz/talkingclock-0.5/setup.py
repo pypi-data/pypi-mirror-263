04ef753b453f4355e9c354eb7dc54e248090a1e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 04:04:58 +0000

    Add package talkingclock/0.5/talkingclock-0.5.tar.gz

---
+++ packages/talkingclock/talkingclock-0.5.tar.gz/talkingclock-0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='talkingclock',
+    version='0.5',
+    packages=find_packages(where="src"),
+    include_package_data=True,
+    package_dir={"":"src"}, 
+    package_data={
+        'talkingclock': ["mp3/*.mp3"],
+    },
+    install_requires=[
+        'playsound',
+    ],
+)
