2c6458b1e4b8ddbfa0bf70d628cafd7a6149f04f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 05:05:37 +0000

    Add package littlehorse-client/0.8.0/littlehorse_client-0.8.0-py3-none-any.whl

---
+++ packages/littlehorse-client/littlehorse_client-0.8.0-py3-none-any.whl/littlehorse/exceptions.py
@@ -0,0 +1,22 @@
+class TaskSchemaMismatchException(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+
+class OAuthException(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+
+class SerdeException(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+
+class LHTaskException(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
