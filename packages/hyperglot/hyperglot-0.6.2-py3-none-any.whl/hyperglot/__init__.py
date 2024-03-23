18f5d565fecca0d6abce627c065f734368d9cd5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 03:04:41 +0000

    Add package hyperglot/0.6.2/hyperglot-0.6.2-py3-none-any.whl

---
+++ packages/hyperglot/hyperglot-0.6.2-py3-none-any.whl/hyperglot/__init__.py
@@ -0,0 +1,63 @@
+"""
+Gather a few package wide constants
+"""
+from os import path
+__version__ = "0.6.2"
+
+DB = path.abspath(path.join(path.dirname(__file__), "data"))
+DB_EXTRA = path.abspath(path.join(path.dirname(__file__), "extra_data"))
+
+SUPPORTLEVELS = {
+    "base": "base",
+    "aux": "auxiliary"
+}
+
+# TODO Refactor these levels and status as Enum's
+
+# Note that order matters, since these may be used like a logging level
+VALIDITYLEVELS = [
+    "todo",
+    "draft",
+    "preliminary",
+    "verified",
+]
+
+
+# Note that "secondary" as status is also used, but on orthographies!
+STATUSES = [
+    "historical",
+    "constructed",
+    "ancient",
+    "living",
+    "extinct",
+    "deprecated",
+]
+
+
+# Possible orthography statuses, in no meaningful order
+# "deprecated" orthography status removed in favour of "historical"
+# Note: Order matters for preference of first found orthography
+ORTHOGRAPHY_STATUSES = [
+    "primary",
+    "local",
+    "secondary",
+    "historical",
+    "transliteration",
+]
+
+# Those attributes of orthographies that contain non-mark characters
+CHARACTER_ATTRIBUTES = [
+    "base",
+    "auxiliary",
+    "numerals",
+    "punctuation",
+]
+
+SORTING = {
+    "alphabetic": lambda lang: lang.get_name(),
+    "speakers": lambda lang: lang["speakers"]
+}
+
+SORTING_DIRECTIONS = ["asc", "desc"]
+
+MARK_BASE = "◌"
