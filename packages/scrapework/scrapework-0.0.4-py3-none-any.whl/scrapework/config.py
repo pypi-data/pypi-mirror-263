dd6918d3d691f4ef5a6cfbe3de77b96587916be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 16:04:38 +0000

    Add package scrapework/0.0.4/scrapework-0.0.4-py3-none-any.whl

---
+++ packages/scrapework/scrapework-0.0.4-py3-none-any.whl/scrapework/config.py
@@ -0,0 +1,31 @@
+import enum
+
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
+load_dotenv()
+
+
+class Config(BaseModel):
+    # S3_ENDPOINT: str = Field(default=os.environ["AWS_ENDPOINT_URL"])
+    # S3_BUCKET: str = Field(default=os.environ["S3_BUCKET"])
+    # S3_ACCESS_KEY_ID: str = Field(default=os.environ["AWS_ACCESS_KEY_ID"])
+    # S3_SECRET_ACCESS_KEY: str = Field(default=os.environ["AWS_SECRET_ACCESS_KEY"])
+    # TELEGRAM_SENDER_TOKEN: str = Field(default=os.environ["TELEGRAM_SENDER_TOKEN"])
+    pass
+
+
+config = Config()
+
+
+class BackendType(enum.Enum):
+    FILE = "file"
+    S3 = "s3"
+    META = "meta"
+
+
+class PipelineConfig(BaseModel):
+    base_url: str
+    backend: BackendType
+    s3_bucket: str
+    filename: str
