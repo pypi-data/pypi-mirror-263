d8daa23e158ced842b09d93fd2f1b2f6df8b5c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 16:04:40 +0000

    Add package odoo14-addon-ssi-l10n-id-taxform-bukti-potong-pph-f113304/14.0.1.6.0/odoo14_addon_ssi_l10n_id_taxform_bukti_potong_pph_f113304-14.0.1.6.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-l10n-id-taxform-bukti-potong-pph-f113304/odoo14_addon_ssi_l10n_id_taxform_bukti_potong_pph_f113304-14.0.1.6.0-py3-none-any.whl/odoo/addons/ssi_l10n_id_taxform_bukti_potong_pph_f113304/models/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import (
+    bukti_potong_pph_f113304_in,
+    bukti_potong_pph_f113304_in_line,
+    bukti_potong_pph_f113304_out,
+    bukti_potong_pph_f113304_out_line,
+)
---
+++ packages/odoo14-addon-ssi-l10n-id-taxform-bukti-potong-pph-f113304/odoo14_addon_ssi_l10n_id_taxform_bukti_potong_pph_f113304-14.0.1.6.0-py3-none-any.whl/odoo/addons/ssi_l10n_id_taxform_bukti_potong_pph_f113304/models/bukti_potong_pph_f113304_in_line.py
@@ -0,0 +1,20 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class BuktiPotongPPhf113304InLine(models.Model):
+    _name = "l10n_id.bukti_potong_pph_f113304_in_line"
+    _inherit = "l10n_id.bukti_potong_pph_line_mixin"
+    _description = "Bukti Potong PPh 22 (f.1.1.33.04) In Line"
+
+    bukti_potong_id = fields.Many2one(
+        comodel_name="l10n_id.bukti_potong_pph_f113304_in",
+    )
+    income_move_line_ids = fields.Many2many(
+        relation="rel_bukpot_f113304_in_line_2_income_move",
+        column1="bukpot_line_id",
+        column2="account_move_id",
+    )
