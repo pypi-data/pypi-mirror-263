1ecde99f9a90cf0fa69cd68ff80308f0bdcf6463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 14:04:35 +0000

    Add package soundtools/0.1.2/soundtools-0.1.2.tar.gz

---
+++ packages/soundtools/soundtools-0.1.2.tar.gz/soundtools-0.1.2/README.md
@@ -0,0 +1,5 @@
+the soundtools library has some basic sound-waves which you can use to create music, and you can modify it and add you own sound-waves as well.
+
+```python
+import soundtools
+```
---
+++ packages/soundtools/soundtools-0.1.2.tar.gz/soundtools-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='soundtools',
+version='0.1.2',
+author='Mohammad Erfan Karami',
+author_email='erfan012amir016@gmail.com',
+description="is used to work with sounds waves",
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
