5a1fa74d5a25c6edfc0175c924fe41f9c561f321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 03:05:22 +0000

    Add package cdk8s-awscdk-resolver/0.0.72/cdk8s-awscdk-resolver-0.0.72.tar.gz

---
+++ packages/cdk8s-awscdk-resolver/cdk8s-awscdk-resolver-0.0.72.tar.gz/cdk8s-awscdk-resolver-0.0.72/src/cdk8s_awscdk_resolver/_jsii/__init__.py
@@ -0,0 +1,28 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import cdk8s._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@cdk8s/awscdk-resolver",
+    "0.0.72",
+    __name__[0:-6],
+    "awscdk-resolver@0.0.72.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
