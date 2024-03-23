a7dcd374b7722b92d5c04f6f8b415f2c7fb887f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 03:05:23 +0000

    Add package cdk8s/1.63.56/cdk8s-1.63.56.tar.gz

---
+++ packages/cdk8s/cdk8s-1.63.56.tar.gz/cdk8s-1.63.56/src/cdk8s/_jsii/__init__.py
@@ -0,0 +1,23 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk8s", "1.63.56", __name__[0:-6], "cdk8s@1.63.56.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
