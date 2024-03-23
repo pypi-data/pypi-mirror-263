1857bd55601d96ec45c6ea5253a9b64908206f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 04:05:25 +0000

    Add package pt-pump-up-orms/0.0.6/pt_pump_up_orms-0.0.6.tar.gz

---
+++ packages/pt-pump-up-orms/pt_pump_up_orms-0.0.6.tar.gz/pt_pump_up_orms-0.0.6/src/pt_pump_up_orms/orms/Author.py
@@ -0,0 +1,16 @@
+from pt_pump_up_orms import CRUD
+from pt_pump_up_orms.orms import Link
+
+
+class Author(CRUD):
+    def __init__(self,
+                 id: int = None,
+                 name: str = None,
+                 institution: str = None,
+                 link: Link = None) -> None:
+
+        super().__init__("author",
+                         id=id,
+                         name=name,
+                         institution=institution,
+                         link=link.json if link else None)
