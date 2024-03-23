0947ee04ac4822cde00177026dac284185b7a5ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-23 04:05:22 +0000

    Add package pt-pump-up-orms/0.0.5/pt_pump_up_orms-0.0.5-py3-none-any.whl

---
+++ packages/pt-pump-up-orms/pt_pump_up_orms-0.0.5-py3-none-any.whl/pt_pump_up_orms/orms/__init__.py
@@ -0,0 +1,7 @@
+from .Author import Author
+from .Dataset import Dataset
+from .Link import Link
+from .Model import Model
+from .NLP_Task import NLP_Task
+from .ResourceStats import ResourceStats
+from .Result import Result
